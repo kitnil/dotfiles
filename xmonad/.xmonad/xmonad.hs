ec400caf91b53772068345233ffea1a1c5e00650
Oleg Pykhalov <go.wigust@gmail.com>
2017-07-23 17:48:49 +0300

    xmonad: xmonad.hs: Change rofi shortcut.
    * xmonad/.xmonad/xmonad.hs (myKeys): Change shortcut.

--- xmonad/.xmonad/xmonad.hs
+++ xmonad/.xmonad/xmonad.hs
@@ -63,7 +63,7 @@ myKeys conf@(XConfig {XMonad.modMask = modm}) = M.fromList $
     [ ((modm .|. shiftMask, xK_Return), spawn $ XMonad.terminal conf)
 
     -- launch dmenu
-    , ((modm,               xK_p     ), spawn "rofi -show run")
+    , ((modm,               xK_d     ), spawn "rofi -show run")
 
     -- launch gmrun
     , ((modm .|. shiftMask, xK_p     ), spawn "gmrun")
