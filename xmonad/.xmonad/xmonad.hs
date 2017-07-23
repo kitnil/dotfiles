b7496f929c3e731d390901da164f4c62ebd30e2a
Oleg Pykhalov <go.wigust@gmail.com>
2017-07-23 18:57:37 +0300

    xmonad: xmonad.hs: Add gaps.
    * xmonad/.xmonad/xmonad.hs (myLayout): Add gaps.

--- xmonad/.xmonad/xmonad.hs
+++ xmonad/.xmonad/xmonad.hs
@@ -3,6 +3,8 @@
 -- Released under the GNU GPLv3 or any later version.
 
 import XMonad
+import XMonad.Layout.Gaps
+import XMonad.Layout.Spacing -- smart space around windows
 import Data.Monoid
 import System.Exit
 
@@ -176,19 +178,14 @@ myMouseBindings (XConfig {XMonad.modMask = modm}) = M.fromList $
 -- The available layouts.  Note that each layout is separated by |||,
 -- which denotes layout choice.
 --
-myLayout = tiled ||| Mirror tiled ||| Full
-  where
-     -- default tiling algorithm partitions the screen into two panes
-     tiled   = Tall nmaster delta ratio
-
-     -- The default number of windows in the master pane
-     nmaster = 1
-
-     -- Default proportion of screen occupied by master pane
-     ratio   = 1/2
-
-     -- Percent of screen to increment by when resizing panes
-     delta   = 3/100
+myLayout = gaps [ (U, gap)
+                , (D, gap)
+                , (R, gap)
+                , (L, gap) ]
+           $ mySpacing
+           $ Tall 1 (3/100) (1/2) ||| Full
+           where gap = 5
+                 mySpacing = spacing gap
 
 ------------------------------------------------------------------------
 -- Window rules:
