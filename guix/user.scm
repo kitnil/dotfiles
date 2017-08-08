2cc10268015b99935e3398f55af4678cce2a22a3
Oleg Pykhalov <go.wigust@gmail.com>
2017-08-08 10:40:09 +0300

    guix: user: Sync with magnolia.

--- guix/user.scm
+++ guix/user.scm
@@ -1,223 +1,170 @@
 (use-modules (gnu)
-             (ng0 packages))
+             (ng0 packages chromium))
 
 (use-package-modules
- admin
+ admin backup compression databases gnupg linux networking package-management
+ bittorrent rsync mail samba ssh wget
+ file ncdu password-utils scanner xdisorg xorg
+ screen tmux
+ man shells
+ spice virtualization
  android
  aspell
- autotools
- backup
- bittorrent
- commencement
- compression
- conkeror
- databases
- dictionaries
- emacs
- file
- fonts
- fontutils
- gcc
- gdb
- gettext
- ghostscript
- gimp
- gnu-doc
- gnupg
- gnuzilla
- graphviz
- guile
- haskell
- imagemagick
- image-viewers
- java
- libreoffice
- linux
- lisp
+ autotools commencement gcc gdb gnu-doc m4 parallel pkg-config qt
  llvm
- m4
- mail
- man
- maths
- ncdu
- networking
- ocaml
- package-management
- parallel
- password-utils
- perl
- pkg-config
- pulseaudio
- qemu
- qt
- rsync
- samba
- scanner
- scheme
- screen
- shells
- spice
- ssh
- tex
- texinfo
- tmux
- version-control
- video
- web
- wget
- wm
- xdisorg
- xorg)
+ code version-control
+ emacs dictionaries
+ games
+ gettext
+ graphviz ghostscript texinfo tex
+ web conkeror gnuzilla
+ image-viewers imagemagick gimp
+ maths libreoffice
+ guile node haskell lisp java ocaml perl php scheme
+ pulseaudio video
+ fonts fontutils wm)
 
 
 
 (packages->manifest
  (list
-  adb
-  alsa-utils
-  aria2
-  aspell
-  aspell-dict-en
-  aspell-dict-ru
-  autoconf
-  automake
-  bzip2
-  chromium
-  conkeror
-  emacs-ag
-  emacs-aggressive-indent
-  emacs-async
-  emacs-auctex
-  emacs-cider
-  emacs-company
-  emacs-debbugs
-  emacs-dired-hacks
-  emacs-expand-region
-  emacs-git-gutter
-  emacs-git-timemachine
-  emacs-hydra
-  emacs-lua-mode
-  emacs-multiple-cursors
-  emacs-org
-  emacs-paredit
-  emacs-pdf-tools
-  emacs-projectile
-  emacs-rainbow-delimiters
-  emacs-rainbow-identifiers
-  emacs-rainbow-mode
-  emacs-rpm-spec-mode
-  emacs-sx
-  emacs-transpose-frame
-  emacs-undo-tree
-  emacs-use-package
-  emacs-web-mode
-  emacs-which-key
-  emacs-writegood-mode
-  emacs-yasnippet
-  emacs-yasnippet-snippets
-  ethtool
-  feh
-  ffmpeg
-  file
-  font-adobe-source-han-sans
-  fontconfig
-  font-hack
-  font-wqy-zenhei
-  gcc-toolchain
-  gdb
-  gettext
-  ghc
-  ghc-xmonad-contrib
-  ghostscript-with-x
-  gimp
+  ;; Sound
+  alsa-utils pulseaudio ponymix pavucontrol password-store
+
+  ;; Spelling
+  aspell aspell-dict-en aspell-dict-ru shellcheck
+
+  ;; Translation
+  translate-shell gnu-gettext
+
+  ;; Compression
+  bzip2 unzip
+
+  ;; Git
+  magit emacs-git-gutter emacs-git-timemachine
   git
-  git
-  gnu-c-manual
-  gnupg
-  gnuplot
-  gs-fonts
-  guile
-  guile-git
-  guile-ssh
-  guix
-  haskell-mode
-  haunt
-  help2man
-  htop
-  icecat
-  icedtea
-  iperf
-  isync
-  json-c
-  libgcrypt
-  libreoffice
-  m4
-  magit
-  mailutils
-  make
-  maxima
+
+  ;; Version control
   mercurial
-  mps-youtube
-  mpv
-  mysql
-  ncdu
-  neofetch
-  nmap
-  node
-  notmuch
-  ocaml
-  openssh
-  paperkey
-  parallel
-  password-store
-  pavucontrol
-  perl
-  php
-  pinentry
-  pkg-config
-  ponymix
-  proof-general
-  pulseaudio
-  python-ghp-import
+
+  ;; Guile
+  guile-2.2 guile-git guile-ssh guix
+
+  ;; LaTex & Tex & Texinfo
+  ghostscript/x gs-fonts emacs-auctex texlive texinfo
+
+  ;; Lua
+  emacs-lua-mode
+
+  ;; Web
+  node php emacs-web-mode
+  conkeror icecat chromium
   python-internetarchive
-  qemu
+
+  ;; Static site
+  haunt python-ghp-import
+
+  ;; Clojure & Java
+  icedtea emacs-cider
+
+  ;; RPM
+  emacs-rpm-spec-mode
+
+  ;; Common Lisp
+  emacs-sx
+
+  ;; Racket
   racket
-  redshift
-  rsnapshot
-  rsync
-  rxvt-unicode
-  samba
-  screen
-  setxkbmap
-  shellcheck
-  sicp
-  sqlite
-  stow
-  strace
-  texinfo
-  texlive
+
+  ;; Haskell
+  haskell-mode ghc xmobar xmonad ghc-xmonad-contrib
+
+  ;; Ocaml
+  ocaml
+
+  ;; org-mode
+  emacs-org emacs-org-edit-latex emacs-org-pomodoro
+
+  ;; Text search
+  emacs-ag
   the-silver-searcher
-  tmux
+
+  ;; Emacs
+  emacs-company emacs-undo-tree emacs-projectile
+  emacs-multiple-cursors paredit emacs-aggressive-indent emacs-transpose-frame
+  emacs-expand-region
+  emacs-dired-hacks emacs-pdf-tools
+  emacs-which-key
+  emacs-yasnippet emacs-yasnippet-snippets
+  emacs-writegood-mode
+  emacs-rainbow-delimiters emacs-rainbow-identifiers emacs-rainbow-mode
+  emacs-async emacs-use-package
+
+  ;; Images
+  feh gimp
+
+  ;; Videos
+  ffmpeg mpv mps-youtube youtube-dl
+
+  ;; Admin
+  file htop ncdu neofetch tree stow strace parallel adb
+
+  ;; Fonts
+  fontconfig font-adobe-source-han-sans font-hack font-wqy-zenhei
+
+  ;; C language
+  gcc-toolchain-7 gdb gnu-c-manual json-c
+  autoconf automake  gnu-make m4 pkg-config
+  clang help2man
+
+  ;; Gnupg
+  gnupg libgcrypt pinentry paperkey
+
+  ;; Office
+  libreoffice
+
+  ;; Mail
+  isync mailutils notmuch emacs-debbugs
+
+  ;; Math
+  maxima proof-general gnuplot
+
+  ;; Databases
+  mysql sqlite
+
+  ;; Networking
+  openssh nmap ethtool wakelan iperf
+
+  ;; Perl
+  perl
+
+  ;; Virtualization
+  qemu virt-viewer
+
+  ;; Rsync
+  rsync rsnapshot
+
+  ;; SMB
+  samba
+
+  ;; Terminal application
+  rxvt-unicode
+
+  ;; Terminal multiplexer
+  screen tmux
+
+  ;; Documentation & Books
+  vera sicp
+
+  ;; Games
   tome4
-  translate-shell
-  tree
-  unzip
-  vera
-  virt-viewer
-  wakelan
-  wget
-  wgetpaste
-  xclip
-  xclip
-  xdotool
-  xev
-  xinit
-  xlsfonts
-  xmobar
-  xmodmap
-  xmonad
-  xorg-server
-  xprop
-  xrdb
-  xset
-  youtube-dl))
+
+  ;; Downloading
+  aria2 wget
+
+  ;; Clipboard
+  xclip wgetpaste
+
+  ;; Xorg
+  xdotool xev xinit xlsfonts xorg-server xprop xrdb xset xmodmap setxkbmap
+  redshift))
