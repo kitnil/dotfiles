69460983536a0bade41badc5dfa66c665dec2771
Oleg Pykhalov <go.wigust@gmail.com>
2017-10-17 23:29:56 +0300

    wigust: Add microsoft-gsl.
    * wigust/packages/telegram.scm (microsoft-gsl): New variable.

---
+++ wigust/packages/telegram.scm
@@ -0,0 +1,126 @@
+;;; GNU Guix --- Functional package management for GNU
+;;; Copyright © 2017 Oleg Pykhalov <go.wigust@gmail.com>
+;;;
+;;; This file is part of GNU Guix.
+;;;
+;;; GNU Guix is free software; you can redistribute it and/or modify it
+;;; under the terms of the GNU General Public License as published by
+;;; the Free Software Foundation; either version 3 of the License, or (at
+;;; your option) any later version.
+;;;
+;;; GNU Guix is distributed in the hope that it will be useful, but
+;;; WITHOUT ANY WARRANTY; without even the implied warranty of
+;;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;;; GNU General Public License for more details.
+;;;
+;;; You should have received a copy of the GNU General Public License
+;;; along with GNU Guix.  If not, see <http://www.gnu.org/licenses/>.
+
+(define-module (wigust packages telegram)
+  #:use-module ((guix licenses) #:prefix license:)
+  #:use-module (guix packages)
+  #:use-module (guix download)
+  #:use-module (guix git-download)
+  #:use-module (guix gexp)
+  #:use-module (guix monads)
+  #:use-module (guix store)
+
+  #:use-module (guix build-system cmake)
+  #:use-module (gnu packages check)
+  #:use-module (gnu packages gcc)
+
+  #:use-module (guix build-system gnu)
+  #:use-module (guix build-system emacs)
+  #:use-module (guix build-system glib-or-gtk)
+  #:use-module (guix build-system trivial)
+  #:use-module (gnu packages)
+  #:use-module (gnu packages audio)
+  #:use-module (gnu packages bash)
+  #:use-module (gnu packages shells)
+  #:use-module (gnu packages chez)
+  #:use-module (gnu packages code)
+  #:use-module (gnu packages guile)
+  #:use-module (gnu packages gtk)
+  #:use-module (gnu packages gnome)
+  #:use-module (gnu packages ncurses)
+  #:use-module (gnu packages python)
+  #:use-module (gnu packages haskell)
+  #:use-module (gnu packages tex)
+  #:use-module (gnu packages texinfo)
+  #:use-module (gnu packages tcl)
+  #:use-module (gnu packages tls)
+  #:use-module (gnu packages pkg-config)
+  #:use-module (gnu packages xorg)
+  #:use-module (gnu packages lesstif)
+  #:use-module (gnu packages image)
+  #:use-module (gnu packages mail)
+  #:use-module (gnu packages linux)
+  #:use-module (gnu packages version-control)
+  #:use-module (gnu packages imagemagick)
+  #:use-module (gnu packages w3m)
+  #:use-module (gnu packages wget)
+  #:use-module (gnu packages autotools)
+  #:use-module (gnu packages base)
+  #:use-module (gnu packages compression)
+  #:use-module (gnu packages xml)
+  #:use-module (gnu packages glib)
+  #:use-module (gnu packages acl)
+  #:use-module (gnu packages package-management)
+  #:use-module (gnu packages perl)
+  #:use-module (gnu packages pdf)
+  #:use-module (gnu packages scheme)
+  #:use-module (gnu packages statistics)
+  #:use-module (gnu packages xiph)
+  #:use-module (gnu packages mp3)
+  #:use-module (gnu packages gettext)
+  #:use-module (gnu packages fribidi)
+  #:use-module (gnu packages gd)
+  #:use-module (gnu packages fontutils)
+  #:use-module (gnu packages emacs)
+  #:use-module (guix utils)
+  #:use-module (srfi srfi-1)
+  #:use-module (ice-9 match))
+
+(define-public microsoft-gsl
+  (let ((commit "4c5fdb541f36211361a05595a3d89fb0afcbec50")
+        (revision "1"))
+    (package
+      (name "microsoft-gsl")
+      (version (string-append "0.0.1-" revision "."
+                              (string-take commit 7)))
+      (source (origin
+                (method git-fetch)
+                (uri (git-reference
+                      (url "https://github.com/Microsoft/GSL")
+                      (commit commit)))
+                (file-name (string-append name "-" version "-checkout"))
+                (sha256
+                 (base32
+                  "1p5i94yqlwr81l3hkj5cpwrnr9pq7j90k9k55v1l53xbinwv4z21"))))
+      (build-system cmake-build-system)
+      (inputs
+       `(("gcc" ,gcc-5)))
+      (native-inputs
+       `(("catch" ,catch-framework)))
+      (arguments
+       `(#:parallel-build? #f
+         #:out-of-source? #f
+         #:make-flags '("LDFLAGS=-fno-strict-aliasing")
+         #:phases
+         (modify-phases %standard-phases
+           (add-after 'unpack 'bootstrap
+             (lambda _
+               (substitute* (find-files "." "\\.[ch]pp$")
+                 (("catch/catch\\.hpp")
+                  "catch.hpp"))
+
+               ;; unknown pragmas
+               (substitute* `(,@(find-files "." "\\.make$")
+                              "tests/CMakeLists.txt")
+                 (("-Werror") "")))))))
+      (home-page "https://github.com/Microsoft/GSL")
+      (synopsis "Guidelines Support Library")
+      (description "The Guideline Support Library (GSL) contains
+functions and types that are suggested for use by the C++ Core
+Guidelines maintained by the Standard C++ Foundation.")
+      (license license:bsd-3))))
