1b1310c76e6e3a3399d4d6986eab6dd13255fa4b
Oleg Pykhalov <go.wigust@gmail.com>
2017-10-30 07:15:27 +0300

    stumpwm: Clean up.
    * stumpwm/.stumpwm.d/init.lisp: Clean up.

--- stumpwm/.stumpwm.d/init.lisp
+++ stumpwm/.stumpwm.d/init.lisp
@@ -2,35 +2,11 @@
 
 (in-package :stumpwm)
 
-(setf *startup-message* nil) ; Disable welcome message.
-(stumpwm:run-shell-command "xsetroot -cursor_name left_ptr") ; Fix cursor icon
-
-(defcommand emms-previous () ()
-            "Emacs Emms previous in playlist."
-            (run-shell-command "emacsclient --eval '(emms-previous)'"))
-
-(defcommand emms-next () ()
-            "Emacs Emms next in playlist."
-            (run-shell-command "emacsclient --eval '(emms-next)'"))
-
-(defcommand emms-stop () ()
-            "Emacs Emms stop."
-            (run-shell-command "emacsclient --eval '(emms-stop)'"))
-
-(define-interactive-keymap (emms) ()
-  ((kbd "SPC") "emms-stop")
-
-  ((kbd "Up") "emms-previous")
-  ((kbd "C-p") "emms-previous")
-  ((kbd "p") "emms-previous")
-  ((kbd "k") "emms-previous")
-
-  ((kbd "Down") "emms-next")
-  ((kbd "C-n") "emms-next")
-  ((kbd "n") "emms-next")
-  ((kbd "j") "emms-next"))
+(setf *startup-message* nil)
 
 (set-module-dir "/home/natsu/.stumpwm.d/modules/")
+
+(stumpwm:run-shell-command "xsetroot -cursor_name left_ptr")
 
 
 ;;;
@@ -46,43 +22,19 @@
 
 
 ;;;
-;;; Gravity
+;;; Windows colors
 ;;;
-
-(load-module "cpu")
-(load-module "disk")
-(load-module "mem")
-(load-module "net")
-
-(setf *timeout-wait* 3
-      *mode-line-timeout* 5)
-
-(setf *time-format-string-default* (format nil "^5*%H:%M:%S~%^2*%A~%^7*%d %B"))
 
 (setf *window-border-style* :tight)
 
-;; Don't set it to “sloppy”,
-;; because it could switch window after switch desktop
-(setf *mouse-focus-policy* :click)
-
-(setf *window-info-format* (format nil "^>^B^5*%c ^b^6*%w^7*x^6*%h^7*~%%t"))
-
-(setf *window-format* "%s%m %n [%i] %c")
-(setf *mode-line-timeout* 1)
-
-(setq *ignore-wm-inc-hints* t)
-
-
-;;;
-;;; Windows colors
-;;;
+(setf *ignore-wm-inc-hints* t)
 
 (set-win-bg-color "#DCDAD5")
 (set-unfocus-color "#000000")
 (set-focus-color "#A52A2A")
-(set-fg-color "#FFFFFF")
-(set-bg-color "#000000")
-(set-border-color "#FF0000")
+(set-fg-color "#000000")
+(set-bg-color "#FFFFFF")
+(set-border-color "#A52A2A")
 (set-msg-border-width 3)
 
 (setf *normal-border-width* 5
@@ -94,6 +46,10 @@
 ;;; Window placement policy
 ;;;
 
+;; Don't set it to “sloppy”,
+;; because it could switch window after switch desktop
+(setf *mouse-focus-policy* :click)
+
 (clear-window-placement-rules)
 
 (define-frame-preference "Default"
@@ -102,11 +58,11 @@
   (2 nil nil :class "XTerm"))
 
 ;; Last rule to match takes precedence!
-;; TIP: if the argument to :title or :role begins with an ellipsis, a substring
-;; match is performed.
-;; TIP: if the :create flag is set then a missing group will be created and
+;; If the argument to :title or :role begins with an ellipsis, a
+;; substring match is performed.
+;; If the :create flag is set then a missing group will be created and
 ;; restored from *data-dir*/create file.
-;; TIP: if the :restore flag is set then group dump is restored even for an
+;; If the :restore flag is set then group dump is restored even for an
 ;; existing group using *data-dir*/restore file.
 
 
@@ -168,6 +124,25 @@
 
 
 ;;;
+;;; Pulseaudio
+;;;
+
+(defcommand vol-up () ()
+  "Increase the volume from the shell"
+  (run-shell-command
+   "pacmd set-sink-volume 0 $(printf '0x%x' $(( $(pacmd dump|grep set-sink-volume|cut -f3 -d' ') + 0xf00)) ) &> /dev/null"))
+
+(defcommand vol-down () ()
+  "Decrease the volume  from the shell"
+  (run-shell-command
+   "pacmd set-sink-volume 0 $(printf '0x%x' $(( $(pacmd dump|grep set-sink-volume|cut -f3 -d' ') - 0xf00)) ) &> /dev/null"))
+
+(define-interactive-keymap (volume) ()
+  ((kbd "Up") "vol-up")
+  ((kbd "Down") "vol-down"))
+
+
+;;;
 ;;; Terminal
 ;;;
 
@@ -199,30 +174,64 @@
 ;;;
 
 (defcommand warp-mouse-active-frame () ()
-            (let* ((current-frame (tile-group-current-frame (current-group)))
-                   (pointer-x (- (+ (frame-x current-frame)
-                                    (frame-width current-frame))
-                                 100))
-                   (pointer-y (+ 100 (frame-y current-frame))))
-              (warp-pointer (current-screen) pointer-x pointer-y)))
+  "Move mouse cursor to the top right of current frame."
+  (let* ((current-frame (tile-group-current-frame (current-group)))
+         (pointer-x (- (+ (frame-x current-frame)
+                          (frame-width current-frame))
+                       100))
+         (pointer-y (+ 100 (frame-y current-frame))))
+    (warp-pointer (current-screen) pointer-x pointer-y)))
 
 
 ;;;
 ;;; Gaps
 ;;;
 
-(load-module "swm-gaps")
-(setf swm-gaps:*inner-gaps-size* 5
-      swm-gaps:*outer-gaps-size* 25)
+;; (load-module "swm-gaps")
+;; (setf swm-gaps:*inner-gaps-size* 5
+;;       swm-gaps:*outer-gaps-size* 25)
+
+
+;;;
+;;; Keyboard layouts
+;;;
 
 (load-module "kbd-layouts")
 (kbd-layouts:keyboard-layout-list "us" "ru")
 
+
+;;;
+;;; Screenshots
+;;;
+
 (load-module "screenshot")
+
+(defun time-date-and-time-restrict ()
+  (time-format "%Y-%m-%d-%H-%M-%S"))
+
+(defun screenshot-filename ()
+  (concat (time-date-and-time-restrict)
+          ".png"))
+
+(defcommand screenshot-default () ()
+  "Screenshot with filename like 2017-10-30-03-29-16.png"
+  (eval-command (concat "screenshot-window " (screenshot-filename))))
+
+(define-key *root-map* (kbd "Print") "screenshot-default")
+
+
+;;;
+;;; Clipboard
+;;;
 
 (load-module "clipboard-history")
 (define-key *root-map* (kbd "C-y") "show-clipboard-history")
 (clipboard-history:start-clipboard-manager)
+
+
+;;;
+;;; Global windows selection
+;;;
 
 (load-module "globalwindows")
 (define-key *root-map* (kbd "M-quoteright") "global-windowlist")
@@ -233,6 +242,7 @@
 ;;; Pinentry
 ;;;
 
+;; Dependencies
 (ql:quickload "cffi")
 (ql:quickload "usocket-server")
 
