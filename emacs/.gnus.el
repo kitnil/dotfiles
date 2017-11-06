c1d1373c9a8644724fe99839e5ab2a7d7628a113
Oleg Pykhalov <go.wigust@gmail.com>
2017-11-06 11:03:41 +0300

    emacs: gnus: Format.
    * emacs/.gnus.el: Format.

--- emacs/.gnus.el
+++ emacs/.gnus.el
@@ -1,25 +1,20 @@
-(setq gnus-select-method
-      '(nnimap "USER"
-               ;; It could also be imap.googlemail.com if that's your server.
-               (nnimap-address "localhost")
-               (nnimap-server-port "imaps"))
+(setq gnus-select-method '(nnimap "USER"
+                                  (nnimap-address "localhost")
+                                  (nnimap-server-port "imaps")))
 
-      gnus-permanently-visible-groups ".*INBOX"
-
-      mm-discouraged-alternatives '("text/html" "text/richtext")
-      gnus-check-new-newsgroups nil
-      gnus-thread-hide-subtree t)
+(setq gnus-permanently-visible-groups ".*INBOX")
+(setq mm-discouraged-alternatives '("text/html" "text/richtext"))
+(setq gnus-check-new-newsgroups nil)
 
 
 ;; (Info-goto-node "(gnus) Scoring On Other Headers")
 ;; I e s p To RET <your name> RET
 
-(setq gnus-extra-headers '(List-Id To)
-      nnmail-extra-headers gnus-extra-headers)
+(setq gnus-extra-headers '(List-Id To))
+(setq nnmail-extra-headers gnus-extra-headers)
 
-(setq gnus-parameters
-      '(("^INBOX$"
-         (gnus-thread-sort-functions 'gnus-thread-sort-by-score))))
+(setq gnus-parameters '(("^INBOX$" (gnus-thread-sort-functions
+                                    'gnus-thread-sort-by-score))))
 
 (mailcap-add-mailcap-entry "image"
                            "jpeg"
