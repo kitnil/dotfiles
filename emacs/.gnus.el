245a55d07a085601a89004fc491ee70e9c81394c
Oleg Pykhalov <go.wigust@gmail.com>
2017-11-16 02:27:53 +0300

    emacs: gnus: Add switch-to-gnus.
    * emacs/.gnus.el (switch-to-gnus): New function.

--- emacs/.gnus.el
+++ emacs/.gnus.el
@@ -18,3 +18,42 @@
 
 (add-hook 'message-sent-hook #'gnus-score-followup-thread)
 (add-hook 'gnus-summary-mode-hook #'hl-line-mode)
+
+;; Code from: https://github.com/jwiegley/dot-emacs
+(defun switch-to-gnus (&optional arg)
+  "Switch to a Gnus related buffer.
+    Candidates are buffers starting with
+     *mail or *reply or *wide reply
+     *Summary or
+     *Group*
+    Use a prefix argument to start Gnus if no candidate exists."
+  (interactive "P")
+  (let (candidate
+        (alist '(("^\\*\\(mail\\|\\(wide \\)?reply\\)" t)
+                 ("^\\*Group")
+                 ("^\\*Summary")
+                 ("^\\*Article" nil (lambda ()
+                                      (buffer-live-p
+                                       gnus-article-current-summary))))))
+    (catch 'none-found
+      (dolist (item alist)
+        (let (last
+              (regexp (nth 0 item))
+              (optional (nth 1 item))
+              (test (nth 2 item)))
+          (dolist (buf (buffer-list))
+            (when (and (string-match regexp (buffer-name buf))
+                       (> (buffer-size buf) 0))
+              (setq last buf)))
+          (cond ((and last (or (not test) (funcall test)))
+                 (setq candidate last))
+                (optional
+                 nil)
+                (t
+                 (throw 'none-found t))))))
+    (cond (candidate
+           (switch-to-buffer candidate))
+          (arg
+           (gnus))
+          (t
+           (error "No candidate found")))))
