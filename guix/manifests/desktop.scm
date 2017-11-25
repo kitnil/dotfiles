8c4937afe8c8ed110c395c3015f27a2cc440b538
Oleg Pykhalov <go.wigust@gmail.com>
2017-11-25 06:40:58 +0300

    guix: desktop: Add packages from current profile.
    * guix/manifests/desktop.scm (gnu): New packages.

--- guix/manifests/desktop.scm
+++ guix/manifests/desktop.scm
@@ -1,25 +1,42 @@
-(use-modules (gnu packages aspell)
+(use-modules (gnu packages admin)
+             (gnu packages android)
+             (gnu packages aspell)
              (gnu packages backup)
+             (gnu packages bittorrent)
+             (gnu packages code)
+             (gnu packages commencement)
              (gnu packages databases)
+             (gnu packages dictionaries)
              (gnu packages emacs)
+             (gnu packages file)
              (gnu packages fonts)
              (gnu packages fontutils)
+             (gnu packages gcc)
              (gnu packages gnome)
              (gnu packages gnupg)
              (gnu packages gnuzilla)
+             (gnu packages graphviz)
+             (gnu packages guile)
              (gnu packages image-viewers)
+             (gnu packages kodi)
+             (gnu packages libreoffice)
+             (gnu packages linux)
              (gnu packages lisp)
              (gnu packages mail)
+             (gnu packages package-management)
              (gnu packages password-utils)
+             (gnu packages pulseaudio)
+             (gnu packages readline)
              (gnu packages screen)
-             (gnu packages bittorrent)
+             (gnu packages ssh)
              (gnu packages version-control)
              (gnu packages video)
              (gnu packages virtualization)
              (gnu packages web)
              (gnu packages wget)
              (gnu packages xdisorg)
-             (gnu packages xorg))
+             (gnu packages xorg)
+             (wigust packages emacs))
 
 (define %desktop-packages
   ;; Basic desktop tasks.
@@ -32,6 +49,8 @@
 
         libreoffice     ; Office suite
 
+        graphviz        ; Graphics dot
+
         translate-shell ; Translation in CLI and Emacs
 
         git             ; Version control
@@ -40,11 +59,18 @@
         icecat          ; Web browser
 
         isync           ; Sync IMAP
+        notmuch         ; Mail indexer based on Xapian
 
         feh             ; Image viewer
         mpv             ; Video and audio player
+        ffmpeg          ; Video, audio, images, gif conversion
+        obs             ; Video streaming and recording
+
+        gnu-c-manual    ; C language documentation
 
         qemu            ; Virtualization
+
+        adb             ; For Replicant (Android distribution) control
 
         rdiff-backup    ; Incremental backups
 
@@ -58,6 +84,8 @@
 
         font-dejavu     ; General fonts
         font-liberation ; Times New Roman replcement
+        font-awesome    ; Icons in fonts
+        font-wqy-zenhei ; Asia fonts
         fontconfig      ; Make `fc-cache -f' after installing addional fonts.
 
         pavucontrol     ; Pulseaudio control GUI
@@ -71,40 +99,68 @@
 
         ;; $EDITOR
         emacs                    ; The best editor
-        emacs-debbugs            ; <https://debbugs.gnu.org/> interface
-        emacs-guix               ; Guix interface
-        geiser                   ; Scheme bridge
-        emacs-engine-mode        ; Define searches on websites
-        emacs-projectile         ; Project functions
-        emacs-helm-projectile    ; Helm interface for Projectile
-        emacs-helm               ; Narrowing framework
-        emacs-yasnippet          ; Snippets
-        emacs-yasnippet-snippets
+        emacs-aggressive-indent  ; Auto indent minor mode
         emacs-company            ; Complition framework
         emacs-company-quickhelp  ; Help pages for Company
-        emacs-smartparens        ; Structured editing
-        emacs-transpose-frame    ; M-x transpose-frame
-        emacs-multiple-cursors   ; Multi cursor
-        emacs-gitpatch           ; Send patches
-        emacs-slime              ; Sbcl repl
+        emacs-debbugs            ; <https://debbugs.gnu.org/> interface
+        emacs-debpaste           ; Front end to <https://paste.debian.net/>
+        emacs-elfeed             ; RSS reader
+        emacs-engine-mode        ; Define searches on websites
         emacs-erc-hl-nicks       ; for ERC
-        emacs-aggressive-indent  ; Auto indent minor mode
-        emacs-markdown           ; Commonmark major mode
+        emacs-eval-in-repl       ; Evaluate to different Repls
+        emacs-ewmctrl            ; Control X windows from Emacs
+        emacs-ggtags             ; Front end to GNU Global
+        emacs-gitpatch           ; Send patches
+        emacs-guix               ; Guix interface
+        emacs-helm               ; Narrowing framework
+        emacs-helm-firefox       ; Search for bookmarks in Icecat
+        emacs-helm-make          ; Front end to `make'
+        emacs-helm-pass          ; Front end to password-store
+        emacs-helm-projectile    ; Helm interface for Projectile
+        emacs-highlight-stages   ; Highlight code stages
+        emacs-markdown-mode      ; Commonmark major mode
+        emacs-multiple-cursors   ; Multi cursor
+        emacs-nix-mode           ; Nix language mode
+        emacs-org-mind-map       ; General mind maps from Org files
+        emacs-projectile         ; Project functions
+        emacs-slime              ; Sbcl repl
+        emacs-smartparens        ; Structured editing
+        emacs-strace-mode        ; Colorize `strace' logs
+        emacs-transmission       ; Front end to transmission-daemon
+        emacs-transpose-frame    ; M-x transpose-frame
+        emacs-use-package        ; Lazy configuration
+        emacs-w3m                ; Front end to w3m command line web browser
+        emacs-which-key          ; Key bindings help
+        emacs-yasnippet          ; Snippets
+        emacs-yasnippet-snippets ; Collection of snippets
+        flycheck                 ; Syntax checker
+        geiser                   ; Scheme bridge
 
-        cflow ; C program call map
+        gcc-toolchain ; For Emacs `semantic-mode'
+        cflow         ; C program call map
+        global        ; Source tagging
 
         haunt            ; Guile static site generator
         guile-commonmark ; Commonmark for Guile
 
-        htop ; Pretty top
+        gwl              ; Guix workflow management
+
+        file   ; Show type of a file
+        htop   ; Pretty top
+        netcat ; TCP calls
 
         ;; X11 utils.  See <~/.xsession> and <~/.stumpwm.d/init.lisp>.
-        setxkbmap  ; Keyboard layout
-        xclip      ; Access clipboard from CLI
+        setxkbmap   ; Keyboard layout
+        xclip       ; Access clipboard from CLI
         xrdb
         xset
         xsetroot
-        xterm      ; $TERM
+        xterm       ; $TERM
+        xorg-server ; For testing Stumpwm.
+        wmctrl      ; For emacs-ewmctrl
+        xwininfo    ; Information about X11 window
+
+        lm-sensors ; Watch for temp of hardware
 
         openssh    ; SSH
 
@@ -114,6 +170,7 @@
 
         ;; Downloaders.
         transmission ; Bittorrent
+        kodi-cli     ; Remote control Kodi
         youtube-dl   ; Video and music from websites
         wget))
 
