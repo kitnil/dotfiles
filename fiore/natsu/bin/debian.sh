4382d42d0dcc5c838e88898de9d7a9c7a7323c22
Oleg Pykhalov <go.wigust@gmail.com>
2017-12-08 23:00:24 +0300

    qemu: debian: Add sound.
    * fiore/natsu/bin/debian.sh: Add sound.

--- fiore/natsu/bin/debian.sh
+++ fiore/natsu/bin/debian.sh
@@ -2,10 +2,15 @@
 
 # -net nic -net bridge,br=bridge0
 
+export QEMU_AUDIO_DRV=pa
+export QEMU_PA_SERVER=/run/user/1000/pulse/native
+
 exec qemu-system-x86_64 \
      -daemonize \
      -m 4096 \
      -enable-kvm \
+     -cpu host \
+     -soundhw hda \
      -hda /srv/virt/debian-gnome.img \
      -virtfs local,path="/home/natsu/Documents",security_model=none,mount_tag="TAG_doc" \
      $@
