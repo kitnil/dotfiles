817d8e722c4fbeeaec8d1a46e16e50bf92714447
Oleg Pykhalov <go.wigust@gmail.com>
2018-01-07 23:23:54 +0300

    guix-clean: Remove exec.
    * fiore/natsu/bin/guix-clean.sh: Remove exec.

--- fiore/natsu/bin/guix-clean.sh
+++ fiore/natsu/bin/guix-clean.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 
-exec find /var/guix/profiles/* -maxdepth 0 -mtime +14 -not -name per-user -exec sudo unlink {} \;
-exec guix package --delete-generations=2w
+find /var/guix/profiles/* -maxdepth 0 -mtime +14 -not -name per-user -exec sudo unlink {} \;
+guix package --delete-generations=2w
