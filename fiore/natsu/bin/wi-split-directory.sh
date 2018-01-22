7360be99aecfa556b45739b10021dd27127f4b87
Oleg Pykhalov <go.wigust@gmail.com>
2018-01-22 19:15:42 +0300

    bin: Add wi-split-directory.
    * fiore/natsu/bin/wi-split-directory.sh: New file.

--- fiore/natsu/bin/wi-split-directory.sh
+++ fiore/natsu/bin/wi-split-directory.sh
@@ -1,3 +1,5 @@
 #!/bin/sh
 
-i=1; while read l; do mkdir $i; mv $l $((i++)); done< <(ls|xargs -n10)
+# Code from <https://stackoverflow.com/a/34463802>.
+
+i=1; while read l; do mkdir $i; mv $l $((i++)); done< <(ls | xargs -n20)
