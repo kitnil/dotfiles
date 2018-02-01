c9d88e9ea1ec04e626bafa806633c3433ee4776f
Oleg Pykhalov <go.wigust@gmail.com>
2018-02-01 11:29:52 +0300

    wigust: Add vc-dwim-git-worktree.
    * wigust/packages/version-control.scm: New file.
    * vc-dwim-find-git-worktree.patch: New file.

---
+++ wigust/packages/version-control.scm
@@ -0,0 +1,35 @@
+;;; GNU Guix --- Functional package management for GNU
+;;; Copyright © 2018 Oleg Pykhalov <go.wigust@gmail.com>
+;;;
+;;; This file is part of GNU Guix.
+;;;
+;;; GNU Guix is free software; you can redistribute it and/or modify it
+;;; under the terms of the GNU General Public License as published by
+;;; the Free Software Foundation; either version 3 of the License, or (at
+;;; your option) any later version.
+;;;
+;;; GNU Guix is distributed in the hope that it will be useful, but
+;;; WITHOUT ANY WARRANTY; without even the implied warranty of
+;;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;;; GNU General Public License for more details.
+;;;
+;;; You should have received a copy of the GNU General Public License
+;;; along with GNU Guix.  If not, see <http://www.gnu.org/licenses/>.
+
+(define-module (wigust packages version-control)
+  #:use-module ((guix licenses) #:prefix license:)
+  #:use-module (guix utils)
+  #:use-module (guix packages)
+  #:use-module (guix download)
+  #:use-module (guix git-download)
+  #:use-module (guix build-system gnu)
+  #:use-module (gnu packages version-control)
+  #:use-module (gnu packages))
+
+(define-public vc-dwim-git-worktree
+  (package
+    (inherit vc-dwim)
+    (name "vc-dwim-git-worktree")
+    (source (origin
+              (inherit (package-source vc-dwim))
+              (patches (search-patches "vc-dwim-find-git-worktree.patch"))))))
