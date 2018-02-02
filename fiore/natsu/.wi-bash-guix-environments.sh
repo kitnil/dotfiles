9d197329b53c98696210cc38133164007fb998f8
Oleg Pykhalov <go.wigust@gmail.com>
2018-02-02 09:16:59 +0300

    fiore: natsu: Add .wi-bash-guix-environments.sh.
    * fiore/natsu/.wi-bash-guix-environments.sh: New file.

---
+++ fiore/natsu/.wi-bash-guix-environments.sh
@@ -0,0 +1,3 @@
+alias wi-guix-environment-vc-dwim="guix environment --pure vc-dwim--ad-hoc\
+ strace gdb coreutils findutils grep which make autoconf sed gcc-toolchain\
+ perl gawk patch diffutils inetutils emacs"
