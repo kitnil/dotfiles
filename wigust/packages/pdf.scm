83ab4c9cb4b2230aa56828f95e3eb759801fb92a
Oleg Pykhalov <go.wigust@gmail.com>
2018-02-02 10:16:01 +0300

    wigust: Add mupdf-dark-background.
    * wigust/packages/pdf.scm (mupdf-dark-background): New public variable.

---
+++ wigust/packages/pdf.scm
@@ -0,0 +1,23 @@
+(define-module (wigust packages pdf)
+  #:use-module (guix utils)
+  #:use-module (guix packages)
+  #:use-module (gnu packages)
+  #:use-module (gnu packages pdf)
+  #:use-module (guix build utils))
+
+(define-public mupdf-dark-background
+  (package
+    (inherit mupdf)
+    (name "mupdf-dark-background")
+    (source
+     (origin
+       (inherit (package-source mupdf))
+       (modules '((guix build utils)))
+       (snippet
+        '(begin
+           ;; TODO: Inherit snippet from mupdf
+           (delete-file-recursively "thirdparty")
+
+           ;; Change color from #777777 #000000
+           (substitute* "platform/x11/x11_main.c"
+             (("0x7000") "0x0000"))))))))
