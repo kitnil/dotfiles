471bed58fbdf48db8f590f3f9b6e2338a325a551
Oleg Pykhalov <go.wigust@gmail.com>
2018-02-12 11:32:48 +0300

    wigust: Add haunt-symlinks.
    * haunt-asset-follow-symlinks.patch: New file.
    * wigust/packages/guile.scm: New file.

---
+++ wigust/packages/guile.scm
@@ -0,0 +1,15 @@
+(define-module (wigust packages guile)
+  #:use-module (guix utils)
+  #:use-module (guix packages)
+  #:use-module (gnu packages)
+  #:use-module (gnu packages guile)
+  #:use-module (guix build utils))
+
+(define-public haunt-symlinks
+  (package
+    (inherit haunt)
+    (name "haunt-symlinks")
+    (source
+     (origin
+       (inherit (package-source haunt))
+       (patches (search-patches "haunt-asset-follow-symlinks.patch"))))))
