1f28f16e7cf65045314ae05077e6eded30c7897b
Oleg Pykhalov <go.wigust@gmail.com>
2018-02-14 00:28:44 +0300

    fiore: natsu: bin: Add wi-show-colors.scm.
    * fiore/natsu/bin/wi-show-colors.scm: New file.

---
+++ fiore/natsu/bin/wi-show-colors.scm
@@ -0,0 +1,7 @@
+(use-modules (oop goops) (rnrs) (ice-9 colorized))
+
+(activate-colorized)
+
+(define %test
+  `(this-is-a-symbol 1 2.5 2/5 #\c "asdf" ,(lambda () #t) ,(cons 1 2) ,
+     (vector 1 2 3) #2u32@2@3((1 2) (3 4)) ,(make-bytevector 10 99) ,<object>))
