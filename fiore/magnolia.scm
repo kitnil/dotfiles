9e7e0a90f81f8b263bcae43aab802e86e4455a0a
Oleg Pykhalov <go.wigust@gmail.com>
2018-03-10 13:29:30 +0300

    Revert "fiore: magnolia: Add NixOS grub configfile."
    This reverts commit a6038b73e48eb430009bc48c572b7953d4dd4af1.

--- fiore/magnolia.scm
+++ fiore/magnolia.scm
@@ -287,14 +287,7 @@ EndSection
 
     (bootloader (bootloader-configuration
                  (bootloader grub-efi-bootloader)
-                 (target "/boot/efi")
-                 (menu-entries
-                  (list (menu-entry
-                         (label "NixOS's Grub")
-                         (linux "")
-                         (initrd "")
-                         (additional-options '("search --label --set nixos"
-                                               "configfile /boot/grub/grub.cfg")))))))
+                 (target "/boot/efi")))
 
     (file-systems (cons* (file-system
                            (device "magnolia-root")
