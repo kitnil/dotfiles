ee492f4c00c975d593211ec808dffb6ea3b04add
Oleg Pykhalov <go.wigust@gmail.com>
2018-03-29 11:02:30 +0300

    Add fiore/guix-weather-amd64.sh

---
+++ fiore/guix-weather-amd64.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+
+# See <https://lists.gnu.org/archive/html/guix-devel/2018-03/msg00365.html>.
+
+env GUILE_LOAD_PATH=$HOME/dotfiles:$GUILE_LOAD_PATH\
+    ./pre-inst-env guix weather\
+    --substitute-urls='https://berlin.guixsd.org https://hydra.gnu.org'\
+    -m ~/dotfiles/fiore/manifests/guix-collection-manifest.scm\
+    --system=x86_64-linux
