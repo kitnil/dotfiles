cc320d475763bb9f4cea22f9fdbf6c9bc98c217b
Oleg Pykhalov <go.wigust@gmail.com>
2018-04-01 21:05:54 +0300

    fiore: natsu: bin: Update wi-deploy.sh.
    * fiore/natsu/bin/wi-deploy.sh: Update.

--- fiore/natsu/bin/wi-deploy.sh
+++ fiore/natsu/bin/wi-deploy.sh
@@ -8,10 +8,12 @@ user=natsu
 user_profile=$(readlink -f $HOME/.guix-profile)
 system_profile=$(readlink /run/current-system)
 pull=$(readlink $HOME/.config/guix/latest)
+clover=$(env GUILE_LOAD_PATH=$HOME/src/guix-wigust:$HOME/src/guix-packages:$GUILE_LOAD_PATH $HOME/src/guix/pre-inst-env guix system build /home/natsu/dotfiles/fiore/clover.scm)
 
 echo -e "Deploy to \"$user@$host\":
-  $pull
-  $user_profile
-  $system_profile"
+  pull:     $pull
+  user:     $user_profile
+  magnolia: $system_profile
+  clover:   $clover"
 
-guix copy --to=$user@$host $pull $user_profile $system_profile
+guix copy --to=$user@$host $pull $user_profile $system_profile $clover
