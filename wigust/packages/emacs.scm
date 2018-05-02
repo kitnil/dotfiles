c3a17a35ddca0a6bdb7540069555262457453b1f
Oleg Pykhalov <go.wigust@gmail.com>
2018-05-02 19:09:55 +0300

    wigust: Merge emacs-rsw-elisp to Guix package collection.
    * wigust/packages/emacs.scm (emacs-rsw-elisp): Merge emacs-rsw-elisp to Guix
      package collection.

--- wigust/packages/emacs.scm
+++ wigust/packages/emacs.scm
@@ -1283,28 +1283,6 @@ editor (without an X toolkit)" )
     (arguments (append '(#:configure-flags '("--with-x-toolkit=no"))
                        (package-arguments emacs)))))
 
-(define-public emacs-rsw-elisp
-  (package
-    (name "emacs-rsw-elisp")
-    (version "1.0.5")
-    (source (origin
-              (method url-fetch)
-              (uri (string-append "https://github.com/rswgnu/rsw-elisp"
-                                  "/archive/" version ".tar.gz"))
-              (file-name (string-append name "-" version ".tar.gz"))
-              (sha256
-               (base32
-                "1jnn7xfwl3wxc87v44ccsf1wwp80par3xgcvfb1icd6zchjmlcps"))))
-    (build-system emacs-build-system)
-    (home-page "https://github.com/rswgnu/rsw-elisp")
-    (synopsis "Improved expressions that interactively evaluate Emacs Lisp")
-    (description "This package improves and replaces the GNU Emacs commands
-that interactively evaluate Emacs Lisp expressions.  The new commands replace
-standard key bindings and are all prefixed with rsw-elisp-.  They work the
-same way as the old commands when called non-interactively; only the
-interactive behavior should be different.")
-    (license license:gpl3+)))
-
 (define-public emacs-pcre2el
   (let ((commit "0b5b2a2c173aab3fd14aac6cf5e90ad3bf58fa7d")
         (revision "1"))
