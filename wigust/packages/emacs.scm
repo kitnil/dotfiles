1a77e116741112a322cee656d086afdac5642314
Oleg Pykhalov <go.wigust@gmail.com>
2018-05-02 19:03:26 +0300

    wigust: Merge emacs-hy-mode to Guix package collection.
    * wigust/packages/emacs.scm (emacs-hy-mode): Merge emacs-hy-mode to Guix
      package collection.

--- wigust/packages/emacs.scm
+++ wigust/packages/emacs.scm
@@ -1010,28 +1010,6 @@ comes with sml-proc which allows interaction with an inferior SML
 interactive loop.")
     (license license:gpl3+)))
 
-(define-public emacs-hy-mode
-  (package
-    (name "emacs-hy-mode")
-    (version "1.0.2")
-    (source
-     (origin
-       (method url-fetch)
-       (uri (string-append "https://github.com/hylang/hy-mode/archive/"
-                           "v" version ".tar.gz"))
-       (file-name (string-append name "-" version ".tar.gz"))
-       (sha256
-        (base32
-         "0sbga36zkyhzrzcczsyjzll7b9qsa215pnlw51m4li2irm23jh17"))))
-    (build-system emacs-build-system)
-    (propagated-inputs
-     `(("emacs-dash" ,emacs-dash)
-       ("emacs-s" ,emacs-s)))
-    (home-page "https://github.com/hylang/hy-mode")
-    (synopsis "Major mode for Hylang")
-    (description "This package provides a major mode for Hylang.")
-    (license license:gpl3+)))
-
 (define-public emacs-faceup
   (let ((commit "6c92dad56a133e14e7b27831e1bcf9b3a71ff154")
         (revision "1"))
