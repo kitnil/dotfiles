2cf8efa328fd85e2a29a363be1dcef845a388a0d
Oleg Pykhalov <go.wigust@gmail.com>
2018-05-02 19:03:04 +0300

    wigust: Merge emacs-ibuffer-projectile to Guix package collection.
    * wigust/packages/emacs.scm (emacs-ibuffer-projectile): Merge
      emacs-ibuffer-projectile to Guix package collection.

--- wigust/packages/emacs.scm
+++ wigust/packages/emacs.scm
@@ -1782,32 +1782,6 @@ editor (without an X toolkit)" )
     (arguments (append '(#:configure-flags '("--with-x-toolkit=no"))
                        (package-arguments emacs)))))
 
-(define-public emacs-ibuffer-projectile
-  (let ((commit "c18ac540ee46cb759fc5df18747f6e8d23563011")
-        (revision "1"))
-    (package
-      (name "emacs-ibuffer-projectile")
-      (version (string-append "0.2" "-" revision "."
-                              (string-take commit 7)))
-      (source
-       (origin
-         (method git-fetch)
-         (uri (git-reference
-               (url "https://github.com/purcell/ibuffer-projectile.git")
-               (commit commit)))
-         (file-name (string-append name "-" version "-checkout"))
-         (sha256
-          (base32
-           "1nd26cwwdpnwj0g4w393rd59klpyr6wqrnyr6scmwb5d06bsm44n"))))
-      (build-system emacs-build-system)
-      (propagated-inputs
-       `(("emacs-projectile" ,emacs-projectile)))
-      (home-page "https://github.com/purcell/ibuffer-projectile")
-      (synopsis "Group ibuffer's list by projectile root")
-      (description "Adds functionality to Emacs @code{ibuffer} for
-grouping buffers by their projectile root directory.")
-      (license license:gpl3+))))
-
 (define-public emacs-fancy-narrow
   (package
     (name "emacs-fancy-narrow")
