1ad82d80864982b6fd2f0fb7609859dedbbaa9dc
Oleg Pykhalov <go.wigust@gmail.com>
2018-05-02 15:55:13 +0300

    wigust: emacs-wordgen: Fix description.

--- wigust/packages/emacs.scm
+++ wigust/packages/emacs.scm
@@ -1017,21 +1017,8 @@ every connection, and is exposed through the callbacks.")
     (build-system emacs-build-system)
     (home-page "https://github.com/Fanael/wordgen.el")
     (synopsis "Random word generator")
-    (description "Generate random words using user-provided rules.
-
-Example:
-(wordgen '((result (concat-reeval [(2 1) (5 2) (4 3)] syl))
-   (syl (++ c v coda))
-   (c [(4 \"p\") (5 \"t\") (5 \"k\") (3 \"m\") (4 \"n\") (3 \"s\") (4
-\"l\") (3 \"r\")])
-   (v [\"a\" \"e\" \"i\" \"o\" \"u\"])
-   (coda [(4 \"\") \"m\" \"n\"]))
- :word-count 5)
-
-=> (\"komlamkim\" \"kepa\" \"mennem\" \"ne\" \"palu\")
-
-See the function `wordgen' for complete description.
-")
+    (description "This package provides functions to generate random words
+using user-provided rules.")
     (license license:gpl3+)))
 
 (define-public emacs-indium-checkout
