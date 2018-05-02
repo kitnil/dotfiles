daf359714a7da01aac251fb323384de6b66714d0
Oleg Pykhalov <go.wigust@gmail.com>
2018-05-02 19:10:02 +0300

    wigust: Merge emacs-default-text-scale to Guix package collection.
    * wigust/packages/emacs.scm (emacs-default-text-scale): Merge
      emacs-default-text-scale to Guix package collection.

--- wigust/packages/emacs.scm
+++ wigust/packages/emacs.scm
@@ -1314,29 +1314,6 @@ editor (without an X toolkit)" )
     (description "Convert between PCRE, Emacs and rx regexp syntax.")
     (license license:gpl3+))))
 
-(define-public emacs-default-text-scale
-  (let ((commit "968e985e219235f3e744d6d967e592acbaf6e0a8")
-        (revision "1"))
-    (package
-      (name "emacs-default-text-scale")
-      (version (string-append "0.1" "-" revision "."
-                              (string-take commit 7)))
-      (source (origin
-                (method git-fetch)
-                (uri (git-reference
-                      (url "https://github.com/purcell/default-text-scale")
-                      (commit commit)))
-                (file-name (string-append name "-" version "-checkout"))
-                (sha256
-                 (base32
-                  "0zds01c3q5yny6ab1fxfkzzgn1kgl3q23lxxap905f4qd70v922h"))))
-      (build-system emacs-build-system)
-      (home-page "https://github.com/purcell/default-text-scale")
-      (synopsis "Adjust the font size in all Emacs frames")
-      (description "This package provides commands for increasing or
-decreasing the default font size in all GUI Emacs frames.")
-      (license license:gpl3+))))
-
 (define-public emacs-helm-emms
   (let ((commit "8133c1a854c8f9e32b3b4c74638fe197535c08f1"))
     (package
