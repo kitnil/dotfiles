2413d178a6bb384caf7df8b7ee9b74b68ec83c66
Oleg Pykhalov <go.wigust@gmail.com>
2018-05-17 20:59:14 +0300

    wigust: Add calamares.
    * wigust/packages/calamares.scm (calamares): New public variable.

---
+++ wigust/packages/calamares.scm
@@ -0,0 +1,72 @@
+(define-module (wigust packages calamares)
+  #:use-module (ice-9 match)
+  #:use-module ((guix licenses) #:prefix license:)
+  #:use-module (guix utils)
+  #:use-module (guix packages)
+  #:use-module (guix download)
+  #:use-module (guix git-download)
+  #:use-module (guix gexp)
+  #:use-module (gnu packages)
+  #:use-module (gnu packages disk)
+  #:use-module (gnu packages kde-frameworks)
+  #:use-module (gnu packages qt)
+  #:use-module (gnu packages serialization)
+  #:use-module (gnu packages polkit)
+  #:use-module (gnu packages boost)
+  #:use-module (gnu packages qt)
+  #:use-module (guix build-system cmake))
+
+(define-public calamares
+  (package
+    (name "calamares")
+    (version "3.1.13")
+    (source
+     (origin
+       (method url-fetch)
+       (uri (string-append "https://github.com/calamares/calamares/archive/"
+                           "v" version ".tar.gz"))
+       (file-name (string-append name "-" version ".tar.gz"))
+       (sha256
+        (base32
+         "1l32qpg17kd6v97w8nx95dm3alrwg9j1nk62p4hn2s302zh56c49"))))
+    (build-system cmake-build-system)
+    (arguments
+     `(#:out-of-source? #f
+       #:tests? #f
+       #:parallel-build? #f
+       #:configure-flags
+       (let ((qtwebkit (assoc-ref %build-inputs "qtwebkit")))
+         `("-DWEBVIEW_FORCE_WEBKIT:BOOL=ON"
+           "-DSKIP_MODULES=\"webview interactiveterminal initramfs \
+initramfscfg dracut dracutlukscfg dummyprocess dummypython dummycpp \
+dummypythonqt plasmalnf\""
+           ,(string-append "-DQt5WebKit_DIR=" qtwebkit "/lib/cmake/Qt5WebKit")
+           ,(string-append "-DQt5WebKitWidgets_DIR="
+                           qtwebkit "/lib/cmake/Qt5WebKitWidgets")
+           "-DCMAKE_VERBOSE_MAKEFILE=True"
+           "-DCMAKE_BUILD_TYPE=Release"
+           "-DWITH_PYTHONQT:BOOL=ON"))))
+    (inputs
+     `(("kconfig" ,kconfig)
+       ("kcoreaddons" ,kcoreaddons)
+       ("kiconthemes" ,kiconthemes)
+       ("ki18n" ,ki18n)
+       ("kio" ,kio)
+       ("kpmcore" ,kpmcore)
+       ("extra-cmake-modules" ,extra-cmake-modules)
+       ("parted" ,parted)
+       ("polkit-qt" ,polkit-qt)
+       ("python-pyqt" ,python-pyqt)
+       ("qt" ,qt)
+       ("qtbase" ,qtbase)
+       ("qtwebkit" ,qtwebkit)
+       ("qttools" ,qttools)
+       ("qtsvg" ,qtsvg)
+       ("solid" ,solid)
+       ("boost" ,boost)
+       ("yaml-cpp" ,yaml-cpp)))
+    (home-page "https://calamares.io")
+    (synopsis "Distribution-independent installer framework")
+    (description
+     "This package provides a distribution-independent installer framework.")
+    (license #f)))
