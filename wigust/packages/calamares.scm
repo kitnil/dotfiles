549047a1f3e056808218765ea0f6f40a9063331b
Oleg Pykhalov <go.wigust@gmail.com>
2018-05-18 14:58:51 +0300

    wigust: Add boost-1.63.0.
    * wigust/packages/calamares.scm (boost-1.63): New public variable.
    (calamares): Use this.

--- wigust/packages/calamares.scm
+++ wigust/packages/calamares.scm
@@ -14,11 +14,26 @@
   #:use-module (gnu packages polkit)
   #:use-module (gnu packages boost)
   #:use-module (gnu packages qt)
+  #:use-module (gnu packages python)
   #:use-module (gnu packages freedesktop)
   #:use-module (gnu packages linux)
   #:use-module (gnu packages bootloaders)
   #:use-module (guix build-system cmake)
   #:use-module (guix build utils))
+
+(define-public boost-1.63
+  (package
+    (inherit boost)
+    (version "1.63.0")
+    (source (origin
+              (method url-fetch)
+              (uri (string-append
+                    "mirror://sourceforge/boost/boost/" version "/boost_"
+                    (string-map (lambda (x) (if (eq? x #\.) #\_ x)) version)
+                    ".tar.bz2"))
+              (sha256
+               (base32
+                "1c5kzhcqahnic55dxcnw7r80qvwx5sfa2sa97yzv7xjrywljbbmy"))))))
 
 (define-public calamares
   (package
@@ -38,8 +53,12 @@
      `(#:out-of-source? #f
        #:tests? #f
        #:parallel-build? #f
+       #:build-type "Release"
        #:configure-flags
-       (let ((qtwebkit (assoc-ref %build-inputs "qtwebkit")))
+       (let ((qtwebkit (assoc-ref %build-inputs "qtwebkit"))
+             (python (assoc-ref %build-inputs "python"))
+             (out (assoc-ref %outputs "out"))
+             #;(boost (assoc-ref %build-inputs "boost")))
          `("-DWEBVIEW_FORCE_WEBKIT:BOOL=ON"
            "-DSKIP_MODULES=\"webview interactiveterminal initramfs \
 initramfscfg dracut dracutlukscfg dummyprocess dummypython dummycpp \
@@ -47,12 +66,26 @@ dummypythonqt plasmalnf\""
            ,(string-append "-DQt5WebKit_DIR=" qtwebkit "/lib/cmake/Qt5WebKit")
            ,(string-append "-DQt5WebKitWidgets_DIR="
                            qtwebkit "/lib/cmake/Qt5WebKitWidgets")
+           ,(string-append "-DCALAMARES_BOOST_PYTHON3_COMPONENT="
+                           (assoc-ref %build-inputs "boost")
+                           "/lib/libboost_python.so")
            "-DCMAKE_VERBOSE_MAKEFILE=True"
-           "-DCMAKE_BUILD_TYPE=Release"
-           "-DWITH_PYTHONQT:BOOL=ON"
+           ,(string-append "-DCMAKE_INSTALL_PREFIX=" out)
+           ;; "-DCMAKE_BUILD_TYPE=Release"
+           ;; ,(string-append "-DPYTHON_LIBRARY="
+           ;;                 python "/lib/libpython3.6m.so.1.0")
+           ;; ,(string-append "-DPYTHON_INCLUDE_DIR="
+           ;;                 python "/include/python3.6m")
+           "-DBoost_DEBUG=1"
+           ;; "-DWITH_PYTHON:BOOL=ON"
+           ;; "-DWITH_PYTHONQT:BOOL=ON"
            ,(string-append "-DPOLKITQT-1_POLICY_FILES_INSTALL_DIR="
-                           (assoc-ref %outputs "out")
-                           "/share/polkit-1/actions")))
+                           out "/share/polkit-1/actions")
+           ;; "-DLIB_SUFFIX="
+           #;"-DSUPPRESS_BOOST_WARNINGS:BOOL=ON"
+           #;,(string-append "-DBoost_INCLUDE_DIRS=" boost "/include")
+           #;,(string-append "-DBoost_LIBRARY_DIRS=" boost "/lib")
+           #;"-DBoost_NO_BOOST_CMAKE=ON"))
        #:modules ((ice-9 match)
                   (guix build cmake-build-system)
                   (guix build utils))
@@ -62,13 +95,24 @@ dummypythonqt plasmalnf\""
            (lambda* (#:key inputs #:allow-other-keys)
              (substitute* "src/modules/partition/core/PartUtils.cpp"
                (("\"os-prober\"") (string-append "\"" (which "os-prober")
-                                                 "\"")))))
+                                                 "\"")))
+             ;; (setenv "BOOST_LIB_SUFFIX" "")
+             (setenv "BOOST_LIBRARYDIR"
+                     (string-append (assoc-ref %build-inputs "boost") "/lib"))
+             ;; (setenv "BOOST_ROOT" (assoc-ref %build-inputs "boost"))
+             ;; (setenv "BOOST_LIBRARYDIR"
+             ;;         (string-append (assoc-ref %build-inputs "boost")
+             ;;                        "/lib"))
+             ;; Required to create a package registry file.
+             (setenv "HOME" (getcwd))))
          (add-before 'install 'patch-cmakelists
            (lambda* (#:key outputs #:allow-other-keys)
              (substitute* "CMakeLists.txt"
                (("\\$\\{POLKITQT-1_POLICY_FILES_INSTALL_DIR\\}")
                 (string-append (assoc-ref outputs "out")
                                "/share/polkit-1/actions")))))
+         (add-after 'configure 'fail
+             (lambda _ #f))
          (add-after 'install 'wrap-executable
            (lambda* (#:key inputs outputs #:allow-other-keys)
              (let ((out (assoc-ref outputs "out"))
@@ -100,7 +144,7 @@ dummypythonqt plasmalnf\""
        ("libatasmart" ,libatasmart)
        ("parted" ,parted)
        ("polkit-qt" ,polkit-qt)
-       ("python-pyqt" ,python-pyqt)
+       ;; ("python-pyqt" ,python-pyqt)
        ("util-linux" ,util-linux)
        ("qt" ,qt)
        ("qtbase" ,qtbase)
@@ -108,7 +152,8 @@ dummypythonqt plasmalnf\""
        ("qttools" ,qttools)
        ("qtsvg" ,qtsvg)
        ("solid" ,solid)
-       ("boost" ,boost)
+       ("boost" ,boost-1.63)
+       ("python" ,python)
        ("yaml-cpp" ,yaml-cpp)))
     (home-page "https://calamares.io")
     (synopsis "Distribution-independent installer framework")
