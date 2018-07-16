83e3eb04af8b7fb81e4f9709fb93b462bb0b7835
Oleg Pykhalov <go.wigust@gmail.com>
2018-07-17 01:19:50 +0300

    natsu: record-video: Fix terminal name.
    * fiore/natsu/bin/record-video.sh: Fix terminal name.

--- fiore/natsu/bin/record-video.sh
+++ fiore/natsu/bin/record-video.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-exec -a ffmpeg urxvt -name htop -e ffmpeg -f x11grab -r 30 -video_size 1920x1080 -i $DISPLAY -c:v ffvhuff /tmp/screencast-$(date +%F-%H-%M).mkv $@
+exec -a ffmpeg urxvt -name ffpeg -e ffmpeg -f x11grab -r 30 -video_size 1920x1080 -i $DISPLAY -c:v ffvhuff /tmp/screencast-$(date +%F-%H-%M).mkv $@
