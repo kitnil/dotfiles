b0ac6514e2c30b25637254061672000adf8e2c86
Oleg Pykhalov <go.wigust@gmail.com>
2018-11-13 19:55:41 +0300

    fiore: modules: services: monitoring: Generate server configuration.
    * fiore/modules/services/monitoring.scm: Generate server configuration.

--- fiore/modules/services/monitoring.scm
+++ fiore/modules/services/monitoring.scm
@@ -22,6 +22,7 @@
   #:use-module (gnu services shepherd)
   #:use-module (gnu packages admin)
   #:use-module (gnu packages monitoring)
+  #:use-module (guix packages)
   #:use-module (gnu system shadow)
   #:use-module (guix gexp)
   #:use-module (guix records)
@@ -31,43 +32,34 @@
 
 (define (uglify-field-name field-name)
   (apply string-append
-         (map string-capitalize
+         (map (lambda (str)
+                (if (or (string= str "ssl") (string= str "db"))
+                    (string-upcase str)
+                    (string-capitalize str)))
               (string-split (string-delete #\?
                                            (symbol->string field-name))
                             #\-))))
 
 (define (serialize-field field-name val)
-  (format #t "~a=~a~%" (uglify-field-name field-name) val))
+  #~(format #f "~a=~a~%" #$(uglify-field-name field-name) #$val))
 
 (define (serialize-number field-name val)
   (serialize-field field-name (number->string val)))
 
 (define (serialize-list field-name val)
-  (if (null? val) "" (serialize-field field-name (string-join val))))
+  (if (null? val) "" (serialize-field field-name (string-join val ","))))
 
 (define (serialize-string field-name val)
   (if (and (string? val) (string=? val ""))
       ""
       (serialize-field field-name val)))
 
-(define (serialize-boolean field-name val)
-  (serialize-string field-name (if val "yes" "no")))
- 
 (define-configuration zabbix-server-configuration
-  (debug-level
-   (number 5)
+  (user
+   (string "zabbix")
    "")
-  (alert-scripts-path
-   (string "${datadir}/zabbix/alertscripts")
-   "")
-  (allow-root?
-   (boolean #f)
-   "")
-  (cache-size
-   (string "8M")
-   "")
-  (cache-update-frequency
-   (number 60)
+  (group
+   (string "zabbix")
    "")
   (db-host
    (string "127.0.0.1")
@@ -81,92 +73,11 @@
   (db-port
    (number 5432)
    "")
-  (db-schema
-   (string "")
-   "")
-  (db-socket
-   (string "")
-   "")
-  (external-scripts
-   (string "${datadir}/zabbix/externalscripts")
-   "")
-  (fping-location
-   (string "/run/setuid-programs/fping")
-   "")
-  (fping6-location
-   (string "/run/setuid-programs/fping")
-   "")
-  (history-cache-size
-   (string "16M")
-   "")
-  (history-index-cache-size
-   (string "4M")
-   "")
-  (history-storage-types
-   (list '("uint" "dbl" "str" "log" "text"))
-   "")
-  (history-storage-url
-   (string "")
-   "")
-  (housekeeping-frequency
-   (number 1)
-   "")
-  (include-file
-   (list '())
-   "")
-  (java-gateway
-   (string "")
-   "")
-  (java-gateway-port
-   (number 10052)
-   "")
-  (listen-ip-addresses
-   (list '("0.0.0.0" "127.0.0.1"))
-   "")
-  (listen-port
-   (number 10051)
-   "")
-  (load-module
-   (string "")
-   "")
-  (load-module-path
-   (string "${libdir}/modules")
-   "")
   (log-file
-   (string "")
-   "")
-  (log-file-size
-   (number 1)
-   "")
-  (log-slow-queries
-   (number 3000)
-   "")
-  (log-type
-   (string "file")
-   "")
-  (max-housekeeper-delete
-   (string "5000")
+   (string "/var/log/zabbix/server.log")
    "")
   (pid-file
    (string "/var/run/zabbix/zabbix-server.pid")
-   "")
-  (proxy-config-frequency
-   (number 3600)
-   "")
-  (proxy-data-frequency
-   (number 1)
-   "")
-  (snmp-trapper-file
-   (string "/tmp/zabbix_traps.tmp")
-   "")
-  (socket-dir
-   (string "/tmp")
-   "")
-  (source-ip
-   (string "")
-   "")
-  (ssh-key-location
-   (string "")
    "")
   (ssl-ca-location
    (string "/etc/ssl/certs/ca-certificates.crt")
@@ -174,247 +85,77 @@
   (ssl-cert-location
    (string "/etc/ssl/certs")
    "")
-  (start-alerters
-   (number 3)
-   "")
-  (start-db-syncers
-   (number 4)
-   "")
-  (start-discoverers
-   (number 1)
-   "")
-  (start-escalators
-   (number 1)
-   "")
-  (start-http-pollers
-   (number 1)
-   "")
-  (start-ipmi-pollers
-   (number 0)
-   "")
-  (start-java-pollers
-   (number 0)
-   "")
-  (start-pingers
-   (number 1)
-   "")
-  (start-pollers
-   (number 5)
-   "")
-  (start-pollers-unreachable
-   (number 1)
-   "")
-  (start-preprocessors
-   (number 3)
-   "")
-  (start-proxy-pollers
-   (number 1)
-   "")
-  (start-snmp-trapper
-   (string "0")
-   "")
-  (start-timers
-   (number 1)
-   "")
-  (start-trappers
-   (number 5)
-   "")
-  (start-vmware-collectors
-   (number 0)
-   "")
-  (timeout
-   (number 3)
-   "")
-  (tls-ca-file
+  (extra-options
    (string "")
-   "")
-  (tls-cert-file
-   (string "")
-   "")
-  (tls-crl-file
-   (string "")
-   "")
-  (tls-key-file
-   (string "")
-   "")
-  (tmp-dir
-   (string "/tmp")
-   "")
-  (trapper-timeout
-   (number 300)
-   "")
-  (trend-cache-size
-   (string "4M")
-   "")
-  (unavailable-delay
-   (number 60)
-   "")
-  (unreachable-delay
-   (number 15)
-   "")
-  (unreachable-period
-   (number 45)
-   "")
-  (user
-   (string "zabbix")
-   "")
-  (value-cache-size
-   (string "8M")
-   "")
-  (vmware-cache-size
-   (string "8M")
-   "")
-  (vmware-frequency
-   (number 60)
-   "")
-  (vmware-perf-frequency
-   (number 60)
-   "")
-  (vmware-timeout
-   (number 10)
    ""))
 
 (define-configuration zabbix-agentd-configuration
-  (alias
-   (string "")
-   "")
-  (allow-root?
-   (boolean #f)
-   "")
-  (buffer-send
-   (number 5)
-   "")
-  (buffer-size
-   (number 100)
-   "")
-  (debug-level
-   (number 3)
-   "")
-  (enable-remote-commands?
-   (boolean #f)
-   "")
-  (host-metadata
-   (string "")
-   "")
-  (host-metadata-item
-   (string "")
-   "")
   (hostname
    (string "Zabbix server")
    "")
-  (hostname-item
-   (string "system.hostname")
-   "")
-  (include-files
-   (string '())
-   "")
-  (listen-ip
-   (string "0.0.0.0")
-   "")
-  (listen-port
-   (string "10050")
-   "")
-  (load-module
-   (string "")
-   "")
-  (load-module-path
-   (string "${libdir}/modules")
-   "")
   (log-file
-   (string "/tmp/zabbix-agentd.log")
-   "")
-  (log-file-size
-   (number 1)
-   "")
-  (log-remote-commands
-   (boolean #f)
-   "")
-  (log-type
-   (string "file")
-   "")
-  (max-lines-per-second
-   (number 20)
+   (string "/var/log/zabbix/agentd.log")
    "")
   (pid-file
    (string "/var/run/zabbix/zabbix-agentd.pid")
    "")
-  (refresh-active-checks
-   (number 120)
-   "")
   (server
-   (string '("127.0.0.1" "192.168.1.0/24" "::1" "2001:db8::/32"))
-   "")
+   (list '("127.0.0.1" "192.168.1.0/24" "::1" "2001:db8::/32"))
+   "")  
   (server-active
-   (string "127.0.0.1:20051,zabbix.domain,[::1]:30051,::1,[12fc::1]")
+   (list '("127.0.0.1:20051" "zabbix.domain" "[::1]:30051" "::1" "[12fc::1]"))
    "")
-  (source-ip
+  (extra-options
    (string "")
-   "")
-  (start-agents
-   (number 3)
-   "")
-  (timeout
-   (number 3)
-   "")
-  (tls-accept
-   (string "unencrypted")
-   "")
-  (tls-ca-file
-   (string "")
-   "")
-  (tls-cert-file
-   (string "")
-   "")
-  (tls-connect
-   (string "unencrypted")
-   "")
-  (tls-crl-file
-   (string "")
-   "")
-  (tls-key-file
-   (string "")
-   "")
-  (tls-psk-file
-   (string "")
-   "")
-  (tls-psk-identity
-   (string "")
-   "")
-  (tls-server-cert-issuer
-   (string "")
-   "")
-  (tls-server-cert-subject
-   (string "")
-   "")
-  (unsafe-user-parameters
-   (string "0")
-   "")
-  (user
-   (string "zabbix")
-   "")
-  (user-parameter
-   (string '())
    ""))
+
+(define-configuration opaque-zabbix-server-configuration
+  (zabbix-server
+   (package zabbix-server)
+   "The zabbix-server package.")
+  (config
+   (string (configuration-missing-field 'opaque-zabbix-server-configuration
+                                        'config))
+   "The contents of the @code{zabbix-serverrc} to use."))
 
 (define (zabbix-server-account config)
   "Return the user accounts and user groups for CONFIG."
-  (let ((zabbix-user "zabbix")
-        (zabbix-group "zabbix"))
-      (list (user-group (name zabbix-group) (system? #t))
-        (user-account
-         (name zabbix-user)
-         (system? #t)
-         (group zabbix-group)
-         (comment "zabbix privilege separation user")
-         (home-directory (string-append "/var/run/" zabbix-user))
-         (shell #~(string-append #$shadow "/sbin/nologin"))))))
+  (let ((zabbix-user (zabbix-server-configuration-user config))
+        (zabbix-group (zabbix-server-configuration-group config)))
+    (list (user-group (name zabbix-group) (system? #t))
+          (user-account
+           (name zabbix-user)
+           (system? #t)
+           (group zabbix-group)
+           (comment "zabbix privilege separation user")
+           (home-directory (string-append "/var/run/" zabbix-user))
+           (shell #~(string-append #$shadow "/sbin/nologin"))))))
 
 (define (zabbix-server-activation config)
+  "Return the activation gexp for CONFIG."
+  ;; (($ <opaque-zabbix-server-configuration> zabbix-server config)
+  ;;  (opaque-zabbix-server-configuration-config config))
+  #~(begin
+      (use-modules (guix build utils))
+      (let ((user (getpw #$(zabbix-server-configuration-user config)))
+            (log-file #$(zabbix-server-configuration-log-file config))
+            (pid-file #$(zabbix-server-configuration-pid-file config)))
+        (for-each (lambda (file)
+                    (let ((directory (dirname file)))
+                      (mkdir-p (pk 'directory directory))
+                      (chown directory (passwd:uid user) (passwd:gid user))
+                      (chmod directory #o755)))
+                  (list log-file pid-file "/etc/zabbix/zabbix_server.conf")))
+      (copy-file
+       #$(mixed-text-file "zabbix_server.conf"
+                          (serialize-configuration config zabbix-server-configuration-fields))
+       "/etc/zabbix/zabbix_server.conf")))
+
+#;(define (zabbix-server-activation config)
   "Return the activation GEXP for CONFIG."
   #~(begin
       (use-modules (guix build utils))
-      (let ((user (getpw "zabbix"))
-            (group (getpw "zabbix")))
+      (let ()
         (let ((dir "/var/run/zabbix"))
           (mkdir-p dir)
           (chmod dir #o700)
@@ -490,7 +231,7 @@
                              zabbix-server-account)
           (service-extension activation-service-type
                              zabbix-server-activation)))
-   (default-value '())))
+   (default-value (zabbix-server-configuration))))
 
 (define zabbix-agentd-service-type
   (service-type
