4014f6d630022f038cbdd322f932aa472a84af08
Oleg Pykhalov <go.wigust@gmail.com>
2019-01-14 01:11:11 +0300

    natsu: stumpwm: Add more rules.
    * fiore/natsu/.stumpwm.d/desktop/3.lisp: New file.
    * fiore/natsu/.stumpwm.d/rules/3.lisp: New file.

---
+++ fiore/natsu/.stumpwm.d/rules/3.lisp
@@ -0,0 +1,6 @@
+(("Default" 2 T T :CLASS "Firefox" :INSTANCE "Navigator" :TITLE
+  "Dashboard - Mozilla Firefox" :ROLE "browser")
+ ("Default" 1 T NIL :CLASS "Firefox") ("Default" 0 T NIL :CLASS "IceCat")
+ ("Default" 0 T NIL :TITLE "pulsemixer") ("Default" 3 NIL NIL :CLASS "mpv")
+ ("Default" 3 NIL NIL :TITLE "youtube-dl")
+ ("Default" 3 NIL T :CLASS "mpv" :TITLE "emacs-emms"))
