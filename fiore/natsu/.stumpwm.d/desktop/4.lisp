236c173a97f865f67117280de5c4ce054aa28bca
Oleg Pykhalov <go.wigust@gmail.com>
2019-02-18 02:22:37 +0300

    natsu: stumpwm: Add rules 4.

---
+++ fiore/natsu/.stumpwm.d/desktop/4.lisp
@@ -0,0 +1,46 @@
+#S(DDUMP
+   :SCREENS (#S(SDUMP
+                :NUMBER 0
+                :GROUPS (#S(GDUMP
+                            :NUMBER 1
+                            :NAME "Default"
+                            :TREE (((#S(FDUMP
+                                        :NUMBER 0
+                                        :X 0
+                                        :Y 0
+                                        :WIDTH 640
+                                        :HEIGHT 364
+                                        :WINDOWS (27262978)
+                                        :CURRENT 27262978)
+                                     #S(FDUMP
+                                        :NUMBER 2
+                                        :X 0
+                                        :Y 364
+                                        :WIDTH 640
+                                        :HEIGHT 716
+                                        :WINDOWS (48234531)
+                                        :CURRENT 48234531))
+                                    #S(FDUMP
+                                       :NUMBER 1
+                                       :X 640
+                                       :Y 0
+                                       :WIDTH 1280
+                                       :HEIGHT 1080
+                                       :WINDOWS (8389268 16777231 23068685
+                                                 33554462)
+                                       :CURRENT 8389268)))
+                            :CURRENT 1)
+                         #S(GDUMP
+                            :NUMBER 2
+                            :NAME "web"
+                            :TREE (#S(FDUMP
+                                      :NUMBER 0
+                                      :X 0
+                                      :Y 0
+                                      :WIDTH 1920
+                                      :HEIGHT 1080
+                                      :WINDOWS NIL
+                                      :CURRENT NIL))
+                            :CURRENT 0))
+                :CURRENT 1))
+   :CURRENT 0)
