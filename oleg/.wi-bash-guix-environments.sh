a6c82402a598419f2a3ba586d0dab7d0e8e46a56
Oleg Pykhalov <go.wigust@gmail.com>
2019-02-23 16:32:01 +0300

    oleg: Delete '.wi-bash-guix-environments.sh'.

--- oleg/.wi-bash-guix-environments.sh
+++
@@ -1,6 +0,0 @@
-alias wi-guix-environment-vc-dwim="guix environment --pure vc-dwim--ad-hoc\
- strace gdb coreutils findutils grep which make autoconf sed gcc-toolchain\
- perl gawk patch diffutils inetutils emacs"
-
-alias wi-guix-environment-mupdf="guix environment -C mupdf --ad-hoc\
- coreutils findutils grep strace gdb"
