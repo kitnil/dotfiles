e0c24fe3ac6837143d6ae579cfb505660deb9c0c
Oleg Pykhalov <go.wigust@gmail.com>
2019-02-25 02:39:55 +0300

    oleg: emacs: Add '.elfeed-feeds.el'.

---
+++ oleg/.emacs.d/.elfeed-feeds.el
@@ -0,0 +1,44 @@
+(setq elfeed-feeds
+      '("http://nullprogram.com/feed/"
+        "http://steckerhalter.tk/index.xml"
+        "https://oremacs.com/atom.xml"
+        ("https://www.bennee.com/~alex/blog/feed/" emacs)
+        ("https://cestlaz.github.io/rss.xml" emacs)
+        ("https://www.reddit.com/r/freegames/.rss" game)
+        "https://h-node.org/rss/modifications/en"
+        "https://lwn.net/headlines/newrss"
+        "https://fedoramagazine.org/feed/"
+        ("http://planet.emacsen.org/atom.xml" planet)
+        ("http://planet.gnu.org/atom.xml" planet)
+        ("http://planet.lisp.org/rss20.xml" planet)
+        ("http://www.scheme.dk/planet/atom.xml" planet)
+        ("https://www.youtube.com/feeds/videos.xml?channel_id=UCZrrEuHiQjN2CUo84g5tk7w"
+         video)
+        ("https://www.youtube.com/feeds/videos.xml?user=LDCNow"
+         video)
+        ("https://www.youtube.com/feeds/videos.xml?channel_id=UC2eYFnH61tmytImy1mTYvhA"
+         video)
+        ("https://www.youtube.com/feeds/videos.xml?channel_id=UCMV8p6Lb-bd6UZtTc_QD4zA"
+         video)
+        ("https://www.youtube.com/feeds/videos.xml?channel_id=UCVls1GmFKf6WlTraIb_IaJg"
+         video)
+        ("https://www.youtube.com/feeds/videos.xml?user=video-dubbeltumme"
+         video)
+        ("https://www.youtube.com/feeds/videos.xml?user=video-tuxreviews"
+         video)
+        ("https://www.youtube.com/feeds/videos.xml?user=video-gotbletu"
+         video)
+        ("https://www.youtube.com/feeds/videos.xml?channel_id=UCbHXJGd7c8Hy4z0-YX1Jf3Q"
+         video)
+        ("https://www.youtube.com/feeds/videos.xml?user=SsethTzeentach"
+         video)
+        ("https://bitlove.org/jupiterbroadcasting/bsdnowhd/feed"
+         video)
+        ("https://www.youtube.com/feeds/videos.xml?channel_id=UCVls1GmFKf6WlTraIb_IaJg"
+         video)
+        ("https://www.youtube.com/feeds/videos.xml?channel_id=UCgU5tUdVPpfM7sLAMWBTsDg"
+         video)
+        ("https://www.youtube.com/feeds/videos.xml?user=video-metalx1000"
+         video)
+        ("https://www.youtube.com/feeds/videos.xml?channel_id=UCkK9UDm_ZNrq_rIXCz3xCGA"
+         video)))
