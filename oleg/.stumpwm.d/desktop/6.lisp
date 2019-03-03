2b2687aa9fc01c65fdce507c6848c14dac91efde
Oleg Pykhalov <go.wigust@gmail.com>
2019-03-04 00:11:19 +0300

    oleg: stumpwm: Add more rules.

---
+++ oleg/.stumpwm.d/desktop/6.lisp
@@ -0,0 +1,227 @@
+#S(DDUMP
+   :SCREENS (#S(SDUMP
+                :NUMBER 0
+                :GROUPS (#S(GDUMP
+                            :NUMBER 1
+                            :NAME "Default"
+                            :TREE (((#S(FDUMP
+                                        :NUMBER 0
+                                        :X 0
+                                        :Y 0
+                                        :WIDTH 480
+                                        :HEIGHT 270
+                                        :WINDOWS (20971522)
+                                        :CURRENT 20971522)
+                                     #S(FDUMP
+                                        :NUMBER 3
+                                        :X 0
+                                        :Y 270
+                                        :WIDTH 480
+                                        :HEIGHT 810
+                                        :WINDOWS (33554751)
+                                        :CURRENT 33554751))
+                                    #S(FDUMP
+                                       :NUMBER 2
+                                       :X 480
+                                       :Y 0
+                                       :WIDTH 1440
+                                       :HEIGHT 1080
+                                       :WINDOWS (16777244 46137357 54525953
+                                                 29360144 52428813 117440542
+                                                 39845901)
+                                       :CURRENT 16777244))
+                                   ((#S(FDUMP
+                                        :NUMBER 1
+                                        :X 1920
+                                        :Y 0
+                                        :WIDTH 480
+                                        :HEIGHT 360
+                                        :WINDOWS (41943046)
+                                        :CURRENT 41943046)
+                                     #S(FDUMP
+                                        :NUMBER 5
+                                        :X 1920
+                                        :Y 360
+                                        :WIDTH 480
+                                        :HEIGHT 720
+                                        :WINDOWS (33560571)
+                                        :CURRENT 33560571))
+                                    (#S(FDUMP
+                                        :NUMBER 7
+                                        :X 2400
+                                        :Y 0
+                                        :WIDTH 1440
+                                        :HEIGHT 360
+                                        :WINDOWS (48234512)
+                                        :CURRENT 48234512)
+                                     #S(FDUMP
+                                        :NUMBER 6
+                                        :X 2400
+                                        :Y 360
+                                        :WIDTH 1440
+                                        :HEIGHT 720
+                                        :WINDOWS (23068702)
+                                        :CURRENT 23068702))))
+                            :CURRENT 2)
+                         #S(GDUMP
+                            :NUMBER 2
+                            :NAME #A((1) BASE-CHAR . "2")
+                            :TREE (#S(FDUMP
+                                      :NUMBER 0
+                                      :X 0
+                                      :Y 0
+                                      :WIDTH 1920
+                                      :HEIGHT 1080
+                                      :WINDOWS NIL
+                                      :CURRENT NIL)
+                                   #S(FDUMP
+                                      :NUMBER 1
+                                      :X 1920
+                                      :Y 0
+                                      :WIDTH 1920
+                                      :HEIGHT 1080
+                                      :WINDOWS NIL
+                                      :CURRENT NIL))
+                            :CURRENT 0)
+                         #S(GDUMP
+                            :NUMBER 3
+                            :NAME #A((1) BASE-CHAR . "3")
+                            :TREE (#S(FDUMP
+                                      :NUMBER 0
+                                      :X 0
+                                      :Y 0
+                                      :WIDTH 1920
+                                      :HEIGHT 1080
+                                      :WINDOWS NIL
+                                      :CURRENT NIL)
+                                   #S(FDUMP
+                                      :NUMBER 1
+                                      :X 1920
+                                      :Y 0
+                                      :WIDTH 1920
+                                      :HEIGHT 1080
+                                      :WINDOWS NIL
+                                      :CURRENT NIL))
+                            :CURRENT 0)
+                         #S(GDUMP
+                            :NUMBER 4
+                            :NAME #A((1) BASE-CHAR . "4")
+                            :TREE (#S(FDUMP
+                                      :NUMBER 0
+                                      :X 0
+                                      :Y 0
+                                      :WIDTH 1920
+                                      :HEIGHT 1080
+                                      :WINDOWS NIL
+                                      :CURRENT NIL)
+                                   #S(FDUMP
+                                      :NUMBER 1
+                                      :X 1920
+                                      :Y 0
+                                      :WIDTH 1920
+                                      :HEIGHT 1080
+                                      :WINDOWS NIL
+                                      :CURRENT NIL))
+                            :CURRENT 0)
+                         #S(GDUMP
+                            :NUMBER 5
+                            :NAME #A((1) BASE-CHAR . "5")
+                            :TREE (#S(FDUMP
+                                      :NUMBER 0
+                                      :X 0
+                                      :Y 0
+                                      :WIDTH 1920
+                                      :HEIGHT 1080
+                                      :WINDOWS NIL
+                                      :CURRENT NIL)
+                                   #S(FDUMP
+                                      :NUMBER 1
+                                      :X 1920
+                                      :Y 0
+                                      :WIDTH 1920
+                                      :HEIGHT 1080
+                                      :WINDOWS NIL
+                                      :CURRENT NIL))
+                            :CURRENT 0)
+                         #S(GDUMP
+                            :NUMBER 6
+                            :NAME #A((1) BASE-CHAR . "6")
+                            :TREE (#S(FDUMP
+                                      :NUMBER 0
+                                      :X 0
+                                      :Y 0
+                                      :WIDTH 1920
+                                      :HEIGHT 1080
+                                      :WINDOWS NIL
+                                      :CURRENT NIL)
+                                   #S(FDUMP
+                                      :NUMBER 1
+                                      :X 1920
+                                      :Y 0
+                                      :WIDTH 1920
+                                      :HEIGHT 1080
+                                      :WINDOWS NIL
+                                      :CURRENT NIL))
+                            :CURRENT 0)
+                         #S(GDUMP
+                            :NUMBER 7
+                            :NAME #A((1) BASE-CHAR . "7")
+                            :TREE (#S(FDUMP
+                                      :NUMBER 0
+                                      :X 0
+                                      :Y 0
+                                      :WIDTH 1920
+                                      :HEIGHT 1080
+                                      :WINDOWS NIL
+                                      :CURRENT NIL)
+                                   #S(FDUMP
+                                      :NUMBER 1
+                                      :X 1920
+                                      :Y 0
+                                      :WIDTH 1920
+                                      :HEIGHT 1080
+                                      :WINDOWS NIL
+                                      :CURRENT NIL))
+                            :CURRENT 0)
+                         #S(GDUMP
+                            :NUMBER 8
+                            :NAME #A((1) BASE-CHAR . "8")
+                            :TREE (#S(FDUMP
+                                      :NUMBER 0
+                                      :X 0
+                                      :Y 0
+                                      :WIDTH 1920
+                                      :HEIGHT 1080
+                                      :WINDOWS NIL
+                                      :CURRENT NIL)
+                                   #S(FDUMP
+                                      :NUMBER 1
+                                      :X 1920
+                                      :Y 0
+                                      :WIDTH 1920
+                                      :HEIGHT 1080
+                                      :WINDOWS NIL
+                                      :CURRENT NIL))
+                            :CURRENT 0)
+                         #S(GDUMP
+                            :NUMBER 9
+                            :NAME #A((1) BASE-CHAR . "9")
+                            :TREE (#S(FDUMP
+                                      :NUMBER 0
+                                      :X 0
+                                      :Y 0
+                                      :WIDTH 1920
+                                      :HEIGHT 1080
+                                      :WINDOWS NIL
+                                      :CURRENT NIL)
+                                   #S(FDUMP
+                                      :NUMBER 1
+                                      :X 1920
+                                      :Y 0
+                                      :WIDTH 1920
+                                      :HEIGHT 1080
+                                      :WINDOWS NIL
+                                      :CURRENT NIL))
+                            :CURRENT 0))
+                :CURRENT 1))
+   :CURRENT 0)
---
+++ oleg/.stumpwm.d/rules/6.lisp
@@ -0,0 +1,6 @@
+(("Default" 2 T NIL :CLASS "Firefox") ("Default" 2 T NIL :CLASS "IceCat")
+ ("Default" 0 T NIL :TITLE "pulsemixer")
+ ("Default" 3 NIL NIL :TITLE "youtube-dl")
+ ("Default" 0 NIL T :CLASS "mpv" :TITLE "emacs-emms")
+ ("Default" 0 NIL NIL :CLASS "mpv") ("youtube" 0 T NIL :CLASS "mpv")
+ ("youtube" 2 T NIL :CLASS "Firefox") ("youtube" 3 T NIL :CLASS "XTerm"))
