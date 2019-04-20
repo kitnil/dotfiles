41435b81dc8d0f11c3d8a796b2750748448f5946
Oleg Pykhalov <go.wigust@gmail.com>
2019-04-20 13:19:44 +0300

    oleg: elfeed: Add more feeds.

--- oleg/.emacs.d/.elfeed-feeds.el
+++ oleg/.emacs.d/.elfeed-feeds.el
@@ -1,44 +1,59 @@
 (setq elfeed-feeds
-      '("http://nullprogram.com/feed/"
+      '("https://oremacs.com/atom.xml"
         "http://steckerhalter.tk/index.xml"
-        "https://oremacs.com/atom.xml"
+        "http://nullprogram.com/feed/"
         ("https://www.bennee.com/~alex/blog/feed/" emacs)
         ("https://cestlaz.github.io/rss.xml" emacs)
+        ("https://sachachua.com/blog/feed/" emacs)
         ("https://www.reddit.com/r/freegames/.rss" game)
         "https://h-node.org/rss/modifications/en"
         "https://lwn.net/headlines/newrss"
         "https://fedoramagazine.org/feed/"
-        ("http://planet.emacsen.org/atom.xml" planet)
-        ("http://planet.gnu.org/atom.xml" planet)
         ("http://planet.lisp.org/rss20.xml" planet)
         ("http://www.scheme.dk/planet/atom.xml" planet)
+        ("http://planet.emacsen.org/atom.xml" planet)
+        ("http://planet.gnu.org/atom.xml" planet)
+        ("https://www.youtube.com/feeds/videos.xml?user=Pashtet495"
+         video)
+        ("https://www.youtube.com/feeds/videos.xml?channel_id=UCF3d6ZcTRBhnrNC0-cvzicw"
+         video)
+        ("https://www.youtube.com/feeds/videos.xml?user=elementaller"
+         video)
+        ("https://www.youtube.com/feeds/videos.xml?channel_id=UC1RZz5_cdVQHhhYJVpCDqHA"
+         video)
+        "https://www.youtube.com/feeds/videos.xml?channel_id=UC_ehNByPcItZU3pXL-4skUA"
+        ("https://twitchrss.appspot.com/vod/artgameslp"
+         video)
+        ("https://www.youtube.com/feeds/videos.xml?channel_id=UCOpm7EqPBtznEwYNNZrz1FQ"
+         video)
         ("https://www.youtube.com/feeds/videos.xml?channel_id=UCZrrEuHiQjN2CUo84g5tk7w"
          video)
-        ("https://www.youtube.com/feeds/videos.xml?user=LDCNow"
+        ("https://www.youtube.com/feeds/videos.xml?user=video-dubbeltumme"
+         video)
+        ("https://www.youtube.com/feeds/videos.xml?channel_id=UCkK9UDm_ZNrq_rIXCz3xCGA"
          video)
         ("https://www.youtube.com/feeds/videos.xml?channel_id=UC2eYFnH61tmytImy1mTYvhA"
          video)
         ("https://www.youtube.com/feeds/videos.xml?channel_id=UCMV8p6Lb-bd6UZtTc_QD4zA"
          video)
-        ("https://www.youtube.com/feeds/videos.xml?channel_id=UCVls1GmFKf6WlTraIb_IaJg"
-         video)
-        ("https://www.youtube.com/feeds/videos.xml?user=video-dubbeltumme"
-         video)
-        ("https://www.youtube.com/feeds/videos.xml?user=video-tuxreviews"
-         video)
-        ("https://www.youtube.com/feeds/videos.xml?user=video-gotbletu"
-         video)
-        ("https://www.youtube.com/feeds/videos.xml?channel_id=UCbHXJGd7c8Hy4z0-YX1Jf3Q"
-         video)
-        ("https://www.youtube.com/feeds/videos.xml?user=SsethTzeentach"
+        ("https://www.youtube.com/feeds/videos.xml?user=video-metalx1000"
          video)
         ("https://bitlove.org/jupiterbroadcasting/bsdnowhd/feed"
          video)
+        ("https://www.youtube.com/feeds/videos.xml?user=video-gotbletu"
+         video)
         ("https://www.youtube.com/feeds/videos.xml?channel_id=UCVls1GmFKf6WlTraIb_IaJg"
          video)
+        ("https://www.youtube.com/feeds/videos.xml?channel_id=UCVls1GmFKf6WlTraIb_IaJg"
+         video)
+        ("https://www.youtube.com/feeds/videos.xml?user=SsethTzeentach"
+         video)
+        "https://twitchrss.appspot.com/vod/carolinhr"
+        ("https://www.youtube.com/feeds/videos.xml?user=LDCNow"
+         video)
+        ("https://www.youtube.com/feeds/videos.xml?user=video-tuxreviews"
+         video)
+        ("https://www.youtube.com/feeds/videos.xml?channel_id=UCbHXJGd7c8Hy4z0-YX1Jf3Q"
+         video)
         ("https://www.youtube.com/feeds/videos.xml?channel_id=UCgU5tUdVPpfM7sLAMWBTsDg"
-         video)
-        ("https://www.youtube.com/feeds/videos.xml?user=video-metalx1000"
-         video)
-        ("https://www.youtube.com/feeds/videos.xml?channel_id=UCkK9UDm_ZNrq_rIXCz3xCGA"
          video)))
