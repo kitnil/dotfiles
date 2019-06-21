7e86d6a5945214c2f5c63fba52aaf6eb2283e9f8
Oleg Pykhalov <go.wigust@gmail.com>
2019-06-21 13:53:58 +0300

    stumpwm: Add new rules file.
    * oleg/.stumpwm.d/rules/7.lisp: New file.

---
+++ oleg/.stumpwm.d/rules/7.lisp
@@ -0,0 +1,12 @@
+(("Default" 3 T T :CLASS "Emacs" :INSTANCE "emacs-26_1" :TITLE "#guix" :ROLE
+  NIL)
+ ("Default" 5 T T :CLASS "Emacs" :INSTANCE "emacs-26_1" :TITLE "#scheme" :ROLE
+  NIL)
+ ("Default" 7 T T :CLASS "Firefox" :INSTANCE "Navigator" :TITLE
+  "Grafana - fiore - Mozilla Firefox" :ROLE "browser")
+ ("Default" 2 T NIL :CLASS "Firefox") ("Default" 2 T NIL :CLASS "IceCat")
+ ("Default" 0 T NIL :TITLE "pulsemixer")
+ ("Default" 3 NIL NIL :TITLE "youtube-dl")
+ ("Default" 0 NIL T :CLASS "mpv" :TITLE "emacs-emms")
+ ("Default" 0 NIL NIL :CLASS "mpv") ("youtube" 0 T NIL :CLASS "mpv")
+ ("youtube" 2 T NIL :CLASS "Firefox") ("youtube" 3 T NIL :CLASS "XTerm"))
