c434fa4f896674acb247a26f31cc4c0db28d3585
Oleg Pykhalov <go.wigust@gmail.com>
2019-06-21 13:40:12 +0300

    stumpwm: Update firefox shortkeys.
    * oleg/.stumpwm.d/init.lisp: Update firefox shortkeys.

--- oleg/.stumpwm.d/init.lisp
+++ oleg/.stumpwm.d/init.lisp
@@ -663,9 +663,6 @@
 ;; (define-key *root-map* (kbd "C-M-v") "scroll-other-window")
 ;; (define-key *top-map* (kbd "Print") "screenshot-default")
 
-(define-key *top-map* (kbd "s-w") "firefox")
-(define-key *top-map* (kbd "s-M-w") "firefox-new-window")
-
 ;; Rebind groups to PREFIX-NUMBER.
 (mapcar #'(lambda (x) (define-key *top-map* (kbd (concat "s-" (write-to-string x)))
 			(format nil "~A ~D" "gselect" x)))
@@ -688,8 +685,8 @@
 ;; (define-key *top-map* (kbd "s-s") "sibling")
 ;; (define-key *top-map* (kbd "s-t") "pull-hidden-other")
 ;; (define-key *top-map* (kbd "s-v") "xclip-mpv")
-;; (define-key *top-map* (kbd "s-w") "firefox")
-;; (define-key *top-map* (kbd "s-w") "firefox")
+(define-key *top-map* (kbd "s-w") "firefox")
+(define-key *top-map* (kbd "s-W") "firefox-new-window")
 
 (defcommand dump-group-to-file (file) (:rest "Dump To File: ")
   "Dumps the frames of the current group of the current screen to the named file."
