f9106e428a6a633e3a9fe801893da0127ad4e3d3
Oleg Pykhalov <go.wigust@gmail.com>
2019-06-21 13:50:19 +0300

    fiore: Add misc manifest.
    * fiore/manifests/misc.scm: Add misc manifest.

---
+++ fiore/manifests/misc.scm
@@ -0,0 +1,38 @@
+(use-modules (wigust packages databases)
+	     (wigust packages ruby)
+	     (gnu packages chromium))
+
+(list
+ emacs-anywhere-mode
+ emacs-atomic-chrome
+ emacs-awk-it
+ emacs-debpaste
+ emacs-eval-in-repl
+ emacs-flyspell-correct
+ emacs-helm-navi
+ emacs-hydra-timestamp
+ emacs-helm-navi
+ emacs-hydra-timestamp
+ emacs-mediawiki
+ emacs-pcre2el
+ emacs-perl-live
+ emacs-psysh
+ emacs-redshift
+ autopostgresqlbackup
+ ruby-gitlab
+ python-starred ; Fetch a list of stars from GitHub user
+ dynamips ;gns3 requirement
+ ungoogled-chromium
+ ghc-pandoc  ; Convert Markdown
+
+
+ chicken go m4 racket perl python python-hy r
+
+ colordiff mercurial gource
+ 
+ colormake
+
+ guile-commonmark ; Commonmark for Guile
+ ;; gwl              ; Guix workflow management ; fails to build
+ haunt            ; Guile static site generator
+ )
