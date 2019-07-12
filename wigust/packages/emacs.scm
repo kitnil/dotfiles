ed5ded8975c04fb1928dd1ac4450cc35fbd876b0
Oleg Pykhalov <go.wigust@gmail.com>
2019-07-12 22:51:35 +0300

    wigust: Add emacs-matcha.
    * wigust/packages/emacs.scm (emacs-matcha): New variable.

--- wigust/packages/emacs.scm
+++ wigust/packages/emacs.scm
@@ -1284,3 +1284,27 @@ variables.  An interface to GitLab’s CI file linter is also provided via
       (synopsis "Jenkins client for Emacs")
       (description "This package provides a Jenkins client for Emacs.")
       (license license:gpl3+))))
+
+(define-public emacs-matcha
+  (let ((commit "c7df5cf5cdac9ae369e241342389ccda0205eab9"))
+    (package
+      (name "emacs-matcha")
+      (version (git-version "0.0.1" "1" commit)) ;no upstream release
+      (source (origin
+                (method git-fetch)
+                (uri (git-reference
+                      (url "https://github.com/jojojames/matcha.git")
+                      (commit commit)))
+                (file-name (git-file-name name version))
+                (sha256
+                 (base32
+                  "1lfnh1glg6al677m7ci0x8g5wjdhjxlfl3nv1f1ppsw4dpnwsj9b"))))
+      (propagated-inputs
+       `(("emacs-hydra" ,emacs-hydra)
+         ("emacs-transient" ,emacs-transient)))
+      (build-system emacs-build-system)
+      (home-page "https://github.com/jojojames/matcha/")
+      (synopsis "Collection of hydras with a generic interface to launch them")
+      (description "Matcha is provides a collection of transients for various
+packages with a consistent way to use them.")
+      (license license:gpl3+))))
