1391e97e3d0726421352458a11c6e0c5df4692e5
Oleg Pykhalov <go.wigust@gmail.com>
2019-07-20 00:34:06 +0300

    channels.scm: Update.
    * channels.scm: Update.

--- channels.scm
+++ channels.scm
@@ -7,4 +7,4 @@
         (name 'guix)
         (url "https://git.savannah.gnu.org/git/guix.git")
         (commit
-          "f24276de9ae3826339e76bb713f2505405e127f4")))
+          "dd9560a050ed99952046a2bbd99dc6b331100c63")))
