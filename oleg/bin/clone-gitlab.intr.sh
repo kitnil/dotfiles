d46aef5523b1b2e273f380100250c4141507d184
Oleg Pykhalov <go.wigust@gmail.com>
2019-08-01 22:55:35 +0300

    bin: Update.
    * oleg/bin/clone-gitlab.intr.sh: Update.

--- oleg/bin/clone-gitlab.intr.sh
+++ oleg/bin/clone-gitlab.intr.sh
@@ -1,3 +1,11 @@
 # curl -H 'PRIVATE-TOKEN: ***REMOVED***' -H 'Content-Type: application/json' -k -X GET "https://gitlab.intr/api/v4/groups/$group/" | jq
 
-for group in $(jq '.[] | .id' < groups.json); do mkdir -p $group; cd $group; git clone $(curl -H 'PRIVATE-TOKEN: ***REMOVED***' -H 'Content-Type: application/json' -k -X GET "https://gitlab.intr/api/v4/groups/$group/" | jq -r '.projects[] | .ssh_url_to_repo'); cd -; done
+# $(jq '.[] | .id' < groups.json)
+for group in 7 8 17 70 71 82; do
+    mkdir -p $group
+    cd $group
+    for project in $(curl -H 'PRIVATE-TOKEN: ***REMOVED***' -H 'Content-Type: application/json' -k -X GET https://gitlab.intr/api/v4/groups/$group/ | jq -r '.projects[] | .ssh_url_to_repo'); do
+        git clone $project;
+    done
+    cd -
+done
