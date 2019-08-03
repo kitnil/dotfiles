02e0fe45787cae0f6d3c66136617ca2311e119e5
Oleg Pykhalov <go.wigust@gmail.com>
2019-08-04 01:17:19 +0300

    channels.scm: Update.
    * channels.scm: Update.

--- channels.scm
+++ channels.scm
@@ -2,9 +2,9 @@
         (name 'guix-wigust)
         (url "https://gitlab.wugi.info/guix/guix-wigust.git")
         (commit
-          "c81bff1cf6e5cd7e4837ef0816a04e6af22036e0"))
+         "689fcd06fc1d27432ff71bc62ac08226eade96d8"))
       (channel
         (name 'guix)
         (url "https://git.savannah.gnu.org/git/guix.git")
         (commit
-          "b2dce6b5a2eecb9e20e5ee03779c020b2644666b")))
+          "b5e912a88cd57108ac12bd04a28b9e17e4a46e86")))
