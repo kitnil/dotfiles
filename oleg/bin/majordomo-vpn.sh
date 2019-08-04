cf6570314299a604e126605056e62dd63baf43cf
Oleg Pykhalov <go.wigust@gmail.com>
2019-08-04 23:28:16 +0300

    bin: majordomo-vpn: Update.
    * oleg/bin/majordomo-vpn.sh: Update.

--- oleg/bin/majordomo-vpn.sh
+++ oleg/bin/majordomo-vpn.sh
@@ -2,9 +2,11 @@
 
 # This scripts establish a VPN tunnel to Majordomo and setups NAT rules.
 
+tap="$1"
+
 openvpn --config /etc/openvpn/mj-client.conf
 
 # NAT
-iptables -t nat -A POSTROUTING -o tap0 -j MASQUERADE
-iptables -A FORWARD -i tap0 -o br0 -m state --state RELATED,ESTABLISHED -j ACCEPT
-iptables -A FORWARD -i br0 -o tap0 -j ACCEPT
+iptables -t nat -A POSTROUTING -o "$tap" -j MASQUERADE
+iptables -A FORWARD -i "$tap" -o br0 -m state --state RELATED,ESTABLISHED -j ACCEPT
+iptables -A FORWARD -i br0 -o "$tap" -j ACCEPT
