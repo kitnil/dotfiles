14b906a7f647c061ffa34c45a3ae1bbbef23d976
Oleg Pykhalov <go.wigust@gmail.com>
2019-08-12 13:38:16 +0300

    gitlab-ci.yml: Update channels.
    * .gitlab-ci.yml: Update channels.

--- channels.scm
+++ channels.scm
@@ -7,4 +7,4 @@
         (name 'guix)
         (url "https://git.savannah.gnu.org/git/guix.git")
         (commit
-         "24446ce299943efe3dfded6c9dd0cf9421d8da04")))
+         "dbc3f1f8b21fddab13d881e21fd8128a2a8ef66f")))
