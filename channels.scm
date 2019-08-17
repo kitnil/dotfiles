c5ecc5ed6c45b979e519803327c769af516aa2bc
Oleg Pykhalov <go.wigust@gmail.com>
2019-08-17 23:41:43 +0300

    channels.scm: Add 'guix-majordomo'.
    * channels.scm: Add 'guix-majordomo'.

--- channels.scm
+++ channels.scm
@@ -4,6 +4,10 @@
         (commit
           "484dae6905026379d4542e160897e20f6cd87788"))
       (channel
+       (name 'guix-majordomo)
+       (url "https://gitlab-ci-token:uhd8qabRUz5dD-HQchLi@anongit.duckdns.org/guix/guix-majordomo.git")
+       (branch "master"))
+      (channel
         (name 'guix)
         (url "https://git.savannah.gnu.org/git/guix.git")
         (commit
