3b7a7a95d08536393c3053967fc42e9ca140e56b
Oleg Pykhalov <go.wigust@gmail.com>
2019-09-01 12:12:35 +0300

    bin: majordomo-vpn: Add HMS billing IP routes.
    * oleg/bin/majordomo-vpn.sh: Add HMS billing IP routes.

--- oleg/bin/majordomo-vpn.sh
+++ oleg/bin/majordomo-vpn.sh
@@ -17,3 +17,7 @@ openvpn --config /etc/openvpn/mj-client.conf
 iptables -t nat -A POSTROUTING -o "$tap" -j MASQUERADE
 iptables -A FORWARD -i "$tap" -o br0 -m state --state RELATED,ESTABLISHED -j ACCEPT
 iptables -A FORWARD -i br0 -o "$tap" -j ACCEPT
+
+# HMS billing
+ip r add 78.108.80.178/32 via 172.16.100.3
+ip r add 78.108.80.171/32 via 172.16.100.3
