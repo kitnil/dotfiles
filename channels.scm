754202e3a9b95a55d3b866c959b5b667406a8d6c
Oleg Pykhalov <go.wigust@gmail.com>
2019-09-02 20:41:03 +0300

    channels.scm: Update.

--- channels.scm
+++ channels.scm
@@ -4,11 +4,12 @@
         (commit
           "484dae6905026379d4542e160897e20f6cd87788"))
       (channel
-       (name 'guix-majordomo)
-       (url "https://gitlab-ci-token:uhd8qabRUz5dD-HQchLi@anongit.duckdns.org/guix/guix-majordomo.git")
-       (branch "master"))
+        (name 'guix-majordomo)
+        (url "https://gitlab-ci-token:uhd8qabRUz5dD-HQchLi@anongit.duckdns.org/guix/guix-majordomo.git")
+        (commit
+          "276f1d2aa0de76a47139b207e6d1af262cb5fd51"))
       (channel
         (name 'guix)
         (url "https://git.savannah.gnu.org/git/guix.git")
         (commit
-          "5042f33e3bf226fd06159953f8fdfb27b95c5ee0")))
+          "cc16f90a074260fa32325217ea1779084d2a615b")))
