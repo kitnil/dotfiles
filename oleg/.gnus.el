cfeb7e1a8c84ea20d6965307ae4466f54210adc2
Oleg Pykhalov <go.wigust@gmail.com>
2019-09-14 22:11:25 +0300

    gnus: Disable add-configuration.
    * oleg/.gnus.el: Disable add-configuration.

--- oleg/.gnus.el
+++ oleg/.gnus.el
@@ -168,8 +168,8 @@ below otherwise."
     (define-key map (kbd "<f5>") 'mbsync)
     (define-key map (kbd "<f6>") 'org-capture)))
 
-(gnus-add-configuration
-   '(article
-    (horizontal 1.0
-               (summary 0.60 point)
-               (article 1.0))))
+;; (gnus-add-configuration
+;;    '(article
+;;     (horizontal 1.0
+;;                (summary 0.60 point)
+;;                (article 1.0))))
