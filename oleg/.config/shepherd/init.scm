2082530580325870f447f129f5946ec0a18d535b
Oleg Pykhalov <go.wigust@gmail.com>
2019-09-15 18:03:38 +0300

    oleg: Update shepherd

--- oleg/.config/shepherd/init.scm
+++ oleg/.config/shepherd/init.scm
@@ -1,14 +1,39 @@
-;; init.scm -- default shepherd configuration file.
+(use-modules (shepherd service))
 
-;; Services known to shepherd:
-;; Add new services (defined using 'make <service>') to shepherd here by
-;; providing them as arguments to 'register-services'.
-(register-services)
+(define %bin-directory "/home/oleg/.guix-profile/bin/")
 
-;; Send shepherd into the background
+(define redshift
+  (make <service>
+    #:docstring '("Adjust the color temperature of your screen.")
+    #:provides '(redshift)
+    #:start (make-forkexec-constructor
+             `(,(string-append %bin-directory "redshift") "-l" "59:57"))
+    #:stop (make-kill-destructor)
+    #:respawn? #t))
+
+(define transmission
+  (make <service>
+    #:docstring '("Light-weight BitTorrent client")
+    #:provides '(transmission)
+    #:start (make-forkexec-constructor
+             (list (string-append %bin-directory "transmission-daemon")))
+    #:stop (make-kill-destructor)
+    #:respawn? #t))
+
+(define emacs-service
+  (make <service>
+    #:docstring '("Emacs daemon")
+    #:provides '(emacs)
+    #:start (make-forkexec-constructor
+             (list (string-append %bin-directory "emacs")
+                   "--fg-daemon")
+             #:log-file "/home/oleg/.config/shepherd/emacs.log")
+    #:stop
+    (make-system-destructor (string-join (list (string-append %bin-directory "emacsclient")
+                                               "--eval" "'(let (kill-emacs-hook) (kill-emacs))'")))
+    #:respawn? #f))
+
+(register-services emacs-service)
+(start 'emacs)
+
 (action 'shepherd 'daemonize)
-
-;; Services to start when shepherd starts:
-;; Add the name of each service that should be started to the list
-;; below passed to 'for-each'.
-(for-each start '())
