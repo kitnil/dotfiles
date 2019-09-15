0976ac3c6e1456da41bfa7021e9faddb73111516
Oleg Pykhalov <go.wigust@gmail.com>
2019-09-15 16:01:36 +0300

    build.sh: Build systems first

--- build.sh
+++ build.sh
@@ -9,11 +9,11 @@ guix pull --verbosity=0 --profile=.guix-profile --channels=channels.scm
 
 GUIX_BIN=".guix-profile/bin/guix"
 "$GUIX_BIN" describe
-for manifest in oleg maintenance; do
-    "$GUIX_BIN" environment --verbosity=0 \
-        --manifest="guix/manifests/$manifest.scm" -- sh -c exit
-done
 for system in config spb; do
     "$GUIX_BIN" system build --verbosity=0 \
         --load-path=fiore/modules "guixsd/$system.scm"
+done
+for manifest in oleg maintenance; do
+    "$GUIX_BIN" environment --verbosity=0 \
+        --manifest="guix/manifests/$manifest.scm" -- sh -c exit
 done
