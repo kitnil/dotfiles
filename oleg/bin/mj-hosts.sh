c1a8b25d079dce0d1915e32a0de7d3d72fac2c85
Oleg Pykhalov <go.wigust@gmail.com>
2019-10-01 00:18:58 +0300

    bin: mj-hosts: Comment everything not my machine.

--- oleg/bin/mj-hosts.sh
+++ oleg/bin/mj-hosts.sh
@@ -1,10 +1,13 @@
 #!/bin/sh
-ssh router <<< busybox 'grep 172.16. /tmp/etc/hosts.dnsmasq' && exit 0
+
+set -e -x
+
+# ssh router <<< busybox 'grep 172.16. /tmp/etc/hosts.dnsmasq' && exit 0
 dig @172.16.103.2 -tAXFR intr |  awk '/\tA\t/ {print $NF" "$1}' |         sort   | sed -e 's|.intr.||g' | awk '{print $1" "$2" "$2".intr"}' > /tmp/mj.zone
 dig @172.16.103.2 -tAXFR  intr | awk '/CNAME/ {print system("dig +short "$NF "| tail -1 | xargs echo -n ")" " $1}' | sed -e 's:0 : :' -e 's|.intr.||' | awk '/172/ {print $1" "$2" "$2".intr"}' >> /tmp/mj.zone
-scp /tmp/mj.zone router:/tmp/
-ssh router <<< busybox 'grep 172.16. /tmp/etc/hosts.dnsmasq' || ssh router <<< busybox 'cat /tmp/mj.zone >> /tmp/etc/hosts.dnsmasq'
-ssh router <<< busybox 'kill -HUP $(pidof dnsmasq)'
-ssh router <<< busybox 'rm /tmp/mj.zone'
+# scp /tmp/mj.zone router:/tmp/
+# ssh router <<< busybox 'grep 172.16. /tmp/etc/hosts.dnsmasq' || ssh router <<< busybox 'cat /tmp/mj.zone >> /tmp/etc/hosts.dnsmasq'
+# ssh router <<< busybox 'kill -HUP $(pidof dnsmasq)'
+# ssh router <<< busybox 'rm /tmp/mj.zone'
 #ssh router <<< busybox 'touch /tmp/mjlocked'
-rm -f /tmp/mj.zone
+# rm -f /tmp/mj.zone
