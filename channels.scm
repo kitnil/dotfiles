42c3fe2e9a4340ba704c52147f40616dbdbfeb60
GitLab Runner <gitlab-runner@wugi.info>
2019-10-10 17:27:45 +0300

    channels.scm: Update channels.scm.

---
+++ channels.scm
@@ -0,0 +1,15 @@
+(list (channel
+        (name 'guix-wigust)
+        (url "https://gitlab.wugi.info/guix/guix-wigust.git")
+        (commit
+          "c1d679d3620943195c5b6a9fb36df956881aa95e"))
+      (channel
+        (name 'guix-majordomo)
+        (url "https://gitlab-ci-token:uhd8qabRUz5dD-HQchLi@anongit.duckdns.org/guix/guix-majordomo.git")
+        (commit
+          "276f1d2aa0de76a47139b207e6d1af262cb5fd51"))
+      (channel
+        (name 'guix)
+        (url "https://git.savannah.gnu.org/git/guix.git")
+        (commit
+          "fc1fe722a05318ac05a71a0b127f231631e2843f")))
