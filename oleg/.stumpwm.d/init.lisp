51ac08200225d70b96a32226ba352874ca0c571a
Oleg Pykhalov <go.wigust@gmail.com>
2019-11-10 14:33:07 +0300

    stumpwm: Uncomment tome4 frame preference.

--- oleg/.stumpwm.d/init.lisp
+++ oleg/.stumpwm.d/init.lisp
@@ -1171,7 +1171,7 @@
        (define-frame-preference "Default" (0 NIL T :CLASS "quassel" :TITLE "Chat Monitor"))
        (define-frame-preference "Default" (3 NIL T :CLASS "XTerm" :TITLE "alerta"))
        (define-frame-preference "Default" (4 NIL T :CLASS "XTerm" :TITLE "notmuch"))
-       ;; (define-frame-preference "Default" (1 NIL T :CLASS "t-engine"))
+       (define-frame-preference "Default" (2 NIL T :CLASS "t-engine"))
        (define-frame-preference "Default" (4 NIL T :CLASS "mpv" :TITLE "emacs-emms"))
        (define-frame-preference "Default" (4 NIL T :CLASS "mpv" :TITLE "firefox"))
        (define-frame-preference "Default" (1 NIL T :CLASS "obs"))
