c2eb6b1b1b588440d6a00696615d919e1f532ad4
Oleg Pykhalov <go.wigust@gmail.com>
2019-11-15 02:35:39 +0300

    guixsd: Use guix deploy.

--- guixsd/workstation.scm
+++ guixsd/workstation.scm
@@ -1,34 +1,54 @@
-(use-modules (gnu))
+(use-modules (gnu) (majordomo packages majordomo) (guix gexp))
+(use-package-modules certs networking)
 (use-service-modules desktop networking ssh xorg)
 
-(operating-system
-  (host-name "workstation-guixsd")
-  (timezone "Europe/Moscow")
-  (locale "en_US.utf8")
+(define %system
+  (operating-system
+    (host-name "workstation-guixsd")
+    (timezone "Europe/Moscow")
+    (locale "en_US.utf8")
 
-  (bootloader (bootloader-configuration
-               (bootloader grub-bootloader)
-               (target "/dev/sda")))
+    (bootloader (bootloader-configuration
+                 (bootloader grub-bootloader)
+                 (target "/dev/sda")))
 
-  (users (cons (user-account (name "oleg")
-                             (comment "Oleg Pykhalov")
-                             (group "users")
-                             (supplementary-groups '("wheel" "audio" "video")))
-               %base-user-accounts))
+    (users (cons (user-account (name "oleg")
+                               (comment "Oleg Pykhalov")
+                               (group "users")
+                               (supplementary-groups '("wheel" "audio" "video")))
+                 %base-user-accounts))
 
-  (file-systems (cons* (file-system
-			(device (uuid "11d541d4-7914-4937-8ce0-7e50687ddbc6"))
-                         (mount-point "/")
-                         (type "ext4"))
-                       (file-system
-                         (device "tmpfs")
-                         (mount-point "/tmp")
-                         (type "tmpfs")
-                         (check? #f)
-                         (flags '(no-dev))
-                         (options "mode=1777,size=50%"))
-                       %base-file-systems))
+    (file-systems (cons* (file-system
+                           (device (uuid "11d541d4-7914-4937-8ce0-7e50687ddbc6"))
+                           (mount-point "/")
+                           (type "ext4"))
+                         (file-system
+                           (device "tmpfs")
+                           (mount-point "/tmp")
+                           (type "tmpfs")
+                           (check? #f)
+                           (flags '(no-dev))
+                           (options "mode=1777,size=50%"))
+                         %base-file-systems))
 
-  (packages %base-packages)
+    (packages (cons* nss-certs ;SSL certificates
+                     majordomo-ca
 
-  (services (cons (service openssh-service-type) %desktop-services)))
+                     %base-packages))
+
+    (services (cons (service openssh-service-type) %desktop-services))
+
+    (setuid-programs (cons* (file-append fping "/sbin/fping")
+                            (file-append mtr "/sbin/mtr")
+                            %setuid-programs))
+
+    (sudoers-file (local-file "sudoers"))))
+
+(list (machine
+       (operating-system %system)
+       (environment managed-host-environment-type)
+       (configuration (machine-ssh-configuration
+                       (host-name "172.16.100.60")
+                       (system "x86_64-linux")
+                       (user "oleg")
+                       (identity "/home/oleg/.ssh/id_rsa")))))
