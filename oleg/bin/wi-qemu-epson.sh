49f734b7a1825cdcfb81db11a9cfbba0acbdc9d4
Oleg Pykhalov <go.wigust@gmail.com>
2019-11-25 22:44:47 +0300

    oleg: wi-qemu-epson.sh: Update.

--- oleg/bin/wi-qemu-epson.sh
+++ oleg/bin/wi-qemu-epson.sh
@@ -1,15 +1,16 @@
 #!/bin/sh
 
-exec qemu-system-x86_64 \
+# -device usb-host,hostbus=2,hostaddr=4 \
+# -net 'user,hostfwd=tcp::3389-:3389,hostfwd=tcp::445-:445' \
+# -net nic \
+# -usb \
+
+exec -a "$0" qemu-system-x86_64 \
      -daemonize \
      -m 4096 \
      -smp 2 \
      -enable-kvm \
      -hda /srv/virt/epson.qcow2 \
-     -smb /srv/share \
      -M q35 \
-     -usb \
-     -device usb-host,hostbus=2,hostaddr=4 \
-     -net 'user,hostfwd=tcp::3389-:3389,hostfwd=tcp::445-:445' \
-     -net nic \
-     $@
+     -net nic -net user,smb=/srv/share \
+     "$@"
