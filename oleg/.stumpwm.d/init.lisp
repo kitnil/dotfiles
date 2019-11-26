d27a0b51c961253af4f2da991d4dd94c1815203c
Oleg Pykhalov <go.wigust@gmail.com>
2019-11-26 08:38:44 +0300

    stumpwm: Rename REPL window names.

--- oleg/.stumpwm.d/init.lisp
+++ oleg/.stumpwm.d/init.lisp
@@ -422,53 +422,53 @@
                       :font '("-fa" "Monospace" "-fs" "8")))
 
 (defcommand repl-python () ()
-  (term-shell-command "python3" :scrollbar t))
+  (term-shell-command "python3" :scrollbar t :title "repl-python"))
 
 (defcommand repl-r () ()
   (term-shell-command "R" :scrollbar t :title "repl-r"))
 
 (defcommand repl-racket () ()
-  (term-shell-command "racket" :scrollbar t))
+  (term-shell-command "racket" :scrollbar t :title "repl-racket"))
 
 (defcommand repl-nix () ()
-  (term-shell-command "nix repl '<nixpkgs>'" :scrollbar t))
+  (term-shell-command "nix repl '<nixpkgs>'" :scrollbar t :title "repl-nix"))
 
 (defcommand repl-node () ()
-  (term-shell-command "node" :scrollbar t))
+  (term-shell-command "node" :scrollbar t :title "repl-node"))
 
 (defcommand repl-sbcl () ()
-  (term-shell-command "rlwrap sbcl" :scrollbar t :title "sbcl"))
+  (term-shell-command "rlwrap sbcl" :scrollbar t :title "repl-sbcl"))
 
 (defcommand repl-chicken () ()
-  (term-shell-command "rlwrap csi" :scrollbar t :title "chicken"))
+  (term-shell-command "rlwrap csi" :scrollbar t :title "repl-chicken"))
 
 (defcommand repl-c () ()
-  (term-shell-command "docker run --rm -it bic:latest" :scrollbar t :title "c"))
+  (term-shell-command "docker run --rm -it bic:latest" :scrollbar t :title "repl-c"))
 
 (defcommand repl-hy () ()
-  (term-shell-command "docker run --rm -it hylang:python3.5-buster" :scrollbar t :title "hy"))
+  (term-shell-command "docker run --rm -it hylang:python3.5-buster" :scrollbar t :title "repl-hy"))
 
 (defcommand repl-java () ()
-  (term-shell-command "docker run --rm -it openjdk:9" :scrollbar t :title "java"))
+  (term-shell-command "docker run --rm -it openjdk:9" :scrollbar t :title "repl-java"))
 
 (defcommand repl-lua () ()
   (term-shell-command "guix environment --pure --ad-hoc lua -- lua"
-                      :scrollbar t :title "lua"))
+                      :scrollbar t :title "repl-lua"))
 
 (defcommand repl-ghci () ()
   (term-shell-command "guix environment --pure --ad-hoc ghc -- ghci"
-                      :scrollbar t :title "ghci"))
+                      :scrollbar t :title "repl-ghci"))
 
 (defcommand repl-go () ()
-  (term-shell-command "gore" :scrollbar t))
+  (term-shell-command "gore" :scrollbar t :title "repl-go"))
 
 (defcommand repl-ruby () ()
   (term-shell-command "guix environment --pure --ad-hoc ruby -- irb"
-                      :scrollbar t :title "ruby"))
+                      :scrollbar t :title "repl-ruby"))
 
 (defcommand repl-bash-pure () ()
   (term-shell-command "env -i \"$(command -v bash)\" --login --noprofile --norc"
-                      :scrollbar t :title "bash"))
+                      :scrollbar t :title "repl-bash-pure"))
 
 (defcommand neofetch () ()
   (term-shell-command "sh -c 'neofetch; read'"))
