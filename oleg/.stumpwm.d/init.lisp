9d1bc69ba2b0d003d51a107b04968ddc49e25b23
Oleg Pykhalov <go.wigust@gmail.com>
2019-11-27 14:30:46 +0300

    stumpwm: Add debian-unstable.

--- oleg/.stumpwm.d/init.lisp
+++ oleg/.stumpwm.d/init.lisp
@@ -724,6 +724,13 @@
   "Run GNOME Debian in QEMU."
   (run-shell-command (concat "exec " (getenv "HOME") "/bin/debian.sh")))
 
+(defcommand debian-unstable () ()
+  (run-shell-command
+   (join (list "qemu-system-x86_64" "-daemonize" "-smp" "cores=4,threads=1"
+               "-m" "4096" "-enable-kvm" "-cpu" "host" "-daemonize"
+               "-vnc" ":5" "-hda" "~/vm/debian-unstable.qcow2"
+               "-cdrom" "~/Downloads/debian-testing-amd64-netinst.iso"))))
+
 (setf *float-window-border* 0)
 (setf *float-window-title-height* 0)
 
