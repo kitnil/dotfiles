3ddb89aa8d22456b68d0cb6543521c2b7f979100
Oleg Pykhalov <go.wigust@gmail.com>
2019-12-23 01:24:35 +0300

    channels.scm: Update.

--- channels.scm
+++ channels.scm
@@ -1,10 +1,8 @@
-(list (channel
+;; Add my personal packages to those Guix provides.
+(cons* (channel
         (name 'guix-wigust)
-        (url "file:///home/oleg/src/guix-wigust")
-        (commit
-          "652681200d430990dcb8c4bf83257820a158fdd3"))
-      (channel
-        (name 'guix)
-        (url "https://git.savannah.gnu.org/git/guix.git")
-        (commit
-          "79b8a2afa2392731590b195c833be27c3d548903")))
+        (url "https://cgit.duckdns.org/git/guix/guix-wigust"))
+       (channel
+        (name 'guix-wigust-services)
+        (url "file:///srv/git/guix/guix-wigust-services.git"))
+       %default-channels)
