c29e95bef0511d8f2c43376c48481c83cb8893c3
Oleg Pykhalov <go.wigust@gmail.com>
2019-12-23 01:26:29 +0300

    stumpwm: Add 10.lisp desktop.

---
+++ oleg/.stumpwm.d/desktop/10.lisp
@@ -0,0 +1,34 @@
+#S(DDUMP
+   :SCREENS (#S(SDUMP
+                :NUMBER 0
+                :GROUPS (#S(GDUMP
+                            :NUMBER 1
+                            :NAME "Default"
+                            :TREE ((#S(FDUMP
+                                       :NUMBER 0
+                                       :X 0
+                                       :Y 0
+                                       :WIDTH 1432
+                                       :HEIGHT 1080
+                                       :WINDOWS (60817444 37748749 39845890
+                                                 39845923 35651590 50331649)
+                                       :CURRENT 60817444)
+                                    (#S(FDUMP
+                                        :NUMBER 1
+                                        :X 1432
+                                        :Y 0
+                                        :WIDTH 488
+                                        :HEIGHT 796
+                                        :WINDOWS (35651594)
+                                        :CURRENT 35651594)
+                                     #S(FDUMP
+                                        :NUMBER 2
+                                        :X 1432
+                                        :Y 796
+                                        :WIDTH 488
+                                        :HEIGHT 284
+                                        :WINDOWS (6291469)
+                                        :CURRENT 6291469))))
+                            :CURRENT 0))
+                :CURRENT 1))
+   :CURRENT 0)
