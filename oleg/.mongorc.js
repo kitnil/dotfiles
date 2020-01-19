d512c1c3db97fa060feecb4ed1eeeeb603cf6af3
Oleg Pykhalov <go.wigust@gmail.com>
2020-01-19 19:27:20 +0300

    mongo: search: Filter values.
    * oleg/.mongorc.js (search): Filter values.

--- oleg/.mongorc.js
+++ oleg/.mongorc.js
@@ -1,6 +1,6 @@
 search = function(id) {
     return db.getMongo().getDBNames().map((database) => {
         let sibling = db.getSiblingDB(database);
-        return [sibling, sibling.getCollectionNames().map((collection) => sibling.getCollection(collection).findOne({"_id" : ObjectId(id)})).filter((notNull) => notNull)];
-    })
+        return sibling.getCollectionNames().map((collection) => sibling.getCollection(collection).findOne({"_id" : ObjectId(id)})).filter((notNull) => notNull);
+    }).filter(String)
 }
