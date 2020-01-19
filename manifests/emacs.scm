97634abd8ca53bffbec518609491c84345cf1afd
Oleg Pykhalov <go.wigust@gmail.com>
2020-01-19 19:12:04 +0300

    manifests: emacs: Add emacs-lsp-ui and emacs-lsp-java.

--- manifests/emacs.scm
+++ manifests/emacs.scm
@@ -86,6 +86,8 @@
        emacs-know-your-http-well
        emacs-lice-el
        emacs-lsp-mode
+       emacs-lsp-java
+       emacs-lsp-ui
        emacs-lua-mode
        emacs-macrostep
        emacs-magit
