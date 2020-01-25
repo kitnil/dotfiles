f7fb269316001067898512ad747b753b8afc4bb7
Oleg Pykhalov <go.wigust@gmail.com>
2020-01-25 23:24:04 +0300

    generate.sh: Print Git URL.

--- generate.sh
+++ generate.sh
@@ -20,7 +20,17 @@ folders()
 #         | tee $(basename $folder | sed 's|json|yml|g')
 # }
 
+# curl --silent --request GET --user "admin:$(pass show jenkins/admin-api-key)" $JENKINS_URL/job/wigust/job/awesome/config.xml | xq --raw-output '.["org.jenkinsci.plugins.workflow.multibranch.WorkflowMultiBranchProject"].sources.data["jenkins.branch.BranchSource"].source.remote'
+
+jobs()
+{
+    folder="$1"
+    curl --silent --request POST --user "admin:$(pass show jenkins/admin-api-key)" "$JENKINS_URL/job/$folder/api/json"
+}
+
 for folder in $(folders); do
-    curl --silent --request POST --user "admin:$(pass show jenkins/admin-api-key)" "$JENKINS_URL/job/$folder/api/json" \
-        | yq --arg FOLDER "$folder" --yaml-output '[.jobs[] | {"job": { "name": "\($FOLDER)/\(.name)", "project-type": "multibranch", "periodic-folder-trigger": "1d", "prune-dead-branches": true, "number-to-keep": "10", "days-to-keep": "10", "script-path": "Jenkinsfile", "scm": {"git": {"url": .url}}}}]'
+    for job in $(jobs $folder | jq --raw-output '.jobs[] | .name'); do
+        giturl=$(curl --silent --request GET --user "admin:$(pass show jenkins/admin-api-key)" "$JENKINS_URL/job/$folder/job/$job/config.xml" | xq --raw-output '.["org.jenkinsci.plugins.workflow.multibranch.WorkflowMultiBranchProject"].sources.data["jenkins.branch.BranchSource"].source.remote')
+        curl --silent --request GET --user "admin:$(pass show jenkins/admin-api-key)" "$JENKINS_URL/job/$folder/job/$job/api/json" | yq --arg GITURL "$giturl" --arg FOLDER "$folder" --arg JOB "$job" --yaml-output '[.jobs[] | {"job": { "name": "\($FOLDER)/\($JOB)", "project-type": "multibranch", "periodic-folder-trigger": "1d", "prune-dead-branches": true, "number-to-keep": "10", "days-to-keep": "10", "script-path": "Jenkinsfile", "scm": {"git": {"url": $GITURL}}}}]'
+    done
 done
