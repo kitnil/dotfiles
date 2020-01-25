56b48d6deaad2628e7540bcf1746273d77e8008d
Oleg Pykhalov <go.wigust@gmail.com>
2020-01-25 23:37:07 +0300

    generate.sh: Format.

--- generate.sh
+++ generate.sh
@@ -1,22 +1,54 @@
 #!/bin/sh
 
+# This script will generate Jenkins jobs in YAML format, so you could
+# use "jenkins-update" program to operate them as described in
+# https://docs.openstack.org/infra/jenkins-job-builder/project_workflow_multibranch.html
+#
+# You should set JENKINS_URL and CURL_USER to your credentials, then
+# run the script, e.g. ./generate.sh
+
 JENKINS_URL="https://jenkins.wugi.info"
+CURL_USER="admin:$(pass show jenkins/admin-api-key)"
+
+jenkins_curl()
+{
+    curl --silent --user "$CURL_USER" $@
+}
 
 folders()
 {
-    curl --silent --request POST --user "admin:$(pass show jenkins/admin-api-key)" "$JENKINS_URL/api/json" \
-        | jq --raw-output '.jobs[] | .name'
+    jenkins_curl --request POST "$JENKINS_URL/api/json"
 }
 
 jobs()
 {
     folder="$1"
-    curl --silent --request POST --user "admin:$(pass show jenkins/admin-api-key)" "$JENKINS_URL/job/$folder/api/json"
+    jenkins_curl --request POST "$JENKINS_URL/job/$folder/api/json"
 }
 
-for folder in $(folders); do
-    for job in $(jobs $folder | jq --raw-output '.jobs[] | .name'); do
-        giturl=$(curl --silent --request GET --user "admin:$(pass show jenkins/admin-api-key)" "$JENKINS_URL/job/$folder/job/$job/config.xml" | xq --raw-output '.["org.jenkinsci.plugins.workflow.multibranch.WorkflowMultiBranchProject"].sources.data["jenkins.branch.BranchSource"].source.remote')
-        curl --silent --request GET --user "admin:$(pass show jenkins/admin-api-key)" "$JENKINS_URL/job/$folder/job/$job/api/json" | yq --arg GITURL "$giturl" --arg FOLDER "$folder" --arg JOB "$job" --yaml-output '[.jobs[] | {"job": { "name": "\($FOLDER)/\($JOB)", "project-type": "multibranch", "periodic-folder-trigger": "1d", "prune-dead-branches": true, "number-to-keep": "10", "days-to-keep": "10", "script-path": "Jenkinsfile", "scm": {"git": {"url": $GITURL}}}}]'
+job()
+{
+    folder="$1"
+    job="$2"
+    jenkins_curl --request GET "$JENKINS_URL/job/$folder/job/$job/api/json"
+}
+
+giturl()
+{
+    folder="$1"
+    job="$2"
+    jenkins_curl --request GET "$JENKINS_URL/job/$folder/job/$job/config.xml"
+}
+
+main()
+{
+    for folder in $(folders | jq --raw-output '.jobs[] | .name'); do
+        for job in $(jobs $folder | jq --raw-output '.jobs[] | .name'); do
+            giturl=$(giturl $folder $job | xq --raw-output '.["org.jenkinsci.plugins.workflow.multibranch.WorkflowMultiBranchProject"].sources.data["jenkins.branch.BranchSource"].source.remote')
+            job $folder $job | yq --arg GITURL "$giturl" --arg FOLDER "$folder" --arg JOB "$job" --yaml-output '[.jobs[] | {"job": { "name": "\($FOLDER)/\($JOB)", "project-type": "multibranch", "periodic-folder-trigger": "1d", "prune-dead-branches": true, "number-to-keep": "10", "days-to-keep": "10", "script-path": "Jenkinsfile", "scm": {"git": {"url": $GITURL}}}}]'
+        done
     done
-done
+}
+
+# Entry point
+main
