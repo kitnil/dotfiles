956a4bc05ef3a9207ada7e92d84177783c7e66ba
Oleg Pykhalov <go.wigust@gmail.com>
2020-01-25 22:32:49 +0300

    generate.sh: Update.

--- generate.sh
+++ generate.sh
@@ -1,12 +1,26 @@
 #!/bin/sh
 
-for folder in /tmp/j/*.json; do
-    jq '[.jobs[] | {"job": { "name": .name, "description": .description, "project-type": "multibranch", "periodic-folder-trigger": "1d", "prune-dead-branches": true, "number-to-keep": "10", "days-to-keep": "10", "script-path": "Jenkinsfile", "scm": {"git": {"url": .url, "credentials-id": "gitlab-git"}}}}]' < $folder \
-        | yq -y . \
-        | sed 's@    git@    - git@g' \
-        | sed 's|https://jenkins.intr/job/|git@gitlab.intr:|g' \
-        | sed 's|/job||g' \
-        | sed 's@        url:@          url:@g' \
-        | sed 's@        credentials-id:@          credentials-id:@g' \
-        | tee $(basename $folder | sed 's|json|yml|g')
+JENKINS_URL="https://jenkins.wugi.info"
+
+folders()
+{
+    curl --silent --request POST --user "admin:$(pass show jenkins/admin-api-key)" "$JENKINS_URL/api/json" \
+        | jq --raw-output '.jobs[] | .name'
+}
+
+# format()
+# {
+#     input=$1
+#     echo $input
+#         | sed 's@    git@    - git@g' \
+#         | sed 's|https://jenkins.intr/job/|git@gitlab.intr:|g' \
+#         | sed 's|/job||g' \
+#         | sed 's@        url:@          url:@g' \
+#         | sed 's@        credentials-id:@          credentials-id:@g' \
+#         | tee $(basename $folder | sed 's|json|yml|g')
+# }
+
+for folder in $(folders); do
+    curl --silent --request POST --user "admin:$(pass show jenkins/admin-api-key)" "$JENKINS_URL/job/$folder/api/json" \
+        | yq --yaml-output '[.jobs[] | {"job": { "name": .name, "description": .description, "project-type": "multibranch", "periodic-folder-trigger": "1d", "prune-dead-branches": true, "number-to-keep": "10", "days-to-keep": "10", "script-path": "Jenkinsfile", "scm": {"git": {"url": .url, "credentials-id": "gitlab-git"}}}}]'
 done
