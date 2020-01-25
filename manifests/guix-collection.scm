45f87079ad05f56c32199572c3a04a05aa677a6c
Oleg Pykhalov <go.wigust@gmail.com>
2020-01-25 13:55:16 +0300

    manifests: guix-collection: Use python-web module.
    This commit follows 2ae89e4aa0df71ab42a013c168c05f2a775940ed.

--- manifests/guix-collection.scm
+++ manifests/guix-collection.scm
@@ -11,11 +11,11 @@ haskell-xyz image-viewers imagemagick inkscape irc kde kodi
 libreoffice license linux lisp logging lsof lxde m4 mail man maths
 messaging ncdu ncurses networking node package-management parallel
 password-utils patchutils pdf perl perl-web php pretty-print
-pulseaudio pv python python-xyz qt ratpoison readline rdesktop rsync
-rust-apps samba scheme screen shellutils ssh statistics sqlite
-suckless syncthing synergy tex texinfo textutils text-editors tigervnc
-tmux tls tor valgrind version-control video virtualization vpn w3m
-xfce web wget wm xdisorg xml xorg)
+pulseaudio pv python python-xyz python-web qt ratpoison readline
+rdesktop rsync rust-apps samba scheme screen shellutils ssh statistics
+sqlite suckless syncthing synergy tex texinfo textutils text-editors
+tigervnc tmux tls tor valgrind version-control video virtualization
+vpn w3m xfce web wget wm xdisorg xml xorg)
 
 (define guix-collection-packages-multiout
   `((,glib "bin")
