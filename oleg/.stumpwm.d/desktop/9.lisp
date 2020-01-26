17090c5f1c46cbac23e44cc141f73413954f6165
Oleg Pykhalov <go.wigust@gmail.com>
2020-01-26 16:10:16 +0300

    stumpwm: Update desktop 9.

--- oleg/.stumpwm.d/desktop/9.lisp
+++ oleg/.stumpwm.d/desktop/9.lisp
@@ -4,22 +4,14 @@
                 :GROUPS (#S(GDUMP
                             :NUMBER 1
                             :NAME "Default"
-                            :TREE (((((#S(FDUMP
-                                          :NUMBER 0
-                                          :X 0
-                                          :Y 0
-                                          :WIDTH 481
-                                          :HEIGHT 348
-                                          :WINDOWS (20971530)
-                                          :CURRENT 20971530)
-                                       #S(FDUMP
-                                          :NUMBER 5
-                                          :X 0
-                                          :Y 348
-                                          :WIDTH 481
-                                          :HEIGHT 312
-                                          :WINDOWS NIL
-                                          :CURRENT NIL))
+                            :TREE ((((#S(FDUMP
+                                         :NUMBER 0
+                                         :X 0
+                                         :Y 0
+                                         :WIDTH 481
+                                         :HEIGHT 660
+                                         :WINDOWS (46137345 14680069)
+                                         :CURRENT 14680069)
                                       #S(FDUMP
                                          :NUMBER 4
                                          :X 0
@@ -34,25 +26,33 @@
                                         :Y 939
                                         :WIDTH 481
                                         :HEIGHT 141
-                                        :WINDOWS (18874381)
-                                        :CURRENT 18874381))
+                                        :WINDOWS (16777229)
+                                        :CURRENT 16777229))
                                     #S(FDUMP
                                        :NUMBER 2
                                        :X 481
                                        :Y 0
                                        :WIDTH 1439
                                        :HEIGHT 1080
-                                       :WINDOWS (6291492 20971526 46137357
-                                                 23068707)
-                                       :CURRENT 6291492))
-                                   #S(FDUMP
-                                      :NUMBER 1
-                                      :X 1920
-                                      :Y 0
-                                      :WIDTH 1920
-                                      :HEIGHT 1080
-                                      :WINDOWS (23068674)
-                                      :CURRENT 23068674))
+                                       :WINDOWS (52428828 20971526 31457313
+                                                 31457327 23068685)
+                                       :CURRENT 52428828))
+                                   (#S(FDUMP
+                                       :NUMBER 1
+                                       :X 1920
+                                       :Y 0
+                                       :WIDTH 960
+                                       :HEIGHT 1080
+                                       :WINDOWS (20971530)
+                                       :CURRENT 20971530)
+                                    #S(FDUMP
+                                       :NUMBER 5
+                                       :X 2880
+                                       :Y 0
+                                       :WIDTH 960
+                                       :HEIGHT 1080
+                                       :WINDOWS (31457384)
+                                       :CURRENT 31457384)))
                             :CURRENT 2))
                 :CURRENT 1))
    :CURRENT 0)
