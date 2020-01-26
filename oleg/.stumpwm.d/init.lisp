b0f25fca9a3ac83eac6427786e7c8b7e23d8b57d
Oleg Pykhalov <go.wigust@gmail.com>
2020-01-26 15:28:29 +0300

    stumpwm: Enable scrollbar in run-xterm.

--- oleg/.stumpwm.d/init.lisp
+++ oleg/.stumpwm.d/init.lisp
@@ -806,7 +806,7 @@
 (defcommand run-xterm () ()
   "Start or focus XTerm."
   (run-prog *shell-program*
-            :args (list "-c" (xterm-command))
+            :args (list "-c" (xterm-command :scrollbar t))
             :wait nil))
 
 (defcommand xterm-dark-no-scrollbar () ()
