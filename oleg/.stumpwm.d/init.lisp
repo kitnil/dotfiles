0717111358f230927c0bcd16c5566764f6cf90b4
Oleg Pykhalov <go.wigust@gmail.com>
2020-01-26 16:25:40 +0300

    stumpwm: Update Quassel "Chat Monitor" rule.

--- oleg/.stumpwm.d/init.lisp
+++ oleg/.stumpwm.d/init.lisp
@@ -1615,7 +1615,7 @@
     (run-shell-command (firefox-command))))
 
 (defun frame-parameters-display-0 ()
-  ;; (define-frame-preference "Default" (0 NIL T :CLASS "quassel" :TITLE "Chat Monitor"))
+  (define-frame-preference "Default" (1 NIL T :CLASS "quassel" :TITLE "Chat Monitor"))
   (define-frame-preference "Default" (3 NIL T :CLASS "XTerm" :TITLE "alerta"))
   (define-frame-preference "Default" (0 NIL T :TITLE "pulsemixer"))
   (define-frame-preference "Default" (0 NIL T :CLASS "XTerm" :TITLE "notmuch"))
