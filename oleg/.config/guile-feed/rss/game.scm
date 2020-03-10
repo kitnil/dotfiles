cde85e7d0e3512bd3798eeb66b1af5da83096f50
Oleg Pykhalov <go.wigust@gmail.com>
2020-03-10 13:50:24 +0300

    Move to chezmoi

--- oleg/.config/guile-feed/rss/game.scm
+++
@@ -1,8 +0,0 @@
-(define-module (rss game)
-  #:use-module (feed rss))
-
-(define-public game-reddit-freegames
-  (feed
-   (name "game-reddit-freegames")
-   (description "Reddit free as beer games.")
-   (uri (reddit-rss "freegames"))))
--- oleg/.config/guile-feed/rss/hardware.scm
+++
@@ -1,8 +0,0 @@
-(define-module (rss hardware)
-  #:use-module (feed rss))
-
-(define-public h-node
-  (feed
-   (name "h-node")
-   (description "Libre hardware list.")
-   (uri "https://h-node.org/rss/modifications/en")))
