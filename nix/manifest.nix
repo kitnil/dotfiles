cdf8c31170fd239f3ea2157ba0037dcd3a8deb94
Oleg Pykhalov <go.wigust@gmail.com>
2020-03-25 15:13:00 +0300

    nix: manifest.nix: Add firefox-esr-52.
    * nix/manifest.nix: Add firefox-esr-52.
    * oleg/bin/firefox-esr: Delete file.

--- nix/manifest.nix
+++ nix/manifest.nix
@@ -151,4 +151,36 @@ in [
   wrk
   wtf
   yq
+
+  (let
+    firefox = (import (builtins.fetchTarball {
+      url =
+        "https://github.com/nixos/nixpkgs/archive/cc3b6aa322f307580d48c975a3b86b4462b645d8.tar.gz";
+    }) {
+      config = {
+        allowBroken = true;
+        allowUnfree = true;
+        allowUnsupportedSystem = true;
+        firefox.icedtea = true;
+        permittedInsecurePackages = [
+          "autotrace-0.31.1"
+          "batik-1.6"
+          "firefox-52.9.0esr"
+          "firefox-esr-unwrapped-52.9.0esr"
+        ];
+      };
+    }).firefox-esr-52;
+
+  in (stdenv.mkDerivation {
+    name = "firefox-esr-52";
+    builder = writeScript "builder.sh" (''
+    source $stdenv/setup
+    mkdir -p $out/bin
+    cat > $out/bin/firefox-esr-52 <<'EOF'
+    #!${bash}/bin/bash -e
+    exec -a firefox-esr-52 ${firefox}/bin/firefox "$@"
+    EOF
+    chmod 555 $out/bin/firefox-esr-52
+  '');
+  }))
 ]
