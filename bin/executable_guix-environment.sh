08de0268e996898cea51f71e678450e96cbb8329
Oleg Pykhalov <go.wigust@gmail.com>
2020-04-01 22:49:47 +0300

    bin: executable_guix-environment.sh: Remove build-env with force.

--- bin/executable_guix-environment.sh
+++ bin/executable_guix-environment.sh
@@ -2,7 +2,7 @@
 
 # Prepare an environment to build Guix
 
-rm build-env
+rm --force build-env
 
 env GUIX_PACKAGE_PATH= guix environment --root=build-env --pure guix --ad-hoc help2man guile-sqlite3 guile-gcrypt "$@"
 # strace git gdb
