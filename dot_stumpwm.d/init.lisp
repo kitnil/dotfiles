4ea99c9fbee680e9e1d30db8345ecc7a0d7670e3
Oleg Pykhalov <go.wigust@gmail.com>
2020-04-04 11:35:52 +0300

    stumpwm: Add notify-to-rest.
    * dot_stumpwm.d/init.lisp (notify-to-rest, notify-to-rest-set-period,
    toggle-notify-to-rest): New commands.
    (*notify-to-rest*, *notify-to-rest-period*): New variables.

--- dot_stumpwm.d/init.lisp
+++ dot_stumpwm.d/init.lisp
@@ -104,6 +104,29 @@
         (or (> (parse-integer (time-format day-of-week)) 5)
             (> (parse-integer (time-format hour)) 18)))))
 
+(defvar *notify-to-rest* t)
+(defvar *notify-to-rest-period* 3600)
+
+(defcommand toggle-notify-to-rest () ()
+  (if *notify-to-rest*
+      (setf *notify-to-rest* nil)
+      (setf *notify-to-rest* t)))
+
+(defcommand notify-to-rest-set-period (period) ((:string "Period of notifications in seconds: "))
+  (if *notify-to-rest-period*
+      (setf *notify-to-rest-period* period)
+      (setf *notify-to-rest-period* 3600)))
+
+(defcommand notify-to-rest () ()
+  (sb-thread:make-thread
+   (lambda ()
+     (loop while (or *notify-to-rest* *work-time*)
+        do (progn (run-shell-command
+                   (format nil "notify-send --urgency=low ~s"
+                           (format nil "Take a break.  Next notification in ~a seconds."
+                                   *notify-to-rest-period*)))
+                  (sleep *notify-to-rest-period*))))))
+
 (defun switch-to-emacs ()
   (unless (uiop/utility:string-prefix-p "Emacs"
                                         (window-class (current-window)))
