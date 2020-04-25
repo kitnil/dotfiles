0ede361f70c6102bf92341613bf63f58eeb57337
Oleg Pykhalov <go.wigust@gmail.com>
2020-04-25 19:20:18 +0300

    stumpwm: Display available memory in mode-line.

--- dot_stumpwm.d/init.lisp
+++ dot_stumpwm.d/init.lisp
@@ -763,6 +763,29 @@
 
 
 ;;;
+;;; Memory
+;;;
+
+(defun get-proc-fd-field (s field)
+  (if s
+      (do ((line (read-line s nil nil) (read-line s nil nil)))
+	  ((null line) nil)
+	(let ((split (cl-ppcre:split "\\s*:\\s*" line)))
+	  (when (string= (car split) field) (return (cadr split)))))
+      ""))
+
+(defun mem-usage ()
+  "Returns amount of available memory."
+  (with-open-file (file #P"/proc/meminfo" :if-does-not-exist nil)
+    (read-from-string (get-proc-fd-field file "MemAvailable"))))
+
+(defun fmt-mem-available (mem)
+  "Returns a string representing the current available memory."
+  (let* ((available (truncate (/ mem 1000))))
+    (format nil "~4D MB" available)))
+
+
+;;;
 ;;; Docker
 ;;;
 
@@ -1010,6 +1033,8 @@
                                wn))))
         ,(make-string 4 :initial-element #\space)
         "^>"
+        ,(make-string 4 :initial-element #\space)
+        ,'(:eval (format nil "MEM: ~a" (fmt-mem-available (mem-usage))))
         ,(make-string 4 :initial-element #\space)
         ,'(:eval (format nil "VPN: ~a" *tapvpn-ip*))
         ,(make-string 4 :initial-element #\space)
