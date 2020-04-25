ab4ee7d757c3fe67aa032dc219aec2c66cbf875d
Oleg Pykhalov <go.wigust@gmail.com>
2020-04-26 01:50:26 +0300

    stumpwm: Uncomment XFT fonts.

--- dot_stumpwm.d/init.lisp
+++ dot_stumpwm.d/init.lisp
@@ -2137,10 +2137,9 @@
                           (mode-line-update)
                           (sleep (* 60 60)))))))))
 
-;; (require :ttf-fonts)
-;; (setf xft:*font-dirs* '("/run/current-system/profile/share/fonts/"))
-;; (setf clx-truetype:+font-cache-filename+ (concat (getenv "HOME") "/.fonts/font-cache.sexp"))
-;; (xft:cache-fonts)
-;; (set-font (make-instance 'xft:font :family "DejaVu Sans Mono" :subfamily "Book" :size 12))
-
+(require :ttf-fonts)
+(setf xft:*font-dirs* '("/run/current-system/profile/share/fonts/"))
+(setf clx-truetype:+font-cache-filename+ (concat (getenv "HOME") "/.fonts/font-cache.sexp"))
+(xft:cache-fonts)
+(set-font (make-instance 'xft:font :family "DejaVu Sans Mono" :subfamily "Book" :size 12))
 ;; (set-font "9x15bold")
