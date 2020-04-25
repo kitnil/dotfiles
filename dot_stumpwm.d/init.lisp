45458126c40bb90b34b927aec4a312c6d23b6e25
Oleg Pykhalov <go.wigust@gmail.com>
2020-04-25 20:51:21 +0300

    stumpwm: Display majordomo recent messages in INBOX counter.

--- dot_stumpwm.d/init.lisp
+++ dot_stumpwm.d/init.lisp
@@ -537,14 +537,17 @@
 
 (defcommand imap-update-recent-count () ()
   (setq *imap-recent*
-        (parse-integer
-         (second (split-string (car (filter (lambda (str)
-                                              (uiop/utility:string-suffix-p str "RECENT"))
-                                            (mapcar (lambda (str)
-                                                      (string-trim '(#\Newline) str))
-                                                    (split-string (run-shell-command (format nil "curl --request 'EXAMINE INBOX' --user 'oleg:~a' imap://localhost" (password-store-show "localhost/imap/oleg")) t)
-                                                                  '(#\^M)))))
-                               '(#\space))))))
+        (apply '+
+               (mapcar (lambda (mailbox)
+                         (parse-integer
+                          (second (split-string (car (filter (lambda (str)
+                                                               (uiop/utility:string-suffix-p str "RECENT"))
+                                                             (mapcar (lambda (str)
+                                                                       (string-trim '(#\Newline) str))
+                                                                     (split-string (run-shell-command (format nil "curl --request 'EXAMINE ~a' --user 'oleg:~a' imap://localhost" mailbox (password-store-show "localhost/imap/oleg")) t)
+                                                                                   '(#\^M)))))
+                                                '(#\space)))))
+                       '("INBOX" "majordomo")))))
 
 (defcommand notmuch () ()
   (term-shell-command (fetch-mail-command "notmuch new")
