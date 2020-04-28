06612afe0882a8a215a7be72e0ee6d40a138077d
Oleg Pykhalov <go.wigust@gmail.com>
2020-04-29 02:52:51 +0300

    guix: Add xplanet.

--- dotfiles/manifests/guix-collection.scm
+++ dotfiles/manifests/guix-collection.scm
@@ -2,20 +2,20 @@
              (guix packages)
              (guix profiles))
 
-(use-package-modules admin algebra aspell audio backup bittorrent
-cdrom chromium chicken ci cmake code commencement compression cpio
-cran curl databases dictionaries diffoscope dns dunst file elf entr
-games gcc gdb gimp ghostscript golang gl glib gnome gnu-doc gnupg
-gnuzilla graphics graphviz gstreamer gtk guile guile-xyz haskell
-haskell-apps haskell-xyz image-viewers imagemagick inkscape irc kde
-kodi libreoffice license linux lisp logging lsof lxde m4 mail man
-maths messaging ncdu ncurses networking node package-management
+(use-package-modules admin algebra aspell astronomy audio backup
+bittorrent cdrom chromium chicken ci cmake code commencement
+compression cpio cran curl databases dictionaries diffoscope dns dunst
+file elf entr games gcc gdb gimp ghostscript golang gl glib gnome
+gnu-doc gnupg gnuzilla graphics graphviz gstreamer gtk guile guile-xyz
+haskell haskell-apps haskell-xyz image-viewers imagemagick inkscape
+irc kde kodi libreoffice license linux lisp logging lsof lxde m4 mail
+man maths messaging ncdu ncurses networking node package-management
 parallel password-utils patchutils pdf perl perl-web php pretty-print
 pulseaudio pv python python-xyz python-web qt ratpoison readline
 rdesktop rsync rust-apps samba scheme screen shellutils ssh statistics
 sqlite suckless syncthing synergy tex texinfo textutils text-editors
-vnc tmux tls tor valgrind version-control video virtualization
-vpn w3m xfce web wget wm xdisorg xml xorg)
+vnc tmux tls tor valgrind version-control video virtualization vpn w3m
+xfce web wget wm xdisorg xml xorg)
 
 (define guix-collection-packages-multiout
   `((,glib "bin")
@@ -243,6 +243,8 @@ vpn w3m xfce web wget wm xdisorg xml xorg)
 
                xfce4-screenshooter
                xfce4-terminal
+
+               xplanet
 
                xmodmap
 
