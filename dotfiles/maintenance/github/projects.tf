985b692f5462ebfed7b9cba566a706008e368ab9
Oleg Pykhalov <go.wigust@gmail.com>
2020-05-01 19:11:29 +0300

    github: Add guix-nonfree.

--- dotfiles/maintenance/github/projects.tf
+++ dotfiles/maintenance/github/projects.tf
@@ -25,6 +25,13 @@ resource "github_repository" "guix-wigust-services" {
   topics         = ["guix", "guile", "scheme"]
 }
 
+resource "github_repository" "guix-nonfree" {
+  name           = "guix-nonfree"
+  description    = "Extra non-free packages for Guix"
+  default_branch = "master"
+  topics         = ["guix", "guile", "scheme"]
+}
+
 resource "github_repository" "guix-tome4" {
   name           = "guix-tome4"
   description    = "Guix package for Tales of Maj'Eyal rogue like game"
