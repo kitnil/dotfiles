754f1cd1ea0c1c55ef83c89efc33d2a7dbd825d6
Oleg Pykhalov <go.wigust@gmail.com>
2020-05-06 02:52:42 +0300

    stumpwm: Add vnc-command procedure.

--- dot_stumpwm.d/vnc.lisp
+++ dot_stumpwm.d/vnc.lisp
@@ -1,16 +1,18 @@
 (in-package :stumpwm)
 
-(defcommand vnc (display &optional view-only) ((:string "display: "))
-  (run-shell-command
-   (join `("vncviewer"
-           "-AutoSelect=0"
-           "-PreferredEncoding=Raw"
-           "-FullColor=1"
-           "-NoJPEG=1"
-           "-CompressLevel=0"
-           "-passwd" ,(concat (getenv "HOME") "/.vnc/passwd")
-           ,@(if view-only '("-ViewOnly") '())
-           ,(concat "127.0.0.1:" display)))))
+(defun vnc-command (port &key (host "127.0.0.1") (view-only nil))
+  (join `("vncviewer"
+          "-AutoSelect=0"
+          "-PreferredEncoding=Raw"
+          "-FullColor=1"
+          "-NoJPEG=1"
+          "-CompressLevel=0"
+          "-passwd" ,(concat (getenv "HOME") "/.vnc/passwd")
+          ,@(if view-only '("-ViewOnly") '())
+          ,(concat host ":" (write-to-string port)))))
+
+(defcommand vnc (display) ((:string "display: "))
+  (run-shell-command (vnc-command (parse-integer display))))
 
 (defcommand vnc-magnolia () ()
   (run-shell-command "exec vncviewer localhost:59555"))
