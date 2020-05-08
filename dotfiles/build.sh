a22e59659f33ae9b926104d31ff6aed8af2745e8
Oleg Pykhalov <go.wigust@gmail.com>
2020-05-09 00:38:18 +0300

    dotfiles: Fix build.sh.

--- dotfiles/build.sh
+++ dotfiles/build.sh
@@ -9,9 +9,9 @@ guix pull --verbosity=0 --profile=.guix-profile --channels=channels.scm
 
 GUIX_BIN=".guix-profile/bin/guix"
 "$GUIX_BIN" describe
-for system in guixsd spb workstation; do
+for system in guixsd spb workstation-guixsd; do
     "$GUIX_BIN" system build --load-path=fiore/modules "guixsd/$system.scm"
 done
-for manifest in oleg workstation; do
+for manifest in guixsd workstation-guixsd; do
     "$GUIX_BIN" environment --manifest="manifests/$manifest.scm" -- sh -c exit
 done
