4cb5abfd7fddabc05832fcaf09d9e296f6eaeef2
Oleg Pykhalov <go.wigust@gmail.com>
2020-05-13 14:13:38 +0300

    emacs: groovy: Invoke fci-mode.
    * private_dot_emacs.d/modules/groovy.el (groovy-mode-hook): Add fci-mode.

--- private_dot_emacs.d/modules/groovy.el
+++ private_dot_emacs.d/modules/groovy.el
@@ -2,6 +2,8 @@
   "Jenkinsfile"
   ["groovy/Jenkinsfile" yas-expand-current-buffer])
 
+(add-hook 'groovy-mode-hook 'fci-mode)
+
 ;; (with-eval-after-load 'groovy-mode
 ;;   (let ((map groovy-mode-map))
 ;;     (define-key map (kbd "M-.") 'dumb-jump-go)))
