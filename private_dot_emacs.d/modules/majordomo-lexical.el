730443c896492776d0ee5bd7b55135bed342ed20
Oleg Pykhalov <go.wigust@gmail.com>
2020-05-14 05:00:46 +0300

    emacs: Move majordomo-lexical inside majordomo.

--- private_dot_emacs.d/modules/majordomo-lexical.el
+++ private_dot_emacs.d/modules/majordomo-lexical.el
@@ -1,11 +1,1 @@
-(mapcar (lambda (directory)
-          (let* ((parts (split-string directory "/"))
-                 (name (first (last parts)))
-                 (group (string-remove-prefix "_"
-                                              (first (last (delete name
-                                                                   parts)))))
-                 (name+group (concat "majordomo-" group "-" name)))
-            (wi-define-browse-url-git-commit
-             name+group directory (lambda (url)
-                                    (first (last (split-string url "/")))))))
-        (wi-project-candidates-groups-direcotory))
+
--- private_dot_emacs.d/modules/majordomo.el
+++ private_dot_emacs.d/modules/majordomo.el
@@ -68,4 +68,22 @@
                       ,@(nthcdr 4 file-name)))
                 "/"))))
 
+
+;;;
+;;; browse-url
+;;;
 
+(defun majordomo-define-browse-url ()
+  (mapcar (lambda (directory)
+            (let* ((parts (split-string directory "/"))
+                   (name (first (last parts)))
+                   (group (string-remove-prefix "_"
+                                                (first (last (delete name
+                                                                     parts)))))
+                   (name+group (concat "majordomo-" group "-" name)))
+              (wi-define-browse-url-git-commit
+               name+group directory (lambda (url)
+                                      (first (last (split-string url "/")))))))
+          (wi-project-candidates-groups-direcotory)))
+
+(majordomo-define-browse-url)
