d83b867d51bd44916e761e734bd158fc623d01f3
Oleg Pykhalov <go.wigust@gmail.com>
2020-05-17 01:52:58 +0300

    stumpwm: Add picom.
    * dot_stumpwm.d/theme.lisp (picom): New command.

--- dot_stumpwm.d/theme.lisp
+++ dot_stumpwm.d/theme.lisp
@@ -101,3 +101,6 @@
 
 (defcommand xcompmgr () ()
   (run-shell-command "xcompmgr -c -C -t-5 -l-5 -r10 -o.55"))
+
+(defcommand picom () ()
+  (run-shell-command "picom -cCGfF -o 0.38 -O 200 -I 200 -t 0 -l 0 -r 3 -D2 -m 0.88"))
