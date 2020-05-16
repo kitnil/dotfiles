6426cd42f39e3a84c4c54e35d6be33fba3b2624a
Oleg Pykhalov <go.wigust@gmail.com>
2020-05-16 06:51:12 +0300

    emacs: Fix vterm.

--- private_dot_emacs.d/modules/term.el
+++ private_dot_emacs.d/modules/term.el
@@ -41,14 +41,11 @@
     (define-key map (kbd "<C-backspace>") 'vterm-send-backspace)
     (define-key map (kbd "<menu>") 'vterm-copy-mode))
   (let ((map vterm-copy-mode-map))
-    (define-key map (kbd "<menu>") 'vterm-copy-mode)))
-
-(push (list "find-file-below"
-            (lambda (path)
-              (if-let* ((buf (find-file-noselect path))
-                        (window (display-buffer-below-selected buf nil)))
-                  (select-window window)
-                (message "Failed to open file: %s" path))))
-      vterm-eval-cmds)
-
-
+    (define-key map (kbd "<menu>") 'vterm-copy-mode))
+  (push (list "find-file-below"
+              (lambda (path)
+                (if-let* ((buf (find-file-noselect path))
+                          (window (display-buffer-below-selected buf nil)))
+                    (select-window window)
+                  (message "Failed to open file: %s" path))))
+        vterm-eval-cmds))
