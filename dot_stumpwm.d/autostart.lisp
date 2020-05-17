dd1c06e945fb26a2190cbad85694a731f6fd6269
Oleg Pykhalov <go.wigust@gmail.com>
2020-05-17 03:19:48 +0300

    stumpwm: Comment xdotool autostart.

--- dot_stumpwm.d/autostart.lisp
+++ dot_stumpwm.d/autostart.lisp
@@ -8,20 +8,21 @@
               (lambda () (picom))
               (lambda () (cursor-theme))
               (lambda () (keynav))
-              (lambda () (sb-thread:make-thread
-                     (lambda ()
-                       (run-shell-command
-                        (join (list (xdotool-behave-screen-edge
-                                     (vnc-command 5901)
-                                     :position "top"))))
-                       (run-shell-command
-                        (join (list (xdotool-behave-screen-edge
-                                     (format nil "sh -c ~s"
-                                             "echo '(music-youtube)' | stumpish -e eval")
-                                     :position "bottom"))))
-                       (run-shell-command
-                        (join (list (xdotool-behave-screen-edge
-                                     (format nil "sh -c ~s"
-                                             "echo '(editor)' | stumpish -e eval")
-                                     :position "left")))))
-                     :name "xdotool"))))
+              ;; (lambda () (sb-thread:make-thread
+              ;;        (lambda ()
+              ;;          (run-shell-command
+              ;;           (join (list (xdotool-behave-screen-edge
+              ;;                        (vnc-command 5901)
+              ;;                        :position "top"))))
+              ;;          (run-shell-command
+              ;;           (join (list (xdotool-behave-screen-edge
+              ;;                        (format nil "sh -c ~s"
+              ;;                                "echo '(music-youtube)' | stumpish -e eval")
+              ;;                        :position "bottom"))))
+              ;;          (run-shell-command
+              ;;           (join (list (xdotool-behave-screen-edge
+              ;;                        (format nil "sh -c ~s"
+              ;;                                "echo '(editor)' | stumpish -e eval")
+              ;;                        :position "left")))))
+              ;;        :name "xdotool"))
+              ))
