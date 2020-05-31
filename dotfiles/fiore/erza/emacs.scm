a4b71dc6bab3ff1eccc9049555c2b16e35078ca5
Oleg Pykhalov <go.wigust@gmail.com>
2020-05-31 17:30:07 +0300

    Delete unused files.

--- dotfiles/fiore/erza/emacs.scm
+++
@@ -1,9 +0,0 @@
-(use-modules (erza emacs))
-
-(emacs-configuration
- (hooks
-  (list (emacs-hook (hook 'prog-mode-hook)
-                    (procedures '(hs-minor-mode
-                                  rainbow-delimiters-mode
-                                  yas-minor-mode)))))
- (extra-options '()))
