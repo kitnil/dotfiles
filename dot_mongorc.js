57472f772bd8cb4c4b1573522ffcd60de24c2de8
Oleg Pykhalov <go.wigust@gmail.com>
2020-06-13 22:56:40 +0300

    .mongorc.js: Add variety.

--- dot_mongorc.js
+++ dot_mongorc.js
@@ -4,3 +4,12 @@ search = function(id) {
         return sibling.getCollectionNames().map((collection) => sibling.getCollection(collection).findOne({"_id" : ObjectId(id)})).filter((notNull) => notNull);
     }).filter(String)
 }
+
+variety = function(id) {
+    return db.getMongo().getDBNames().map((database) => {
+        let sibling = db.getSiblingDB(database);
+        sibling.getCollectionNames().map((collection) => {
+            return (typeof(sibling.getCollection(collection)))
+        })
+    }).filter(String)
+}
