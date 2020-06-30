8ca46f67ce6b3274fdf0fa30d8181cc2b49fb770
Oleg Pykhalov <go.wigust@gmail.com>
2020-06-30 23:04:03 +0300

    emacs: lsp: Add missing seq-first procedure for lsp-java-add-import.

--- private_dot_emacs.d/modules/lsp.el
+++ private_dot_emacs.d/modules/lsp.el
@@ -6,3 +6,9 @@
 (setq lsp-ui-sideline-enable nil) ;right menu
 (setq lsp-file-watch-threshold nil)
 (setq lsp-lua-emmy-jar-path (expand-file-name "EmmyLua-LS-all.jar" user-emacs-directory))
+
+;; GNU Emacs 26.3 (build 1, x86_64-pc-linux-gnu, GTK+ Version 3.24.20)
+;; https://github.com/emacs-lsp/lsp-java/issues/173
+(defun seq-first (sequence)
+  "Return the first element of SEQUENCE."
+  (seq-elt sequence 0))
