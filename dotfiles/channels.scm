e3b04cb33b543b05d1b8f61ac130daf6f6d9d81f
Oleg Pykhalov <go.wigust@gmail.com>
2020-07-05 22:35:03 +0300

    guix: channels: Switch to public sources.

--- dotfiles/channels.scm
+++ dotfiles/channels.scm
@@ -3,17 +3,17 @@
 ;; WARNING: Don't use commits because of bin/executable_dotfiles
 (list (channel
        (name 'guix-wigust)
-       (url "https://cgit.duckdns.org/git/guix/guix-wigust"))
+       (url "https://github.com/kitnil/guix-wigust"))
       (channel
        (name 'guix-wigust-services)
-       (url "https://cgit.duckdns.org/git/guix/guix-wigust-services"))
+       (url "https://github.com/kitnil/guix-wigust-services"))
       (channel
        (name 'guix-majordomo)
-       (url "https://cgit.duckdns.org/git/guix/guix-majordomo"))
+       (url "https://gitlab.com/wigust/guix-majordomo"))
       (channel
        (name 'guix-tome4)
-       (url "https://cgit.duckdns.org/git/guix/guix-tome4"))
+       (url "https://github.com/kitnil/guix-tome4"))
       (channel
        (name 'guix)
-       (url "https://cgit.duckdns.org/git/guix/guix")
+       (url "https://github.com/kitnil/guix")
        (branch "wip-local")))
