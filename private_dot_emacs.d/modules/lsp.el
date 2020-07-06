eb1e56dd5bc08c7ad87529b40c023fd31961734d
Oleg Pykhalov <go.wigust@gmail.com>
2020-07-06 14:27:32 +0300

    emacs: lsp: Add to python-mode-hook.

--- private_dot_emacs.d/modules/lsp.el
+++ private_dot_emacs.d/modules/lsp.el
@@ -3,6 +3,7 @@
 
 (require 'lsp-java)
 (add-hook 'java-mode-hook #'lsp)
+(add-hook 'python-mode-hook 'lsp)
 (setq lsp-ui-sideline-enable nil) ;right menu
 (setq lsp-file-watch-threshold nil)
 (setq lsp-lua-emmy-jar-path (expand-file-name "EmmyLua-LS-all.jar" user-emacs-directory))
