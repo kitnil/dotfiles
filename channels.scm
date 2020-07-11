69556f7c89c5a322a13b97bb897716de8190e8be
Oleg Pykhalov <go.wigust@gmail.com>
2020-07-12 00:12:51 +0300

    Delete channels.scm.

--- channels.scm
+++
@@ -1,9 +0,0 @@
-(list (channel
-        (name 'guix)
-        (url "https://git.savannah.gnu.org/git/guix.git")
-        (commit
-         "ee6bfffe1984c63070e5c9510722b586ccd4b35d"))
-      (channel
-       (name 'guix-wigust)
-       (url "@CI_PROJECT_URL@")
-       (branch "@CI_COMMIT_REF_NAME@")))
