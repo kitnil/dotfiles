9b6ab2d20bc2486d85e14b99df2ef1dd078d424c
Oleg Pykhalov <go.wigust@gmail.com>
2020-07-19 23:31:59 +0300

    stumpwm: Add docker-lem.

--- dot_stumpwm.d/docker.lisp
+++ dot_stumpwm.d/docker.lisp
@@ -2,3 +2,8 @@
 
 (defcommand docker-debian () ()
   (term-shell-command "docker run --rm -it debian:10" :scrollbar t :title "docker-debian"))
+
+;; Lem is the editor/IDE well-tuned for Common Lisp.
+;; https://github.com/cxxxr/lem
+(defcommand docker-lem () ()
+  (term-shell-command "docker run --rm -ti 40ants/lem:latest" :title "docker-lem"))
