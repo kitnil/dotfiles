fce99da0be4384aede2f15f8317c5385c5aa64a8
Oleg Pykhalov <go.wigust@gmail.com>
2020-08-08 18:08:19 +0300

    Add idesk generator.

---
+++ dotfiles/idesk/idesk.scm
@@ -0,0 +1,31 @@
+#!/usr/bin/env -S guile --no-auto-compile
+!#
+
+(use-modules (ice-9 match))
+
+(define x
+  (make-parameter 39))
+
+(define y
+  (make-parameter 29))
+
+(for-each (match-lambda ((file caption command icon)
+                         (with-output-to-file file
+                           (lambda ()
+                             (display "table Icon\n")
+                             (format #t "  Caption: ~a~%" caption)
+                             (format #t "  Command: ~a~%" command)
+                             (format #t "  Icon: ~a~%" icon)
+                             (display "  Width: 48\n")
+                             (display "  Height: 48\n")
+                             (format #t "  X: ~a~%" (x))
+                             (format #t "  Y: ~a~%" (y (+ (y) 100)))
+                             (display "end\n")))))
+          '(("xterm.lnk"
+             "XTerm"
+             "/home/oleg/.guix-profile/bin/xterm"
+             "/home/oleg/.guix-profile/share/icons/gnome/48x48/apps/gnome-terminal.png")
+            ("guix.vm.wugi.info.lnk"
+             "VNC guix.vm"
+             "/home/oleg/.guix-profile/bin/vncviewer -AutoSelect=0 -PreferredEncoding=Raw -FullColor=1 -NoJPEG=1 -CompressLevel=0 -passwd ~/.vnc/passwd 127.0.0.1:5911"
+             "/home/oleg/.guix-profile/share/icons/hicolor/48x48/apps/tigervnc.png")))
