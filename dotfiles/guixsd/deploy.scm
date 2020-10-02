3f8e9e5c455e93a48ed49e1d28019f19dbbb18a1
Oleg Pykhalov <go.wigust@gmail.com>
2020-10-02 09:54:56 +0300

    guixsd: deploy: Add workstation-guixsd.

--- dotfiles/guixsd/deploy.scm
+++ dotfiles/guixsd/deploy.scm
@@ -6,4 +6,13 @@
                        (system "x86_64-linux")
                        (user "oleg")
                        (host-key "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAICnqb74jWKmirhfbXKrt047ECzaSiCsZ4j3mYpTSbYEa root@gnu")
+                       (identity "/home/oleg/.ssh/id_rsa"))))
+      (machine
+       (operating-system (load "workstation-guixsd.scm"))
+       (environment managed-host-environment-type)
+       (configuration (machine-ssh-configuration
+                       (host-name "172.16.100.60")
+                       (system "x86_64-linux")
+                       (user "oleg")
+                       (host-key "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIA66f0IXG9Od7RImafwGjXP/RfAXssiOIwiAilC3KG4H root@workstation")
                        (identity "/home/oleg/.ssh/id_rsa")))))
