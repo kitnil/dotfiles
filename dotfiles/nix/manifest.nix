b375d7aafd6db92ad96f11b4072a30ae35a56c91
Oleg Pykhalov <go.wigust@gmail.com>
2020-10-20 09:28:23 +0300

    nix: manifest: Unpin lexicon.

--- dotfiles/nix/manifest.nix
+++ dotfiles/nix/manifest.nix
@@ -100,11 +100,7 @@ in [
   hy
   hyperfine
   knot-resolver
-  (import (fetchgit {
-    url = "https://github.com/NixOS/nixpkgs";
-    rev = "c59ea8b8a0e7f927e7291c14ea6cd1bd3a16ff38";
-    sha256 = "1ak7jqx94fjhc68xh1lh35kh3w3ndbadprrb762qgvcfb8351x8v";
-  }) { }).lexicon
+  lexicon
   ldns
   # litecli TODO: fails to build
   navi
