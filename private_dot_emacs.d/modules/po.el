eb70df6acdf28062e8cc508dc53ffd1c1a2e475b
Oleg Pykhalov <go.wigust@gmail.com>
2020-10-27 00:50:06 +0300

    emacs: po: Use russian language by default.

--- private_dot_emacs.d/modules/po.el
+++ private_dot_emacs.d/modules/po.el
@@ -1,3 +1,4 @@
 (add-hook 'po-subedit-mode-hook (lambda () (ispell-change-dictionary "ru")))
+(add-hook 'po-subedit-mode-hook '(lambda () (activate-input-method "russian-computer")))
 (add-hook 'po-subedit-exit-hook (lambda () (ispell-buffer)))
 (add-hook 'po-mode-hook 'git-auto-commit-mode)
