a4274a654af98417876bedce5503f1167dd9bbf6
Oleg Pykhalov <go.wigust@gmail.com>
2020-10-26 19:43:31 +0300

    emacs: po: Add git-auto-commit-mode to po-mode-hook.

--- private_dot_emacs.d/modules/po.el
+++ private_dot_emacs.d/modules/po.el
@@ -1,2 +1,3 @@
 (add-hook 'po-subedit-mode-hook (lambda () (ispell-change-dictionary "ru")))
 (add-hook 'po-subedit-exit-hook (lambda () (ispell-buffer)))
+(add-hook 'po-mode-hook 'git-auto-commit-mode)
