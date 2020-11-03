efbd33dc7defd58e21d3e8e49ce84216903846d1
Oleg Pykhalov <go.wigust@gmail.com>
2020-11-03 10:17:02 +0300

    bin: mjru-git-clone: Fix shellcheck warnings.

--- bin/executable_mjru-git-clone.sh
+++ bin/executable_mjru-git-clone.sh
@@ -8,11 +8,11 @@ size=$((1024 * 1024 * 100)) # 100 MB
 echo "#!/bin/sh"
 for file in /home/oleg/majordomo/*/*
 do
-    if [ -d $file ] && [[ $(du -s "$file" | cut -f 1) -lt $size ]]
+    if [ -d "$file" ] && [ "$(du -s "$file" | cut -f 1)" -lt "$size" ]
     then
-        echo mkdir -p $(dirname $file)
+        echo mkdir -p "$(dirname "$file")"
         repository="git clone $(printf "%s $file\n" "$(git -C "$file" remote get-url origin 2>/dev/null)")"
-        echo $repository
+        echo "$repository"
     fi
 done
 
