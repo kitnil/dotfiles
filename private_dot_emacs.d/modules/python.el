c0460d649c7cbd22accf1829b925dbf2cfff9df1
Oleg Pykhalov <go.wigust@gmail.com>
2020-11-13 13:54:42 +0300

    emacs: python: Set python-shell-interpreter.

--- private_dot_emacs.d/modules/python.el
+++ private_dot_emacs.d/modules/python.el
@@ -1,2 +1,3 @@
 ;; TODO: Fix `File mode specification error: (void-function yasnippet-snippets--fixed-indent)'
 (remove-hook 'python-mode-hook 'yasnippet-snippets--fixed-indent)
+(setq python-shell-interpreter "python3")
