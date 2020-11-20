7bb1039959225a1857e61c2182f0c6e4c57adde6
Oleg Pykhalov <go.wigust@gmail.com>
2020-11-20 08:52:47 +0300

    guixsd: Add vm32653.

---
+++ dotfiles/guixsd/vm32653.scm
@@ -0,0 +1,62 @@
+;; This is an operating system configuration template
+;; for a "bare bones" setup, with no X11 display server.
+
+(use-modules (gnu))
+(use-service-modules networking ssh)
+(use-package-modules screen ssh)
+
+(use-modules (config))
+
+(operating-system
+  (host-name "vm32653")
+  (timezone "Europe/Moscow")
+  (locale "en_US.utf8")
+
+  ;; Boot in "legacy" BIOS mode, assuming /dev/sdX is the
+  ;; target hard disk, and "my-root" is the label of the target
+  ;; root file system.
+  (bootloader (bootloader-configuration
+                (bootloader grub-bootloader)
+                (target "/dev/vda")))
+  (file-systems (cons (file-system
+                        (device (file-system-label "guix-root"))
+                        (mount-point "/")
+                        (type "ext4"))
+                      %base-file-systems))
+
+  ;; This is where user accounts are specified.  The "root"
+  ;; account is implicit, and is initially created with the
+  ;; empty password.
+  (users (cons (user-account
+                (name "oleg")
+                (comment "Oleg Pykhalov")
+                (group "users")
+
+                ;; Adding the account to the "wheel" group
+                ;; makes it a sudoer.  Adding it to "audio"
+                ;; and "video" allows the user to play sound
+                ;; and access the webcam.
+                (supplementary-groups '("wheel"
+                                        "audio" "video")))
+               %base-user-accounts))
+
+  (sudoers-file (plain-file "sudoers" "\
+root ALL=(ALL) ALL
+%wheel ALL=(ALL) ALL
+oleg ALL=(ALL) NOPASSWD:ALL\n"))
+
+  ;; Globally-installed packages.
+  (packages (cons screen %base-packages))
+
+  ;; Add services to the baseline: a DHCP client and
+  ;; an SSH server.
+  (services (append (list (static-networking-service "eth0" "78.108.87.14"
+                                                     #:netmask "255.255.254.0"
+                                                     #:gateway "78.108.87.254"
+                                                     #:name-servers '("8.8.8.8" "8.8.4.4"))
+                          (service openssh-service-type
+                                   (openssh-configuration
+                                    (openssh openssh-sans-x)
+                                   )))
+                    (modify-services %base-services
+                      (guix-service-type config => %guix-daemon-config)))))
