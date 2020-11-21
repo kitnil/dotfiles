f359ed77d09c126253f2288ea314d75f3c193e70
Oleg Pykhalov <go.wigust@gmail.com>
2020-11-21 05:28:36 +0300

    guixsd: Update deploy.

--- dotfiles/guixsd/deploy.scm
+++ dotfiles/guixsd/deploy.scm
@@ -2,7 +2,7 @@
        (operating-system (load "guix.vm.wugi.info.scm"))
        (environment managed-host-environment-type)
        (configuration (machine-ssh-configuration
-                       (host-name "guix.vm.wugi.info")
+                       (host-name "vm1.wugi.info")
                        (system "x86_64-linux")
                        (user "oleg")
                        (host-key "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAICnqb74jWKmirhfbXKrt047ECzaSiCsZ4j3mYpTSbYEa root@gnu")
@@ -15,6 +15,15 @@
                        (system "x86_64-linux")
                        (user "oleg")
                        (host-key "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIK4vfgz+tUUJFROCgTg+mRtHnr4yx1FeItXaAzmpP4lE root@(none)")
+                       (identity "/home/oleg/.ssh/id_rsa"))))
+      (machine
+       (operating-system (load "vm32653.scm"))
+       (environment managed-host-environment-type)
+       (configuration (machine-ssh-configuration
+                       (host-name "vm3.wugi.info")
+                       (system "x86_64-linux")
+                       (user "oleg")
+                       (host-key "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIAc6oVzRE16IuSJUmlJWtS1QHRFxuKm9ex1gqYsRPE/z root@(none)")
                        (identity "/home/oleg/.ssh/id_rsa"))))
       (machine
        (operating-system (load "workstation-guixsd.scm"))
