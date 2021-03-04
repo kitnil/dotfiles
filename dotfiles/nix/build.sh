1900aaf7d74c3904d05187e032627447e83ccf9f
Oleg Pykhalov <go.wigust@gmail.com>
2021-03-05 02:36:04 +0300

    nix: build.sh: Fix shellcheck warning.

--- dotfiles/nix/build.sh
+++ dotfiles/nix/build.sh
@@ -3,5 +3,5 @@
 for package in $(nix flake show |& awk '/: package/ { print $2 }' | sed 's/├───//; s/://; s,\x1B\[[0-9;]*[a-zA-Z],,g')
 do
     printf "@ %s\n" "$package"
-    nix build .#${package}
+    nix build .#"${package}"
 done
