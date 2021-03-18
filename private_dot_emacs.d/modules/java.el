068e5f8c42ab08580021a0aaa5db903daa29fcaf
Oleg Pykhalov <go.wigust@gmail.com>
2021-03-18 07:04:51 +0300

    java: Add wi-java--prettify-symbols-alist.

--- private_dot_emacs.d/modules/java.el
+++ private_dot_emacs.d/modules/java.el
@@ -1,1 +1,13 @@
 (add-hook 'java-mode-hook 'highlight-indent-guides-mode)
+
+(defconst wi-java--prettify-symbols-alist
+  `(("@Bean" . ,(string-to-symbols "@instance-of-a-class")) ;fancy word
+    ("@Autowired" . ,(string-to-symbols "@field-injection"))
+    ("$$" . ,(string-to-symbols "enhanced")) ;you could this in debug
+    ))
+
+(add-hooks
+ '(((java-mode-hook)
+    . (lambda ()
+        (set (make-local-variable 'prettify-symbols-alist)
+             wi-java--prettify-symbols-alist)))))
