11f7a176d22a86fe22fc50e00956eaf2c2b6c728
Oleg Pykhalov <go.wigust@gmail.com>
2021-03-23 20:13:15 +0300

    .dir-locals: Set geiser-guile-load-path.

--- .dir-locals.el
+++ .dir-locals.el
@@ -1,4 +1,7 @@
 ;; Per-directory local variables for GNU Emacs 23 and later.
 
 ((nil
-  . ((projectile-project-test-cmd . "make check"))))
+  . ((projectile-project-test-cmd . "make check")
+     (eval . (setq-local geiser-guile-load-path
+                         (list (concat (projectile-project-root)
+                                       "dotfiles/guixsd/modules")))))))
