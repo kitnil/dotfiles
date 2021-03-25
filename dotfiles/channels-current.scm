3ed40a6017246ca872b24694c981c3892ba516dd
Oleg Pykhalov <go.wigust@gmail.com>
2021-03-25 09:06:34 +0300

    dotfiles: Add channels-current.scm file.

---
+++ dotfiles/channels-current.scm
@@ -0,0 +1,15 @@
+(list (channel
+        (name 'guix)
+        (url "https://git.savannah.gnu.org/git/guix.git")
+        (commit
+          "ba0dc1dbc150a95746ffb89596862533e533c2c9"))
+      (channel
+        (name 'guix-wigust)
+        (url "https://github.com/kitnil/guix-wigust")
+        (commit
+          "676f5211cf9e69776265fda8df946ceabc51ef77"))
+      (channel
+        (name 'nonguix)
+        (url "https://gitlab.com/nonguix/nonguix")
+        (commit
+          "1e57ef8dc4e1130e40f6d8e17218ca82c8512d9e")))
