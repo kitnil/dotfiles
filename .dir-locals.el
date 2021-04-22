3a8dc61098589172dc5067325ccf92068c8a9faa
Oleg Pykhalov <go.wigust@gmail.com>
2021-04-22 12:17:57 +0300

    .dir-locals: Add projectile-project-configure-cmd.

--- .dir-locals.el
+++ .dir-locals.el
@@ -1,7 +1,8 @@
 ;; Per-directory local variables for GNU Emacs 23 and later.
 
 ((nil
-  . ((projectile-project-test-cmd . "make check")
+  . ((projectile-project-configure-cmd . "./configure")
+     (projectile-project-test-cmd . "make check")
      (projectile-project-install-cmd . "make install")
      (eval . (setq-local geiser-guile-load-path
                          (list (concat (projectile-project-root)
