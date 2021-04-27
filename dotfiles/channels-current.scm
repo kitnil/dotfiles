7630c417046372fe3b2f52a83a66e03162df8b02
Oleg Pykhalov <go.wigust@gmail.com>
2021-04-27 23:10:48 +0300

    channels-current.scm: Update.
    * dotfiles/channels-current.scm: Update.

--- dotfiles/channels-current.scm
+++ dotfiles/channels-current.scm
@@ -2,14 +2,14 @@
         (name 'guix-wigust)
         (url "https://github.com/kitnil/guix-wigust")
         (commit
-          "151bf1232a6ff53f4f180b50283283ebd0c043ae"))
+          "1f23d5909c5c604afad62c8d7db5ef09aecd67ab"))
       (channel
         (name 'nonguix)
         (url "https://gitlab.com/nonguix/nonguix")
         (commit
-          "0a5cd133a3097b192702c8b806f2a6f54182118d"))
+          "9e912f2224f7627b77a0bb77af77483c26ca702c"))
       (channel
         (name 'guix)
         (url "https://git.savannah.gnu.org/git/guix.git")
         (commit
-          "2ffd92abf23e3fb3b3bac3c3efd43630ba616f4b")))
+          "862a88612f903ffe5cca1213ba40010876d19c62")))
