e85e7a3499609cf742fd2688163b4a5c4a9ccf72
Oleg Pykhalov <go.wigust@gmail.com>
2021-06-14 18:52:03 +0300

    dotfiles: channels: Update.

--- dotfiles/channels.scm
+++ dotfiles/channels.scm
@@ -1,13 +1,25 @@
-(cons* (channel
-        (name 'guix-wigust)
-        (url "https://github.com/kitnil/guix-wigust"))
-       (channel
+(list (channel
+        (name 'guix)
+        (url "https://git.savannah.gnu.org/git/guix.git")
+        (commit
+          "6a9abdcd5db7621a451af310c9a42a9bbfb228d6")
+        (introduction
+          (make-channel-introduction
+            "9edb3f66fd807b096b48283debdcddccfea34bad"
+            (openpgp-fingerprint
+              "BBB0 2DDF 2CEA F6A8 0D1D  E643 A2A0 6DF2 A33A 54FA"))))
+      (channel
         (name 'nonguix)
         (url "https://gitlab.com/nonguix/nonguix")
-        ;; Enable signature verification:
+        (commit
+          "c9d7f30bcbd3a6e3076e56a972c33963c73c4d58")
         (introduction
-         (make-channel-introduction
-          "897c1a470da759236cc11798f4e0a5f7d4d59fbc"
-          (openpgp-fingerprint
-           "2A39 3FFF 68F4 EF7A 3D29  12AF 6F51 20A0 22FB B2D5"))))
-       %default-channels)
+          (make-channel-introduction
+            "897c1a470da759236cc11798f4e0a5f7d4d59fbc"
+            (openpgp-fingerprint
+              "2A39 3FFF 68F4 EF7A 3D29  12AF 6F51 20A0 22FB B2D5"))))
+      (channel
+        (name 'guix-wigust)
+        (url "https://github.com/kitnil/guix-wigust")
+        (commit
+          "1f23d5909c5c604afad62c8d7db5ef09aecd67ab")))
