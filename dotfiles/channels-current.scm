7c9bd7d8158cb898f794f7db8f922c762365acd6
Oleg Pykhalov <go.wigust@gmail.com>
2021-07-31 19:32:20 +0300

    dotfiles: channels-current: Update.

--- dotfiles/channels-current.scm
+++ dotfiles/channels-current.scm
@@ -1,4 +1,21 @@
 (list (channel
+        (name 'guix)
+        (url "https://git.savannah.gnu.org/git/guix.git")
+        (branch "master")
+        (commit
+          "145e897530d300a923c9a12a0f003a372491d873")
+        (introduction
+          (make-channel-introduction
+            "9edb3f66fd807b096b48283debdcddccfea34bad"
+            (openpgp-fingerprint
+              "BBB0 2DDF 2CEA F6A8 0D1D  E643 A2A0 6DF2 A33A 54FA"))))
+      (channel
+        (name 'guix-wigust)
+        (url "https://github.com/kitnil/guix-wigust")
+        (branch "master")
+        (commit
+          "0e1bd83ee7611694a26115df6fbf684492ed680b"))
+      (channel
         (name 'nonguix)
         (url "https://gitlab.com/nonguix/nonguix")
         (branch "master")
@@ -8,21 +25,4 @@
           (make-channel-introduction
             "897c1a470da759236cc11798f4e0a5f7d4d59fbc"
             (openpgp-fingerprint
-              "2A39 3FFF 68F4 EF7A 3D29  12AF 6F51 20A0 22FB B2D5"))))
-      (channel
-        (name 'guix-wigust)
-        (url "https://github.com/kitnil/guix-wigust")
-        (branch "master")
-        (commit
-          "0e1bd83ee7611694a26115df6fbf684492ed680b"))
-      (channel
-        (name 'guix)
-        (url "https://git.savannah.gnu.org/git/guix.git")
-        (branch "master")
-        (commit
-          "94ece4f8f7c80133d759d302b11351b69919209c")
-        (introduction
-          (make-channel-introduction
-            "9edb3f66fd807b096b48283debdcddccfea34bad"
-            (openpgp-fingerprint
-              "BBB0 2DDF 2CEA F6A8 0D1D  E643 A2A0 6DF2 A33A 54FA")))))
+              "2A39 3FFF 68F4 EF7A 3D29  12AF 6F51 20A0 22FB B2D5")))))
--- dotfiles/manifests/nongnu.scm
+++ dotfiles/manifests/nongnu.scm
@@ -1,3 +1,29 @@
-(use-modules (nongnu packages mozilla))
+(use-modules (guix channels)
+             (guix inferior)
+             (guix profiles)
+             (guix store)
+             (srfi srfi-1))
 
-(packages->manifest (list firefox))
+(define channels
+  (list (channel
+         (name 'guix)
+         (url "https://git.savannah.gnu.org/git/guix.git")
+         (introduction (@@ (guix channels) %guix-channel-introduction))
+         (commit "f7e14782025bf87aaef694a21f34010b1a95f7f6")) ;v1.0.1
+        (channel
+         (name 'nonguix)
+         (url "https://gitlab.com/nonguix/nonguix")
+         (commit "7d824b5780558288881602a7a3cf8b0f56ebc08c"))))
+
+(define cached
+  (with-store store
+    (cached-channel-instance store
+                             channels
+                             #:authenticate? #t
+                             #:cache-directory (%inferior-cache-directory)
+                             #:ttl (* 3600 24 30))))
+
+(define inferior
+  (open-inferior cached))
+
+(packages->manifest (list (first (lookup-inferior-packages inferior "firefox"))))
