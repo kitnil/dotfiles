771d48a8daba107b6a18d67cc79e157daebaeb8a
Oleg Pykhalov <go.wigust@gmail.com>
2021-08-18 00:00:27 +0300

    stumpwm: Move restore-from-file inside display check.

--- dot_stumpwm.d/display-0.lisp
+++ dot_stumpwm.d/display-0.lisp
@@ -18,11 +18,10 @@
 (add-group (current-screen) "0" :background t :type 'float-group)
 (run-commands "gselect 1")
 
-(restore-from-file "/home/oleg/.stumpwm.d/group-1.lisp")
-
 (define-frame-preference "1" (0 NIL T :CLASS "Nightly" :TITLE "YouTube"))
 
 (when (string-equal (screen-display-string (current-screen)) "DISPLAY=:0.0")
+  (restore-from-file "/home/oleg/.stumpwm.d/group-1.lisp")
   (mode-line)
   (setf *mouse-focus-policy* :sloppy))
 
