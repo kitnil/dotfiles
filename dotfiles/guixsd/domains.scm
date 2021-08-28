97e197321dc3b4f4e31de8c089a69cc0a7c46d91
Oleg Pykhalov <go.wigust@gmail.com>
2021-08-28 13:53:02 +0300

    bin: Add dnscheck.

---
+++ dotfiles/guixsd/domains.scm
@@ -0,0 +1,98 @@
+'(("checks" .
+   #((("values" . #("ns.majordomo.ru."
+                    "ns2.majordomo.ru."
+                    "ns3.majordomo.ru."))
+      ("type" . "ns")
+      ("domain" . "mjru.wugi.info"))
+
+     (("type" . "a")
+      ("ip" . "78.108.82.157")
+      ("domain" . "vm1.wugi.info"))
+
+     (("type" . "a")
+      ("ip" . "78.108.82.157")
+      ("domain" . "vm1.wugi.info"))
+
+     (("type" . "a")
+      ("ip" . "78.108.87.161")
+      ("domain" . "vm2.wugi.info"))
+
+     (("type" . "a")
+      ("ip" . "78.108.87.14")
+      ("domain" . "vm3.wugi.info"))
+
+     (("type" . "a")
+      ("ip" . "78.108.82.44")
+      ("domain" . "vm4.wugi.info"))
+
+     (("type" . "a")
+      ("ip" . "178.250.247.231")
+      ("domain" . "vm5.wugi.info"))
+
+     (("type" . "a")
+      ("ip" . "78.108.87.161")
+      ("domain" . "smtp.wugi.info"))
+
+     (("type" . "a")
+      ("ip" . "130.61.95.6")
+      ("domain" . "znc.wugi.info"))
+
+     (("type" . "a")
+      ("ip" . "78.108.87.14")
+      ("domain" . "back.wugi.info"))
+
+     (("value" . "wigust.gitlab.io.")
+      ("type" . "cname")
+      ("domain" . "blog.wugi.info"))
+
+     (("value" . "guix.duckdns.org.")
+      ("type" . "cname")
+      ("domain" . "cuirass.wugi.info"))
+
+     (("value" . "eu.mailgun.org.")
+      ("type" . "cname")
+      ("domain" . "email.mg.wugi.info"))
+
+     (("value" . "guix.duckdns.org.")
+      ("type" . "cname")
+      ("domain" . "git.wugi.info"))
+
+     (("value" . "guix.duckdns.org.")
+      ("type" . "cname")
+      ("domain" . "githunt.wugi.info"))
+
+     (("value" . "add.")
+      ("type" . "cname")
+      ("domain" . "godaddy.wugi.info"))
+
+     (("value" . "guix.duckdns.org.")
+      ("type" . "cname")
+      ("domain" . "guix.wugi.info"))
+
+     (("value" . "guix.duckdns.org.")
+      ("type" . "cname")
+      ("domain" . "homer.wugi.info"))
+
+     (("value" . "guix.duckdns.org.")
+      ("type" . "cname")
+      ("domain" . "nextcloud.wugi.info"))
+
+     (("value" . "pop3.majordomo.ru.")
+      ("type" . "cname")
+      ("domain" . "pop3.test.wugi.info"))
+
+     (("value" . "smtp.majordomo.ru.")
+      ("type" . "cname")
+      ("domain" . "smtp.test.wugi.info"))
+
+     (("value" . "guix.duckdns.org.")
+      ("type" . "cname")
+      ("domain" . "torrent.wugi.info"))
+
+     (("value" . "guix.duckdns.org.")
+      ("type" . "cname")
+      ("domain" . "webssh.wugi.info"))
+
+     (("value" . "vm1.wugi.info.")
+      ("type" . "cname")
+      ("domain" . "webssh.vm1.wugi.info")))))
