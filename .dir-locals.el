ae9a68135ffe4144a4043bb17b00fbbbe2d253c2
Oleg Pykhalov <go.wigust@gmail.com>
2021-08-29 10:35:06 +0300

    .dir-locals: Add guix-wigust to geiser-guile-load-path.

--- .dir-locals.el
+++ .dir-locals.el
@@ -6,4 +6,5 @@
      (projectile-project-install-cmd . "make install")
      (eval . (setq-local geiser-guile-load-path
                          (list (concat (projectile-project-root)
-                                       "dotfiles/guixsd/modules")))))))
+                                       "dotfiles/guixsd/modules")
+                               (concat (getenv "HOME") "/src/guix-wigust/guix")))))))
