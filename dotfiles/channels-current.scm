8ac27383675187c3d18e9193aa9efc4006d99cf7
Oleg Pykhalov <go.wigust@gmail.com>
2021-09-03 01:11:37 +0300

    channels-current.scm: Update.

--- dotfiles/channels-current.scm
+++ dotfiles/channels-current.scm
@@ -3,7 +3,7 @@
         (url "https://git.savannah.gnu.org/git/guix.git")
         (branch "master")
         (commit
-          "a8dd285d5a0670abf124a721e6ba94da045b24ba")
+          "c60daa8e9dfa5f641ecb5f4f3a9135e27b58d2d7")
         (introduction
           (make-channel-introduction
             "9edb3f66fd807b096b48283debdcddccfea34bad"
@@ -20,7 +20,7 @@
         (url "https://gitlab.com/nonguix/nonguix")
         (branch "master")
         (commit
-          "46fb69251c49853f72880e4e8a188884525a0379")
+          "0aac8ec7ec74ac8f9423a4277ec6360505ba658d")
         (introduction
           (make-channel-introduction
             "897c1a470da759236cc11798f4e0a5f7d4d59fbc"
