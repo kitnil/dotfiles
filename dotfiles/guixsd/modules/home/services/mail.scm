a1565c31d03c450eeacd22f6bdfbdd3b9f2e8d3b
Oleg Pykhalov <go.wigust@gmail.com>
2021-10-12 01:41:31 +0300

    home: services: mail: Add config-file.

--- dotfiles/guixsd/modules/home/services/mail.scm
+++ dotfiles/guixsd/modules/home/services/mail.scm
@@ -2,22 +2,37 @@
   #:use-module (gnu home services)
   #:use-module (gnu home services shepherd)
   #:use-module (guix gexp)
+  #:use-module (guix records)
   #:use-module (gnu packages mail)
-  #:export (home-goimapnotify-service-type))
+  #:export (home-goimapnotify-service-type
+            goimapnotify-configuration))
+
+(define-record-type* <goimapnotify-configuration>
+  goimapnotify-configuration make-goimapnotify-configuration
+  goimapnotify-configuration?
+  (goimapnotify  goimapnotify-configuration-goimapnotify       ;<package>
+                 (default go-gitlab.com-shackra-goimapnotify))
+  (config-file   goimapnotify-configuration-config-file        ;<file-like>
+                 (default #f)))
 
 (define (home-goimapnotify-shepherd-service config)
-  (list (shepherd-service
-         (documentation "User goimapnotify.")
-         (provision '(goimapnotify))
-         (start #~(make-forkexec-constructor
-                   (list #$(file-append go-gitlab.com-shackra-goimapnotify "/bin/goimapnotify")
-                         "-conf" #$(local-file "/home/oleg/.config/imapnotify/gmail.conf"))
-                   #:log-file (string-append
-                               (or (getenv "XDG_LOG_HOME")
-                                   (format #f "~a/.local/var/log"
-                                           (getenv "HOME")))
-                               "/goimapnotify/gmail.log")))
-         (stop #~(make-kill-destructor)))))
+  (let ((goimapnotify (goimapnotify-configuration-goimapnotify config))
+        (config-file (goimapnotify-configuration-config-file config)))
+    (list (shepherd-service
+           (documentation "User goimapnotify.")
+           (provision (list (string->symbol (string-append "goimapnotify-"
+                                                           (computed-file-name config-file)))))
+           (start #~(make-forkexec-constructor
+                     (list #$(file-append goimapnotify "/bin/goimapnotify")
+                           "-conf" #$config-file)
+                     #:log-file (string-append
+                                 (or (getenv "XDG_LOG_HOME")
+                                     (format #f "~a/.local/var/log"
+                                             (getenv "HOME")))
+                                 (string-append "/goimapnotify/"
+                                                #$(computed-file-name config-file)
+                                                ".log"))))
+           (stop #~(make-kill-destructor))))))
 
 (define home-goimapnotify-service-type
   (service-type (name 'home-goimapnotify)
