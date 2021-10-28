293ff8801ab362d31cab4c4b0aff306bab659cb5
Oleg Pykhalov <go.wigust@gmail.com>
2021-10-29 01:56:31 +0300

    services: homer: Extend NGINX service.

--- dotfiles/guixsd/modules/services/web.scm
+++ dotfiles/guixsd/modules/services/web.scm
@@ -16,13 +16,17 @@
 ;;; You should have received a copy of the GNU General Public License
 ;;; along with GNU Guix.  If not, see <http://www.gnu.org/licenses/>.
 
-(define-module (services homer)
+(define-module (services web)
   #:use-module (gnu services shepherd)
   #:use-module (gnu services)
+  #:use-module (gnu services web)
   #:use-module (guix gexp)
-  #:use-module (srfi srfi-1)
+  #:use-module (wigust packages web)
+  #:use-module (gnu services configuration)
   #:use-module (json)
-  #:export (homer-service-type))
+  #:export (homer-service-type
+            homer-configuration
+            %homer-nginx-configuration-nginx))
 
 ;;; Commentary:
 ;;;
@@ -30,15 +34,38 @@
 ;;;
 ;;; Code:
 
+
+;;;
+;;; Homer
+;;;
+
+(define %homer-nginx-configuration-nginx
+  (nginx-server-configuration
+   (root (file-append homer "/share/homer"))
+   (listen '("80"))))
+
+(define (file-object? val)
+  (or (file-like? val) (file-name? val)))
+(define (serialize-file-object field-name val)
+  (serialize-string field-name val))
+
+(define (nginx-server-configuration-list? val)
+  (and (list? val) (and-map nginx-server-configuration? val)))
+(define (serialize-nginx-server-configuration-list field-name val)
+  "")
+
+(define-configuration homer-configuration
+  (config-file
+   (file-object "homer.scm")
+   "Path to a file containing configuration in JSON or YAML format.")
+  (nginx
+   (nginx-server-configuration-list
+    (list %homer-nginx-configuration-nginx))
+   "NGINX configuration."))
+
 (define (homer-service-etc config)
   "Return a @file{/etc} entry for an @file{homer/config.yml}."
-  `(("homer/config.yml"
-     ,(computed-file
-       "config.yml"
-       #~(begin
-           (with-output-to-file #$output
-             (lambda ()
-               (display #$(scm->json-string config)))))))))
+  `(("homer/config.yml" ,(homer-configuration-config-file config))))
 
 (define homer-service-type
   ;; The /etc/homer service.
@@ -46,7 +73,9 @@
    (name 'homer)
    (extensions
     (list (service-extension etc-service-type
-                             homer-service-etc)))
+                             homer-service-etc)
+          (service-extension nginx-service-type
+                             homer-configuration-nginx)))
    (default-value '())
    (description
     "Populate the @file{/etc/homer/config.yml} based on the given file object.")))
