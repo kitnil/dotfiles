72326296c010cd97a3a522101b6f14e6d1e00f24
Oleg Pykhalov <go.wigust@gmail.com>
2021-11-13 06:31:32 +0300

    maintenance: gitlab: Add gitlab-project configuration record.

--- dotfiles/maintenance/gitlab/main.scm
+++ dotfiles/maintenance/gitlab/main.scm
@@ -6,10 +6,80 @@
              (gnu packages bash)
              (guix modules)
              (gnu packages guile)
-             (json builder))
+             (json builder)
+             (guix records)
+             (gnu services configuration)
+             (srfi srfi-1))
 
 (define %terraform-state
   (string-append (dirname (current-filename)) "/terraform.tfstate"))
+
+(define (uglify-field-name field-name)
+  "Convert symbol FIELD-NAME to a camel case string.
+@code{symbol-name} => \"@code{symbolName}\"."
+  (let* ((str (string-delete #\? (symbol->string field-name)))
+         (spl-str (string-split str #\-)))
+    (string-join spl-str "_")))
+
+(define (serialize-field field-name val)
+  (cons (uglify-field-name field-name) val))
+
+(define serialize-boolean serialize-field)
+
+(define serialize-string serialize-field)
+
+(define-configuration home-gitlab-project-configuration
+  (name
+   (string (configuration-missing-field 'home-gitlab-project-configuration 'name))
+   "Name of the project.")
+  (default-branch
+   (string "master")
+   "Default branch for the project.")
+  (description
+   (string (configuration-missing-field 'home-gitlab-project-configuration 'description))
+   "Description of the project.")
+  (visibility-level
+   (string "public")
+   "Project access level.")
+  (wiki-enabled?
+   (boolean #f)
+   "Enable wiki for the project.")
+  (snippets-enabled?
+   (boolean #f)
+   "Enable snippets for the project.")
+  (issues-enabled?
+   (boolean #t)
+   "Enable issue tracking for the project.")
+  (container-registry-enabled?
+   (boolean #f)
+   "Enable container registry for the project.")
+  (shared-runners-enabled?
+   (boolean #f)
+   "Enable shared runners for this project.")
+  (request-access-enabled?
+   (boolean #f)
+   "Allow users to request member access.")
+  (pipelines-enabled?
+   (boolean #f)
+   "Enable pipelines for the project."))
+
+(define (serialize-gitlab-project-configuration config fields)
+  "Similar to serialize-configuration from gnu/services/configuration.scm."
+  (plain-file
+   (string-append "gitlab-project-"
+                  (home-gitlab-project-configuration-name config) ".tf.json")
+   (with-output-to-string
+     (lambda ()
+       (scm->json
+        `((resource
+           (gitlab_project
+            (,(home-gitlab-project-configuration-name config)
+             ,@(map (lambda (field)
+                      ((configuration-field-serializer field)
+                       (configuration-field-name field)
+                       ((configuration-field-getter field) config)))
+                    fields)))))
+        #:pretty #t)))))
 
 (define %terraform-configuration
   `((variable
@@ -18,21 +88,15 @@
     (provider
      (gitlab
       (token . "${var.GITLAB_TOKEN}")
-      (base_url . "https://gitlab.com/api/v4/")))
-    (resource
-     (gitlab_project
-      (notes
-       (name . "notes")
-       (default_branch . "master")
-       (description . "My notes")
-       (visibility_level . "public")
-       (wiki_enabled . #false)
-       (snippets_enabled . #false)
-       (issues_enabled . #true)
-       (container_registry_enabled . #false)
-       (shared_runners_enabled . #false)
-       (request_access_enabled . #false)
-       (pipelines_enabled . #true))))))
+      (base_url . "https://gitlab.com/api/v4/")))))
+
+(define %gitlab-projects
+  (list (home-gitlab-project-configuration
+         (name "notes")
+         (description "My notes"))
+        (home-gitlab-project-configuration
+         (name "ansible-majordomo-history")
+         (description "Jenkins pipeline which runs Ansible to fetch various information from servers"))))
 
 (define (terraform-gitlab)
   (mlet* %store-monad ((main.tf.json (text-file* "main.tf.json"
@@ -53,6 +117,12 @@
                                          (mkdtemp! "/tmp/terraform.XXXXXX"))
                                        (chdir instance-dir)
                                        (copy-file #$main.tf.json "main.tf.json")
+                                       (for-each
+                                        (lambda (file)
+                                          (install-file file "."))
+                                        '#$(map (lambda (project)
+                                                  (serialize-gitlab-project-configuration project home-gitlab-project-configuration-fields))
+                                                %gitlab-projects))
                                        (invoke "terraform" "init")
                                        (invoke "terraform" "apply"
                                                #$(string-append
