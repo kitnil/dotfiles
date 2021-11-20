be98e8c4d13baaae916c96e9a7aac340ded9b6c0
Oleg Pykhalov <go.wigust@gmail.com>
2021-11-20 20:35:34 +0300

    home: Add tmux config.

--- dotfiles/guixsd/home.scm
+++ dotfiles/guixsd/home.scm
@@ -641,6 +641,10 @@ exec -a \"$0\" ~a/bin/shellcheck --shell=bash \"$@\"\n"
                    home-files-service-type
                    (list `("screenrc" ,(local-file "../../dot_screenrc"))))
 
+   (simple-service 'tmux-config
+                   home-files-service-type
+                   (list `("tmux.conf" ,(local-file "../../dot_tmux.conf"))))
+
    (simple-service 'parallel-config
                    home-activation-service-type
                    #~(begin
