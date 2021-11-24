77fb54d355d8888a91e4158be49fa339509ad94a
Oleg Pykhalov <go.wigust@gmail.com>
2021-11-25 01:57:33 +0300

    deploy: Add ws2.wugi.info.

--- dotfiles/guixsd/deploy.scm
+++ dotfiles/guixsd/deploy.scm
@@ -6,4 +6,13 @@
                        (system "x86_64-linux")
                        (user "oleg")
                        (host-key "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIJ/LXhRvCkY5f3ZlawDmtnoqtbd9MaNdNIGLMXevbPgR root@(none)")
+                       (identity "/home/oleg/.ssh/id_rsa"))))
+      (machine
+       (operating-system (load "ws2.wugi.info.scm"))
+       (environment managed-host-environment-type)
+       (configuration (machine-ssh-configuration
+                       (host-name "172.16.103.226")
+                       (system "x86_64-linux")
+                       (user "oleg")
+                       (host-key "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIPvDZSe5l2m9FPxvmtkQTYcyXgj13+O43cTe1338xVYa root@gnu")
                        (identity "/home/oleg/.ssh/id_rsa")))))
