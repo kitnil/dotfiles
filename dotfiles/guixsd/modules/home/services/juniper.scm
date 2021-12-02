e32c63de2988d434a612d731f0e49ede0858c033
Oleg Pykhalov <go.wigust@gmail.com>
2021-12-02 23:39:33 +0300

    home: services: Add juniper.

---
+++ dotfiles/guixsd/modules/home/services/juniper.scm
@@ -0,0 +1,81 @@
+(define-module (home services juniper)
+  #:use-module (gnu home services)
+  #:use-module (gnu home services mcron)
+  #:use-module (guix gexp)
+  #:use-module (guix store)
+  #:export (juniper-service-type))
+
+(define %home
+  (and=> (getenv "HOME")
+         (lambda (home)
+           home)))
+
+(define %state-directory
+  (string-append %home "/ansible-out/files"))
+
+(define %connect-program
+  (string-append %home "/.local/bin/connect"))
+
+(define (juniper-show-configuration host)
+  #~(begin
+      (use-modules (ice-9 rdelim)
+                   (ice-9 popen))
+      (let* ((port (open-pipe* OPEN_READ #$%connect-program #$host
+                               "cli" "show" "configuration"))
+             (output (read-string port)))
+        (close-port port)
+        output)))
+
+(define (juniper-configuration->file host)
+  (program-file
+   "juniper-show-configuration-program"
+   (with-imported-modules '((guix build utils))
+     #~(begin
+         (use-modules (guix build utils))
+         (let* ((directory
+                 (string-append #$%state-directory "/" #$host "/config"))
+                (file (string-append directory "/juniper.conf")))
+           (mkdir-p directory)
+           (call-with-output-file (string-append directory "/juniper.conf")
+             (lambda (port)
+               (display #$(juniper-show-configuration host) port))))))))
+
+(define (juniper-configuration->vc host)
+  (program-file
+   "juniper-configuration-to-version-control"
+   (with-imported-modules '((guix build utils))
+     #~(begin
+         (use-modules (guix build utils))
+         (invoke #$(juniper-configuration->file host))
+         (with-directory-excursion #$%state-directory
+           (invoke "git" "add" "--all")
+           (invoke "git" "commit" "--message=Update.")
+           (invoke "git" "push"))))))
+
+(define (juniper-mcron-jobs config)
+  (list
+   #~(job
+      '(next-hour '(20))
+      #$(run-with-store (open-connection)
+          (lower-object
+           (juniper-configuration->vc "br1-mr14.intr"))))
+   #~(job
+      '(next-hour '(21))
+      #$(run-with-store (open-connection)
+          (lower-object
+           (juniper-configuration->vc "sr1-mr13-14.intr"))))
+   #~(job
+      '(next-hour '(22))
+      #$(run-with-store (open-connection)
+          (lower-object
+           (juniper-configuration->vc "sr1-dh507-508.intr"))))))
+
+(define juniper-service-type
+  (service-type
+   (name 'juniper)
+   (extensions
+    (list (service-extension home-mcron-service-type
+                             juniper-mcron-jobs)))
+   (description
+    "Periodically run Juniper configuration dump.")
+   (default-value '())))
