cf39b46c666f505269ccbb27f8f68c17f96c8a25
Oleg Pykhalov <go.wigust@gmail.com>
2021-12-05 23:13:58 +0300

    guixsd: Disable broken vnc services.

--- dotfiles/guixsd/guixsd.scm
+++ dotfiles/guixsd/guixsd.scm
@@ -666,25 +666,25 @@ location / {
 
       (services (append (list
 
-                         (service vncserver-service-type (vncserver-configuration
-                                                          (vncserver tigervnc-server-1.10.1)
-                                                          (display 1)
-                                                          (user "oleg")
-                                                          (group "users")
-                                                          (directory "/home/oleg")
-                                                          (xstartup "/home/oleg/.vnc/xstartup-firefox")
-                                                          (host-name "guixsd")))
+                         ;; (service vncserver-service-type (vncserver-configuration
+                         ;;                                  (vncserver tigervnc-server-1.10.1)
+                         ;;                                  (display 1)
+                         ;;                                  (user "oleg")
+                         ;;                                  (group "users")
+                         ;;                                  (directory "/home/oleg")
+                         ;;                                  (xstartup "/home/oleg/.vnc/xstartup-firefox")
+                         ;;                                  (host-name "guixsd")))
 
-                         (service vncserver-service-type (vncserver-configuration
-                                                          (vncserver tigervnc-server-1.10.1)
-                                                          (display 2)
-                                                          (user "oleg")
-                                                          (group "users")
-                                                          (directory "/home/oleg")
-                                                          (xstartup "/home/oleg/.vnc/xstartup-stumpwm")
-                                                          (host-name "guixsd")
-                                                          (supplementary-groups
-                                                           '("docker" "kvm" "libvirt" "audio" "video" "wheel" "users"))))
+                         ;; (service vncserver-service-type (vncserver-configuration
+                         ;;                                  (vncserver tigervnc-server-1.10.1)
+                         ;;                                  (display 2)
+                         ;;                                  (user "oleg")
+                         ;;                                  (group "users")
+                         ;;                                  (directory "/home/oleg")
+                         ;;                                  (xstartup "/home/oleg/.vnc/xstartup-stumpwm")
+                         ;;                                  (host-name "guixsd")
+                         ;;                                  (supplementary-groups
+                         ;;                                   '("docker" "kvm" "libvirt" "audio" "video" "wheel" "users"))))
 
                          ;; (service vncserver-service-type (vncserver-configuration
                          ;;                                  (vncserver tigervnc-server-1.10.1)
