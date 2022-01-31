2d4705875951685b3db2c17350d07e9aed8ff95c
Oleg Pykhalov <go.wigust@gmail.com>
2022-01-31 16:45:37 +0300

    home: services: Add h3c.

---
+++ dotfiles/guixsd/modules/home/services/h3c.scm
@@ -0,0 +1,58 @@
+(define-module (home services h3c)
+  #:use-module (gnu home services)
+  #:use-module (gnu home services mcron)
+  #:use-module (guix gexp)
+  #:use-module (guix store)
+  #:use-module (home config)
+  #:use-module (guile pass)
+  #:export (h3c-configuration->vc-sw4-mr14.intr))
+
+(define (h3c-command host command)
+  #~(begin
+      (use-modules (ice-9 rdelim)
+                   (ice-9 popen))
+      (let* ((port (open-pipe* OPEN_READ "sshpass"
+                               #$(string-append "-p" (pass "show" "majordomo/private/h3c/oleg"))
+                               "ssh" #$host "--" #$@command))
+             (output (read-string port)))
+        (close-port port)
+        output)))
+
+(define* (h3c-configuration->vc host #:optional ssh?)
+  (program-file
+   (string-append "h3c-configuration-to-version-control-" host)
+   (with-imported-modules '((guix build utils))
+     #~(begin
+         (use-modules (guix build utils)
+                      (guix utils))
+         (let* ((directory (string-append #$%ansible-state-directory "/" #$host "/config"))
+                (file (string-append directory "/h3c.conf")))
+           (mkdir-p directory)
+           (call-with-output-file (string-append directory "/current-configuration.txt")
+             (lambda (port)
+               (display #$(h3c-command host '("show" "current-configuration")) port)))
+           (call-with-output-file (string-append directory "/interface.txt")
+             (lambda (port)
+               (display #$(h3c-command host '("show" "interface")) port))))
+         (with-directory-excursion #$%ansible-state-directory
+           (invoke "git" "add" "--all")
+           (invoke "git" "commit" "--message=Update."))))))
+
+(define h3c-configuration->vc-sw4-mr14.intr
+  (h3c-configuration->vc "sw4-mr14.intr"))
+
+(define (h3c-mcron-jobs config)
+  (list
+   #~(job
+      '(next-hour '(21))
+      #$(h3c-configuration->vc-sw4-mr14.intr))))
+
+(define h3c-service-type
+  (service-type
+   (name 'h3c)
+   (extensions
+    (list (service-extension home-mcron-service-type
+                             h3c-mcron-jobs)))
+   (description
+    "Periodically run h3c configuration dump.")
+   (default-value '())))
