f4b00663c0b3fc1cfae1a30c631f221e3c38d0be
Oleg Pykhalov <go.wigust@gmail.com>
2022-02-12 07:52:58 +0300

    services: Add vault.

--- dotfiles/guixsd/modules/services/web.scm
+++ dotfiles/guixsd/modules/services/web.scm
@@ -1,5 +1,5 @@
 ;;; GNU Guix --- Functional package management for GNU
-;;; Copyright © 2020, 2021 Oleg Pykhalov <go.wigust@gmail.com>
+;;; Copyright © 2020, 2021, 2022 Oleg Pykhalov <go.wigust@gmail.com>
 ;;;
 ;;; This file is part of GNU Guix.
 ;;;
@@ -17,16 +17,22 @@
 ;;; along with GNU Guix.  If not, see <http://www.gnu.org/licenses/>.
 
 (define-module (services web)
-  #:use-module (gnu services shepherd)
   #:use-module (gnu services)
-  #:use-module (gnu services web)
-  #:use-module (guix gexp)
-  #:use-module (wigust packages web)
   #:use-module (gnu services configuration)
+  #:use-module (gnu services shepherd)
+  #:use-module (gnu services web)
+  #:use-module (gnu system shadow)
+  #:use-module (guix gexp)
+  #:use-module (guix records)
   #:use-module (json)
+  #:use-module (packages password-utils)
+  #:use-module (wigust packages web)
   #:export (homer-service-type
             homer-configuration
-            %homer-nginx-configuration-nginx))
+            %homer-nginx-configuration-nginx
+
+            vault-configuration
+            vault-service-type))
 
 ;;; Commentary:
 ;;;
@@ -80,4 +86,82 @@
    (description
     "Populate the @file{/etc/homer/config.yml} based on the given file object.")))
 
-;;; homer.scm ends here
+
+;;;
+;;; Vault
+;;;
+
+(define-record-type* <vault-configuration>
+  vault-configuration make-vault-configuration
+  vault-configuration?
+  (vault       vault-configuration-vault ;<package>
+               (default vault))
+  (user        vault-configuration-user  ;string
+               (default "vault"))
+  (group       vault-configuration-group ;string
+               (default "vault"))
+  (config-file vault-configuration-config-file ;<file-like>
+               (default #f)))
+
+(define (vault-account configuration)
+  ;; Return the user accounts and user groups for CONFIG.
+  (let ((vault-user (vault-configuration-user configuration))
+        (vault-group (vault-configuration-group configuration)))
+    (list (user-group
+           (name vault-user)
+           (system? #t))
+          (user-account
+           (name vault-user)
+           (group vault-group)
+           (system? #t)
+           (comment "vault privilege separation user")
+           (home-directory "/var/lib/vault")))))
+
+(define (vault-activation config)
+  "Return the activation GEXP for CONFIG."
+  (with-imported-modules '((guix build utils))
+    #~(begin
+        (use-modules (guix build utils))
+        (let* ((user (getpw #$(vault-configuration-user config)))
+               (group (getpw #$(vault-configuration-group config)))
+               (home (passwd:dir user))
+               (data (string-append home "/data")))
+          (mkdir-p data)
+          (chown data
+                 (passwd:uid user)
+                 (group:gid group))))))
+
+(define (vault-shepherd-service config)
+  (list
+   (shepherd-service
+    (provision '(vault))
+    (documentation "Run vault.")
+    (requirement '())
+    (start #~(make-forkexec-constructor
+              (list (string-append #$(vault-configuration-vault config)
+                                   "/bin/vault")
+                    "server"
+                    "-config" #$(vault-configuration-config-file config))
+              #:directory "/var/lib/vault"
+              #:user #$(vault-configuration-user config)
+              #:group #$(vault-configuration-group config)
+              #:log-file "/var/log/vault.log"))
+    (respawn? #f)
+    (stop #~(make-kill-destructor)))))
+
+(define vault-service-type
+  (service-type
+   (name 'vault)
+   (extensions
+    (list
+     (service-extension shepherd-root-service-type
+                        vault-shepherd-service)
+     (service-extension account-service-type
+                        vault-account)
+     (service-extension activation-service-type
+                        vault-activation)))
+   (default-value (vault-configuration))
+   (description
+    "Run the vault.")))
+
+;;; web.scm ends here
