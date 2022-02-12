3b8d26dd2bc73b2808d5908ee92175006f8cf6b8
Oleg Pykhalov <go.wigust@gmail.com>
2022-02-12 07:31:31 +0300

    packages: Add vault.
    * dotfiles/guixsd/modules/packages/password-utils.scm (vault): New variable.

---
+++ dotfiles/guixsd/modules/packages/password-utils.scm
@@ -0,0 +1,65 @@
+;;; GNU Guix --- Functional package management for GNU
+;;; Copyright © Oleg Pykhalov <go.wigust@gmail.com>
+;;;
+;;; This file is part of GNU Guix.
+;;;
+;;; GNU Guix is free software; you can redistribute it and/or modify it
+;;; under the terms of the GNU General Public License as published by
+;;; the Free Software Foundation; either version 3 of the License, or (at
+;;; your option) any later version.
+;;;
+;;; GNU Guix is distributed in the hope that it will be useful, but
+;;; WITHOUT ANY WARRANTY; without even the implied warranty of
+;;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;;; GNU General Public License for more details.
+;;;
+;;; You should have received a copy of the GNU General Public License
+;;; along with GNU Guix.  If not, see <http://www.gnu.org/licenses/>.
+
+(define-module (packages password-utils)
+  #:use-module ((guix licenses) #:prefix license:)
+  #:use-module (gnu packages autotools)
+  #:use-module (gnu packages bash)
+  #:use-module (gnu packages gnupg)
+  #:use-module (gnu packages guile)
+  #:use-module (gnu packages package-management)
+  #:use-module (gnu packages pkg-config)
+  #:use-module (gnu packages python)
+  #:use-module (gnu packages python-xyz)
+  #:use-module (gnu packages ssh)
+  #:use-module (gnu packages tls)
+  #:use-module (guix build utils)
+  #:use-module (guix build-system gnu)
+  #:use-module (guix build-system python)
+  #:use-module (guix build-system trivial)
+  #:use-module (guix gexp)
+  #:use-module (guix download)
+  #:use-module (guix packages)
+  #:use-module (ice-9 popen)
+  #:use-module (ice-9 rdelim))
+
+(define-public vault
+  (package
+    (name "vault")
+    (version "1.9.3")
+    (source (origin
+              (method url-fetch/zipbomb)
+              (uri (string-append "https://releases.hashicorp.com/vault/"
+                                  version "/vault_" version "_linux_amd64.zip"))
+              (sha256
+               (base32
+                "0fc6a8d5jm17sk7ppqs8adhw5adbcvm218vbzq02ipxlbwj9y18n"))))
+    (build-system trivial-build-system)
+    (arguments
+     `(#:modules ((guix build utils))
+       #:builder
+       (begin
+         (use-modules (guix build utils))
+         (mkdir-p (string-append %output "/bin"))
+         (install-file (string-append (assoc-ref %build-inputs "source")
+                                      "/vault")
+                       (string-append %output "/bin")))))
+    (home-page "https://www.vaultproject.io/")
+    (synopsis "A tool for managing secrets, this binary includes the UI")
+    (description "A tool for managing secrets, this binary includes the UI")
+    (license license:mpl2.0)))
