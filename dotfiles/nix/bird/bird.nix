85d656e2dfdb8e21b09445a3865a0b9a5c6f436e
Oleg Pykhalov <go.wigust@gmail.com>
2022-03-11 15:26:49 +0300

    nix: Add bird container.

---
+++ dotfiles/nix/bird/bird.nix
@@ -0,0 +1,21 @@
+{ pkgs }:
+
+pkgs.dockerTools.buildLayeredImage rec {
+  name = "oracle1.local:5000/bird";
+  tag = "latest";
+  contents = with pkgs; [ bashInteractive bird2 ];
+  config = {
+    Entrypoint = [ "${pkgs.bird2}/sbin/bird" "-c" ./bird.conf "-d" ];
+    Env = [
+      "TZ=Europe/Moscow"
+      "LC_ALL=en_US.UTF-8"
+    ];
+  };
+  extraCommands = ''
+    set -x -e
+    mkdir -p tmp
+    chmod 777 tmp
+    mkdir -p var/run
+    chmod 755 var/run
+  '';
+}
---
+++ dotfiles/nix/bird/flake.nix
@@ -0,0 +1,21 @@
+#!/usr/bin/env -S bash -c "nix-shell --run 'deploy . -- -L'"
+
+{
+  description = "Nix package manifest";
+  inputs = {
+    nixpkgs.url = "nixpkgs/nixpkgs-unstable";
+  };
+
+  outputs = { self, nixpkgs}:
+    let
+      system = "x86_64-linux";
+      pkgs = import nixpkgs { inherit system; };
+    in {
+      devShell.${system} = with pkgs; mkShell {
+        buildInputs = [ nixFlakes ];
+      };
+      packages.${system} = {
+        container-bird = pkgs.callPackage ./bird.nix {};
+      };
+    };
+}
