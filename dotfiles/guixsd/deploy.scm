0d3965330aaf1cbe7128e800679b5030a010c76c
Oleg Pykhalov <go.wigust@gmail.com>
2022-04-05 00:01:11 +0300

    deploy: Comment ws2.wugi.info.

--- dotfiles/guixsd/deploy.scm
+++ dotfiles/guixsd/deploy.scm
@@ -1,4 +1,13 @@
-(list (machine
+(list ;; (machine
+      ;;  (operating-system (load "ws2.wugi.info.scm"))
+      ;;  (environment managed-host-environment-type)
+      ;;  (configuration (machine-ssh-configuration
+      ;;                  (host-name "172.16.103.226")
+      ;;                  (system "x86_64-linux")
+      ;;                  (user "oleg")
+      ;;                  (host-key "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIPvDZSe5l2m9FPxvmtkQTYcyXgj13+O43cTe1338xVYa root@gnu")
+      ;;                  (identity "/home/oleg/.ssh/id_rsa"))))
+      (machine
        (operating-system (load "vm1.wugi.info.scm"))
        (environment managed-host-environment-type)
        (configuration (machine-ssh-configuration
@@ -6,13 +15,4 @@
                        (system "x86_64-linux")
                        (user "oleg")
                        (host-key "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIJ/LXhRvCkY5f3ZlawDmtnoqtbd9MaNdNIGLMXevbPgR root@(none)")
-                       (identity "/home/oleg/.ssh/id_rsa"))))
-      (machine
-       (operating-system (load "ws2.wugi.info.scm"))
-       (environment managed-host-environment-type)
-       (configuration (machine-ssh-configuration
-                       (host-name "172.16.103.226")
-                       (system "x86_64-linux")
-                       (user "oleg")
-                       (host-key "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIPvDZSe5l2m9FPxvmtkQTYcyXgj13+O43cTe1338xVYa root@gnu")
                        (identity "/home/oleg/.ssh/id_rsa")))))
