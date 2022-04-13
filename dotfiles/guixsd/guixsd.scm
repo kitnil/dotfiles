8e50508ac1717add01cec2781d1d92ff91d615b7
Oleg Pykhalov <go.wigust@gmail.com>
2022-04-13 03:53:54 +0300

    guixsd: Add irc.local to hosts.

--- dotfiles/guixsd/guixsd.scm
+++ dotfiles/guixsd/guixsd.scm
@@ -783,7 +783,7 @@ location / {
            "172.16.100.61 lyashenko.intr"
            "127.0.0.1 example.com"
 
-           "192.168.25.2 oracle1.local"
+           "192.168.25.2 oracle1.local irc.local"
 
            "78.108.86.20 r1"
            "78.108.87.99 r2"
