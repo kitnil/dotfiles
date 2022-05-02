cc2ff5b54249ab32bc9c3918ab8b359b068609b6
Oleg Pykhalov <go.wigust@gmail.com>
2022-05-02 16:31:59 +0300

    stumpwm: swank: Bind only to 127.0.0.1.

--- dot_stumpwm.d/swank.lisp
+++ dot_stumpwm.d/swank.lisp
@@ -5,5 +5,7 @@
 (defcommand swank (port) ((:string "Port number: "))
   (sb-thread:make-thread
    (lambda ()
-     (swank:create-server :port (parse-integer port) :dont-close t))
+     (let ((swank::*loopback-interface* "127.0.0.1"))
+       (swank:create-server :port (parse-integer port)
+                            :dont-close t)))
    :name "swank"))
