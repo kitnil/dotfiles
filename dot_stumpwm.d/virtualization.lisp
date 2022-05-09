4070001736fdb2d4e52c6189e6a7321598de8041
Oleg Pykhalov <go.wigust@gmail.com>
2022-05-09 10:46:45 +0300

    stumpwm: Move vm.lisp to virtualization.lisp.

--- dot_stumpwm.d/virtualization.lisp
+++ dot_stumpwm.d/virtualization.lisp
@@ -8,3 +8,20 @@
   "Start of focus looking-glass-client-wrapper."
   (run-or-raise "looking-glass-client-wrapper -F"
                 '(:class "looking-glass-client")))
+
+(defcommand qemu-debian () ()
+  "Run GNOME Debian in QEMU."
+  (run-shell-command (concat "exec " (getenv "HOME") "/bin/debian.sh")))
+
+(defcommand debian-unstable () ()
+  (run-shell-command
+   (join (list "qemu-system-x86_64" "-daemonize" "-smp" "cores=4,threads=1"
+               "-m" "4096" "-enable-kvm" "-cpu" "host" "-daemonize"
+               "-vnc" ":5" "-hda" "~/vm/debian-unstable.qcow2"
+               "-cdrom" "~/Downloads/debian-testing-amd64-netinst.iso"))))
+
+(defcommand epson () ()
+  (term-shell-command "sudo qemu-epson.sh"))
+
+(defcommand epson-no-graphic () ()
+  (term-shell-command "sudo qemu-epson.sh -display none"))
