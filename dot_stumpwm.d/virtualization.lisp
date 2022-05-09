57f237062f34a60028ba8e1f42db8593ac95bd49
Oleg Pykhalov <go.wigust@gmail.com>
2022-05-09 09:39:28 +0300

    stumpwm: Add looking-glass-client command.

--- dot_stumpwm.d/virtualization.lisp
+++ dot_stumpwm.d/virtualization.lisp
@@ -3,3 +3,8 @@
 (defcommand virt-manager () ()
   "Start or focus virt-manager."
   (run-or-raise "virt-manager" '(:class ".virt-manager-real")))
+
+(defcommand looking-glass-client () ()
+  "Start of focus looking-glass-client-wrapper."
+  (run-or-raise "looking-glass-client-wrapper -F"
+                '(:class "looking-glass-client")))
