8848bc7d5c76cc35a41bd8a48649e45bdb6b4d5f
Oleg Pykhalov <go.wigust@gmail.com>
2022-06-06 14:26:28 +0300

    guixsd: Add smartmontools package.

--- dotfiles/guixsd/guixsd.scm
+++ dotfiles/guixsd/guixsd.scm
@@ -711,7 +711,7 @@ location / {
       ;; (initrd-modules (append '("vfio_pci" "vfio" "vfio_iommu_type1" "vfio_virqfd")
       ;;                         %base-initrd-modules))
 
-      (packages (append (list dn42-ca ovmf cifs-utils btrfs-progs lvm2)
+      (packages (append (list dn42-ca ovmf cifs-utils btrfs-progs lvm2 smartmontools)
                         %my-system-packages))
 
       (groups (cons* (user-group (name "nixbld")
