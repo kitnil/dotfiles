3c1b4323cb8961f45e505c94fcc6f37924346cc9
Oleg Pykhalov <go.wigust@gmail.com>
2022-07-21 03:42:09 +0300

    emacs: Fix hooks.el.

--- private_dot_emacs.d/modules/hooks.el
+++ private_dot_emacs.d/modules/hooks.el
@@ -1,10 +1,6 @@
 (when (functionp #'add-hooks)
   (add-hooks
-   '(((diff-mode-hook dired-mode-hook proced-post-display-hook
-                      shell-mode-hook ibuffer-mode-hook guix-env-var-mode-hook)
-      . guix-prettify-mode)
-     ((dired-mode-hook) . toggle-truncate-lines)
-     ((scheme-mode-hook) . guix-devel-mode)
+   '(((dired-mode-hook) . toggle-truncate-lines)
      ((guix-devel-mode magit-revision-mode-hook) . goto-address-mode)
      ((geiser-repl-mode-hook shell-mode-hook)
       . (lambda ()
@@ -15,17 +11,26 @@
      ;;  . (lambda ()
      ;;      (font-lock-mode -1)))
 
-     ((prog-mode-hook
-       minibuffer-inactive-mode-hook
-       geiser-repl-mode-hook
-       git-commit-mode-hook
-       org-mode-hook)
-      . smartparens-strict-mode)
-     ((prog-mode-hook) . hs-minor-mode)
      ((erc-mode-hook) . (lambda () (setq truncate-lines t))))))
 
-(when (and (functionp #'add-hooks) (functionp 'yas-minor-mode))
-  (add-hooks '(((prog-mode-hook org-mode-hook text-mode-hook) . yas-minor-mode))))
-
-(when (and (functionp #'add-hooks) (boundp 'rainbow-delimiters-mode))
-  (add-hooks '(((prog-mode-hook geiser-repl-mode-hook) . rainbow-delimiters-mode))))
+(when (functionp #'add-hooks)
+  (when (functionp 'guix-prettify-mode)
+    (add-hooks
+     '(((diff-mode-hook dired-mode-hook proced-post-display-hook
+                        shell-mode-hook ibuffer-mode-hook guix-env-var-mode-hook)
+        . guix-prettify-mode))))
+  (when (functionp 'yas-minor-mode)
+    (add-hooks '(((prog-mode-hook org-mode-hook text-mode-hook) . yas-minor-mode))))
+  (when (boundp 'rainbow-delimiters-mode)
+    (add-hooks '(((prog-mode-hook geiser-repl-mode-hook) . rainbow-delimiters-mode))))
+  (when (functionp 'smartparens-strict-mode)
+    (add-hooks '(((prog-mode-hook
+                   minibuffer-inactive-mode-hook
+                   geiser-repl-mode-hook
+                   git-commit-mode-hook
+                   org-mode-hook)
+                  . smartparens-strict-mode))))
+  (when (functionp 'hs-minor-mode)
+    (add-hooks '(((prog-mode-hook) . hs-minor-mode))))
+  (when (functionp 'guix-devel-mode)
+    (add-hooks '(((scheme-mode-hook) . guix-devel-mode)))))
