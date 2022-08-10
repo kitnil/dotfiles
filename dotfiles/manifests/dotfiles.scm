44e89421a2ea8988ac75a56a1b2079a426506949
Oleg Pykhalov <go.wigust@gmail.com>
2022-08-10 13:08:50 +0300

    manifests: dotfiles: Add AMQP and Kubernetes packages.

--- dotfiles/manifests/dotfiles.scm
+++ dotfiles/manifests/dotfiles.scm
@@ -1,3 +1,16 @@
-(use-modules (packages networking))
+(use-modules (packages containers)
+             (packages kubernetes)
+             (packages networking))
 
-(packages->manifest (list cisco))
+(packages->manifest
+ (list cisco
+
+       plumber
+
+       k3d
+       k9s
+       kompose
+       kubectl
+       kubernetes-helm
+       nerdctl
+       virtctl))
