bd332a814713c871498fc856744acc225ab8bb74
Oleg Pykhalov <go.wigust@gmail.com>
2022-08-14 06:05:48 +0300

    stumpwm: Check if disk is in standby status.

--- dot_stumpwm.d/disk.lisp
+++ dot_stumpwm.d/disk.lisp
@@ -1,17 +1,29 @@
 (in-package :stumpwm)
 
-(defun disk-free (target &key remote)
+(defun disk-free (target &key remote (standby ""))
   (let ((size (fourth (split-string (second (split-string (run-shell-command (if remote
                                                                                  (format nil "ssh ~a -- df -h ~a" remote target)
                                                                                  (format nil "df -h ~a" target))
                                                                              t)
                                                           '(#\newline)))
-                                    '(#\space)))))
-    (cond ((uiop/utility:string-suffix-p size "G")
-           (format nil "~a GB" (delete #\G size)))
-          ((uiop/utility:string-suffix-p size "M")
-           (format nil "~a MB" (delete #\M size)))
-          (t size))))
+                                    '(#\space))))
+        (standby
+          (if (string= standby "")
+              nil
+              (string= (car
+                        (last (split-string
+                               (string-trim
+                                '(#\Newline)
+                                (run-shell-command (format nil "sudo hdparm -C ~a" standby) t))
+                               '(#\space))))
+                       "standby"))))
+    (if standby
+        "standby"
+        (cond ((uiop/utility:string-suffix-p size "G")
+               (format nil "~a GB" (delete #\G size)))
+              ((uiop/utility:string-suffix-p size "M")
+               (format nil "~a MB" (delete #\M size)))
+              (t size)))))
 
 (defvar *disk-free-root-counter* 0)
 
@@ -21,4 +33,5 @@
 (defvar *disk-free-srv-counter* 0)
 
 (defcommand disk-free-srv-update-counter () ()
-  (setq *disk-free-srv-counter* (disk-free "/srv")))
+  (setq *disk-free-srv-counter*
+        (disk-free "/srv" :standby "/dev/disk/by-label/data18")))
