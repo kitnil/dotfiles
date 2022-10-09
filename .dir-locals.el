41e7ca2e63e2ac8b0afcdc75d5a72bfed255ad5e
Oleg Pykhalov <go.wigust@gmail.com>
2022-10-09 08:52:35 +0300

    .dir-locals: Set GUIX_PACKAGE_PATH.
    This commit sets GUIX_PACKAGE_PATH environment variable.

--- .dir-locals.el
+++ .dir-locals.el
@@ -4,10 +4,15 @@
   . ((projectile-project-configure-cmd . "./configure")
      (projectile-project-test-cmd . "make check")
      (projectile-project-install-cmd . "make install")
-     (eval . (setq-local geiser-guile-load-path
-                         (append (list (concat (getenv "HOME") "/src/cgit.duckdns.org/guix/guix-wigust/guix"))
-                                 (list (concat (getenv "HOME") "/src/gitlab.com/nonguix/nonguix"))
-                                 (if (boundp #'projectile-project-root)
-                                     (list (concat (projectile-project-root)
-                                                   "dotfiles/guixsd/modules"))
-                                   '())))))))
+     (eval . (progn
+               (when (boundp #'projectile-project-root)
+                     (setenv "GUIX_PACKAGE_PATH"
+                             (concat (projectile-project-root)
+                                     "dotfiles/guixsd/modules")))
+               (setq-local geiser-guile-load-path
+                           (append (list (concat (getenv "HOME") "/src/cgit.duckdns.org/guix/guix-wigust/guix"))
+                                   (list (concat (getenv "HOME") "/src/gitlab.com/nonguix/nonguix"))
+                                   (if (boundp #'projectile-project-root)
+                                       (list (concat (projectile-project-root)
+                                                     "dotfiles/guixsd/modules"))
+                                     '()))))))))
