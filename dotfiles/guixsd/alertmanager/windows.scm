7cf28e968d6b2f93da0416ab88b3057ff3fdd6b2
Oleg Pykhalov <go.wigust@gmail.com>
2022-11-06 15:55:36 +0300

    alertmanager: windows: Add alerts for file system space.

--- dotfiles/guixsd/alertmanager/windows.scm
+++ dotfiles/guixsd/alertmanager/windows.scm
@@ -9,5 +9,17 @@
             .
             "Windows exporter collector {{ $labels.collector }} failed (instance {{ $labels.instance }})"))
           ("alert" . "WindowsCollectorFail"))
-         ))
+         (("labels" ("severity" . "warning"))
+          ("for" . "1h")
+          ("expr"
+           .
+           "windows_logical_disk_free_bytes / windows_logical_disk_size_bytes * 100 < 5")
+          ("annotations"
+           ("summary"
+            .
+            "Filesystem has less than 5% space left.")
+           ("description"
+            .
+            "Filesystem on {{ $labels.volume }} at {{ $labels.instance }} has only {{ printf \"%.2f\" $value }}% available space left."))
+          ("alert" . "WindowsFilesystemAlmostOutOfSpace"))))
       ("name" . "windows-exporter")))))
