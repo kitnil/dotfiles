8864453703f89a0697f51843fae31bb292c5d92e
Oleg Pykhalov <go.wigust@gmail.com>
2022-11-07 11:19:14 +0300

    guixsd: Change 192.168.25.3 to vm1.corp.

--- dotfiles/guixsd/guixsd.scm
+++ dotfiles/guixsd/guixsd.scm
@@ -906,7 +906,7 @@ location / {
                   ""
                   (format #f "~a oracle.ygg" ip)))
            "192.168.25.2 windows.home"
-           "192.168.25.3 vm1.ovpn"
+           "192.168.25.3 vm1.corp"
 
            "172.16.100.65 zdetovetskiy.intr"
            "172.16.100.61 lyashenko.intr"
