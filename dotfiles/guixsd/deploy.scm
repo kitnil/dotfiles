cf72334ede277b9e6c7312801e4cc2e1e42e04d3
Oleg Pykhalov <go.wigust@gmail.com>
2022-11-19 23:34:17 +0300

    Add vm2.wugi.info.

--- dotfiles/guixsd/deploy.scm
+++ dotfiles/guixsd/deploy.scm
@@ -15,4 +15,13 @@
                        (system "x86_64-linux")
                        (user "oleg")
                        (host-key "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIJ/LXhRvCkY5f3ZlawDmtnoqtbd9MaNdNIGLMXevbPgR root@(none)")
+                       (identity "/home/oleg/.ssh/id_rsa"))))
+      (machine
+       (operating-system (load "vm2.wugi.info.scm"))
+       (environment managed-host-environment-type)
+       (configuration (machine-ssh-configuration
+                       (host-name "78.108.92.69")
+                       (system "x86_64-linux")
+                       (user "oleg")
+                       (host-key "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAICxmYxMhEv54Bou6xpSv67i+OGTwMI5yxySo1cIjeptZ root@(none)")
                        (identity "/home/oleg/.ssh/id_rsa")))))
---
+++ dotfiles/guixsd/vm2.wugi.info.scm
@@ -0,0 +1,60 @@
+;; This is an operating system configuration template
+;; for a "bare bones" setup, with no X11 display server.
+
+(use-modules (gnu))
+(use-service-modules networking ssh)
+(use-package-modules certs screen ssh)
+
+(use-modules (config))
+
+(operating-system
+  (host-name "vm2.wugi.info")
+  (timezone "Europe/Moscow")
+  (locale "en_US.utf8")
+
+  ;; Boot in "legacy" BIOS mode, assuming /dev/sdX is the
+  ;; target hard disk, and "my-root" is the label of the target
+  ;; root file system.
+  (bootloader (bootloader-configuration
+                (bootloader grub-bootloader)
+                (target "/dev/vda")))
+  (file-systems (cons (file-system
+                        (device (file-system-label "guix-root"))
+                        (mount-point "/")
+                        (type "ext4"))
+                      %base-file-systems))
+
+  ;; This is where user accounts are specified.  The "root"
+  ;; account is implicit, and is initially created with the
+  ;; empty password.
+  (users (cons (user-account
+                (name "oleg")
+                (comment "Oleg Pykhalov")
+                (group "users")
+
+                ;; Adding the account to the "wheel" group
+                ;; makes it a sudoer.  Adding it to "audio"
+                ;; and "video" allows the user to play sound
+                ;; and access the webcam.
+                (supplementary-groups '("wheel"
+                                        "audio" "video")))
+               %base-user-accounts))
+
+  ;; Globally-installed packages.
+  (packages (cons* nss-certs screen %base-packages))
+
+  ;; Add services to the baseline.
+  (services (append (list (static-networking-service "eth0" "78.108.92.69"
+                                                     #:netmask "255.255.254.0"
+                                                     #:gateway "78.108.93.254"
+                                                     #:name-servers '("127.0.0.1"
+                                                                      "8.8.8.8"
+                                                                      "8.8.4.4"))
+                          (service openssh-service-type))
+                    (modify-services %base-services
+                      (guix-service-type config => %guix-daemon-config-with-substitute-urls))))
+
+  (sudoers-file (plain-file "sudoers" "\
+root ALL=(ALL) ALL
+%wheel ALL=(ALL) ALL
+oleg ALL=(ALL) NOPASSWD:ALL\n")))
