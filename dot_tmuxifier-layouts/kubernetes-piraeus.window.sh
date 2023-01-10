98d3a1c551983ead107d5c74308f8d21bdfefd89
Oleg Pykhalov <go.wigust@gmail.com>
2023-01-10 08:51:31 +0300

    tmuxifier: Run "kubectl watch piraeus" in piraeus window.

--- dot_tmuxifier-layouts/kubernetes-piraeus.window.sh
+++ dot_tmuxifier-layouts/kubernetes-piraeus.window.sh
@@ -6,11 +6,14 @@
 # layout file name.
 new_window "piraeus"
 
+run_cmd "kubectl watch piraeus"
+
 mapfile -t pod_names < <(kubectl get --no-headers=true -n piraeus -o custom-columns='NAME:metadata.name,NAME:spec.nodeName' pods | awk '/piraeus-op-ns-node/ && (/kube1/ || /kube2/) { print $1 }')
 
+split_v 50
 run_cmd "kubectl -n piraeus exec -it ${pod_names[0]} -- /bin/bash"
-split_h 50
 
+split_h 50
 run_cmd "kubectl -n piraeus exec -it ${pod_names[1]} -- /bin/bash"
 
 
