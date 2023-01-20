4863b457bb0f225017079c0caef530f2e81a64d4
Oleg Pykhalov <go.wigust@gmail.com>
2023-01-20 03:38:26 +0300

    home: guixsd: Install kubelet to bin.

--- dotfiles/guixsd/modules/home/services/shell.scm
+++ dotfiles/guixsd/modules/home/services/shell.scm
@@ -169,6 +169,7 @@
                           "my-docker"
                           "my-swank"
                           "my-xorg"
+                          "kubelet"
                           "nginx-server-name"
                           "nixos.sh"
                           "nix-repl"
