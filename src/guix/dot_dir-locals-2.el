b1b4d38e9cdf79fadf5c9886d5e0840a19520f66
Oleg Pykhalov <go.wigust@gmail.com>
2023-03-04 12:22:05 +0300

    src: guix: Configure projectile-project-compilation-cmd.

--- src/guix/dot_dir-locals-2.el
+++ src/guix/dot_dir-locals-2.el
@@ -1,4 +1,5 @@
 ((nil
   . ((magit-todos-depth . nil)
      (magit-todos-exclude-globs . ("*.go" "*.info" "*.po"))
-     (magit-todos-update . nil))))
+     (magit-todos-update . nil)
+     (projectile-project-compilation-cmd . ".direnv/aliases/shell -- .direnv/aliases/build"))))
