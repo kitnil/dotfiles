4148bc2b3f05cba06bf7b06b7d4da9fa37194b99
Oleg Pykhalov <go.wigust@gmail.com>
2023-06-09 02:43:12 +0300

    tmuxifier: Do not exec to pods in piraeus namespace.

--- dot_tmuxifier-layouts/kubernetes-piraeus.window.sh
+++ dot_tmuxifier-layouts/kubernetes-piraeus.window.sh
@@ -11,9 +11,9 @@ run_cmd "kubectl watch piraeus"
 mapfile -t pod_names < <(kubectl get --no-headers=true -n piraeus -o custom-columns='NAME:metadata.name,NAME:spec.nodeName' pods | awk '/piraeus-op-ns-node/ && (/kube1/ || /kube2/) { print $1 }')
 
 split_v 50
-run_cmd "kubectl -n piraeus exec -it ${pod_names[0]} -- /bin/bash"
+run_cmd "echo kubectl -n piraeus exec -it ${pod_names[0]} -- /bin/bash"
 
 split_h 50
-run_cmd "kubectl -n piraeus exec -it ${pod_names[1]} -- /bin/bash"
+run_cmd "echo kubectl -n piraeus exec -it ${pod_names[1]} -- /bin/bash"
 
 
