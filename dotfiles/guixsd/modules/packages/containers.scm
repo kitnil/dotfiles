8a5ded5556f221f68c2de663e7ed45248099278b
Oleg Pykhalov <go.wigust@gmail.com>
2023-06-18 14:42:21 +0300

    packages: Add crictl.

--- dotfiles/guixsd/modules/packages/containers.scm
+++ dotfiles/guixsd/modules/packages/containers.scm
@@ -61,3 +61,51 @@
     (synopsis "Docker-compatible CLI for containerd")
     (description "nerdctl is a Docker-compatible CLI for containerd.")
     (license license:asl2.0)))
+
+(define-public crictl
+  (package
+    (name "crictl")
+    (version "1.26.0")
+    (source (origin
+              (method url-fetch)
+              (uri (string-append "https://github.com/kubernetes-sigs/cri-tools/releases/download/v"
+                                  version "/crictl-v"
+                                  version "-linux-amd64.tar.gz"))
+              (sha256
+               (base32
+                "1vaf2g22zmp3zqjxdb9zfkl06pjnwc7vmzqhh5a6p7zi7caf59fd"))))
+    (build-system trivial-build-system)
+    (native-inputs (list source gzip tar))
+    (arguments
+     (list
+      #:modules '((guix build utils))
+      #:builder
+      #~(begin
+          (use-modules (guix build utils)
+                       (ice-9 popen)
+                       (ice-9 rdelim))
+          (let ((bin (string-append #$output "/bin")))
+            (mkdir-p (string-append #$output "/bin"))
+            (setenv "PATH" (string-append
+                            #$(this-package-native-input "tar") "/bin" ":"
+                            #$(this-package-native-input "gzip") "/bin"))
+            (invoke "tar"
+                    "-xf" (assoc-ref %build-inputs "source")
+                    "-C" bin
+                    "crictl")
+            ;; Install shell completion.
+            (let ((bash (string-append #$output "/etc/bash_completion.d")))
+              (mkdir-p bash)
+              (call-with-output-file (string-append bash "/crictl")
+                (lambda (port)
+                  (display
+                   (let* ((port (open-pipe* OPEN_READ (string-append #$output "/bin/crictl")
+                                            "completion" "bash"))
+                          (output (read-string port)))
+                     (close-port port)
+                     (string-trim-right output #\newline))
+                   port))))))))
+    (home-page "https://github.com/kubernetes-sigs/cri-tools")
+    (synopsis "")
+    (description "")
+    (license #f)))
