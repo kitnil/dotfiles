ef510603227123ec272f3a8471bdf15723f333b8
Oleg Pykhalov <go.wigust@gmail.com>
2023-06-18 15:59:25 +0300

    kubelet.scm

---
+++ kubelet.scm
@@ -0,0 +1,44 @@
+(use-modules (gnu packages base)
+             (gnu packages bash)
+             (gnu packages compression)
+             (gnu packages docker)
+             (gnu packages linux)
+             (gnu packages package-management)
+             (guix build utils)
+             (guix gexp)
+             (guix store)
+             (packages kubernetes))
+
+(define coredns-image
+  (local-file "/nix/store/1crdy15nv25jpbvknrzyhg6khv9ikhl9-docker-image-coredns-coredns-1.7.1.tar"))
+
+(define pause-image
+  (local-file "/nix/store/xjlwhyqjhx0j2sc41wfpsw1zvhn98vh5-docker-image-pause.tar.gz"))
+
+(run-with-store (open-connection)
+  (gexp->derivation
+   "kubelet"
+   (with-imported-modules '((guix build utils))
+     #~(begin
+         (use-modules (guix build utils))
+         (let* ((bin (string-append #$output "/bin"))
+                (kubelet.sh (string-append bin "/kubelet.sh")))
+           (mkdir-p bin)
+           (copy-file #$(local-file "dot_local/bin/executable_kubelet")
+                      kubelet.sh)
+           (chmod kubelet.sh #o555)
+           (setenv "PATH" (string-append #$(file-append tar "/bin")
+                                         ":" #$(file-append gzip "/bin")
+                                         ":" #$(file-append bash "/bin")))
+           (patch-shebang kubelet.sh)
+           (wrap-program kubelet.sh
+             '("KUBELET_COREDNS_IMAGE" "" = (#$coredns-image))
+             '("KUBELET_PAUSE_IMAGE" "" = (#$pause-image))
+             '("PATH" ":" = (#$(file-append containerd "/bin")
+                             #$(file-append coreutils "/bin")
+                             #$(file-append grep "/bin")
+                             #$(file-append gzip "/bin")
+                             #$(file-append kmod "/bin")
+                             #$(file-append kubernetes-binary "/bin")
+                             #$(file-append util-linux "/bin")))))))))
+
