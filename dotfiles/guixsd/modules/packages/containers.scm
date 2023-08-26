2f1f122ce4f6ffaf685f954161deab18bb4fb1e6
Oleg Pykhalov <go.wigust@gmail.com>
2023-08-26 14:48:27 +0300

    packages: Add diffoci.
    * dotfiles/guixsd/modules/packages/containers.scm (diffoci): New variable.

--- dotfiles/guixsd/modules/packages/containers.scm
+++ dotfiles/guixsd/modules/packages/containers.scm
@@ -7,7 +7,8 @@
   #:use-module (guix build-system trivial)
   #:use-module (guix utils)
   #:use-module (gnu packages base)
-  #:use-module (gnu packages compression))
+  #:use-module (gnu packages compression)
+  #:use-module (nonguix build-system binary))
 
 (define-public nerdctl
   (package
@@ -109,3 +110,47 @@
     (synopsis "")
     (description "")
     (license #f)))
+
+(define-public diffoci
+  (package
+    (name "diffoci")
+    (version "0.1.1")
+    (source (origin
+              (method url-fetch)
+              (uri (string-append "https://github.com/reproducible-containers/diffoci/releases/download/v"
+                                  version "/diffoci-v" version ".linux-amd64"))
+              (sha256
+               (base32
+                "1f0jr0jigk4kj1a1jhxwzjls82f483z7376l1z3czbn949361nkg"))))
+    (build-system binary-build-system)
+    (arguments
+     (list
+      #:phases
+      #~(modify-phases %standard-phases
+          (add-after 'install 'move-binary-file-and-cleanup
+            (lambda _
+              (delete-file (string-append #$output "/environment-variables"))
+              (mkdir-p (string-append #$output "/bin"))
+              (rename-file (string-append #$output "/diffoci-v" #$version ".linux-amd64")
+                           (string-append #$output "/bin/diffoci"))
+              (chmod (string-append #$output "/bin/diffoci") #o555)))
+          (add-after 'move-binary-file-and-cleanup 'install-shell-completions
+            (lambda _
+              (use-modules (ice-9 popen)
+                           (ice-9 rdelim))
+              (let ((bash (string-append #$output "/etc/bash_completion.d")))
+                (mkdir-p bash)
+                (call-with-output-file (string-append bash "/diffoci")
+                  (lambda (port)
+                    (display
+                     (let* ((port (open-pipe* OPEN_READ (string-append #$output "/bin/diffoci")
+                                              "completion" "bash"))
+                            (output (read-string port)))
+                       (close-port port)
+                       (string-trim-right output #\newline))
+                     port)))))))))
+    (home-page "https://github.com/reproducible-containers/diffoci")
+    (synopsis "Show diff for Docker and OCI container images")
+    (description "This package provides a program to show diff for Docker and
+OCI container images")
+    (license license:asl2.0)))
