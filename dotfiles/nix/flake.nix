c425896582bd8f934e991aac3fc5aa7585792c20
Oleg Pykhalov <go.wigust@gmail.com>
2023-09-24 05:07:41 +0300

    nix: Replace 7tv and twitch-points-autoclicker with betterttv.

--- dotfiles/nix/flake.nix
+++ dotfiles/nix/flake.nix
@@ -361,40 +361,6 @@
                           ./patches/viddy-add-maxhistory-argument.patch
                         ];
                       });
-                      "twitch-points-autoclicker" = buildFirefoxXpiAddon rec {
-                        pname = "twitch-points-autoclicker";
-                        version = "1.6.1";
-                        addonId = "{3c9b993f-29b9-44c2-a913-def7b93a70b1}";
-                        url = "https://addons.mozilla.org/firefox/downloads/file/3708581/twitch_points_autoclicker-${version}.xpi";
-                        sha256 = "1bpm11h6xia7flp608581l1mrnpy35gw9g8pfgakl2pfyanccbvd";
-                        meta = with lib; {
-                          homepage = "https://xinitrc.ca/autoclicker-permissions/";
-                          description = "Automatically claims 'Channel Points' on Twitch";
-                          license = licenses.mit;
-                          mozPermissions = [
-                            "*://*.twitch.tv/*"
-	                          "tabs"
-	                          "storage"
-	                          "notifications"
-                            "webNavigation"
-                          ];
-                          platforms = platforms.all;
-                        };
-                      };
-                      "7tv" = buildFirefoxXpiAddon rec {
-                        pname = "7tv";
-                        version = "2.2.4";
-                        addonId = "{7ef0f00c-2ebe-4626-8ed7-3185847fcfad}";
-                        url = "https://iso.wugi.info/7tv-2.2.4.xpi";
-                        sha256 = "1c6p8ig2gfawik5bhb1ng4jn24235w88d5pas5a8rsx14n6qpwvp";
-                        meta = with lib; {
-                          homepage = "https://extension.7tv.gg/";
-                          description = "7TV is an emote service and extension for Twitch and YouTube";
-                          license = licenses.mit;
-                          mozPermissions = [ "*://*.twitch.tv/*" "storage" ];
-                          platforms = platforms.all;
-                        };
-                      };
                     }
                   );
                 pkgs = import nixpkgs {
@@ -405,6 +371,10 @@
                   ];
                   inherit system;
                   config = {
+                    allowUnfreePredicate = pkg:
+                      builtins.elem (lib.getName pkg) [
+                        "betterttv"
+                      ];
                     permittedInsecurePackages = [
                       "qtwebkit-5.212.0-alpha4"
                     ];
