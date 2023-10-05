30e00f933ffcc13a134e3cefcf95f3bfe3db3e1d
Oleg Pykhalov <go.wigust@gmail.com>
2023-10-05 21:39:43 +0300

    emacs: java: Install lombok.

--- private_dot_emacs.d/modules/java.el
+++ private_dot_emacs.d/modules/java.el
@@ -12,3 +12,28 @@
       . (lambda ()
           (set (make-local-variable 'prettify-symbols-alist)
                wi-java--prettify-symbols-alist))))))
+
+;; Copied and modified from:
+;; https://github.com/emacs-lsp/lsp-java/issues/26
+;;
+;; Java Configuration
+(let ((directory
+       ;; TODO: Do not hardcode, e.g. use `lsp-java-server-install-dir'.
+       (concat (file-name-directory "/home/oleg/.emacs.d/.cache/lsp/eclipse.jdt.ls/")
+               "lombok")))
+
+  (setq lombok-library-path (concat (file-name-directory directory)
+                                    "lombok.jar"))
+
+  (unless (file-exists-p lombok-library-path)
+    (url-copy-file "https://projectlombok.org/downloads/lombok.jar"
+                   lombok-library-path))
+
+  ;; TODO: do not hardcode, e.g. use `lsp-java-vmargs'.
+  (setq lsp-java-vmargs
+        (append (list (concat "-javaagent:"
+                              (expand-file-name lombok-library-path)))
+                '("-noverify"
+		  "-Xmx1G"
+		  "-XX:+UseG1GC"
+		  "-XX:+UseStringDeduplication"))))
