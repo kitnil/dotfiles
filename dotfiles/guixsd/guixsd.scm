bd3717c1a218be2d474c4aced82726a6df1e302f
Oleg Pykhalov <go.wigust@gmail.com>
2023-11-20 03:17:56 +0300

    guix: Do not load amdgpu and snd_hda_intel with modprobe.

--- dotfiles/guixsd/guixsd.scm
+++ dotfiles/guixsd/guixsd.scm
@@ -2235,9 +2235,7 @@ localhost ecdsa-sha2-nistp256 AAAAE2VjZHNhLXNoYTItbmlzdHAyNTYAAAAIbmlzdHAyNTYAAA
 127.0.0.1 ecdsa-sha2-nistp256 AAAAE2VjZHNhLXNoYTItbmlzdHAyNTYAAAAIbmlzdHAyNTYAAABBBOnaDeOzwmrcrq1D8slYaeFozXZ0cpqNU0EvGmgnO29aiKkSD1ehbIV4vSxk3IDXz9ClMVPc1bTUTrYhEVHdCks="))))
 
                          (service kernel-module-loader-service-type
-                                  '("snd_hda_intel"
-                                    "amdgpu" ;GPU passthrough
-                                    "vfio-pci"
+                                  '("vfio-pci"
                                     "vendor-reset" ;reset NAVI10 (5500XT)
                                     "dm-snapshot"
                                     "dm-thin-pool"
