d40e80c5695e115b64cb0b3af11de045598c9d56
Oleg Pykhalov <go.wigust@gmail.com>
2023-11-22 17:34:54 +0300

    channels-current: Update.

--- dotfiles/channels-current.scm
+++ dotfiles/channels-current.scm
@@ -3,7 +3,7 @@
         (url "https://git.savannah.gnu.org/git/guix.git")
         (branch "master")
         (commit
-          "fc6bdaad57bf91609849623c5f485403c030cb49")
+          "857f5017507cfe6cbf742bfb7d33c1520f8c6d15")
         (introduction
           (make-channel-introduction
             "9edb3f66fd807b096b48283debdcddccfea34bad"
@@ -16,19 +16,19 @@
         (commit
           "1cb2b18e4b7e83c396dcdde75ed6b532440d627a"))
       (channel
-       (name 'prometheus-shepherd-exporter)
-       (url "https://gitlab.com/wigust/prometheus-shepherd-exporter")
-       (branch "master")
-       (commit
-         "542ec52c4955c854e770f615148ced99de5e9fec"))
-      (channel
         (name 'nonguix)
         (url "https://gitlab.com/nonguix/nonguix")
         (branch "master")
         (commit
-          "16e9c0d69b7e786f3c86ab5f6f7a20e1f28603ce")
+          "639b70063b73cb9ff553b77d51d42c07eb208d91")
         (introduction
           (make-channel-introduction
             "897c1a470da759236cc11798f4e0a5f7d4d59fbc"
             (openpgp-fingerprint
-              "2A39 3FFF 68F4 EF7A 3D29  12AF 6F51 20A0 22FB B2D5")))))
+              "2A39 3FFF 68F4 EF7A 3D29  12AF 6F51 20A0 22FB B2D5"))))
+      (channel
+        (name 'prometheus-shepherd-exporter)
+        (url "https://gitlab.com/wigust/prometheus-shepherd-exporter")
+        (branch "master")
+        (commit
+          "542ec52c4955c854e770f615148ced99de5e9fec")))
