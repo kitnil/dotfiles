fcc077f7a74777fe9e11a8f9a9c5f7455fa78b6c
Oleg Pykhalov <go.wigust@gmail.com>
2023-11-24 13:02:05 +0300

    guix: Return to linux-5.13 kernel.
    Also reconfigured with previous 'guix pull' generation.

---
+++ dotfiles/channels-current-guixsd.scm
@@ -0,0 +1,34 @@
+(list (channel
+        (name 'guix)
+        (url "https://git.savannah.gnu.org/git/guix.git")
+        (branch "master")
+        (commit
+          "95a1a819dffa833bdd7a47f272da7ee4ff3260c8")
+        (introduction
+          (make-channel-introduction
+            "9edb3f66fd807b096b48283debdcddccfea34bad"
+            (openpgp-fingerprint
+              "BBB0 2DDF 2CEA F6A8 0D1D  E643 A2A0 6DF2 A33A 54FA"))))
+      (channel
+        (name 'guix-wigust)
+        (url "https://github.com/kitnil/guix-wigust")
+        (branch "master")
+        (commit
+          "1cb2b18e4b7e83c396dcdde75ed6b532440d627a"))
+      (channel
+       (name 'prometheus-shepherd-exporter)
+       (url "https://gitlab.com/wigust/prometheus-shepherd-exporter")
+       (branch "master")
+       (commit
+         "542ec52c4955c854e770f615148ced99de5e9fec"))
+      (channel
+        (name 'nonguix)
+        (url "https://gitlab.com/nonguix/nonguix")
+        (branch "master")
+        (commit
+          "639c25bb691129607b4aa5bd87ad13f9c3d2667c")
+        (introduction
+          (make-channel-introduction
+            "897c1a470da759236cc11798f4e0a5f7d4d59fbc"
+            (openpgp-fingerprint
+              "2A39 3FFF 68F4 EF7A 3D29  12AF 6F51 20A0 22FB B2D5")))))
