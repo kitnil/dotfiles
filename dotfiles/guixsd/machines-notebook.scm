41c8d541141679fc60fc85c6a86d50cd7c155b43
Oleg Pykhalov <go.wigust@gmail.com>
2024-01-21 02:03:09 +0300

    notebook: Add machines.scm file.

---
+++ dotfiles/guixsd/machines-notebook.scm
@@ -0,0 +1,8 @@
+(list
+ (build-machine
+  (name "guix.wugi.info")
+  (systems '("x86_64-linux"))
+  (host-key "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIMRy+enQECs2CsjrbSIfnNHExcUwzHFa7KUnhwDIeWOV root@(none)")
+  (user "oleg")
+  (private-key "/home/oleg/.ssh/id_rsa")
+  (speed 2.0)))
