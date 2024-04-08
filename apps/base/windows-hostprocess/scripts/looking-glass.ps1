b99909026d6f867c959e3e325b1e0a76d8c5c589
Oleg Pykhalov <go.wigust@gmail.com>
2024-04-08 13:23:55 +0300

    apps: base: windows-hostprocess: scripts: looking-glass: Use samba.

--- apps/base/windows-hostprocess/scripts/looking-glass.ps1
+++ apps/base/windows-hostprocess/scripts/looking-glass.ps1
@@ -3,7 +3,7 @@ $filePath = "C:\Program Files\Looking Glass (host)\looking-glass-host.exe"
 if (Test-Path $filePath) {
     Write-Host "looking-glass-host: '$filePath' file already exists."
 } else {
-    $url="https://looking-glass.io/artifact/B5/host"
+    $url="\\samba.home\public\looking-glass-host-B6.zip"
     (New-Object System.Net.WebClient).DownloadFile($url, "$env:TEMP\looking-glass.zip")
     Expand-Archive -Path $env:TEMP\looking-glass.zip -DestinationPath $env:TEMP\looking-glass -Force
     rm "$env:TEMP\looking-glass.zip"
