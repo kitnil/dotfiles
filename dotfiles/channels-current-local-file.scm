e24b690ec434fe79711833cb973a73c594a7f270
Oleg Pykhalov <go.wigust@gmail.com>
2024-06-02 15:47:28 +0300

    Update dotfiles/channels-current-local-file.scm.

--- dotfiles/channels-current-local-file.scm
+++ dotfiles/channels-current-local-file.scm
@@ -3,7 +3,7 @@
         (url "https://cgit.wugi.info/git/guix/guix")
         (branch "master")
         (commit
-          "aad9b6ca254c1589e53834726182889e6fa4dbd7")
+          "32bd53cdb28cf35310f9067d4450e0113071a900")
         (introduction
           (make-channel-introduction
             "9edb3f66fd807b096b48283debdcddccfea34bad"
