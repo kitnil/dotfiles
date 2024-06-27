13c656a6def6ead331d062e10d0e19752f158af7
Oleg Pykhalov <go.wigust@gmail.com>
2024-06-27 17:59:30 +0300

    emacs: yaml: Add prettify-symbols-alist.

--- private_dot_emacs.d/modules/yaml.el
+++ private_dot_emacs.d/modules/yaml.el
@@ -2,3 +2,11 @@
           '(lambda () (set (make-local-variable 'yas-indent-line) 'fixed)))
 
 (add-hook 'yaml-mode-hook 'yaml-pro-mode)
+
+
+(defun wi-yaml-prettify-symbols ()
+  (set (make-local-variable 'prettify-symbols-alist)
+       `(("xn--c1ajpcpgs.xn--90a1af.xn--p1ai" . ,(string-to-symbols "хостинг.спб.рф"))
+         ("xn--e1arhcfp.xn--90a1af.xn--p1ai" . ,(string-to-symbols "хостер.спб.рф")))))
+
+(add-hook 'yaml-mode-hook 'wi-yaml-prettify-symbols)
