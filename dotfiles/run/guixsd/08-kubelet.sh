6f4863cc7cd3f6e7631b1e7241aced0fbadbc9d8
Oleg Pykhalov <go.wigust@gmail.com>
2024-08-12 14:36:45 +0300

    run: Add 08-kubelet.sh.

---
+++ dotfiles/run/guixsd/08-kubelet.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/env bash
+
+set -o nounset -o errexit -o pipefail -o xtrace
+
+sudo mv /run/setuid-programs/mount.nfs /run/setuid-programs/mount.nfs.1
+
+sudo herd restart kubelet
+
+mapfile -t pods < <(kubectl get -o name --namespace=openebs pods)
+for pod in "${pods[@]}"
+do
+    kubectl wait --namespace=openebs --for=jsonpath='{.status.phase}'=Running --timeout=3600s "$pod"
+done
+
+sudo mv /run/setuid-programs/mount.nfs.1 /run/setuid-programs/mount.nfs
