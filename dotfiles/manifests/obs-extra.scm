ac2519ba6b1da4934c30f17814e5cf884f57d459
Oleg Pykhalov <go.wigust@gmail.com>
2024-08-17 15:30:37 +0300

    manifests: obs-extra: Delete duplicate.

--- dotfiles/manifests/obs-extra.scm
+++ dotfiles/manifests/obs-extra.scm
@@ -3,14 +3,6 @@
 
 (packages->manifest
  (list
-  obs-exporter
-  obs-pipewire-audio-capture
-  obs-source-record
-  obs-with-cef
-  obs-wlrobs))
-
-(packages->manifest
- (list
   obs-advanced-masks
   obs-composite-blur
   obs-gradient-source
