a4b7637153286b0ab659b5aae550f04eecc66a5d
Oleg Pykhalov <go.wigust@gmail.com>
2024-08-20 03:34:17 +0300

    run: guixsd: 07-byedpi.sh: Route only specific ipset.

--- dotfiles/run/guixsd/07-byedpi.sh
+++ dotfiles/run/guixsd/07-byedpi.sh
@@ -5,4 +5,7 @@ set -o nounset -o errexit -o pipefail -o xtrace
 sudo iptables -t nat -N REDSOCKS
 sudo iptables -t nat -A REDSOCKS -d 192.168.0.0/24 -j RETURN
 sudo iptables -t nat -A REDSOCKS -p tcp -j REDIRECT --to-ports 1082
-sudo iptables -t nat -A PREROUTING -i br0 -p tcp -j REDSOCKS
+
+sudo ipset -exist create byedpi hash:ip hashsize 1024 maxelem 655360
+sudo iptables -t nat -A OUTPUT -p tcp -m set --match-set byedpi dst -j REDSOCKS
+sudo iptables -t nat -A PREROUTING -p tcp -m set --match-set byedpi dst -j REDSOCKS
