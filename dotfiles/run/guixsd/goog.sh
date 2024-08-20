a0039b3117ed14611ca5d8ee040719a7e8c5201b
Oleg Pykhalov <go.wigust@gmail.com>
2024-08-20 20:32:11 +0300

    run: guixsd: Add goog.json URL.

--- dotfiles/run/guixsd/goog.sh
+++ dotfiles/run/guixsd/goog.sh
@@ -2,6 +2,7 @@
 
 set -o nounset -o errexit -o pipefail -o xtrace
 
+# https://www.gstatic.com/ipranges/goog.json
 mapfile -t networks < <(jq --raw-output '.prefixes[] | select(.ipv4Prefix) | .ipv4Prefix' /home/oleg/.local/share/chezmoi/dotfiles/run/guixsd/goog.json)
 
 for network in "${networks[@]}"
