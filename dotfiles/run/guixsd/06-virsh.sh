dd3f7d936641a5dc4da938bd7a9d2afa9fb13462
Oleg Pykhalov <go.wigust@gmail.com>
2024-08-23 02:23:04 +0300

    run: guixsd: 06-virsh.sh: Show how to reattach device.

--- dotfiles/run/guixsd/06-virsh.sh
+++ dotfiles/run/guixsd/06-virsh.sh
@@ -5,3 +5,13 @@ set -o nounset -o errexit -o pipefail -o xtrace
 sudo virsh nodedev-detach pci_0000_12_00_0
 sudo virsh nodedev-detach pci_0000_12_00_1
 sudo virsh start win10
+
+cat <<'EOF'
+It's possible to reattach devices, e.g.:
+
+root@guixsd ~# virsh nodedev-reattach pci_0000_12_00_0
+Device pci_0000_12_00_0 re-attached
+
+root@guixsd ~# virsh nodedev-reattach pci_0000_12_00_1
+Device pci_0000_12_00_1 re-attached
+EOF
