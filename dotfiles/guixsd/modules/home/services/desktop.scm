96e6b0c05f9511e7ed2da8307c847cdf2da0f26d
Oleg Pykhalov <go.wigust@gmail.com>
2024-08-26 18:16:34 +0300

    home: services: Add sway service.

--- dotfiles/guixsd/modules/home/services/desktop.scm
+++ dotfiles/guixsd/modules/home/services/desktop.scm
@@ -4,8 +4,10 @@
   #:use-module (guix gexp)
   #:use-module (guix records)
   #:use-module (gnu packages haskell-apps)
+  #:use-module (gnu packages wm)
   #:export (home-greenclip-service-type
-            greenclip-configuration))
+            greenclip-configuration
+            sway-service))
 
 (define-record-type* <greenclip-configuration>
   greenclip-configuration make-greenclip-configuration
@@ -39,3 +41,50 @@
                 (default-value (greenclip-configuration))
                 (description
                  "Run greenclip clipboard manager daemon.")))
+
+(define sway-program
+  (program-file
+   "sway-wrapper"
+   (with-imported-modules '((guix build utils))
+     #~(begin
+         (define (wait-for-seatd)
+           ;; Wait until someone's listening on udevd's control
+           ;; socket.
+           (let ((sock (socket AF_UNIX SOCK_SEQPACKET 0)))
+             (let try ()
+               (catch 'system-error
+                 (lambda ()
+                   (connect sock PF_UNIX "/run/seatd.sock")
+                   (close-port sock))
+                 (lambda args
+                   (format #t "waiting for seatd...~%")
+                   (usleep 500000)
+                   (try))))))
+         ;; Wait until seatd is up and running.
+         (wait-for-seatd)
+
+         (setenv "DESKTOP_SESSION"
+                 "sway")
+         (setenv "XDG_CURRENT_DESKTOP"
+                 "sway")
+         (setenv "XDG_SESSION_DESKTOP"
+                 "sway")
+         (setenv "XDG_SESSION_TYPE"
+                 "wayland")
+         (setenv "WLR_BACKENDS"
+                 "headless,libinput")
+
+         (execl #$(file-append sway "/bin/sway")
+                "sway")))))
+
+(define sway-service
+  (simple-service 'sway shepherd-root-service-type
+                  (list
+                   (shepherd-service
+                    (provision '(sway))
+                    (documentation "Run sway.")
+                    (requirement '())
+                    (start #~(make-forkexec-constructor
+                              (list #$sway-program)))
+                    (respawn? #f)
+                    (stop #~(make-kill-destructor))))))
