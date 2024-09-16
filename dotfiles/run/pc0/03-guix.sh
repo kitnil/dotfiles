6f15d30fa2b0ffb98ef6add095ebedbaea509049
Oleg Pykhalov <go.wigust@gmail.com>
2024-09-17 00:20:17 +0300

    run: pc0: Delete unused scripts.

--- dotfiles/run/pc0/03-guix.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env bash
-
-set -o nounset -o errexit -o pipefail -o xtrace
-
-sudo mount -t nfs 192.168.0.145:/srv/lib/video /srv/lib/video
-sshfs 192.168.0.145:/srv/obs /srv/obs
-sshfs 192.168.0.145:/srv/video /srv/video
--- dotfiles/run/pc0/05-gw.sh
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env bash
-
-set -o nounset -o errexit -o pipefail -o xtrace
-
-sudo ip route replace default via 192.168.0.145
