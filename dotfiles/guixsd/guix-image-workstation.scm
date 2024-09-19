fd7cd2349ac5720c9b574edb285a9e2f2b551725
Oleg Pykhalov <go.wigust@gmail.com>
2024-09-19 16:56:11 +0300

    Add guix-image-workstation.

---
+++ dotfiles/guixsd/guix-image-workstation.scm
@@ -0,0 +1,51 @@
+;; This is an operating system configuration template for a "Docker image"
+;; setup, so it has barely any services at all.
+
+;; guix system image -t docker --network ./docker-image-workstation.scm
+;; skopeo copy docker-archive:/gnu/store/…-tor-docker-pack.tar.gz docker-daemon:example.org:5000/tor:latest --insecure-policy
+;; docker run --network=host --security-opt seccomp=unconfined --detach --name tor --network=host example.org:5000/tor
+;; docker exec --detach tor /gnu/store/…-tor-0.4.6.10/bin/tor -f /gnu/store/…-torrc
+
+(use-modules (gnu))
+
+(operating-system
+  (host-name "workstation")
+  (timezone "Europe/Moscow")
+  (locale "en_US.utf8")
+
+  ;; This is where user accounts are specified.  The "root" account is
+  ;; implicit, and is initially created with the empty password.
+  (users (cons (user-account
+                (name "oleg")
+                (comment "Oleg Pykhalov")
+                (group "users")
+                (supplementary-groups '("wheel"
+                                        "audio" "video")))
+               %base-user-accounts))
+
+  ;; Because the system will run in a Docker container, we may omit many
+  ;; things that would normally be required in an operating system
+  ;; configuration file.  These things include:
+  ;;
+  ;;   * bootloader
+  ;;   * file-systems
+  ;;   * services such as mingetty, udevd, slim, networking, dhcp
+  ;;
+  ;; Either these things are simply not required, or Docker provides
+  ;; similar services for us.
+
+  ;; This will be ignored.
+  (bootloader (bootloader-configuration
+               (bootloader grub-bootloader)
+               (targets '("does-not-matter"))))
+  ;; This will be ignored, too.
+  (file-systems (list (file-system
+                        (device "does-not-matter")
+                        (mount-point "/")
+                        (type "does-not-matter"))))
+
+  ;; Globally-installed packages.
+  (packages %base-packages)
+
+  (services (list (service guix-service-type)
+                  (syslog-service))))
