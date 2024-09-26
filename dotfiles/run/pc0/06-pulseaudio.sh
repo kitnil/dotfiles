d2204520439ec13c6c220d742ca6145f3f2a4629
Oleg Pykhalov <go.wigust@gmail.com>
2024-09-26 20:37:38 +0300

    run: pc0: 06-pulseaudio: Add ladspa.

--- dotfiles/run/pc0/06-pulseaudio.sh
+++ dotfiles/run/pc0/06-pulseaudio.sh
@@ -3,3 +3,4 @@
 set -o nounset -o errexit -o pipefail -o xtrace
 
 pactl load-module module-native-protocol-tcp
+pactl load-module module-ladspa-sink sink_name=compressor-stereo plugin=sc4_1882 label=sc4 control=1,1.5,401,-30,20,5,30
