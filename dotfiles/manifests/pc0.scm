a591deb53d6db249891e52f6891850398bf0dd6f
Oleg Pykhalov <go.wigust@gmail.com>
2024-09-28 19:04:35 +0300

    Revert "manifests: pc0: Add obs-extra."
    This reverts commit 0881da06f194e6505e14c1f91a317abad7a73986.

--- dotfiles/manifests/pc0.scm
+++ dotfiles/manifests/pc0.scm
@@ -17,7 +17,6 @@
                 "dotfiles.scm"
 	        "emacs.scm"
                 "obs.scm"
-                "obs-extra.scm"
                 "deprecated.scm"
                 "telegram.scm"))
          (list manifest-wm)))
