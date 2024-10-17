a1526d0d68b2a046411caa7fad361bc9aeddc013
Oleg Pykhalov <go.wigust@gmail.com>
2024-10-17 10:09:43 +0300

    run: guix-workstation: Add 04-net.sh.

---
+++ dotfiles/run/guix-workstation/04-net.sh
@@ -0,0 +1,7 @@
+#!/usr/bin/env bash
+
+set -o nounset -o errexit -o pipefail -o xtrace
+
+gateway="$(ip -json route | jq --raw-output '.[] | select(."dst" == "default") | .gateway')"
+
+sudo ip route add 192.168.0.192/32 via "$gateway"
