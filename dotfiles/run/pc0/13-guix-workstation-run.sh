77117c56c4a8207885e106c71a3b335e7fb0b928
Oleg Pykhalov <go.wigust@gmail.com>
2024-10-21 22:30:09 +0300

    run: pc0: 13-guix-workstation-run: Wait until container is up.

--- dotfiles/run/pc0/13-guix-workstation-run.sh
+++ dotfiles/run/pc0/13-guix-workstation-run.sh
@@ -5,7 +5,17 @@ set -o nounset -o errexit -o pipefail
 PATH="/home/oleg/.guix-profile/bin:/gnu/store/3q2x34wg1fff833wwzxnagnv7vbfxb0w-jc-1.25.2/bin:$PATH"
 export PATH
 
-container_id="$(nerdctl -n k8s.io ps --format=json | jq --raw-output '. | select (.Image | startswith("harbor.home.wugi.info/library/guix-image-workstation")) | .ID')"
+guix_workstation_id()
+{
+    nerdctl -n k8s.io ps --format=json \
+        | jq --exit-status --raw-output '. | select (.Image | startswith("harbor.home.wugi.info/library/guix-image-workstation")) | .ID'
+}
+
+until guix_workstation_id
+do
+    sleep 2
+done
+container_id="$(guix_workstation_id)"
 
 /home/oleg/.local/share/chezmoi/dotfiles/run/pc0/10-avahi-namespace.sh
 /home/oleg/.local/share/chezmoi/dotfiles/run/pc0/09-firefox-twitch-namespace.sh
