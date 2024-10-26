372e5292512306c8839a9fa8d63c7c385a26295a
Oleg Pykhalov <go.wigust@gmail.com>
2024-10-26 17:14:26 +0300

    nix: container-systemd: Configure fonts.

--- dotfiles/nix/container-systemd/hosts/nixos-systemd.nix
+++ dotfiles/nix/container-systemd/hosts/nixos-systemd.nix
@@ -102,6 +102,14 @@
 
   environment.noXlibs = false;
 
+  fonts = {
+    enableDefaultPackages = true;
+    fonts = with pkgs; [
+      dejavu_fonts
+      wqy_microhei
+    ];
+  };
+
   # This value determines the NixOS release from which the default
   # settings for stateful data, like file locations and database versions
   # on your system were taken. It‘s perfectly fine and recommended to leave
