f62b30157a1ee96cefd08668b09f250178e7c167
Oleg Pykhalov <go.wigust@gmail.com>
2024-10-28 23:46:20 +0300

    Add channels-current-local-file-pc0.

---
+++ dotfiles/channels-current-local-file-pc0.scm
@@ -0,0 +1,34 @@
+(list (channel
+        (name 'guix)
+        (url "https://cgit.wugi.info/git/guix/guix")
+        (branch "master")
+        (commit
+          "222fbcb37e13943b687e6b2e8cb0031077932a8a")
+        (introduction
+          (make-channel-introduction
+            "9edb3f66fd807b096b48283debdcddccfea34bad"
+            (openpgp-fingerprint
+              "BBB0 2DDF 2CEA F6A8 0D1D  E643 A2A0 6DF2 A33A 54FA"))))
+      (channel
+        (name 'guix-wigust)
+        (url "https://github.com/kitnil/guix-wigust")
+        (branch "master")
+        (commit
+          "83e86a2891dd57f54fc3568d6a56581fabbb02d2"))
+      (channel
+        (name 'nonguix)
+        (url "https://gitlab.com/nonguix/nonguix")
+        (branch "master")
+        (commit
+          "ad966c3844385e8f7b60b0dc915f92a1eba47e09")
+        (introduction
+          (make-channel-introduction
+            "897c1a470da759236cc11798f4e0a5f7d4d59fbc"
+            (openpgp-fingerprint
+              "2A39 3FFF 68F4 EF7A 3D29  12AF 6F51 20A0 22FB B2D5"))))
+      (channel
+        (name 'prometheus-shepherd-exporter)
+        (url "https://gitlab.com/wigust/prometheus-shepherd-exporter")
+        (branch "master")
+        (commit
+          "542ec52c4955c854e770f615148ced99de5e9fec")))
