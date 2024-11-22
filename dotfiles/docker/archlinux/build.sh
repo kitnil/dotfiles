fd34f2f68f9720305485e35ebde14dc29fb74b57
Oleg Pykhalov <go.wigust@gmail.com>
2024-11-22 12:32:56 +0300

    docker: archlinux: Add bash script.

---
+++ dotfiles/docker/archlinux/build.sh
@@ -0,0 +1,69 @@
+#!/usr/bin/env bash
+
+set -o nounset -o errexit -o pipefail
+
+packages=(
+    alsa-lib
+    atk
+    bash-completion
+    cups
+    fuse-overlayfs
+    fuse2
+    gdk-pixbuf2
+    gtk3
+    less
+    nss
+    pulseaudio
+    strace
+    sudo
+    tmux
+    unzip
+    wmenu
+    xorg-xwayland
+)
+
+packages+=(
+    noto-fonts
+    noto-fonts-cjk
+    noto-fonts-emoji
+    noto-fonts-extra
+)
+
+base_system()
+{
+    groups=(
+        video
+        wheel
+        kmem
+    )
+    useradd -m oleg
+    passwd --delete oleg
+    for group in "${groups[@]}"
+    do
+        gpasswd -a oleg "$group"
+    done
+
+    sed -i 's|# %wheel ALL=(ALL:ALL) NOPASSWD: ALL|%wheel ALL=(ALL:ALL) NOPASSWD: ALL|' /etc/sudoers
+}
+
+install_socialstream()
+{
+    curl -o /tmp/socialstream-linux-AppImage.zip https://iso.wugi.info/socialstream-0.1.52-linux-AppImage.zip
+
+    cd /usr/local/bin
+    unzip /tmp/socialstream-linux-AppImage.zip
+    rm /tmp/socialstream-linux-AppImage.zip
+    mv socialstream-0.1.52-x86_64.AppImage socialstream
+    chmod 555 socialstream
+}
+
+main()
+{
+    base_system
+    pacman --noconfirm -Syu
+    pacman --noconfirm -S "${packages[@]}"
+
+    install_socialstream
+}
+
+main "$@"
