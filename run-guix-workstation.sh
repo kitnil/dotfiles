61a1c2f4ec55e329da9bd08ba69f70e6b93acd5a
Oleg Pykhalov <go.wigust@gmail.com>
2024-12-02 21:12:23 +0300

    run-guix-workstation: Do not delete pod.

--- run-guix-workstation.sh
+++ run-guix-workstation.sh
@@ -2,32 +2,6 @@
 
 set -o nounset -o errexit -o pipefail
 
-if kubectl -n workstation delete --wait=false pod/workstation
-then
-    :
-fi
-
-check()
-{
-    if ssh 192.168.0.192 sudo nerdctl -n k8s.io ps | grep --quiet workstation
-    then
-        return 1
-    else
-        if kubectl -n workstation delete --force pod/workstation
-        then
-            :
-        fi
-    fi
-}
-
-until check
-do
-    sleep 2
-done
-
-cd /home/oleg/.local/share/chezmoi/apps/cluster1/workstation || exit 1
-flux reconcile kustomization workstation
-
 ssh 192.168.0.192 <<'EOF'
 PATH="/home/oleg/.guix-profile/bin:/gnu/store/3q2x34wg1fff833wwzxnagnv7vbfxb0w-jc-1.25.2/bin:$PATH"
 export PATH
