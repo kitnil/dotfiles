ca14c6aac78fd7ec3c5b2aaf3971d321d6328e99
Oleg Pykhalov <go.wigust@gmail.com>
2024-12-20 15:53:17 +0300

    nix: container-systemd: Add firefox home service.

--- dotfiles/nix/pc0/home-manager.nix
+++ dotfiles/nix/pc0/home-manager.nix
@@ -138,6 +138,8 @@
       };
   };
 
+  services.firefox.enable = true;
+
   # The home.stateVersion option no longer has a default value. It used to
   # default to “18.09”, which was the Home Manager version that introduced the
   # option. If your configuration does not explicitly set this option then you
