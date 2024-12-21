73f3dff459ad86abf7decf06538aeb8788818212
Oleg Pykhalov <go.wigust@gmail.com>
2024-12-21 14:59:26 +0300

    docker: kali-rolling: Update.

---
+++ dotfiles/docker/kali-rolling/build.sh
@@ -0,0 +1,33 @@
+#!/usr/bin/env bash
+
+set -o nounset -o errexit -o pipefail -o xtrace
+
+packages=(
+    bash-completion
+    foot
+    less
+    procps
+    pulseaudio
+    strace
+    sudo
+    systemd
+    tmux
+    udev
+    xwayland
+)
+
+main()
+{
+    apt update
+    apt install -y "${packages[@]}"
+
+    useradd -m oleg 
+    passwd --delete oleg 
+    gpasswd -a oleg video 
+    gpasswd -a oleg sudo 
+    chsh --shell /bin/bash oleg
+
+    sed -i 's|# %wheel ALL=(ALL:ALL) NOPASSWD: ALL|%wheel ALL=(ALL:ALL) NOPASSWD: ALL|' /etc/sudoers
+}
+
+main "$@"
