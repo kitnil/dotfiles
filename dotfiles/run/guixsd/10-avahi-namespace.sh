e5a0155d1b1932ef1bdeeff4d081cee22afbe2db
Oleg Pykhalov <go.wigust@gmail.com>
2024-12-22 20:32:12 +0300

    guixsd: Add avahi script.

---
+++ dotfiles/run/guixsd/10-avahi-namespace.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env bash
+
+set -o nounset -o errexit -o pipefail -o xtrace
+
+NAMESPACE="$(/home/oleg/.local/share/chezmoi/dotfiles/run/pc0/11-guix-workstation-get-namespace.sh)"
+
+ip link add macvlan2 link br0 type macvlan mode bridge
+ip link set macvlan2 netns "$NAMESPACE"
+ip netns exec "$NAMESPACE" ip link set macvlan2 up
+ip netns exec "$NAMESPACE" ip addr add 192.168.0.196/24 dev macvlan2
+ip netns exec "$NAMESPACE" ping -c 3 192.168.0.145
