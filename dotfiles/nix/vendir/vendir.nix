d14a93bea5d63aa8ec8548db5c86ec1e5b32baed
Oleg Pykhalov <go.wigust@gmail.com>
2025-01-09 15:44:13 +0300

    nix: vendir: Add git.

--- dotfiles/nix/vendir/vendir.nix
+++ dotfiles/nix/vendir/vendir.nix
@@ -3,7 +3,10 @@
 pkgs.dockerTools.buildLayeredImage rec {
   name = "harbor.home.wugi.info/library/vendir";
   tag = "latest";
-  contents = [ pkgs.vendir ];
+  contents = [
+    pkgs.vendir
+    pkgs.gitAndTools.git
+  ];
   config = {
     Entrypoint = [ "/bin/vendir" ];
   };
