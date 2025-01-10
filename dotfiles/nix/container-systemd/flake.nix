79409cc1c92422b1cd8bd614ba2a059cbf22452b
Oleg Pykhalov <go.wigust@gmail.com>
2025-01-10 12:37:45 +0300

    nix: container-systemd: Add home-manager-pycharm-community.

--- dotfiles/nix/container-systemd/flake.nix
+++ dotfiles/nix/container-systemd/flake.nix
@@ -39,6 +39,7 @@
                   inputs.dotfiles-home-manager.nixosModules.home-manager-firefox
                   inputs.dotfiles-home-manager.nixosModules.home-manager-foot
                   inputs.dotfiles-home-manager.nixosModules.home-manager-google-chrome-stable
+                  inputs.dotfiles-home-manager.nixosModules.home-manager-pycharm-community
                   inputs.dotfiles-home-manager.nixosModules.home-manager-vendir
                   inputs.dotfiles-home-manager.nixosModules.home-manager-wayvnc
                 ];
