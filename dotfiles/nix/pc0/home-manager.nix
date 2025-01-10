647a1c10cf8426ef45919169b4291dd9d7903e4f
Oleg Pykhalov <go.wigust@gmail.com>
2025-01-10 11:28:15 +0300

    nix: pc0: Add google-chrome-stable service.

--- dotfiles/nix/pc0/home-manager.nix
+++ dotfiles/nix/pc0/home-manager.nix
@@ -150,6 +150,7 @@
   };
 
   services.foot.enable = true;
+  services.google-chrome-stable.enable = true;
   services.pycharm-community.enable = true;
   services.wayvnc.enable = true;
   services.vendir.enable = true;
