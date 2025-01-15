0c96e38f41911a8fb63c94f159eb063a9b5c0950
Oleg Pykhalov <go.wigust@gmail.com>
2025-01-15 23:43:35 +0300

    nix: services: Add idea-community.

--- dotfiles/nix/flake.nix
+++ dotfiles/nix/flake.nix
@@ -357,6 +357,7 @@
         home-manager-firefox = import ./modules/services/firefox.nix;
         home-manager-foot = import ./modules/services/foot.nix;
         home-manager-google-chrome = import ./modules/services/google-chrome.nix;
+        home-manager-idea-community = import ./modules/services/idea-community.nix;
         home-manager-pycharm-community = import ./modules/services/pycharm-community.nix;
         home-manager-vendir = import ./modules/services/vendir.nix;
         home-manager-wayvnc = import ./modules/services/wayvnc.nix;
