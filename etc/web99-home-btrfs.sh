fec8f7f97c2be563a5d5c703145b69ea0a8036d0
Oleg Pykhalov <go.wigust@gmail.com>
2025-01-16 18:35:17 +0300

    etc: web99-home-btrfs: Force create filesystem.
    Required if filesystem was previously created.

--- etc/web99-home-btrfs.sh
+++ etc/web99-home-btrfs.sh
@@ -3,7 +3,7 @@
 set -o nounset -o errexit -o pipefail -o xtrace
 
 lvcreate -L 45G -n webbtrfs vg0
-mkfs.btrfs -L webbtrfs /dev/vg0/webbtrfs
+mkfs.btrfs -f -L webbtrfs /dev/vg0/webbtrfs
 
 mkdir -p /mnt/web-btrfs
 mount -o compress=zstd:3,ssd,noatime,degraded /dev/vg0/webbtrfs /mnt/web-btrfs
