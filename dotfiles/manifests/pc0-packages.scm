8a375e338d9ab2c26a2f58ac5665922921de0db3
Oleg Pykhalov <go.wigust@gmail.com>
2025-01-23 07:10:26 +0300

    manifests: pc0-packages: Add adwaita-icon-theme.

--- dotfiles/manifests/pc0-packages.scm
+++ dotfiles/manifests/pc0-packages.scm
@@ -44,6 +44,7 @@
                "pinentry"
                "sshpass"
                "freerdp"
+               "adwaita-icon-theme"
                "remmina"
                "looking-glass-client"
                "netcat-openbsd"
