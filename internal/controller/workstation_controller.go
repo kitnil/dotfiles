e79a3fbbe96d7862f6b42113a0ebfe5dff8509da
Oleg Pykhalov <go.wigust@gmail.com>
2025-01-30 14:37:45 +0300

    Revert "internal: controller: workstation_controller: Do not hardcode affinity."
    This reverts commit 4209c88705c80d9b2b86575449b6bde0b1ecc3b1.

--- internal/controller/workstation_controller.go
+++ internal/controller/workstation_controller.go
@@ -146,7 +146,25 @@ func (r *WorkstationReconciler) Reconcile(ctx context.Context, req ctrl.Request)
 		},
 		Spec: corev1.PodSpec{
 			AutomountServiceAccountToken: &[]bool{false}[0],
-			Affinity:                     workstation.Spec.Affinity,
+			Affinity: &corev1.Affinity{
+				NodeAffinity: &corev1.NodeAffinity{
+					RequiredDuringSchedulingIgnoredDuringExecution: &corev1.NodeSelector{
+						NodeSelectorTerms: []corev1.NodeSelectorTerm{
+							{
+								MatchFields: []corev1.NodeSelectorRequirement{
+									{
+										Key:      "metadata.name",
+										Operator: corev1.NodeSelectorOperator("In"),
+										Values: []string{
+											"kube4",
+										},
+									},
+								},
+							},
+						},
+					},
+				},
+			},
 			Tolerations: []corev1.Toleration{
 				{
 					Effect:   corev1.TaintEffectNoSchedule,
