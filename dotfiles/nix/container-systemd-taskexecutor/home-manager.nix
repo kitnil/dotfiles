3e613243493fae96be9571ab84567a98832973ac
Oleg Pykhalov <go.wigust@gmail.com>
2025-02-07 19:22:05 +0300

    nix: container-systemd-taskexecutor: Add known_hosts.

--- dotfiles/nix/container-systemd-taskexecutor/home-manager.nix
+++ dotfiles/nix/container-systemd-taskexecutor/home-manager.nix
@@ -10,6 +10,16 @@
   home.homeDirectory = "/home/taskexecutor";
   manual.manpages.enable = false;
 
+  home.file = {
+    ".ssh/known_hosts" = {
+      text = ''
+        gitlab.corp1.majordomo.ru ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIJw9vd+rL+MwVdVKSKW32+k6irAULLUFv5dmRUve2nUW
+        gitlab.corp1.majordomo.ru ecdsa-sha2-nistp256 AAAAE2VjZHNhLXNoYTItbmlzdHAyNTYAAAAIbmlzdHAyNTYAAABBBIpKca//ukVhXODbccv/mv4oG74h8jyNQmF7ZbWd/qaolkBv0ptb/ocPc47+btv+FQTx3Fj/cPyi83kwf3ow7C8=
+        gitlab.intr ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIJw9vd+rL+MwVdVKSKW32+k6irAULLUFv5dmRUve2nUW
+      '';
+    };
+  };
+
   home.packages = with pkgs; [
     python-taskexecutor
     python-taskexecutor-wrapper
