bb25b9c8f50458b8931cff1fd0179bc483a556df
Oleg Pykhalov <go.wigust@gmail.com>
2025-03-20 21:32:58 +0300

    run: pc0: 14-sway-run-all: Do not change workspace.

--- dotfiles/run/pc0/14-sway-run-all.sh
+++ dotfiles/run/pc0/14-sway-run-all.sh
@@ -3,8 +3,6 @@
 # set -e
 set -x
 
-/home/oleg/bin/nerdctl-guix-exec /bin/sh -lc 'export SWAYSOCK="$(echo /run/user/1000/sway-ipc*sock)"; /home/oleg/.guix-home/profile/bin/swaymsg workspace 2'
-
 /home/oleg/bin/nerdctl-remmina &
 sleep 2
 
