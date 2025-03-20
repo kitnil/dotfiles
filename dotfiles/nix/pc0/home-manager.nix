e2b27f18d2428655d353a6507494e69efaf8716a
Oleg Pykhalov <go.wigust@gmail.com>
2025-03-20 13:06:25 +0300

    nix: pc0: home-manager: Set cursor.

--- dotfiles/nix/pc0/home-manager.nix
+++ dotfiles/nix/pc0/home-manager.nix
@@ -178,6 +178,12 @@
   services.wayvnc.enable = true;
   services.vendir.enable = true;
 
+  home.pointerCursor.x11 = {
+    enable = true;
+    name = "Adwaita";
+    size = 48;
+  };
+
   # The home.stateVersion option no longer has a default value. It used to
   # default to “18.09”, which was the Home Manager version that introduced the
   # option. If your configuration does not explicitly set this option then you
