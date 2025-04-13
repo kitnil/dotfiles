4fdde9f83cabd5d5bd6978e5f5f057c16ebeb64c
Oleg Pykhalov <go.wigust@gmail.com>
2025-04-13 10:09:07 +0300

    run: pc0: Update dedicated GPU detach.

--- dotfiles/run/pc0/02-virsh-detach-gpu.sh
+++ dotfiles/run/pc0/02-virsh-detach-gpu.sh
@@ -2,5 +2,9 @@
 
 set -o nounset -o errexit -o pipefail -o xtrace
 
+echo "0000:03:00.0" > /sys/bus/pci/drivers/vfio-pci/unbind
+echo 3 > /sys/bus/pci/devices/0000:03:00.0/resource2_resize
+echo > /sys/bus/pci/devices/0000:03:00.0/reset_method
+
 virsh nodedev-detach pci_0000_03_00_0
 virsh nodedev-detach pci_0000_03_00_1
