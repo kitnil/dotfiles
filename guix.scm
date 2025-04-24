6c7d493158576ee539cc36d79f8bd607d9e4f38b
Oleg Pykhalov <go.wigust@gmail.com>
2025-04-25 00:58:10 +0300

    guix.scm: Update.

--- guix.scm
+++ guix.scm
@@ -1,11 +1,17 @@
-(use-modules
- (ice-9 popen)
- (ice-9 rdelim)
- (guix build utils)
- (guix gexp)
- (guix git-download)
- (guix packages)
- (wigust packages dotfiles))
+(use-modules ((guix licenses) #:prefix license:)
+             (gnu packages base)
+             (gnu packages bash)
+             (guix build utils)
+             (guix build utils)
+             (guix build-system trivial)
+             (guix gexp)
+             (guix gexp)
+             (guix git-download)
+             (guix git-download)
+             (guix packages)
+             (guix packages)
+             (ice-9 popen)
+             (ice-9 rdelim))
 
 (define %source-dir "/home/oleg/.local/share/chezmoi")
 
@@ -23,9 +29,38 @@ newspace."
 
 (let ((commit (current-commit)))
   (package
-    (inherit dotfiles)
-    (version (string-append (package-version dotfiles)
-                            "-" (string-take commit 7)))
+    (name "dotfiles")
+    (version (git-version "2.0.0" "1" (string-take commit 7)))
     (source (local-file %source-dir
                         #:recursive? #t
-                        #:select? (git-predicate %source-dir)))))
+                        #:select? (git-predicate %source-dir)))
+    (build-system trivial-build-system)
+    (inputs
+     `(("bash" ,bash)
+       ("make" ,gnu-make)))
+    (arguments
+     `(#:modules ((guix build utils))
+       #:builder
+       (begin
+         (use-modules (guix build utils))
+         (let ((bin (string-append %output "/bin"))
+               (dotfiles (string-append %output "/share/dotfiles"))
+               (executable (string-append %output "/bin/dotfiles")))
+           (copy-recursively (assoc-ref %build-inputs "source") ".")
+           (substitute* "bin/executable_dotfiles"
+             (("@GUIX@") dotfiles)
+             (("/bin/sh")
+              (string-append (assoc-ref %build-inputs "bash") "/bin/bash")))
+
+           (mkdir-p dotfiles)
+           (copy-recursively "." dotfiles)
+
+           (mkdir-p bin)
+           (copy-file "bin/executable_dotfiles" executable)
+           (chmod executable #o555))
+         #t)))
+    (home-page "https://github.com/kitnil/dotfiles")
+    (license license:gpl3+)
+    (synopsis "WiGust dotfiles")
+    (description "This package provides wigust dotfiles which could
+be installed with chezmoi.")))
