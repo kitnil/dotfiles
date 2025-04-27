283e554176ae36a71cee94808779913c30df0094
Oleg Pykhalov <go.wigust@gmail.com>
2025-04-28 02:23:15 +0300

    nix: container-systemd: Enable graphics.

--- dotfiles/nix/container-systemd/hosts/nixos-systemd.nix
+++ dotfiles/nix/container-systemd/hosts/nixos-systemd.nix
@@ -131,6 +131,11 @@
     ];
   };
 
+  hardware.graphics = {
+    enable = true;
+    enable32Bit = true;
+  };
+
   # This value determines the NixOS release from which the default
   # settings for stateful data, like file locations and database versions
   # on your system were taken. It‘s perfectly fine and recommended to leave
