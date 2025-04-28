a7de4eb2cc1b33261b6b7f93252c394e454bd2c9
Oleg Pykhalov <go.wigust@gmail.com>
2025-04-28 07:29:03 +0300

    run: pc0: Update 14-sway-run-all.sh.

--- dotfiles/run/pc0/14-sway-run-all.sh
+++ dotfiles/run/pc0/14-sway-run-all.sh
@@ -3,94 +3,185 @@
 # set -e
 set -x
 
-/home/oleg/bin/nerdctl-guix-exec /bin/sh -lc 'export SWAYSOCK="$(echo /run/user/1000/sway-ipc*sock)"; export WAYLAND_DISPLAY=wayland-1; /home/oleg/.guix-home/profile/bin/alacritty -e sh -c "sleep 120; exit"' &
-sleep 2
+container_id_guix="$(nerdctl -n k8s.io ps --format=json | /home/oleg/.guix-profile/bin/jq --exit-status --raw-output '. | select (.Names | startswith("k8s://workstation/workstation-kube3/guix")) | .ID')"
+container_id_archlinux="$(nerdctl -n k8s.io ps --format=json | /home/oleg/.guix-profile/bin/jq --exit-status --raw-output '. | select (.Image | startswith("harbor.home.wugi.info/library/archlinux-systemd")) | .ID')"
+container_id_nixos="$(nerdctl -n k8s.io ps --format=json | /home/oleg/.guix-profile/bin/jq --exit-status --raw-output '. | select (.Names | startswith("k8s://workstation/workstation-kube3/nixos")) | .ID')"
 
-/home/oleg/bin/nerdctl-guix-exec /bin/sh -lc 'export SWAYSOCK="$(echo /run/user/1000/sway-ipc*sock)"; /home/oleg/.guix-home/profile/bin/swaymsg splitv'
-sleep 2
+nerdctl_guix_exec()
+{
+    nerdctl -n k8s.io exec -u 1000:998 --interactive "$container_id_guix" "$@"
+}
 
-/home/oleg/bin/nerdctl-guix-exec /bin/sh -lc 'export SWAYSOCK="$(echo /run/user/1000/sway-ipc*sock)"; export WAYLAND_DISPLAY=wayland-1; /home/oleg/.guix-home/profile/bin/alacritty -e sh -c "sleep 20; exit"' &
-sleep 2
+nerdctl_archlinux_exec()
+{
+    nerdctl -n k8s.io exec -u 1000:998 --interactive "$container_id_archlinux" "$@"
+}
 
-/home/oleg/bin/nerdctl-guix-exec /bin/sh -lc 'export SWAYSOCK="$(echo /run/user/1000/sway-ipc*sock)"; /home/oleg/.guix-home/profile/bin/swaymsg resize set height 387px'
-sleep 2
+nerdctl_nixos_exec()
+{
+    nerdctl -n k8s.io exec "$container_id_nixos" /run/current-system/sw/bin/machinectl shell --setenv=WAYLAND_DISPLAY=wayland-1 --setenv=XDG_RUNTIME_DIR=/mnt/guix/run/user/1000 oleg@ "$@"
+}
 
-/home/oleg/bin/nerdctl-guix-exec /bin/sh -lc 'export SWAYSOCK="$(echo /run/user/1000/sway-ipc*sock)"; /home/oleg/.guix-home/profile/bin/swaymsg splith'
-sleep 2
+nerdctl_guix_exec /bin/sh -l <<'EOF'
+sway_pid="$(/run/current-system/profile/bin/pidof sway)"
 
-/home/oleg/bin/nerdctl-nixos-exec /bin/sh -lc 'export XDG_RUNTIME_DIR=/mnt/guix/run/user/1000; exec firefox --profile /home/oleg/.mozilla/firefox/messaging' &
-sleep 5
+SWAYSOCK="$(echo /run/user/1000/sway-ipc*${sway_pid}*sock)"
+export SWAYSOCK
 
-/home/oleg/bin/nerdctl-nixos-exec /bin/sh -lc 'export XDG_RUNTIME_DIR=/mnt/guix/run/user/1000; export WAYLAND_DISPLAY=wayland-1; export QT_QPA_PLATFORM=wayland; exec chatterino' &
-sleep 2
+WAYLAND_DISPLAY=wayland-1
+export WAYLAND_DISPLAY
 
-/home/oleg/bin/nerdctl-guix-exec /bin/sh -lc 'export SWAYSOCK="$(echo /run/user/1000/sway-ipc*sock)"; /home/oleg/.guix-home/profile/bin/swaymsg focus up'
-sleep 2
+PATH=/home/oleg/.guix-home/profile/bin:/run/current-system/profile/bin
+export PATH
 
-/home/oleg/bin/nerdctl-guix-exec /bin/sh -lc 'export SWAYSOCK="$(echo /run/user/1000/sway-ipc*sock)"; /home/oleg/.guix-home/profile/bin/swaymsg splith'
-sleep 2
+alacritty -e sh -c "sleep 20; exit" &
+sleep 0.4
 
-/home/oleg/bin/nerdctl-nixos-exec /bin/sh -lc 'export XDG_RUNTIME_DIR=/mnt/guix/run/user/1000; exec firefox --profile /home/oleg/.mozilla/firefox/twitch' &
-sleep 5
+swaymsg splitv
+sleep 0.4
 
-/home/oleg/bin/nerdctl-guix-exec /bin/sh -lc 'export SWAYSOCK="$(echo /run/user/1000/sway-ipc*sock)"; /home/oleg/.guix-home/profile/bin/swaymsg splith'
-sleep 2
+alacritty -e sh -c "sleep 5; exit" &
+sleep 0.4
 
-/home/oleg/bin/nerdctl-guix-exec /bin/sh -lc 'export SWAYSOCK="$(echo /run/user/1000/sway-ipc*sock)"; /home/oleg/.guix-home/profile/bin/swaymsg layout tabbed'
-sleep 2
+swaymsg resize set height 387px
+sleep 0.4
 
-/home/oleg/bin/nerdctl-guix-exec /bin/sh -lc 'export SWAYSOCK="$(echo /run/user/1000/sway-ipc*sock)"; /home/oleg/.guix-home/profile/bin/swaymsg focus left'
-sleep 2
+swaymsg splith
+sleep 0.4
+EOF
 
-/home/oleg/bin/nerdctl-guix-exec /bin/sh -lc 'export SWAYSOCK="$(echo /run/user/1000/sway-ipc*sock)"; /home/oleg/.guix-home/profile/bin/swaymsg splith'
-sleep 2
+# login
+nerdctl -n k8s.io exec "$container_id_archlinux" /bin/machinectl shell --setenv=WAYLAND_DISPLAY=wayland-1 --setenv=XDG_RUNTIME_DIR=/mnt/guix/run/user/1000 oleg@ /bin/sleep infinity &
 
-/home/oleg/bin/nerdctl-guix-exec /bin/sh -lc 'export SWAYSOCK="$(echo /run/user/1000/sway-ipc*sock)"; /home/oleg/.guix-home/profile/bin/swaymsg layout tabbed'
-sleep 2
+sleep 0.4
 
-/home/oleg/bin/nerdctl-guix-exec /bin/sh -lc 'export SWAYSOCK="$(echo /run/user/1000/sway-ipc*sock)"; /home/oleg/.guix-home/profile/bin/swaymsg exec obs'
-sleep 2
+# login
+nerdctl -n k8s.io exec "$container_id_nixos" /run/current-system/sw/bin/machinectl shell oleg@ /run/current-system/sw/bin/sleep infinity &
 
-/home/oleg/bin/nerdctl-guix-exec /bin/sh -lc 'export SWAYSOCK="$(echo /run/user/1000/sway-ipc*sock)"; /home/oleg/.guix-home/profile/bin/swaymsg exec pavucontrol'
-sleep 2
+sleep 0.4
 
-/home/oleg/bin/nerdctl-nixos-exec /bin/sh -lc 'export XDG_RUNTIME_DIR=/mnt/guix/run/user/1000; exec firefox --profile /home/oleg/.mozilla/firefox/video' &
-sleep 5
+nerdctl_nixos_exec /run/current-system/sw/bin/systemctl --user start firefox@messaging
+sleep 3
 
-/home/oleg/bin/nerdctl-nixos-exec /bin/sh -lc 'export XDG_RUNTIME_DIR=/mnt/guix/run/user/1000; export DISPLAY=:0; exec google-chrome-stable' &
-sleep 5
+nerdctl -n k8s.io exec "$container_id_nixos" /run/current-system/sw/bin/machinectl shell oleg@ /bin/sh -lc 'export XDG_RUNTIME_DIR=/mnt/guix/run/user/1000; export WAYLAND_DISPLAY=wayland-1; export QT_QPA_PLATFORM=wayland; exec chatterino' &
+sleep 0.4
 
-/home/oleg/bin/nerdctl-guix-exec /bin/sh -lc 'export SWAYSOCK="$(echo /run/user/1000/sway-ipc*sock)"; export WAYLAND_DISPLAY=wayland-1; /home/oleg/.guix-home/profile/bin/alacritty -e sh -c "sleep 20; exit"' &
-sleep 2
+nerdctl_guix_exec /bin/sh -l <<'EOF'
+sway_pid="$(/run/current-system/profile/bin/pidof sway)"
 
-/home/oleg/bin/nerdctl-guix-exec /bin/sh -lc 'export SWAYSOCK="$(echo /run/user/1000/sway-ipc*sock)"; /home/oleg/.guix-home/profile/bin/swaymsg splitv'
-sleep 2
+SWAYSOCK="$(echo /run/user/1000/sway-ipc*${sway_pid}*sock)"
+export SWAYSOCK
 
-/home/oleg/bin/nerdctl-guix-exec /bin/sh -lc 'export SWAYSOCK="$(echo /run/user/1000/sway-ipc*sock)"; /home/oleg/.guix-home/profile/bin/swaymsg layout tabbed'
-sleep 2
+WAYLAND_DISPLAY=wayland-1
+export WAYLAND_DISPLAY
 
-/home/oleg/bin/nerdctl-nixos-exec /bin/sh -lc 'export XDG_RUNTIME_DIR=/mnt/guix/run/user/1000; exec firefox --profile /home/oleg/.mozilla/firefox/pcaaxem9.default' &
-sleep 5
+PATH=/home/oleg/.guix-home/profile/bin:/run/current-system/profile/bin
+export PATH
 
-/home/oleg/bin/nerdctl-guix-exec /bin/sh -lc 'export SWAYSOCK="$(echo /run/user/1000/sway-ipc*sock)"; /home/oleg/.guix-home/profile/bin/swaymsg focus parent'
-sleep 2
+swaymsg focus up
+sleep 0.4
 
-/home/oleg/bin/nerdctl-archlinux-exec /bin/sh -lc 'export XDG_RUNTIME_DIR=/mnt/guix/run/user/1000; export DISPLAY=:0; exec socialstream' &
-sleep 5
+swaymsg splith
+sleep 0.4
+EOF
 
-/home/oleg/bin/nerdctl-guix-exec /bin/sh -lc 'export SWAYSOCK="$(echo /run/user/1000/sway-ipc*sock)"; /home/oleg/.guix-home/profile/bin/swaymsg splitv'
-sleep 2
-
-/home/oleg/bin/nerdctl-guix-exec /bin/sh -lc 'export SWAYSOCK="$(echo /run/user/1000/sway-ipc*sock)"; /home/oleg/.guix-home/profile/bin/swaymsg layout tabbed'
-sleep 2
-
-/home/oleg/bin/nerdctl-guix-exec /bin/sh -lc 'pactl set-default-sink alsa_output.pci-0000_12_00.6.analog-stereo'
+nerdctl_nixos_exec /run/current-system/sw/bin/systemctl --user start firefox@twitch
 sleep 1
 
-/home/oleg/bin/nerdctl-guix-exec /bin/sh -lc 'pactl set-source-volume alsa_input.usb-FIFINE_Microphones_FIFINE_K670_Microphone_REV1.0-00.analog-stereo 80%'
+nerdctl_guix_exec /bin/sh -l <<'EOF'
+sway_pid="$(/run/current-system/profile/bin/pidof sway)"
+
+SWAYSOCK="$(echo /run/user/1000/sway-ipc*${sway_pid}*sock)"
+export SWAYSOCK
+
+WAYLAND_DISPLAY=wayland-1
+export WAYLAND_DISPLAY
+
+PATH=/home/oleg/.guix-home/profile/bin:/run/current-system/profile/bin
+export PATH
+
+swaymsg splith
+sleep 0.4
+
+swaymsg layout tabbed
+sleep 0.4
+EOF
+
+nerdctl_nixos_exec /run/current-system/sw/bin/systemctl --user start google-chrome
 sleep 1
 
-/home/oleg/bin/nerdctl-nixos-exec /bin/sh -lc 'export XDG_RUNTIME_DIR=/mnt/guix/run/user/1000; exec wayvnc --output=HEADLESS-2' &
-sleep 5
+nerdctl_guix_exec /bin/sh -l <<'EOF'
+sway_pid="$(/run/current-system/profile/bin/pidof sway)"
+
+SWAYSOCK="$(echo /run/user/1000/sway-ipc*${sway_pid}*sock)"
+export SWAYSOCK
+
+WAYLAND_DISPLAY=wayland-1
+export WAYLAND_DISPLAY
+
+PATH=/home/oleg/.guix-home/profile/bin:/run/current-system/profile/bin
+export PATH
+
+swaymsg focus parent
+sleep 0.4
+
+swaymsg focus left
+sleep 0.4
+
+swaymsg splith
+sleep 0.4
+
+swaymsg layout tabbed
+sleep 0.4
+
+swaymsg exec pavucontrol
+sleep 0.4
+EOF
+
+nerdctl_nixos_exec /run/current-system/sw/bin/systemctl --user start firefox@default
+sleep 0.4
+
+nerdctl_archlinux_exec /bin/sh -lc 'export XDG_RUNTIME_DIR=/mnt/guix/run/user/1000; export DISPLAY=:0; exec socialstream' &
+sleep 1
+
+nerdctl_guix_exec /bin/sh -l <<'EOF'
+sway_pid="$(/run/current-system/profile/bin/pidof sway)"
+
+SWAYSOCK="$(echo /run/user/1000/sway-ipc*{sway_pid}*sock)"
+export SWAYSOCK
+
+WAYLAND_DISPLAY=wayland-1
+export WAYLAND_DISPLAY
+
+PATH=/home/oleg/.guix-home/profile/bin:/run/current-system/profile/bin
+export PATH
+
+swaymsg splitv
+sleep 0.4
+swaymsg layout tabbed
+sleep 0.4
+EOF
+
+nerdctl_guix_exec /bin/sh -l <<'EOF'
+sway_pid="$(/run/current-system/profile/bin/pidof sway)"
+
+SWAYSOCK="$(echo /run/user/1000/sway-ipc*${sway_pid}*sock)"
+export SWAYSOCK
+
+WAYLAND_DISPLAY=wayland-1
+export WAYLAND_DISPLAY
+
+PATH=/home/oleg/.guix-home/profile/bin:/run/current-system/profile/bin
+export PATH
+
+swaymsg splitv
+sleep 0.4
+
+swaymsg layout tabbed
+
+pactl set-default-sink alsa_output.pci-0000_12_00.6.analog-stereo
+
+pactl set-source-volume alsa_input.usb-FIFINE_Microphones_FIFINE_K670_Microphone_REV1.0-00.analog-stereo 80%
+EOF
 
 exec sleep infinity
