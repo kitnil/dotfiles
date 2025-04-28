45bdcfacaa5a7a5ba1b2dad65d86109bae88d0b8
Oleg Pykhalov <go.wigust@gmail.com>
2025-04-28 07:54:43 +0300

    nix: services: Simplify firefox service.

--- dotfiles/nix/modules/services/firefox.nix
+++ dotfiles/nix/modules/services/firefox.nix
@@ -1,44 +1,25 @@
 { config, lib, pkgs, ... }:
 
-with lib;
-
 {
-  config =
-    let
-      profiles = attrNames config.programs.firefox.profiles;
-      firefox-profile = profile:
-        {
-          Unit = {
-            Description = "Firefox web browser";
-          };
-          Service = {
-            ExecStart =
-              let
-                script = pkgs.writeScript "firefox.sh" ''
-                    #!${pkgs.runtimeShell}
-
-                    XDG_RUNTIME_DIR=/mnt/guix/run/user/1000
-                    export XDG_RUNTIME_DIR
-
-                    WAYLAND_DISPLAY=wayland-1
-                    export WAYLAND_DISPLAY
-
-                    exec -a firefox ${pkgs.firefox}/bin/firefox -P "$@"
-                  '';
-              in
-                "${script} %i";
-            Type = "simple";
-          };
-        };
-    in
-      {
-        systemd.user.services =
-          listToAttrs
-            (map
-              (profile:
-                nameValuePair
-                  "firefox@${profile}"
-                  (firefox-profile profile))
-              profiles);
+  config = {
+    systemd.user.services."firefox@" = {
+      Unit = {
+        Description = "Firefox web browser";
       };
+      Service = {
+        ExecStart = ''
+          #!${pkgs.runtimeShell}
+
+          XDG_RUNTIME_DIR=/mnt/guix/run/user/%U
+          export XDG_RUNTIME_DIR
+
+          WAYLAND_DISPLAY=wayland-1
+          export WAYLAND_DISPLAY
+
+          exec -a firefox ${pkgs.firefox}/bin/firefox --profile %h/.mozilla/firefox/%i
+        '';
+        Type = "simple";
+      };
+    };
+  };
 }
