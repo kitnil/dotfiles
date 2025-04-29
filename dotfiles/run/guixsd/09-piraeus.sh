46dc09b9e7dc18686baf21214125fd5f9c5738f2
Oleg Pykhalov <go.wigust@gmail.com>
2025-04-30 02:46:20 +0300

    run: guixsd: 09-piraeus: Check if satellite is ready.

--- dotfiles/run/guixsd/09-piraeus.sh
+++ dotfiles/run/guixsd/09-piraeus.sh
@@ -2,6 +2,17 @@
 
 set -o nounset -o errexit -o pipefail -o xtrace
 
+linstor_satellite_ready()
+{
+    kubectl -n piraeus get pod -o json \
+        | jq --raw-output '.items[] | select(.metadata.name | startswith("piraeus-op-ns-node-")) | select(.spec.nodeName == "kube1") | .status.containerStatuses[] | select(.name == "linstor-satellite") | .ready'
+}
+
+until [[ $(linstor_satellite_ready) == true ]]
+do
+    sleep 5
+done
+
 jq_expression='
 .items[] | select(.spec.nodeName == "kube1") | select(.metadata.name | startswith("piraeus-op-ns")) | .metadata.name
 '
