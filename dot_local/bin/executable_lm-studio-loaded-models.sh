7d7d43f8037c5a43c8459d976b20c4fb47e0d8de
Oleg Pykhalov <go.wigust@gmail.com>
2025-04-30 08:41:52 +0300

    bin: lm-studio-loaded-models.sh: Change host.

--- dot_local/bin/executable_lm-studio-loaded-models.sh
+++ dot_local/bin/executable_lm-studio-loaded-models.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
 
-curl --fail --silent https://lm-studio.home.wugi.info/api/v0/models | jq --compact-output --raw-output '.data[] | select(.state == "loaded") | { text: .id, tooltip: .max_context_length }'
+curl --fail --silent http://192.168.0.178:1234/api/v0/models | jq --compact-output --raw-output '.data[] | select(.state == "loaded") | { text: .id, tooltip: .max_context_length }'
