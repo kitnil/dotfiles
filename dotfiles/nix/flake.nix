5000d5921b2f16038d2683a7db20c2c3eef4f9b5
Oleg Pykhalov <go.wigust@gmail.com>
2025-05-07 16:58:56 +0300

    nix: Use unstable home-manager.

--- dotfiles/nix/flake.nix
+++ dotfiles/nix/flake.nix
@@ -34,7 +34,7 @@
     nixpkgs-ddcutil.url = "nixpkgs/nixpkgs-unstable";
 
     nixpkgs-home-manager.url = "nixpkgs/nixpkgs-unstable";
-    home-manager.url = "github:nix-community/home-manager?ref=release-23.05";
+    home-manager.url = "github:nix-community/home-manager";
     nur.url = "github:nix-community/NUR";
     rycee-nur-expressions.url =
       "git+https://gitlab.com/rycee/nur-expressions?dir=pkgs/firefox-addons";
