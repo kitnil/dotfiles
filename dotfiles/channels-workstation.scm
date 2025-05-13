0528d99abe05266a8f4b94234fc881bc86e4b060
Oleg Pykhalov <go.wigust@gmail.com>
2025-05-13 21:19:41 +0300

    dotfiles: channels-workstation.scm: : Update.

--- dotfiles/channels-workstation.scm
+++ dotfiles/channels-workstation.scm
@@ -17,10 +17,10 @@
           "ec542755f860e050f2355c0b305cafc386496abf"))
       (channel
         (name 'nonguix)
-        (url "https://gitlab.com/nonguix/nonguix")
-        (branch "master")
+        (url "https://cgit.wugi.info/git/nonguix/nonguix")
+        (branch "aichat")
         (commit
-          "e899121adbaa13cfcaeae7a5c24921bffa645771")
+          "7c29f2df877a83c5e860d3a98ff96e1b2caeb05c")
         (introduction
           (make-channel-introduction
             "897c1a470da759236cc11798f4e0a5f7d4d59fbc"
