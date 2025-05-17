f49586da9c1907dde66cd72821ffdcd12107dea4
Oleg Pykhalov <go.wigust@gmail.com>
2025-05-17 12:42:33 +0300

    wugi: manifests: nonguix.scm: Update.

---
+++ wugi/manifests/nonguix.scm
@@ -0,0 +1,36 @@
+(define-module (wugi manifests nonguix)
+  #:use-module (guix profiles)
+  #:use-module (guix packages)
+  #:use-module (gnu packages)
+  #:use-module (nongnu packages fonts)
+  #:export (%nonguix-manifest))
+
+(define (%nonguix-manifest)
+  (define fonts
+    (list font-apl2741-unicode
+          font-apple-color-emoji
+          font-apple-new-york
+          font-apple-sf-arabic
+          font-apple-sf-compact
+          font-apple-sf-mono
+          font-apple-sf-pro
+          font-apple-sf-symbols
+          font-apple-symbols
+          font-awesome-nonfree
+          font-ghostscript
+          font-mathjax
+          font-microsoft-andale-mono
+          font-microsoft-arial
+          font-microsoft-arial-black
+          font-microsoft-comic-sans-ms
+          font-microsoft-couirer-new
+          font-microsoft-courier-new
+          font-microsoft-georgia
+          font-microsoft-impact
+          font-microsoft-times-new-roman
+          font-microsoft-trebuchet-ms
+          font-microsoft-verdana
+          font-microsoft-web-core-fonts
+          font-microsoft-webdings
+          font-ubuntu))
+  (packages->manifest fonts))
