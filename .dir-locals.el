27dceadb15eafef7beb1cd9d001014eb0106c7c0
Oleg Pykhalov <go.wigust@gmail.com>
2025-05-17 15:53:26 +0300

    .dir-locals.el: Update.

--- .dir-locals.el
+++ .dir-locals.el
@@ -5,16 +5,11 @@
      (projectile-project-test-cmd . "make check")
      (projectile-project-install-cmd . "make install")
      (eval . (progn
-               (when (boundp #'projectile-project-root)
-                     (setenv "GUIX_PACKAGE_PATH"
-                             (concat (projectile-project-root)
-                                     "dotfiles/guixsd/modules")))
                (setq-local geiser-guile-load-path
                            (append (list (concat (getenv "HOME") "/src/cgit.duckdns.org/guix/guix-wigust/guix"))
                                    (list (concat (getenv "HOME") "/src/gitlab.com/nonguix/nonguix"))
                                    (if (boundp #'projectile-project-root)
-                                       (list (concat (projectile-project-root)
-                                                     "dotfiles/guixsd/modules"))
+                                       (list (projectile-project-root))
                                      '())))
                (git-auto-commit-mode)
                (aggressive-indent-mode))))))
