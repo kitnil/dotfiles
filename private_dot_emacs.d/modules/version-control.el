b31b7738e71bc68329bd925444ebb5c00e77e6f5
Oleg Pykhalov <go.wigust@gmail.com>
2025-05-18 21:09:16 +0300

    emacs: Refine version control module.
    Adjusted string replacements for improved clarity and consistency in the
    version-control.el file. This includes updating how GuixSD paths are handled.
    
    * private_dot_emacs.d/modules/version-control.el: Improve path handling.

--- private_dot_emacs.d/modules/version-control.el
+++ private_dot_emacs.d/modules/version-control.el
@@ -387,7 +387,9 @@ _p_rev       _u_pper              _=_: upper/lower       _r_esolve
         (save-excursion
           (replace-string ".el" "" nil 0 end))
         (save-excursion
-          (replace-string " -" ":" nil 0 end)))
+          (replace-string " -" ":" nil 0 end))
+        (save-excursion
+          (replace-string "^guixsd.scm " "system: guixsd: " nil 0 end)))
 
       (goto-char 2)
       (next-line)
