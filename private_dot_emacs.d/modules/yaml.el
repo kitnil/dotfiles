b5084848ccac1400ce0ef2ef93e10bc770cdb6e6
Oleg Pykhalov <go.wigust@gmail.com>
2025-05-20 07:52:16 +0300

    emacs: yaml: Improve left margin handling.
    Fixes an issue with edit-indirect where the left margin was not correctly
    handled, leading to incorrect indentation after committing changes.  Adds
    functions to compute and restore the left margin before and after using
    edit-indirect.
    
    * private_dot_emacs.d/modules/yaml.el: Implement left margin fix.

--- private_dot_emacs.d/modules/yaml.el
+++ private_dot_emacs.d/modules/yaml.el
@@ -23,3 +23,40 @@
           (lambda (&rest _)
             (scheme-mode))))
     (edit-indirect-region begin end 'display-buffer)))
+
+
+;;;
+;;; edit-indirect
+;;;
+
+;; https://github.com/Fanael/edit-indirect/issues/6
+(require 's)
+(require 'dash)
+
+(defvar edit-indirect--left-margin 0)
+
+(defun vbe/compute-left-margin (code)
+  "Compute left margin of a string of code."
+  (-min
+   (-map #'(lambda (line) (length (car (s-match "^\\s-*" line))))
+         (-remove 's-blank? (s-lines code)))))
+
+(defun vbe/edit-indirect/remove-left-margin ()
+  "Remove left-margin and save it into a local variable."
+  (let ((lm (vbe/compute-left-margin (buffer-substring (point-min) (point-max)))))
+    (indent-rigidly (point-min) (point-max) (* -1 lm))
+    (setq-local edit-indirect--left-margin lm)
+    ;; https://github.com/Fanael/edit-indirect/issues/6#issuecomment-1055542145
+    ;; buffer-local variable whose value should not be reset when changing major modes
+    (put 'edit-indirect--left-margin 'permanent-local t)))
+
+(defun vbe/edit-indirect/restore-left-margin ()
+  "Restore left-margin before commiting."
+  (indent-rigidly (point-min) (point-max) edit-indirect--left-margin))
+
+(add-hook 'edit-indirect-after-creation-hook #'vbe/edit-indirect/remove-left-margin)
+(add-hook 'edit-indirect-before-commit-hook #'vbe/edit-indirect/restore-left-margin)
+
+(require 'edit-indirect)
+;; https://github.com/Fanael/edit-indirect/issues/6#issuecomment-1284144173
+(define-key edit-indirect-mode-map [remap save-buffer] #'edit-indirect-commit)
