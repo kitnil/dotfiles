5f336877fc194c99408b018c98f28e5f7d6c7995
Oleg Pykhalov <go.wigust@gmail.com>
2025-05-23 22:20:25 +0300

    guix: dotfiles: run: pc0: Update 10-avahi-namespace.sh.
    Corrected path to namespace script and bridge interface.
    
    * guix/dotfiles/run/pc0/10-avahi-namespace.sh: Updated the script to use the
    correct path for fetching the Avahi namespace and changed the bridge interface
    from eth0 to br0.

--- guix/dotfiles/run/pc0/10-avahi-namespace.sh
+++ guix/dotfiles/run/pc0/10-avahi-namespace.sh
@@ -2,9 +2,9 @@
 
 set -o nounset -o errexit -o pipefail -o xtrace
 
-NAMESPACE="$(/home/oleg/.local/share/chezmoi/dotfiles/run/pc0/11-guix-workstation-get-namespace.sh)"
+NAMESPACE="$(/home/oleg/.local/share/chezmoi/guix/dotfiles/run/pc0/11-guix-workstation-get-namespace.sh)"
 
-ip link add macvlan2 link eth0 type macvlan mode bridge
+ip link add macvlan2 link br0 type macvlan mode bridge
 ip link set macvlan2 netns "$NAMESPACE"
 ip netns exec "$NAMESPACE" ip link set macvlan2 up
 ip netns exec "$NAMESPACE" ip addr add 192.168.0.194/24 dev macvlan2
