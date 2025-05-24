f82b6cb0a3d504b15c1caa9c6a0238045a65a4ed
Oleg Pykhalov <go.wigust@gmail.com>
2025-05-24 13:56:45 +0300

    guix: manifests: Add pkg-config to dotfiles manifest.
    Includes pkg-config in the list of packages for the dotfiles manifest.
    
    * guix/wugi/manifests/dotfiles.scm: Add pkg-config to the package list.

--- guix/wugi/manifests/dotfiles.scm
+++ guix/wugi/manifests/dotfiles.scm
@@ -2,6 +2,7 @@
   #:use-module (gnu packages autotools)
   #:use-module (gnu packages base)
   #:use-module (gnu packages gnupg)
+  #:use-module (gnu packages pkg-config)
   #:use-module (gnu packages version-control)
   #:use-module (gnu packages virtualization)
   #:use-module (gnu packages web)
@@ -10,4 +11,4 @@
 
 (define (%dotfiles-manifest)
   (packages->manifest
-   (list autoconf automake skopeo git gnupg gnu-make yq)))
+   (list autoconf automake git gnu-make gnupg pkg-config skopeo yq)))
