be45dd9a6699388e117c672cd5a7ac7c4b62e9be
Oleg Pykhalov <go.wigust@gmail.com>
2025-05-25 15:48:54 +0300

    Revert update drbd module version.
    Updates the drbd module to version 9.1.7, removing unused dependencies.
    
    * guix/wugi/packages/linux-modules.scm: Updates version and removes unused
    modules.

--- guix/wugi/packages/linux-modules.scm
+++ guix/wugi/packages/linux-modules.scm
@@ -5,9 +5,6 @@
   #:use-module (gnu packages compression)
   #:use-module (gnu packages cpio)
   #:use-module (gnu packages curl)
-  #:use-module (gnu packages flex)
-  #:use-module (gnu packages patchutils)
-  #:use-module (gnu packages python)
   #:use-module (gnu packages version-control)
   #:use-module (guix build-system copy)
   #:use-module (guix build-system gnu)
@@ -26,7 +23,7 @@
 (define-public drbd-module
   (package
     (name "drbd-module")
-    (version "9.2.13")
+    (version "9.1.7")
     (source
      (origin
        (method url-fetch)
@@ -34,16 +31,12 @@
                            version ".tar.gz"))
        (sha256
         (base32
-         "0njhcpry0hr4ibclfv3xva21gzp7vsd876qmnvcyhr6mvc2xmjrc"))))
+         "1iak07vpynimbyh4lhpf8xpn6vhgxnn3jmckm28r09m3a5adyrj1"))))
     (build-system linux-module-build-system)
     (inputs
-     `(("bash" ,bash)
-       ("coccinelle" ,coccinelle)
-       ("flex" ,flex)
-       ("python" ,python)))
+     `(("bash" ,bash)))
     (arguments
      (list
-      #:make-flags #~(list "--debug" "SPAAS=false")
       #:tests? #f ;there are none.
       #:source-directory "drbd"
       #:phases
