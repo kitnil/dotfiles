1becab5737602ba118b2ab6d8a004fa8834871f0
Oleg Pykhalov <go.wigust@gmail.com>
2025-05-29 18:02:29 +0300

    manifests: telegram: Move Guix channels.
    Adds new Guix channels for guix and johnlepikhin, including commit hashes and
    PGP fingerprints.
    
    * guix/wugi/etc/guix/channels/telegram.scm: New file.
    * guix/wugi/manifests/telegram.scm: Integrates the newly added Telegram
    channels into the manifest file by referencing them from %channels-telegram.

---
+++ guix/wugi/etc/guix/channels/telegram.scm
@@ -0,0 +1,26 @@
+(define-module (wugi etc guix channels telegram)
+  #:use-module (guix channels)
+  #:export (%channels-telegram))
+
+(define %channels-telegram
+  (list (channel
+         (name 'guix)
+         (url "https://cgit.wugi.info/git/guix/guix")
+         (commit "b53fac227836bcec3a2a7a44f7720b9a23db90f2")
+         (introduction
+          (make-channel-introduction
+           "9edb3f66fd807b096b48283debdcddccfea34bad"     ;2020-05-26
+           (openpgp-fingerprint                           ;mbakke
+            "BBB0 2DDF 2CEA F6A8 0D1D  E643 A2A0 6DF2 A33A 54FA"))))
+        (channel
+         (name 'johnlepikhin)
+         (url "https://cgit.wugi.info/git/guix/johnlepikhin-guix-channel.git")
+         (branch "master")
+         (commit "d254b40b5edb9130629dda43f4b7ae74b26fdc41")
+         (introduction
+          (make-channel-introduction
+           "2fbee0b655fdb9742cbafc2c4d457db06f8eaeeb"
+           (openpgp-fingerprint
+            "7238 7123 8EAC EB63 4548  5857 167F 8EA5 001A FA9C"))))))
+
+%channels-telegram
--- guix/wugi/manifests/telegram.scm
+++ guix/wugi/manifests/telegram.scm
@@ -4,27 +4,12 @@
   #:use-module (guix profiles)
   #:use-module (guix store)
   #:use-module (srfi srfi-1)
+  #:use-module (wugi etc guix channels telegram)
   #:export (%telegram-manifest))
-
-(define channels
-  (list (channel
-         (name 'guix)
-         (url "https://cgit.wugi.info/git/guix/guix")
-         (commit "b53fac227836bcec3a2a7a44f7720b9a23db90f2")
-         (introduction
-          (make-channel-introduction
-           "9edb3f66fd807b096b48283debdcddccfea34bad"     ;2020-05-26
-           (openpgp-fingerprint                           ;mbakke
-            "BBB0 2DDF 2CEA F6A8 0D1D  E643 A2A0 6DF2 A33A 54FA"))))
-        (channel
-         (name 'johnlepikhin)
-         (url "https://cgit.wugi.info/git/guix/johnlepikhin-guix-channel.git")
-         (branch "master")
-         (commit "d254b40b5edb9130629dda43f4b7ae74b26fdc41"))))
 
 (define (%telegram-manifest)
   (define inferior
-    (inferior-for-channels channels))
+    (inferior-for-channels %channels-telegram))
 
   (define telegram-desktop
     (first (lookup-inferior-packages inferior "telegram-desktop")))
