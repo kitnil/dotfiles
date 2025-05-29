eaea1f2488ace5e4f5a541ea521d23eebd7bab48
Oleg Pykhalov <go.wigust@gmail.com>
2025-05-29 17:50:35 +0300

    guix: channels: Introduce Majordomo channel.
    Adds definitions for Guix and Guix Majordomo channels, including URLs,
    branches, commit hashes, and OpenPGP fingerprints. This enables the use of
    these channels within Guix environments.
    
    * guix/wugi/etc/guix/channels/majordomo.scm: New file.
    * guix/wugi/manifests/majordomo.scm (channels): Use this.

---
+++ guix/wugi/etc/guix/channels/majordomo.scm
@@ -0,0 +1,29 @@
+(define-module (wugi etc guix channels majordomo)
+  #:use-module (guix channels)
+  #:export (%channels-majordomo))
+
+(define %channels-majordomo
+  (list (channel
+         (name 'guix)
+         (url "https://cgit.wugi.info/git/guix/guix")
+         (branch "master")
+         (commit
+          "1f734a6f0a7db5b0e12091a0c869c5c4810ac80e")
+         (introduction
+          (make-channel-introduction
+           "9edb3f66fd807b096b48283debdcddccfea34bad"
+           (openpgp-fingerprint
+            "BBB0 2DDF 2CEA F6A8 0D1D  E643 A2A0 6DF2 A33A 54FA"))))
+        (channel
+         (name 'guix-majordomo)
+         (url "https://cgit.wugi.info/git/guix/guix-majordomo.git")
+         (branch "master")
+         (commit
+          "f8dd95eff628421b726cc4d387996080a0ab6465")
+         (introduction
+          (make-channel-introduction
+           "f8dd95eff628421b726cc4d387996080a0ab6465"
+           (openpgp-fingerprint
+            "7238 7123 8EAC EB63 4548  5857 167F 8EA5 001A FA9C"))))))
+
+%channels-majordomo
--- guix/wugi/manifests/majordomo.scm
+++ guix/wugi/manifests/majordomo.scm
@@ -4,35 +4,12 @@
   #:use-module (guix profiles)
   #:use-module (guix store)
   #:use-module (srfi srfi-1)
+  #:use-module (wugi etc guix channels majordomo)
   #:export (%majordomo-manifest))
-
-(define channels
-  (list (channel
-         (name 'guix)
-         (url "https://cgit.wugi.info/git/guix/guix")
-         (branch "master")
-         (commit
-          "1f734a6f0a7db5b0e12091a0c869c5c4810ac80e")
-         (introduction
-          (make-channel-introduction
-           "9edb3f66fd807b096b48283debdcddccfea34bad"
-           (openpgp-fingerprint
-            "BBB0 2DDF 2CEA F6A8 0D1D  E643 A2A0 6DF2 A33A 54FA"))))
-        (channel
-         (name 'guix-majordomo)
-         (url "https://cgit.wugi.info/git/guix/guix-majordomo.git")
-         (branch "master")
-         (commit
-          "f8dd95eff628421b726cc4d387996080a0ab6465")
-         (introduction
-          (make-channel-introduction
-           "f8dd95eff628421b726cc4d387996080a0ab6465"
-           (openpgp-fingerprint
-            "7238 7123 8EAC EB63 4548  5857 167F 8EA5 001A FA9C"))))))
 
 (define (%majordomo-manifest)
   (define inferior
-    (inferior-for-channels channels))
+    (inferior-for-channels %channels-majordomo))
 
   (packages->manifest
    (list (first (lookup-inferior-packages inferior "guile-ihs")))))
