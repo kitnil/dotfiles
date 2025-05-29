816f32a490056e5224ede6cc98e64401380601f1
Oleg Pykhalov <go.wigust@gmail.com>
2025-05-29 17:25:24 +0300

    manifests: deprecated: Hardcode introduction.
    Updates the Guix channel manifest with a new commit and PGP fingerprint for
    improved security and verification.
    
    * guix/wugi/manifests/deprecated.scm: Modifies the deprecated Guix channel
    manifest to include updated information.

--- guix/wugi/manifests/deprecated.scm
+++ guix/wugi/manifests/deprecated.scm
@@ -12,7 +12,11 @@
          (name 'guix)
          (url "https://cgit.wugi.info/git/guix/guix")
          (commit "f7e14782025bf87aaef694a21f34010b1a95f7f6") ;v1.0.1
-         (introduction (@@ (guix channels) %guix-channel-introduction)))))
+         (introduction
+          (make-channel-introduction
+           "9edb3f66fd807b096b48283debdcddccfea34bad"
+           (openpgp-fingerprint
+            "BBB0 2DDF 2CEA F6A8 0D1D  E643 A2A0 6DF2 A33A 54FA"))))))
 
 (define (inferior)
   (inferior-for-channels channels))
