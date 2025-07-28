97129ccd037cb54bbf3965e58e255452d4f104af
Oleg Pykhalov <go.wigust@gmail.com>
2025-07-28 23:52:29 +0300

    guix: pc0: Fix run scripts.

--- guix/dotfiles/run/pc0/00-run.sh
+++ guix/dotfiles/run/pc0/00-run.sh
@@ -4,7 +4,7 @@ set -o nounset -o errexit -o pipefail
 
 swapon /dev/vg0/swap
 
-/home/oleg/.local/share/chezmoi/dotfiles/run/pc0/12-run-win10.sh
+/home/oleg/.local/share/chezmoi/guix/dotfiles/run/pc0/12-run-win10.sh
 # /home/oleg/.local/share/chezmoi/dotfiles/run/pc0/03-net.sh
 
 rm /var/lib/kubelet/.maintenance
--- guix/dotfiles/run/pc0/12-run-win10.sh
+++ guix/dotfiles/run/pc0/12-run-win10.sh
@@ -2,7 +2,7 @@
 
 set -o nounset -o errexit -o pipefail
 
-/home/oleg/.local/share/chezmoi/dotfiles/run/pc0/02-virsh-detach-gpu.sh
-/home/oleg/.local/share/chezmoi/dotfiles/run/pc0/07-video.sh
-/home/oleg/.local/share/chezmoi/dotfiles/run/pc0/08-lg.sh
+/home/oleg/.local/share/chezmoi/guix/dotfiles/run/pc0/02-virsh-detach-gpu.sh
+/home/oleg/.local/share/chezmoi/guix/dotfiles/run/pc0/07-video.sh
+/home/oleg/.local/share/chezmoi/guix/dotfiles/run/pc0/08-lg.sh
 virsh start win10
