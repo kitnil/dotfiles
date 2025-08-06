2a00278b6254bc5555597034441e3eca5c650fa1
Oleg Pykhalov <go.wigust@gmail.com>
2025-08-06 11:07:51 +0300

    guix: Configure docker-image.

--- guix/wugi/system/docker-image.scm
+++ guix/wugi/system/docker-image.scm
@@ -8,6 +8,24 @@
 
 (define-module (wugi system docker-image)
   #:use-module (gnu)
+  #:use-module (gnu packages)
+  #:use-module (gnu packages bash)
+  #:use-module (gnu packages package-management)
+  #:use-module (gnu packages ssh)
+  #:use-module (gnu services)
+  #:use-module (gnu services base)
+  #:use-module (gnu services desktop)
+  #:use-module (gnu services guix)
+  #:use-module (gnu services shepherd)
+  #:use-module (gnu services web)
+  #:use-module (gnu system linux-container)
+  #:use-module (guix channels)
+  #:use-module (guix gexp)
+  #:use-module (guix inferior)
+  #:use-module (guix packages)
+  #:use-module (guix profiles)
+  #:use-module (guix ui)
+  #:use-module (srfi srfi-1)
   #:export (%docker-image))
 
 (define (%docker-image)
@@ -22,14 +40,19 @@
                            (name "oleg")
                            (comment "Oleg Pykhalov")
                            (group "users")
-                           (supplementary-groups '("wheel" "audio" "video"))
+                           (supplementary-groups '("wheel"
+                                                   "audio"
+                                                   "video"
+                                                   "kvm"
+                                                   "input"))
                            (password (crypt "oleg" "NmhJoj")))
                          (user-account (inherit %root-account)
                                        (password (crypt "root" "uUxBgD"))))
                    %base-user-accounts))
 
     ;; Globally-installed packages.
-    (packages %base-packages)
+    (packages (append (list bash-completion openssh)
+                      %base-packages))
 
     ;; Because the system will run in a Docker container, we may omit many
     ;; things that would normally be required in an operating system
@@ -54,4 +77,69 @@
                           (type "does-not-matter"))))
 
     ;; Guix is all you need!
-    (services (list (service guix-service-type)))))
+    (services (append (list (service guix-service-type)
+                            (service nginx-service-type
+                                     (nginx-configuration
+                                       (server-blocks
+                                        (list
+                                         (nginx-server-configuration
+                                           (server-name '("guix.localhost"))
+                                           (listen '("*:80"))
+                                           (locations
+                                            (list
+                                             (nginx-location-configuration
+                                               (uri "/")
+                                               (body
+                                                '("resolver 80.80.80.80 ipv6=off;"
+                                                  "proxy_pass https://mirrors.sjtug.sjtu.edu.cn/guix/;"
+                                                  "proxy_set_header Host mirrors.sjtug.sjtu.edu.cn;"
+                                                  "proxy_ssl_server_name on;"
+                                                  "client_max_body_size 0;"
+                                                  "proxy_busy_buffers_size 512k;"
+                                                  "proxy_buffers 4 512k;"
+                                                  "proxy_buffer_size 256k;"
+                                                  "add_header Access-Control-Allow-Origin *;"))))))
+                                         (nginx-server-configuration
+                                           (server-name '("nonguix.localhost"))
+                                           (listen '("*:80"))
+                                           (locations
+                                            (list
+                                             (nginx-location-configuration
+                                               (uri "/")
+                                               (body
+                                                '("resolver 80.80.80.80 ipv6=off;"
+                                                  "proxy_pass https://nonguix-proxy.ditigal.xyz/;"
+                                                  "proxy_set_header Host nonguix-proxy.ditigal.xyz;"
+                                                  "proxy_ssl_server_name on;"
+                                                  "client_max_body_size 0;"
+                                                  "proxy_busy_buffers_size 512k;"
+                                                  "proxy_buffers 4 512k;"
+                                                  "proxy_buffer_size 256k;"
+                                                  "add_header Access-Control-Allow-Origin *;"))))))))))
+                            (elogind-service))
+                      (modify-services %base-services
+                        (guix-service-type config =>
+                                           (guix-configuration
+                                             (channels my-channels)
+                                             (guix (guix-for-channels my-channels))
+                                             (authorized-keys (append (list (local-file "/etc/substitutes/guix.wugi.info.pub")
+                                                                            (local-file "/etc/substitutes/vm1.wugi.info.pub")
+                                                                            (local-file "/etc/substitutes/vm2.wugi.info.pub")
+                                                                            (local-file "/etc/substitutes/mirror.brielmaier.net.pub")
+                                                                            (local-file "/etc/substitutes/substitutes.nonguix.org.pub")
+                                                                            (local-file "/etc/substitutes/bordeaux.guix.gnu.org.pub"))
+                                                                      %default-authorized-guix-keys))
+                                             (substitute-urls '("https://bordeaux.guix.gnu.org"))))
+                        (syslog-service-type config =>
+                                             (syslog-configuration
+                                               (extra-options '("--rcfile=/etc/syslog.conf"
+                                                                "--no-forward"
+                                                                "--no-unixaf"
+                                                                "--no-klog")))))))
+
+    (sudoers-file (plain-file "sudoers"
+                              (string-join `("Defaults:root runcwd=*"
+                                             "root ALL=(ALL) ALL"
+                                             "%wheel ALL=(ALL) ALL"
+                                             "oleg ALL=(ALL) NOPASSWD:ALL")
+                                           "\n")))))
