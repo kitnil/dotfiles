cfb7002603d79aceb26b06b9b0845ca52a0b6989
Oleg Pykhalov <go.wigust@gmail.com>
2025-08-07 07:13:41 +0300

    guix: Fix alacritty service.
    Changes alacritty theme and config file paths.
    
    * guix/wugi/home/services/terminals.scm: Adjusts Alacritty configuration for
    light and dark themes.

--- guix/wugi/home/services/terminals.scm
+++ guix/wugi/home/services/terminals.scm
@@ -16,49 +16,6 @@
 (define alacritty-theme-xterm-light
   (plain-file "xterm.toml" "\
 [colors.primary]
-foreground = \"0xFFFFFF\"
-background = \"0x000000\"
-
-[colors.normal]
-black = \"0x000000\"
-red = \"0xB21818\"
-green = \"0x66CD00\"
-yellow = \"0xB26818\"
-blue = \"0x4169e1\"
-magenta = \"0xB218B2\"
-cyan = \"0x18B2B2\"
-white = \"0xB2B2B2\"
-
-[colors.bright]
-black = \"0x686868\"
-red = \"0xFF5454\"
-green = \"0x54FF54\"
-yellow = \"0xEEC900\"
-blue = \"0x5454FF\"
-magenta = \"0xFF54FF\"
-cyan = \"0x54FFFF\"
-white = \"0xFFFFFF\"
-"))
-
-(define alacritty-config-file
-  (mixed-text-file "alacritty.toml" "\
-[env]
-TERM = \"xterm-256color\"
-
-[font]
-size = 10
-normal.family = \"Adwaita Mono\"
-bold.family = \"Adwaita Mono\"
-italic.family = \"Adwaita Mono\"
-bold_italic.family = \"Adwaita Mono\"
-
-[general]
-import = [ \"" alacritty-theme-xterm-light "\" ]
-live_config_reload = false"))
-
-(define alacritty-theme-xterm-dark
-  (plain-file "xterm.toml" "\
-[colors.primary]
 foreground = \"0x000000\"
 background = \"0xFFFFFF\"
 
@@ -83,7 +40,7 @@ cyan = \"0x54FFFF\"
 white = \"0xFFFFFF\"
 "))
 
-(define alacritty-xterm-dark-config-file
+(define alacritty-xterm-light-config-file
   (mixed-text-file "alacritty.toml" "\
 [env]
 TERM = \"xterm-256color\"
@@ -96,32 +53,92 @@ italic.family = \"Adwaita Mono\"
 bold_italic.family = \"Adwaita Mono\"
 
 [general]
-import = [ \"" alacritty-theme-xterm-dark "\" ]
+import = [ \"" alacritty-theme-xterm-light "\" ]
 live_config_reload = false"))
+
+(define alacritty-xterm-light-program
+  (program-file "alacritty-xterm-light"
+                #~(let ((args (cdr (command-line)))
+                        (alacritty-binary #$(file-append alacritty "/bin/alacritty")))
+                    (apply execl
+                           (append (list alacritty-binary
+                                         alacritty-binary
+                                         "--config-file" #$alacritty-xterm-light-config-file)
+                                   args)))))
+
+(define alacritty-theme-xterm-dark
+  (plain-file "xterm.toml" "\
+[colors.primary]
+foreground = \"0xFFFFFF\"
+background = \"0x000000\"
+
+[colors.normal]
+black = \"0x000000\"
+red = \"0xB21818\"
+green = \"0x66CD00\"
+yellow = \"0xB26818\"
+blue = \"0x4169e1\"
+magenta = \"0xB218B2\"
+cyan = \"0x18B2B2\"
+white = \"0xB2B2B2\"
+
+[colors.bright]
+black = \"0x686868\"
+red = \"0xFF5454\"
+green = \"0x54FF54\"
+yellow = \"0xEEC900\"
+blue = \"0x5454FF\"
+magenta = \"0xFF54FF\"
+cyan = \"0x54FFFF\"
+white = \"0xFFFFFF\"
+"))
+
+(define alacritty-xterm-dark-config-file
+  (mixed-text-file "alacritty.toml"
+                   "\
+[env]
+TERM = \"xterm-256color\"
+
+[font]
+size = 10
+normal.family = \"Adwaita Mono\"
+bold.family = \"Adwaita Mono\"
+italic.family = \"Adwaita Mono\"
+bold_italic.family = \"Adwaita Mono\"
+
+[general]
+import = [ \"" alacritty-theme-xterm-dark "\" ]
+live_config_reload = false"
+"\n"))
 
 (define alacritty-xterm-dark-program
   (program-file "alacritty-xterm-dark"
-              #~(let ((args (cdr (command-line))))
-                  (execl #$(file-append alacritty "/bin/alacritty")
-                         "--config-file" alacritty-xterm-dark-config-file
-                         (string-join args)))))
+                #~(let ((args (cdr (command-line)))
+                        (alacritty-binary #$(file-append alacritty "/bin/alacritty")))
+                    (apply execl
+                           (append (list alacritty-binary
+                                         alacritty-binary
+                                         "--config-file" #$alacritty-xterm-dark-config-file)
+                                   args)))))
 
 (define home-alacritty-service-type
   (service-type
-    (name 'alacritty)
-    (extensions
-     (list
-      (service-extension home-profile-service-type
-                         (lambda (config)
-                           (list alacritty
-                                 (package-from-program-file
-                                  alacritty-xterm-dark-program))))
-      (service-extension home-files-service-type
-                         (lambda (config)
-                           (list `(".config/alacritty/alacritty.toml"
-                                   ,alacritty-config-file))))))
-    (default-value '())
-    (description "Configure Alacritty, terminal emulator.")))
+   (name 'alacritty)
+   (extensions
+    (list
+     (service-extension home-profile-service-type
+                        (lambda (config)
+                          (list alacritty
+                                (package-from-program-file
+                                 alacritty-xterm-light-program)
+                                (package-from-program-file
+                                 alacritty-xterm-dark-program))))
+     (service-extension home-files-service-type
+                        (lambda (config)
+                          (list `(".config/alacritty/alacritty.toml"
+                                  ,alacritty-xterm-dark-config-file))))))
+   (default-value '())
+   (description "Configure Alacritty, terminal emulator.")))
 
 (define home-screen-service
   (simple-service 'screen-config
