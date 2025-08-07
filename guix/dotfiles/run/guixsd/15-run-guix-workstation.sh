121c9a219b818a0d70b063b6a4b33bf86be1439e
Oleg Pykhalov <go.wigust@gmail.com>
2025-08-07 16:15:57 +0300

    run: guixsd: Replace 10-avahi-namespace with 15-run-guix-workstation.
    Removes the avahi namespace setup script.
    
    * guix/dotfiles/run/guixsd/15-run-guix-workstation.sh: Create a new
    workstation network configuration.

---
+++ guix/dotfiles/run/guixsd/15-run-guix-workstation.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+ip netns add guix-workstation
+ip link add name guix0 type veth peer name guix1
+ip link set dev guix1 netns guix-workstation
+ip netns exec guix-workstation ip link set guix1 name eth0
+ip netns exec guix-workstation ip link set eth0 up
+ip link set guix0 master br0
+ip link set guix0 up
+ip netns exec guix-workstation ip addr add 192.168.0.196/24 dev eth0
+ip netns exec guix-workstation ip route add default via 192.168.0.1
