c35b43dde01f07d7098a16758acafabbccf5095c
Oleg Pykhalov <go.wigust@gmail.com>
2025-08-08 03:09:59 +0300

    guix: docker-image: Fix indentation.

--- guix/wugi/system/docker-image.scm
+++ guix/wugi/system/docker-image.scm
@@ -43,137 +43,137 @@ program.")))
 
   (define %my-operating-system
     (operating-system
-     (host-name "workstation")
-     (timezone "Europe/Moscow")
-     (locale "en_US.utf8")
+      (host-name "workstation")
+      (timezone "Europe/Moscow")
+      (locale "en_US.utf8")
 
-     ;; This is where user accounts are specified.  The "root" account is
-     ;; implicit, and is initially created with the empty password.
-     (users (append (list (user-account
-                           (name "oleg")
-                           (comment "Oleg Pykhalov")
-                           (group "users")
-                           (supplementary-groups '("wheel"
-                                                   "audio"
-                                                   "video"
-                                                   "kvm"
-                                                   "input"))
-                           (password (crypt "oleg" "NmhJoj")))
-                          (user-account (inherit %root-account)
-					(password (crypt "root" "uUxBgD"))))
-                    %base-user-accounts))
+      ;; This is where user accounts are specified.  The "root" account is
+      ;; implicit, and is initially created with the empty password.
+      (users (append (list (user-account
+                            (name "oleg")
+                            (comment "Oleg Pykhalov")
+                            (group "users")
+                            (supplementary-groups '("wheel"
+                                                    "audio"
+                                                    "video"
+                                                    "kvm"
+                                                    "input"))
+                            (password (crypt "oleg" "NmhJoj")))
+                           (user-account (inherit %root-account)
+                                         (password (crypt "root" "uUxBgD"))))
+                     %base-user-accounts))
 
-     ;; Globally-installed packages.
-     (packages (append (list bash-completion openssh)
-                       %base-packages))
+      ;; Globally-installed packages.
+      (packages (append (list bash-completion openssh)
+                        %base-packages))
 
-     ;; Because the system will run in a Docker container, we may omit many
-     ;; things that would normally be required in an operating system
-     ;; configuration file.  These things include:
-     ;;
-     ;;   * bootloader
-     ;;   * file-systems
-     ;;   * services such as mingetty, udevd, slim, networking, dhcp
-     ;;
-     ;; Either these things are simply not required, or Docker provides
-     ;; similar services for us.
+      ;; Because the system will run in a Docker container, we may omit many
+      ;; things that would normally be required in an operating system
+      ;; configuration file.  These things include:
+      ;;
+      ;;   * bootloader
+      ;;   * file-systems
+      ;;   * services such as mingetty, udevd, slim, networking, dhcp
+      ;;
+      ;; Either these things are simply not required, or Docker provides
+      ;; similar services for us.
 
-     ;; This will be ignored.
-     (bootloader (bootloader-configuration
-                  (bootloader grub-bootloader)
-                  (targets '("does-not-matter"))))
+      ;; This will be ignored.
+      (bootloader (bootloader-configuration
+                   (bootloader grub-bootloader)
+                   (targets '("does-not-matter"))))
 
-     ;; This will be ignored, too.
-     (file-systems (list (file-system
-                          (device "does-not-matter")
-                          (mount-point "/")
-                          (type "does-not-matter"))))
+      ;; This will be ignored, too.
+      (file-systems (list (file-system
+                            (device "does-not-matter")
+                            (mount-point "/")
+                            (type "does-not-matter"))))
 
-     ;; Guix is all you need!
-     (services
-      (append
-       (list
-	(service nginx-service-type
-                 (nginx-configuration
-                  (server-blocks
-                   (list
-                    (nginx-server-configuration
-                     (server-name '("guix.localhost"))
-                     (listen '("*:80"))
-                     (locations
-                      (list
-                       (nginx-location-configuration
-                        (uri "/")
-                        (body
-                         '("resolver 80.80.80.80 ipv6=off;"
-                           "proxy_pass https://mirrors.sjtug.sjtu.edu.cn/guix/;"
-                           "proxy_set_header Host mirrors.sjtug.sjtu.edu.cn;"
-                           "proxy_ssl_server_name on;"
-                           "client_max_body_size 0;"
-                           "proxy_busy_buffers_size 512k;"
-                           "proxy_buffers 4 512k;"
-                           "proxy_buffer_size 256k;"
-                           "add_header Access-Control-Allow-Origin *;"))))))
-                    (nginx-server-configuration
-                     (server-name '("nonguix.localhost"))
-                     (listen '("*:80"))
-                     (locations
-                      (list
-                       (nginx-location-configuration
-                        (uri "/")
-                        (body
-                         '("resolver 80.80.80.80 ipv6=off;"
-                           "proxy_pass https://nonguix-proxy.ditigal.xyz/;"
-                           "proxy_set_header Host nonguix-proxy.ditigal.xyz;"
-                           "proxy_ssl_server_name on;"
-                           "client_max_body_size 0;"
-                           "proxy_busy_buffers_size 512k;"
-                           "proxy_buffers 4 512k;"
-                           "proxy_buffer_size 256k;"
-                           "add_header Access-Control-Allow-Origin *;"))))))))))
-	(service elogind-service-type)
-	seatd-service
-	(service container-mingetty-service-type
-                                     (mingetty-configuration (tty "tty8")))
-	(service dbus-root-service-type))
-       (modify-services %base-services
-			(guix-service-type
-			 config =>
-			 (guix-configuration
-			  (channels %channels-docker-image)
-			  (guix (guix-for-channels %channels-docker-image))
-			  (authorized-keys
-			   (append
-			    (map (lambda (file-name)
-				   (local-file
-				    (string-append %distro-directory
-						   "/wugi/etc/substitutes/" file-name)))
-				 '("bordeaux.guix.gnu.org.pub"
-				   "guix-builder.pub"
-				   "guix.wugi.info.pub"
-				   "mirror.brielmaier.net.pub"
-				   "substitutes.nonguix.org.pub"
-				   "vm1.wugi.info.pub"
-				   "vm2.wugi.info.pub"))
-			    %default-authorized-guix-keys))
-			  (substitute-urls '("http://runc-kube1-guix-builder.guix:5556"
-					     "http://guix.localhost"
-					     "http://nonguix.localhost"))))
-			(syslog-service-type
-			 config =>
-			 (syslog-configuration
-			  (extra-options '("--rcfile=/etc/syslog.conf"
-					   "--no-forward"
-					   "--no-unixaf"
-					   "--no-klog")))))))
+      ;; Guix is all you need!
+      (services
+       (append
+        (list
+         (service nginx-service-type
+                  (nginx-configuration
+                   (server-blocks
+                    (list
+                     (nginx-server-configuration
+                      (server-name '("guix.localhost"))
+                      (listen '("*:80"))
+                      (locations
+                       (list
+                        (nginx-location-configuration
+                         (uri "/")
+                         (body
+                          '("resolver 80.80.80.80 ipv6=off;"
+                            "proxy_pass https://mirrors.sjtug.sjtu.edu.cn/guix/;"
+                            "proxy_set_header Host mirrors.sjtug.sjtu.edu.cn;"
+                            "proxy_ssl_server_name on;"
+                            "client_max_body_size 0;"
+                            "proxy_busy_buffers_size 512k;"
+                            "proxy_buffers 4 512k;"
+                            "proxy_buffer_size 256k;"
+                            "add_header Access-Control-Allow-Origin *;"))))))
+                     (nginx-server-configuration
+                      (server-name '("nonguix.localhost"))
+                      (listen '("*:80"))
+                      (locations
+                       (list
+                        (nginx-location-configuration
+                         (uri "/")
+                         (body
+                          '("resolver 80.80.80.80 ipv6=off;"
+                            "proxy_pass https://nonguix-proxy.ditigal.xyz/;"
+                            "proxy_set_header Host nonguix-proxy.ditigal.xyz;"
+                            "proxy_ssl_server_name on;"
+                            "client_max_body_size 0;"
+                            "proxy_busy_buffers_size 512k;"
+                            "proxy_buffers 4 512k;"
+                            "proxy_buffer_size 256k;"
+                            "add_header Access-Control-Allow-Origin *;"))))))))))
+         (service elogind-service-type)
+         seatd-service
+         (service container-mingetty-service-type
+                  (mingetty-configuration (tty "tty8")))
+         (service dbus-root-service-type))
+        (modify-services %base-services
+          (guix-service-type
+           config =>
+           (guix-configuration
+            (channels %channels-docker-image)
+            (guix (guix-for-channels %channels-docker-image))
+            (authorized-keys
+             (append
+              (map (lambda (file-name)
+                     (local-file
+                      (string-append %distro-directory
+                                     "/wugi/etc/substitutes/" file-name)))
+                   '("bordeaux.guix.gnu.org.pub"
+                     "guix-builder.pub"
+                     "guix.wugi.info.pub"
+                     "mirror.brielmaier.net.pub"
+                     "substitutes.nonguix.org.pub"
+                     "vm1.wugi.info.pub"
+                     "vm2.wugi.info.pub"))
+              %default-authorized-guix-keys))
+            (substitute-urls '("http://runc-kube1-guix-builder.guix:5556"
+                               "http://guix.localhost"
+                               "http://nonguix.localhost"))))
+          (syslog-service-type
+           config =>
+           (syslog-configuration
+            (extra-options '("--rcfile=/etc/syslog.conf"
+                             "--no-forward"
+                             "--no-unixaf"
+                             "--no-klog")))))))
 
-     (sudoers-file (plain-file "sudoers"
-                               (string-join `("Defaults:root runcwd=*"
-                                              "root ALL=(ALL) ALL"
-                                              "%wheel ALL=(ALL) ALL"
-                                              "oleg ALL=(ALL) NOPASSWD:ALL")
-                                            "\n")))))
+      (sudoers-file (plain-file "sudoers"
+                                (string-join `("Defaults:root runcwd=*"
+                                               "root ALL=(ALL) ALL"
+                                               "%wheel ALL=(ALL) ALL"
+                                               "oleg ALL=(ALL) NOPASSWD:ALL")
+                                             "\n")))))
 
   (containerized-operating-system %my-operating-system
-				  (cons %store-mapping '())
-				  #:shared-network? #t))
+                                  (cons %store-mapping '())
+                                  #:shared-network? #t))
