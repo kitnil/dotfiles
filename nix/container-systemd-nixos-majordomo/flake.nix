6a4887c39f1531a4784a33b4eb124bb5ec79fb6f
Oleg Pykhalov <go.wigust@gmail.com>
2025-08-14 13:18:15 +0300

    nix: Add container-systemd-nixos-majordomo.

---
+++ nix/container-systemd-nixos-majordomo/flake.nix
@@ -0,0 +1,29 @@
+{
+  description = "";
+
+  inputs = {
+    original.url = "git+file:/home/oleg/src/cgit.wugi.info/wigust/dotfiles?dir=nix/container-systemd-taskexecutor";
+  };
+
+  outputs = { self, original, ... }:
+    let
+      system = "x86_64-linux";
+    in
+      original.outputs // {
+        nixosConfigurations = {
+          nixos-systemd = original.nixosConfigurations.nixos-systemd.extendModules {
+            modules = [
+              {
+                home-manager = {
+                  users = {
+                    oleg = ./oleg/home-manager.nix;
+                  };
+                };
+              }
+              ./hosts/nixos-systemd.nix
+            ];
+          };
+        };
+   };
+}
+
