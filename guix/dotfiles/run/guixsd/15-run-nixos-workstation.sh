8f001a8cc7b411a368500e0817763ff5539a7d30
Oleg Pykhalov <go.wigust@gmail.com>
2025-08-20 18:25:21 +0300

    run: guixsd: Add runc script.

---
+++ guix/dotfiles/run/guixsd/15-run-nixos-workstation.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+ip netns add nixos-workstation
+ip link add name nixos0 type veth peer name nixos1
+ip link set dev nixos1 netns nixos-workstation
+ip netns exec nixos-workstation ip link set nixos1 name eth0
+ip netns exec nixos-workstation ip link set eth0 up
+ip link set nixos0 master br0
+ip link set nixos0 up
+ip netns exec nixos-workstation ip addr add 192.168.0.199/24 dev eth0
+ip netns exec nixos-workstation ip route add default via 192.168.0.1
