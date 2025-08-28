94fadefc5679332e2448b8aa29d67912abb32624
Oleg Pykhalov <go.wigust@gmail.com>
2025-08-28 17:14:11 +0300

    guix: wugi: Add pc0-guix-workstation channels.
    Defines a new docker image channel for Guix.
    
    * guix/wugi/etc/guix/channels/pc0-guix-workstation.scm: Introduces the
    %channels-docker-image definition.

---
+++ guix/wugi/etc/guix/channels/pc0-guix-workstation.scm
@@ -0,0 +1,18 @@
+(define-module (wugi etc guix channels docker-image)
+  #:use-module (guix channels)
+  #:export (%channels-docker-image))
+
+(define %channels-docker-image
+  (list (channel
+         (name 'guix)
+         (url "https://cgit.wugi.info/git/guix/guix")
+         (branch "master")
+         (commit
+          "a667d499181157d5382b53a467af156196627e7b")
+         (introduction
+          (make-channel-introduction
+           "9edb3f66fd807b096b48283debdcddccfea34bad"
+           (openpgp-fingerprint
+            "BBB0 2DDF 2CEA F6A8 0D1D  E643 A2A0 6DF2 A33A 54FA"))))))
+
+%channels-docker-image
