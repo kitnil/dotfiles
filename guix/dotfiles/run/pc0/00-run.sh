9ddd764769670f8a1c9513bf666bd12a903b0fce
Oleg Pykhalov <go.wigust@gmail.com>
2025-08-28 17:04:35 +0300

    guix: pc0: Update run scripts.
    * guix/dotfiles/run/pc0/00-run.sh: Modify script execution flow and add network namespaces.
    * guix/dotfiles/run/pc0/02-virsh-detach-gpu.sh: Fix GPU detach.  Correct GPU
    detachment sequence for stability.
    * guix/dotfiles/run/pc0/12-run-win10.sh: Win10 run changes. Add vfio module
    loading for GPU passthrough.

--- guix/dotfiles/run/pc0/00-run.sh
+++ guix/dotfiles/run/pc0/00-run.sh
@@ -2,11 +2,23 @@
 
 set -o nounset -o errexit -o pipefail
 
+iptables "-P" "FORWARD" "ACCEPT"
+
 swapon /dev/vg0/swap
 
+/home/oleg/.local/share/chezmoi/guix/dotfiles/run/pc0/15-guix-nanokvm-networking-namespace.sh
+/home/oleg/.local/share/chezmoi/guix/dotfiles/run/pc0/15-guix-rde-networking-namespace.sh
+/home/oleg/.local/share/chezmoi/guix/dotfiles/run/pc0/15-guix-workstation-networking-namespace.sh
+/home/oleg/.local/share/chezmoi/guix/dotfiles/run/pc0/15-nixos-majordomo-networking-namespace.sh
+/home/oleg/.local/share/chezmoi/guix/dotfiles/run/pc0/15-nixos-workstation-networking-namespace.sh
+
+mount -t tmpfs -o rw,relatime,size=100M,rshared none /mnt/guix-workstation/run
+mount -t tmpfs -o rw,relatime,size=100M,rshared none /mnt/guix-workstation/tmp
+
 /home/oleg/.local/share/chezmoi/guix/dotfiles/run/pc0/12-run-win10.sh
+
 # /home/oleg/.local/share/chezmoi/dotfiles/run/pc0/03-net.sh
 
-rm /var/lib/kubelet/.maintenance
-herd restart kubelet
+# rm /var/lib/kubelet/.maintenance
+# herd restart kubelet
 # /home/oleg/.local/share/chezmoi/dotfiles/run/pc0/13-guix-workstation-run.sh
