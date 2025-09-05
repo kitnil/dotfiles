e993335b8807c13d6de0f65147c5086a2cc5bb3e
Oleg Pykhalov <go.wigust@gmail.com>
2025-09-05 03:02:17 +0300

    guix: pc0: Mount guix-workstation rootfs.

--- guix/dotfiles/run/pc0/00-run.sh
+++ guix/dotfiles/run/pc0/00-run.sh
@@ -15,6 +15,7 @@ swapon /dev/vg0/swap
 mount -t tmpfs -o rw,relatime,size=100M,rshared none /mnt/guix-workstation/run
 mount -t tmpfs -o rw,relatime,size=100M,rshared none /mnt/guix-workstation/tmp
 
+mount /dev/vg0/guixworkstation /srv/runc/guix-workstation
 mount /dev/vg0/nixosmajordomo /srv/runc/nixos-majordomo
 
 /home/oleg/src/cgit.wugi.info/wigust/dotfiles/guix/dotfiles/run/pc0/12-run-win10.sh
