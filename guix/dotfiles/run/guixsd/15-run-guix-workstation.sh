81d0d8a8a86e016a678b20d7f84b1f5231877217
Oleg Pykhalov <go.wigust@gmail.com>
2025-10-06 21:23:49 +0300

    guix: Use different ip for guix-workstation on guixsd.

--- guix/dotfiles/run/guixsd/15-run-guix-workstation.sh
+++ guix/dotfiles/run/guixsd/15-run-guix-workstation.sh
@@ -6,5 +6,5 @@ ip netns exec guix-workstation ip link set guix1 name eth0
 ip netns exec guix-workstation ip link set eth0 up
 ip link set guix0 master br0
 ip link set guix0 up
-ip netns exec guix-workstation ip addr add 192.168.0.198/24 dev eth0
+ip netns exec guix-workstation ip addr add 192.168.0.191/24 dev eth0
 ip netns exec guix-workstation ip route add default via 192.168.0.1
