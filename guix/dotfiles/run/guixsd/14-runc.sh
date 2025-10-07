36c62b683410a158bcb192331a41c5bd3a2d2bda
Oleg Pykhalov <go.wigust@gmail.com>
2025-10-07 03:40:25 +0300

    run: guixsd: Delete 14-runc.

--- guix/dotfiles/run/guixsd/14-runc.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh -e
-
-mkdir -p /mnt/guix-workstation/tmp
-mount -t tmpfs -o rw,relatime,size=100M,rshared none /mnt/guix-workstation/tmp
-
-mkdir -p /mnt/guix-workstation/run
-mount -t tmpfs -o rw,relatime,size=100M,rshared none /mnt/guix-workstation/run
