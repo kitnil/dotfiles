435b001ebfe63ada591a4aa032108c2baeb7f7fe
Oleg Pykhalov <go.wigust@gmail.com>
2025-10-17 17:13:00 +0300

    nix: container-systemd-nixos-workstation: Sort plugins.

--- nix/container-systemd-nixos-workstation/oleg/home-manager.nix
+++ nix/container-systemd-nixos-workstation/oleg/home-manager.nix
@@ -8,10 +8,10 @@
   programs.obs-studio = {
     enable = true;
     plugins = with pkgs; [
-      obs-studio-plugins.obs-ndi
       obs-studio-plugins.looking-glass-obs
-      obs-studio-plugins.obs-pipewire-audio-capture
       obs-studio-plugins.obs-multi-rtmp
+      obs-studio-plugins.obs-ndi
+      obs-studio-plugins.obs-pipewire-audio-capture
     ];
   };
 }
