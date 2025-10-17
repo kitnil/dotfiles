bf202060d56ccb4e82ba5b065c7bcf695953ee64
Oleg Pykhalov <go.wigust@gmail.com>
2025-10-17 17:13:08 +0300

    nix: container-systemd-nixos-workstation: Delete looking-glass-obs.
    Take a lot of FPS from games.

--- nix/container-systemd-nixos-workstation/oleg/home-manager.nix
+++ nix/container-systemd-nixos-workstation/oleg/home-manager.nix
@@ -8,7 +8,6 @@
   programs.obs-studio = {
     enable = true;
     plugins = with pkgs; [
-      obs-studio-plugins.looking-glass-obs
       obs-studio-plugins.obs-multi-rtmp
       obs-studio-plugins.obs-ndi
       obs-studio-plugins.obs-pipewire-audio-capture
