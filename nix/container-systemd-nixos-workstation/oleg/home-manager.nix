324c8dfdee30a5618af3c5b371dcf7494997541a
Oleg Pykhalov <go.wigust@gmail.com>
2025-10-21 02:56:22 +0300

    nix: container-systemd-nixos-workstation: Add path-of-building-json.

--- nix/container-systemd-nixos-workstation/oleg/home-manager.nix
+++ nix/container-systemd-nixos-workstation/oleg/home-manager.nix
@@ -1,5 +1,28 @@
 { pkgs, lib, config, ... }:
 
+let
+  path-of-building-data-json = with pkgs;
+    let
+      lua-json = fetchFromGitHub {
+        owner = "rxi";
+        repo = "json.lua";
+        rev = "v0.1.2";
+        hash = "sha256-JSKMxF5NSHW3QaELFPWm1sx7kHmOXEPsUkM3i/px7Gk=";
+      };
+      poeJson = writeScript "pob-json.lua" ''
+        json = require('json')
+        require('HeadlessWrapper')
+        print(json.encode(data.itemBases))
+      '';
+    in
+    pkgs.writeScriptBin "path-of-building-data-json" ''
+      #!${pkgs.runtimeShell}
+      export LUA_PATH=';;;${lua-json}/?.lua;${pkgs.path-of-building.data}/?.lua;${pkgs.luajit}/share/lua/5.1/?.lua;${pkgs.luajit.pkgs.lua-curl}/share/lua/5.1/?.lua;'
+      export LUA_CPATH=';;;${pkgs.luajit.pkgs.lua-curl}/lib/lua/5.1/?.so;${pkgs.luajit.pkgs.luautf8}/lib/lua/5.1/?.so'
+      cd ${pkgs.path-of-building.data}/src
+      ${pkgs.luajit}/bin/luajit ${poeJson}
+    '';
+in
 {
   programs.chromium = {
     enable = true;
@@ -13,6 +36,7 @@
       obs-studio-plugins.obs-pipewire-audio-capture
 
       path-of-building
+      path-of-building-data-json
     ];
   };
 }
