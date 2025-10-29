2e237d370003dfb9ec367be166be6657846edf05
Oleg Pykhalov <go.wigust@gmail.com>
2025-10-29 23:07:08 +0300

    guix: pc0-guix-workstation: Add guix-nanokvm runc container.

--- guix/wugi/system/pc0-guix-workstation.scm
+++ guix/wugi/system/pc0-guix-workstation.scm
@@ -142,7 +142,12 @@ program.")))
          (service runc-container-service-type
                   (runc-container-configuration
                    (bundle "/srv/runc/nixos-majordomo")
-                   (name "nixos-majordomo"))))
+                   (name "nixos-majordomo")))
+
+         (service runc-container-service-type
+                  (runc-container-configuration
+                   (bundle "/srv/runc/guix-nanokvm")
+                   (name "guix-nanokvm"))))
         (modify-services %base-services
           (delete console-font-service-type)
           (delete shepherd-system-log-service-type)
