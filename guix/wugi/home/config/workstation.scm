de4d24d993ae6f25f98b2c2d0e8b38076c34daaf
Oleg Pykhalov <go.wigust@gmail.com>
2025-10-30 14:25:34 +0300

    guix: workstation: Specify GUIX_PROFILE.
    Fixes dired and other code depending on GUIX_PROFILE variable.

--- guix/wugi/home/config/workstation.scm
+++ guix/wugi/home/config/workstation.scm
@@ -217,7 +217,11 @@ allow-preset-passphrase"))))
                                  ("PERL5LIB" . "${HOME}/perl5/lib/perl5${PERL5LIB:+:${PERL5LIB}}")
                                  ("PERL_LOCAL_LIB_ROOT" . "${HOME}/perl5${PERL_LOCAL_LIB_ROOT:+:${PERL_LOCAL_LIB_ROOT}}")
                                  ("PERL_MB_OPT" . "--install_base ${HOME}/perl5")
-                                 ("PERL_MM_OPT" . "INSTALL_BASE=${HOME}/perl5")))))
+                                 ("PERL_MM_OPT" . "INSTALL_BASE=${HOME}/perl5")))
+
+                              ;; GUIX_PROFILE variable used in my custom
+                              ;; .bashrc file.
+                              (variables '(("GUIX_PROFILE" . "${HOME}/.guix-home/profile")))))
                     home-mime-service
                     home-direnv-service
                     home-git-service
