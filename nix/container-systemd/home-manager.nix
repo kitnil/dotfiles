898bbe692001ccced7301d3fe81194d50f0958db
Oleg Pykhalov <go.wigust@gmail.com>
2025-10-30 08:01:17 +0300

    nix: container-systemd: Add firefoxBaseProfile.

--- nix/container-systemd/home-manager.nix
+++ nix/container-systemd/home-manager.nix
@@ -1,4 +1,4 @@
-{ pkgs, packages, lib, config, ... }:
+{ pkgs, packages, lib, config, firefoxBaseProfile, ... }:
 
 let
   inherit (lib) fold;
@@ -118,48 +118,6 @@ in {
     enable = true;
     profiles =
       let
-        firefoxBaseProfile = { ech ? false }: {
-          # TODO: Manage ~/.mozilla/firefox/nix/containers.json file with Nix.
-          # TODO: Manage ~/.mozilla/firefox/nix/cookies.sqlite somehow.
-          # TODO: Import ~/src/ssl/cert.p12 file with Nix.
-          settings = {
-            "browser.aboutConfig.showWarning" = false;
-            "browser.search.region" = "GB";
-            "browser.shell.checkDefaultBrowser" = false;
-            "browser.startup.homepage" = "about:newtab";
-            "browser.startup.page" = 3;
-            "browser.urlbar.placeholderName" = "DuckDuckGo";
-            "browser.urlbar.placeholderName.private" = "DuckDuckGo";
-            "distribution.searchplugins.defaultLocale" = "en-GB";
-            "doh-rollout.disable-heuristics" = true; # disable DNS over HTTPS
-            "extensions.pocket.enabled" = false;
-            "extensions.update.enabled" = false;
-            "general.useragent.locale" = "en-GB";
-            "general.warnOnAboutConfig" = false;
-            "startup.homepage_welcome_url" = "about:newtab";
-            "toolkit.telemetry.reportingpolicy.firstRun" = false;
-          } // (if ech then {} else {
-            "network.dns.echconfig.enabled" = false;
-            "network.dns.http3_echconfig.enabled" = false;
-          });
-          search = {
-            force = true;
-            default = "searxng";
-            engines = {
-              searxng = {
-                name = "SearxNG";
-                urls = [
-                  {
-                    template =
-                      "https://searxng.home.wugi.info"
-                      + "/search?q={searchTerms}&categories=general";
-                  }
-                ];
-                definedAliases = [ "@sng" ];
-              };
-            };
-          };
-        };
         firefoxBaseProfileWithExtensions = { ech ? true }: (firefoxBaseProfile { inherit ech; }) // {
           extensions = {
             packages =
