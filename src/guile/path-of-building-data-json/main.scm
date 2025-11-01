5b421ee8e4dca220842c6beef53ab25c98f274a8
Oleg Pykhalov <go.wigust@gmail.com>
2025-11-01 03:50:01 +0300

    path-of-building-data-json: Add base defence percentage.

--- src/guile/path-of-building-data-json/main.scm
+++ src/guile/path-of-building-data-json/main.scm
@@ -297,6 +297,9 @@
   (gem-level
    (conditional-value (poe-item-filter-conditional-value-configuration))
    "")
+  (base-defence-percentile
+   (conditional-value (poe-item-filter-conditional-value-configuration))
+   "")
   (base-types
    (base-types '())
    "")
@@ -572,6 +575,19 @@
                           (commentary "Decrease identified items font size.")
                           (identified? #t)
                           (set-font-size 20)
+                          (continue? #t))
+
+                         (poe-item-filter-block-configuration
+                          (commentary "Highlight good percentage.")
+                          (classes '("Body Armours" "Helmets" "Gloves" "Boots" "Shields"))
+                          (base-defence-percentile (poe-item-filter-conditional-value-configuration
+                                                    (value 70)
+                                                    (operator '>=)))
+                          (set-border-color (poe-item-filter-color-configuration
+                                             (red 0)
+                                             (green 255)
+                                             (blue 0)
+                                             (alpha 255)))
                           (continue? #t))
 
                          (poe-item-filter-block-configuration
