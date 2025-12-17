7106daf752c442b84c2672b45f72e1e7a4afbd76
Oleg Pykhalov <go.wigust@gmail.com>
2025-12-17 04:03:55 +0300

    nix: container-systemd-nixos-zapret: Configure zapret.

--- nix/container-systemd-nixos-zapret/hosts/nixos-systemd.nix
+++ nix/container-systemd-nixos-zapret/hosts/nixos-systemd.nix
@@ -36,9 +36,35 @@
   services.zapret = {
     enable = true;
     params = [
-      "--dpi-desync=fake,disorder2"
-      "--dpi-desync-ttl=1"
-      "--dpi-desync-autottl=2"
+      "--debug=1"
+
+      # youtube
+      "--dpi-desync=multidisorder"
+      "--dpi-desync-split-pos=midsld"
+      "--dpi-desync-split-pos=host+1"
     ];
+    # whitelist = [
+    #   "ggpht.com"
+    #   "googlevideo.com"
+    #   "jnn-pa.googleapis.com"
+    #   "stable.dl2.discordapp.net"
+    #   "wide-youtube.l.google.com"
+    #   "youtu.be"
+    #   "youtube.com"
+    #   "youtubeembeddedplayer.googleapis.com"
+    #   "youtubei.googleapis.com"
+    #   "youtubekids.com"
+    #   "youtube-nocookie.com"
+    #   "youtube-ui.l.google.com"
+    #   "yt3.ggpht.com"
+    #   "yt3.googleusercontent.com"
+    #   "yt4.ggpht.com"
+    #   "ytimg.com"
+    #   "ytimg.l.google.com"
+    #   "yt-video-upload.l.google.com"
+
+    #   "discord.com"
+    #   "discordapp.com"
+    # ];
   };
 }
