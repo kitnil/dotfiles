921dc2c6b28737b01f906ddcad7e6f9ad784cf77
Oleg Pykhalov <go.wigust@gmail.com>
2025-12-19 00:13:22 +0300

    nix: flake: Format with nixpkgs-fmt.

--- nix/flake.nix
+++ nix/flake.nix
@@ -53,13 +53,26 @@
 
   # nixConfig.allowUnfree = true;
 
-  outputs = { self, nixpkgs, nixpkgs-20-03, nixpkgs-20-03-firefox
-    , nixpkgs-phantomjs, nixpkgs-home-manager, home-manager, nur
+  outputs =
+    { self
+    , nixpkgs
+    , nixpkgs-20-03
+    , nixpkgs-20-03-firefox
+    , nixpkgs-phantomjs
+    , nixpkgs-home-manager
+    , home-manager
+    , nur
     , rycee-nur-expressions
-    , nixpkgs-idea, nixpkgs-idea-community
-    , nixpkgs-nixd, nixpkgs-copyq, nixpkgs-chatterino2
-    , nixpkgs-phpactor, kamadorueda-alejandra, flake-utils-plus
-    , ... }:
+    , nixpkgs-idea
+    , nixpkgs-idea-community
+    , nixpkgs-nixd
+    , nixpkgs-copyq
+    , nixpkgs-chatterino2
+    , nixpkgs-phpactor
+    , kamadorueda-alejandra
+    , flake-utils-plus
+    , ...
+    }:
     let
       system = "x86_64-linux";
       pkgs = import nixpkgs { inherit system; };
@@ -81,43 +94,48 @@
       };
       lib = pkgs.lib;
       inherit (lib) fold;
-    in {
+    in
+    {
       overlay = final: prev:
-        self.packages.${system} // (let
-          inherit (rycee-nur-expressions.lib.${system})
-            buildFirefoxXpiAddon;
-          inherit (prev) callPackage;
-        in {
-          inherit (import
-            (rycee-nur-expressions.outPath + "/default.nix") {
-              pkgs = prev;
+        self.packages.${system} // (
+          let
+            inherit (rycee-nur-expressions.lib.${system})
+              buildFirefoxXpiAddon;
+            inherit (prev) callPackage;
+          in
+          {
+            inherit (import
+              (rycee-nur-expressions.outPath + "/default.nix")
+              {
+                pkgs = prev;
+              })
+              mozilla-addons-to-nix;
+            inherit (nixpkgs-nixd.legacyPackages.${system}) nixd;
+            inherit (nixpkgs-copyq.legacyPackages.${system}) clipboard-jh;
+            inherit (nixpkgs-chatterino2.legacyPackages.${system})
+              chatterino2;
+            inherit (callPackage ./firefox/generated-firefox-addons.nix {
+              inherit buildFirefoxXpiAddon;
             })
-            mozilla-addons-to-nix;
-          inherit (nixpkgs-nixd.legacyPackages.${system}) nixd;
-          inherit (nixpkgs-copyq.legacyPackages.${system}) clipboard-jh;
-          inherit (nixpkgs-chatterino2.legacyPackages.${system})
-            chatterino2;
-          inherit (callPackage ./firefox/generated-firefox-addons.nix {
-            inherit buildFirefoxXpiAddon;
-          })
-            access-control-allow-origin auto_highlight certificate-pinner
-            cookie-quick-manager copy-all-tab-urls-we copy-as-org-mode
-            foxscroller google-container metube-downloader night-video-tuner
-            hello-goodbye hide-twitch-chat-users prometheus-formatter
-            right-click-search rocker_gestures russian-ru-language-pack
-            scroll_anywhere sitedelta-watch snaplinksplus soundfixer
-            tab-slideshow-we twitch-error-autorefresher view-page-archive
-            visited-link-enabler ultrawidify;
-          inherit (rycee-nur-expressions.packages.${system})
-            container-proxy copy-link-text copy-selection-as-markdown
-            ghosttext link-gopher lovely-forks redirector single-file
-            tab-reloader return-youtube-dislikes ublacklist;
-          firefox-addon-libredirect =
-            rycee-nur-expressions.packages.${system}.libredirect;
-          alejandra =
-            (kamadorueda-alejandra.packages.${system}).alejandra-x86_64-unknown-linux-gnu;
-          inherit (nixpkgs-home-manager.legacyPackages.${system}) robo3t;
-        });
+              access-control-allow-origin auto_highlight certificate-pinner
+              cookie-quick-manager copy-all-tab-urls-we copy-as-org-mode
+              foxscroller google-container metube-downloader night-video-tuner
+              hello-goodbye hide-twitch-chat-users prometheus-formatter
+              right-click-search rocker_gestures russian-ru-language-pack
+              scroll_anywhere sitedelta-watch snaplinksplus soundfixer
+              tab-slideshow-we twitch-error-autorefresher view-page-archive
+              visited-link-enabler ultrawidify;
+            inherit (rycee-nur-expressions.packages.${system})
+              container-proxy copy-link-text copy-selection-as-markdown
+              ghosttext link-gopher lovely-forks redirector single-file
+              tab-reloader return-youtube-dislikes ublacklist;
+            firefox-addon-libredirect =
+              rycee-nur-expressions.packages.${system}.libredirect;
+            alejandra =
+              (kamadorueda-alejandra.packages.${system}).alejandra-x86_64-unknown-linux-gnu;
+            inherit (nixpkgs-home-manager.legacyPackages.${system}) robo3t;
+          }
+        );
 
       devShell.${system} =
         let
@@ -125,7 +143,8 @@
             overlays = [
               (final: prev: {
                 inherit (import
-                  (rycee-nur-expressions.outPath + "/default.nix") {
+                  (rycee-nur-expressions.outPath + "/default.nix")
+                  {
                     pkgs = prev;
                   })
                   mozilla-addons-to-nix;
@@ -135,158 +154,173 @@
           };
           inherit (pkgs) mkShell;
         in
-          mkShell {
-            buildInputs = with pkgs; [
-              nix
-              mozilla-addons-to-nix
-            ];
-          };
-      packages.${system} = let
-        jenkins-plugins =
-          (import ./plugins.nix { inherit (pkgs) fetchurl stdenv; });
-      in fold (x: xs: xs // x) { } [
-        {
-          inherit (pkgs-20-03) nixfmt;
+        mkShell {
+          buildInputs = with pkgs; [
+            nix
+            mozilla-addons-to-nix
+          ];
+        };
+      packages.${system} =
+        let
+          jenkins-plugins =
+            (import ./plugins.nix { inherit (pkgs) fetchurl stdenv; });
+        in
+        fold (x: xs: xs // x) { } [
+          {
+            inherit (pkgs-20-03) nixfmt;
 
-          inherit (pkgs-20-03.python3Packages) yamllint;
+            inherit (pkgs-20-03.python3Packages) yamllint;
 
-          alerta = with pkgs-20-03;
-            python3Packages.alerta.overrideAttrs (old: {
-              patches = [
-                (fetchurl {
-                  url =
-                    "https://raw.githubusercontent.com/kitnil/dotfiles/af9f1d52f78955f482d33b8c113c68728b0619f1/dotfiles/nix/patches/alerta-top-narrow-output.patch";
-                  sha256 =
-                    "07mwlm5x2ia5k05h8b8i53db0r4qpava1jlj6q96r0204qjmi897";
+            alerta = with pkgs-20-03;
+              python3Packages.alerta.overrideAttrs (old: {
+                patches = [
+                  (fetchurl {
+                    url =
+                      "https://raw.githubusercontent.com/kitnil/dotfiles/af9f1d52f78955f482d33b8c113c68728b0619f1/dotfiles/nix/patches/alerta-top-narrow-output.patch";
+                    sha256 =
+                      "07mwlm5x2ia5k05h8b8i53db0r4qpava1jlj6q96r0204qjmi897";
+                  })
+                ];
+              });
+
+            # TODO: androidenv.androidPkgs_9_0.platform-tools
+
+            firefox-52-wrapper = with pkgs-20-03-firefox;
+              callPackage
+                ({ stdenv, firefox-esr-52 }:
+                  writeScriptBin "firefox-esr-52" ''
+                    #!${runtimeShell} -e
+                    test_directory="$(mktemp -d)"
+                    trap 'chmod -Rf +w "$test_directory"; rm -rf "$test_directory"' EXIT
+                    exec -a "$0" ${firefox-esr-52}/bin/firefox --new-instance --profile "$test_directory" --private-window "$@"
+                  '')
+                { };
+
+            jenkins = with pkgs;
+              let
+                pluginCmds = lib.attrsets.mapAttrsToList
+                  (n: v:
+                    "cp --recursive ${v}/*pi /home/oleg/.jenkins/plugins/${v.name}.jpi")
+                  jenkins-plugins;
+              in
+              callPackage
+                ({ stdenv, lib, openjdk8 }:
+                  stdenv.mkDerivation {
+                    inherit (jenkins) version;
+                    name = "jenkins";
+                    src = false;
+                    dontUnpack = true;
+                    buildInputs = [ openjdk8 jenkins ];
+                    buildPhase = ''
+                      cat > jenkins <<'EOF'
+                      #!${bash}/bin/bash
+                      rm --force --recursive /home/oleg/.jenkins/{plugins,war}
+                      mkdir --parents /home/oleg/.jenkins/plugins
+                      ${lib.strings.concatStringsSep "\n" pluginCmds}
+                      exec -a "$0" ${openjdk8}/bin/java -Xmx512m -jar ${jenkins}/webapps/jenkins.war "$@"
+                      EOF
+                    '';
+                    installPhase = ''
+                      mkdir -p $out/bin
+                      install -m555 jenkins $out/bin/jenkins
+                    '';
+                  })
+                { };
+
+            # TODO: Flake Add run-headphones.
+            # operating-system = nixos { config = { services.headphones.enable = true; }; };
+            # (stdenv.mkDerivation {
+            #   name = "run-headphones";
+            #   builder = writeScript "builder.sh" (''
+            #     source $stdenv/setup
+            #     mkdir -p $out/bin
+            #     cat > $out/bin/run-headphones <<'EOF'
+            #     #!${bash}/bin/bash
+            #     exec -a headphones ${with lib; (head ((filterAttrs (n: v: n == "headphones") (foldAttrs (n: a: [ n ] ++ a) [ ] operating-system.options.systemd.services.definitions)).headphones)).serviceConfig.ExecStart} "$@"
+            #     EOF
+            #     chmod 555 $out/bin/run-headphones
+            #   '');
+            # });
+
+            inherit ((import nixpkgs-idea {
+              inherit system;
+              config = { allowUnfree = true; };
+            }).idea)
+              idea-ultimate pycharm-professional;
+
+            inherit (nixpkgs-idea-community.legacyPackages.${system}.jetbrains)
+              idea-community;
+
+            inherit (nixpkgs-idea-community.legacyPackages.${system}) sunshine;
+
+            inherit (nixpkgs-phpactor.legacyPackages.${system}) phpactor;
+          }
+
+          ({ inherit (pkgs.nodePackages) node2nix; })
+
+          (
+            let
+              pkgs = import nixpkgs {
+                inherit system;
+                config = { allowUnfree = true; };
+              };
+            in
+            {
+              inherit (pkgs) discord google-chrome;
+              chromium-wrapper = with pkgs;
+                callPackage
+                  ({ stdenv, google-chrome }:
+                    stdenv.mkDerivation {
+                      name = "chromium";
+                      src = false;
+                      dontUnpack = true;
+                      installPhase = ''
+                        mkdir -p $out/bin
+                        ln -s ${google-chrome}/bin/google-chrome-stable $out/bin/chromium
+                      '';
+                    })
+                  { };
+            }
+          )
+
+          {
+            jenkins-job-builder = pkgs.callPackage
+              ({ stdenv, bash, jenkins-job-builder }:
+                stdenv.mkDerivation {
+                  pname = "jenkins-job-builder";
+                  version = jenkins-job-builder.version;
+                  src = false;
+                  dontUnpack = true;
+                  buildInputs = [ bash jenkins-job-builder ];
+                  buildPhase = ''
+                    cat > jenkins-jobs <<'EOF'
+                    #!${bash}/bin/bash -e
+                    PYTHONPATH="" exec ${jenkins-job-builder}/bin/jenkins-jobs "$@"
+                    EOF
+                  '';
+                  installPhase = ''
+                    mkdir -p "$out"/bin
+                    install jenkins-jobs "$out"/bin/jenkins-jobs
+                  '';
                 })
-              ];
-            });
-
-          # TODO: androidenv.androidPkgs_9_0.platform-tools
-
-          firefox-52-wrapper = with pkgs-20-03-firefox;
-            callPackage ({ stdenv, firefox-esr-52 }:
-              writeScriptBin "firefox-esr-52" ''
+              { inherit (pkgs-20-03.pythonPackages) jenkins-job-builder; };
+            python-selenium = with import nixpkgs-phantomjs { inherit system; };
+              let
+                python3WithSelenium = python3.withPackages (python-packages:
+                  with python-packages; [
+                    selenium
+                    prometheus_client
+                    pyaml
+                  ]);
+                inherit coreutils;
+              in
+              writeScriptBin "python-selenium" ''
                 #!${runtimeShell} -e
-                test_directory="$(mktemp -d)"
-                trap 'chmod -Rf +w "$test_directory"; rm -rf "$test_directory"' EXIT
-                exec -a "$0" ${firefox-esr-52}/bin/firefox --new-instance --profile "$test_directory" --private-window "$@"
-              '') { };
-
-          jenkins = with pkgs;
-            let
-              pluginCmds = lib.attrsets.mapAttrsToList (n: v:
-                "cp --recursive ${v}/*pi /home/oleg/.jenkins/plugins/${v.name}.jpi")
-                jenkins-plugins;
-            in callPackage ({ stdenv, lib, openjdk8 }:
-              stdenv.mkDerivation {
-                inherit (jenkins) version;
-                name = "jenkins";
-                src = false;
-                dontUnpack = true;
-                buildInputs = [ openjdk8 jenkins ];
-                buildPhase = ''
-                  cat > jenkins <<'EOF'
-                  #!${bash}/bin/bash
-                  rm --force --recursive /home/oleg/.jenkins/{plugins,war}
-                  mkdir --parents /home/oleg/.jenkins/plugins
-                  ${lib.strings.concatStringsSep "\n" pluginCmds}
-                  exec -a "$0" ${openjdk8}/bin/java -Xmx512m -jar ${jenkins}/webapps/jenkins.war "$@"
-                  EOF
-                '';
-                installPhase = ''
-                  mkdir -p $out/bin
-                  install -m555 jenkins $out/bin/jenkins
-                '';
-              }) { };
-
-          # TODO: Flake Add run-headphones.
-          # operating-system = nixos { config = { services.headphones.enable = true; }; };
-          # (stdenv.mkDerivation {
-          #   name = "run-headphones";
-          #   builder = writeScript "builder.sh" (''
-          #     source $stdenv/setup
-          #     mkdir -p $out/bin
-          #     cat > $out/bin/run-headphones <<'EOF'
-          #     #!${bash}/bin/bash
-          #     exec -a headphones ${with lib; (head ((filterAttrs (n: v: n == "headphones") (foldAttrs (n: a: [ n ] ++ a) [ ] operating-system.options.systemd.services.definitions)).headphones)).serviceConfig.ExecStart} "$@"
-          #     EOF
-          #     chmod 555 $out/bin/run-headphones
-          #   '');
-          # });
-
-          inherit ((import nixpkgs-idea {
-            inherit system;
-            config = { allowUnfree = true; };
-          }).idea)
-            idea-ultimate pycharm-professional;
-
-          inherit (nixpkgs-idea-community.legacyPackages.${system}.jetbrains)
-            idea-community;
-
-          inherit (nixpkgs-idea-community.legacyPackages.${system}) sunshine;
-
-          inherit (nixpkgs-phpactor.legacyPackages.${system}) phpactor;
-        }
-
-        ({ inherit (pkgs.nodePackages) node2nix; })
-
-        (let
-          pkgs = import nixpkgs {
-            inherit system;
-            config = { allowUnfree = true; };
-          };
-        in {
-          inherit (pkgs) discord google-chrome;
-          chromium-wrapper = with pkgs;
-            callPackage ({ stdenv, google-chrome }:
-              stdenv.mkDerivation {
-                name = "chromium";
-                src = false;
-                dontUnpack = true;
-                installPhase = ''
-                  mkdir -p $out/bin
-                  ln -s ${google-chrome}/bin/google-chrome-stable $out/bin/chromium
-                '';
-              }) { };
-        })
-
-        {
-          jenkins-job-builder = pkgs.callPackage
-            ({ stdenv, bash, jenkins-job-builder }:
-              stdenv.mkDerivation {
-                pname = "jenkins-job-builder";
-                version = jenkins-job-builder.version;
-                src = false;
-                dontUnpack = true;
-                buildInputs = [ bash jenkins-job-builder ];
-                buildPhase = ''
-                  cat > jenkins-jobs <<'EOF'
-                  #!${bash}/bin/bash -e
-                  PYTHONPATH="" exec ${jenkins-job-builder}/bin/jenkins-jobs "$@"
-                  EOF
-                '';
-                installPhase = ''
-                  mkdir -p "$out"/bin
-                  install jenkins-jobs "$out"/bin/jenkins-jobs
-                '';
-              }) { inherit (pkgs-20-03.pythonPackages) jenkins-job-builder; };
-          python-selenium = with import nixpkgs-phantomjs { inherit system; };
-            let
-              python3WithSelenium = python3.withPackages (python-packages:
-                with python-packages; [
-                  selenium
-                  prometheus_client
-                  pyaml
-                ]);
-              inherit coreutils;
-            in writeScriptBin "python-selenium" ''
-              #!${runtimeShell} -e
-              PATH=${geckodriver}/bin:${firefox}/bin:"$PATH"
-              exec ${coreutils}/bin/env --ignore-environment ${runtimeShell} -c "PATH=$PATH DISPLAY=$DISPLAY exec -a $0 ${python3WithSelenium}/bin/python $*"
-            '';
-        }
-      ];
+                PATH=${geckodriver}/bin:${firefox}/bin:"$PATH"
+                exec ${coreutils}/bin/env --ignore-environment ${runtimeShell} -c "PATH=$PATH DISPLAY=$DISPLAY exec -a $0 ${python3WithSelenium}/bin/python $*"
+              '';
+          }
+        ];
 
       nixosModules = {
         home-manager-chatterino = import ./modules/services/chatterino.nix;
