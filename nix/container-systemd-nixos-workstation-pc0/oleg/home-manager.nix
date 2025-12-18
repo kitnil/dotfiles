1c4d9b6168fa812b1253e0cb2ee996f1b877d443
Oleg Pykhalov <go.wigust@gmail.com>
2025-12-18 15:32:47 +0300

    nix: Delete path-of-building-data-json.

--- nix/container-systemd-nixos-workstation-pc0/oleg/home-manager.nix
+++ nix/container-systemd-nixos-workstation-pc0/oleg/home-manager.nix
@@ -2,27 +2,6 @@
 
 let
   inherit (customLib) firefoxBaseProfile;
-  path-of-building-data-json = with pkgs;
-    let
-      lua-json = fetchFromGitHub {
-        owner = "rxi";
-        repo = "json.lua";
-        rev = "v0.1.2";
-        hash = "sha256-JSKMxF5NSHW3QaELFPWm1sx7kHmOXEPsUkM3i/px7Gk=";
-      };
-      poeJson = writeScript "pob-json.lua" ''
-        json = require('json')
-        require('HeadlessWrapper')
-        print(json.encode(data.itemBases))
-      '';
-    in
-    pkgs.writeScriptBin "path-of-building-data-json" ''
-      #!${pkgs.runtimeShell}
-      export LUA_PATH=';;;${lua-json}/?.lua;${pkgs.path-of-building.data}/?.lua;${pkgs.luajit}/share/lua/5.1/?.lua;${pkgs.luajit.pkgs.lua-curl}/share/lua/5.1/?.lua;'
-      export LUA_CPATH=';;;${pkgs.luajit.pkgs.lua-curl}/lib/lua/5.1/?.so;${pkgs.luajit.pkgs.luautf8}/lib/lua/5.1/?.so'
-      cd ${pkgs.path-of-building.data}/src
-      ${pkgs.luajit}/bin/luajit ${poeJson}
-    '';
 in
 {
   programs.chromium = {
@@ -37,9 +16,6 @@ in
       obs-studio-plugins.obs-multi-rtmp
       obs-studio-plugins.distroav
       obs-studio-plugins.obs-pipewire-audio-capture
-
-      path-of-building
-      path-of-building-data-json
 
       teamspeak3
 
