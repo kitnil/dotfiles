0c9afbd1ef3f837441162e06645304a1776b635c
Oleg Pykhalov <go.wigust@gmail.com>
2026-01-07 05:40:35 +0300

    nix: container-systemd-nixos-workstation-pc0: Configure bird.

--- nix/container-systemd-nixos-workstation-pc0/hosts/nixos-systemd.nix
+++ nix/container-systemd-nixos-workstation-pc0/hosts/nixos-systemd.nix
@@ -13,7 +13,35 @@
   };
   services.bird = {
     enable = true;
-    config = builtins.readFile ./bird.conf;
+    config = lib.readFile ./../bird.1.conf;
     checkConfig = false;
   };
+  environment.etc = {
+    "bird/bird.conf" = {
+      mode = "0644";
+    };
+    "bird/peers/guixsd.conf" = {
+      text = lib.readFile ./../peers/guixsd.conf;
+      mode = "0644";
+    };
+    "bird/peers/nixos-gw.conf" = {
+      text = lib.readFile ./../peers/nixos-gw.conf;
+      mode = "0644";
+    };
+    "bird/peers/nixos-majordomo.conf" = {
+      text = lib.readFile ./../peers/nixos-majordomo.conf;
+      mode = "0644";
+    };
+    "bird/peers/nixos-tor.conf" = {
+      text = lib.readFile ./../peers/nixos-tor.conf;
+      mode = "0644";
+    };
+    "bird/peers/nixos-zapret.conf" = {
+      text = lib.readFile ./../peers/nixos-zapret.conf;
+      mode = "0644";
+    };
+  };
+  systemd.tmpfiles.rules = [
+    "f /var/log/bird.log 0644 bird bird -"
+  ];
 }
