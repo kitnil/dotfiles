791b8abb5bdcccffdf99b64493db7aaf6da737df
Oleg Pykhalov <go.wigust@gmail.com>
2026-01-07 04:56:42 +0300

    nix: container-systemd-nixos-antifilter: Configure bird.

--- nix/container-systemd-nixos-antifilter/hosts/nixos-systemd.nix
+++ nix/container-systemd-nixos-antifilter/hosts/nixos-systemd.nix
@@ -7,7 +7,35 @@
 {
   services.bird = {
     enable = true;
-    config = builtins.readFile ./bird.conf;
+    config = lib.readFile ./../bird.1.conf;
     checkConfig = false;
   };
+  environment.etc = {
+    "bird/bird.conf" = {
+      mode = "0644";
+    };
+    "bird/peers/nixos-antifilter.0.conf" = {
+      text = lib.readFile ./../peers/nixos-antifilter.0.conf;
+      mode = "0644";
+    };
+    # "bird/peers/nixos-antifilter.1.conf" = {
+    #   text = lib.readFile ./../peers/nixos-antifilter.1.conf;
+    #   mode = "0644";
+    # };
+    "bird/peers/nixos-gw.conf" = {
+      text = lib.readFile ./../peers/nixos-gw.conf;
+      mode = "0644";
+    };
+    "bird/peers/nixos-tor.conf" = {
+      text = lib.readFile ./../peers/nixos-tor.conf;
+      mode = "0644";
+    };
+    "bird/peers/nixos-zapret.conf" = {
+      text = lib.readFile ./../peers/nixos-zapret.conf;
+      mode = "0644";
+    };
+  };
+  systemd.tmpfiles.rules = [
+    "f /var/log/bird.log 0644 bird bird -"
+  ];
 }
