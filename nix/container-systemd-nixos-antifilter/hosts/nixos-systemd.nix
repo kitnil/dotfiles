faf81e4730c4a630863dac44d6d90ab6b9960f5a
Oleg Pykhalov <go.wigust@gmail.com>
2026-01-08 04:13:37 +0300

    nix: container-systemd-nixos-antifilter: Add ipv4 forwarding.

--- nix/container-systemd-nixos-antifilter/hosts/nixos-systemd.nix
+++ nix/container-systemd-nixos-antifilter/hosts/nixos-systemd.nix
@@ -5,6 +5,9 @@
 { pkgs, lib, ... }:
 
 {
+  boot.kernel.sysctl = {
+    "net.ipv4.conf.all.forwarding" = 1;
+  };
   services.bird = {
     enable = true;
     config = lib.readFile ./../bird.1.conf;
