2f992eb03ce8b84ed921ee76b70793cd3087a7a7
Oleg Pykhalov <go.wigust@gmail.com>
2026-01-10 19:28:20 +0300

    nix: container-systemd-nixos-dante: Add nixos-wan peer.

--- nix/container-systemd-nixos-dante/hosts/nixos-systemd.nix
+++ nix/container-systemd-nixos-dante/hosts/nixos-systemd.nix
@@ -42,6 +42,10 @@
       text = lib.readFile ./../peers/nixos-gw.conf;
       mode = "0644";
     };
+    "bird/peers/nixos-wan.conf" = {
+      text = lib.readFile ./../peers/nixos-wan.conf;
+      mode = "0644";
+    };
   };
   systemd.services.bird.reloadTriggers = [
     config.environment.etc."bird/bird.conf".source
@@ -52,6 +56,7 @@
     config.environment.etc."bird/peers/nixos-zapret.conf".source
     config.environment.etc."bird/peers/nixos-awg.conf".source
     config.environment.etc."bird/peers/nixos-gw.conf".source
+    config.environment.etc."bird/peers/nixos-wan.conf".source
   ];
   systemd.tmpfiles.rules = [
     "f /var/log/bird.log 0644 bird bird -"
