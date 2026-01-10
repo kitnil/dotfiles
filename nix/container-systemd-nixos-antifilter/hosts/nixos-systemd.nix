b197eb1edc615d590584231ef8ba3c86d608bb0d
Oleg Pykhalov <go.wigust@gmail.com>
2026-01-10 22:08:54 +0300

    nix: container-systemd-nixos-antifilter: Add Prometheus MTR exporter.

--- nix/container-systemd-nixos-antifilter/hosts/nixos-systemd.nix
+++ nix/container-systemd-nixos-antifilter/hosts/nixos-systemd.nix
@@ -104,4 +104,15 @@
       }
     '';
   };
+  services.mtr-exporter = {
+    enable = true;
+    address = "0.0.0.0";
+    port = 31247;
+    jobs = [
+      {
+        name = "vm1.wugi.info";
+        address = "78.108.82.44";
+      }
+    ];
+  };
 }
