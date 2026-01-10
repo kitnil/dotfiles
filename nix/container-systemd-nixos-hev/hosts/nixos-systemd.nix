fc9b2d86964a3d7ac83da7eb4be0c5b9eaf857d0
Oleg Pykhalov <go.wigust@gmail.com>
2026-01-10 11:16:53 +0300

    nix: container-systemd-nixos-hev: Add dante peer.

--- nix/container-systemd-nixos-hev/hosts/nixos-systemd.nix
+++ nix/container-systemd-nixos-hev/hosts/nixos-systemd.nix
@@ -22,10 +22,15 @@
       text = lib.readFile ./../peers/nixos-workstation.conf;
       mode = "0644";
     };
+    "bird/peers/nixos-dante.conf" = {
+      text = lib.readFile ./../peers/nixos-dante.conf;
+      mode = "0644";
+    };
   };
   systemd.services.bird.reloadTriggers = [
     config.environment.etc."bird/bird.conf".source
     config.environment.etc."bird/peers/nixos-antifilter.conf".source
+    config.environment.etc."bird/peers/nixos-dante.conf".source
     config.environment.etc."bird/peers/nixos-workstation.conf".source
   ];
   systemd.tmpfiles.rules = [
