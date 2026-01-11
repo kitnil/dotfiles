c2c8753587572ee845afd5ec4d68e99d419b3082
Oleg Pykhalov <go.wigust@gmail.com>
2026-01-11 09:45:46 +0300

    nix: Delete all sessions with nixos-workstation except hev.

--- nix/container-systemd-nixos-workstation-pc0/hosts/nixos-systemd.nix
+++ nix/container-systemd-nixos-workstation-pc0/hosts/nixos-systemd.nix
@@ -20,38 +20,6 @@
     "bird/bird.conf" = {
       mode = "0644";
     };
-    "bird/peers/guixsd.conf" = {
-      text = lib.readFile ./../peers/guixsd.conf;
-      mode = "0644";
-    };
-    "bird/peers/pc0.conf" = {
-      text = lib.readFile ./../peers/pc0.conf;
-      mode = "0644";
-    };
-    "bird/peers/nixos-gw.conf" = {
-      text = lib.readFile ./../peers/nixos-gw.conf;
-      mode = "0644";
-    };
-    "bird/peers/nixos-majordomo.conf" = {
-      text = lib.readFile ./../peers/nixos-majordomo.conf;
-      mode = "0644";
-    };
-    "bird/peers/nixos-tor.conf" = {
-      text = lib.readFile ./../peers/nixos-tor.conf;
-      mode = "0644";
-    };
-    "bird/peers/nixos-zapret.conf" = {
-      text = lib.readFile ./../peers/nixos-zapret.conf;
-      mode = "0644";
-    };
-    "bird/peers/nixos-awg.conf" = {
-      text = lib.readFile ./../peers/nixos-awg.conf;
-      mode = "0644";
-    };
-    "bird/peers/nixos-ws.conf" = {
-      text = lib.readFile ./../peers/nixos-ws.conf;
-      mode = "0644";
-    };
     "bird/peers/nixos-hev.conf" = {
       text = lib.readFile ./../peers/nixos-hev.conf;
       mode = "0644";
@@ -59,14 +27,6 @@
   };
    systemd.services.bird.reloadTriggers = [
     config.environment.etc."bird/bird.conf".source
-    config.environment.etc."bird/peers/guixsd.conf".source
-    config.environment.etc."bird/peers/pc0.conf".source
-    config.environment.etc."bird/peers/nixos-gw.conf".source
-    config.environment.etc."bird/peers/nixos-majordomo.conf".source
-    config.environment.etc."bird/peers/nixos-tor.conf".source
-    config.environment.etc."bird/peers/nixos-zapret.conf".source
-    config.environment.etc."bird/peers/nixos-awg.conf".source
-    config.environment.etc."bird/peers/nixos-ws.conf".source
     config.environment.etc."bird/peers/nixos-hev.conf".source
   ];
  systemd.tmpfiles.rules = [
