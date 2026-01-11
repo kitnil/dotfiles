06d5bfc6aa834664564a109f82dde9797cb03b98
Oleg Pykhalov <go.wigust@gmail.com>
2026-01-11 20:31:23 +0300

    nix: container-systemd-nixos-wan: Configure bird.

--- nix/container-systemd-nixos-wan/hosts/nixos-systemd.nix
+++ nix/container-systemd-nixos-wan/hosts/nixos-systemd.nix
@@ -21,20 +21,10 @@
       text = lib.readFile ./../peers/nixos-antifilter.conf;
       mode = "0644";
     };
-    "bird/peers/nixos-gw.conf" = {
-      text = lib.readFile ./../peers/nixos-gw.conf;
-      mode = "0644";
-    };
-    "bird/peers/nixos-dante.conf" = {
-      text = lib.readFile ./../peers/nixos-dante.conf;
-      mode = "0644";
-    };
   };
   systemd.services.bird.reloadTriggers = [
     config.environment.etc."bird/bird.conf".source
     config.environment.etc."bird/peers/nixos-antifilter.conf".source
-    config.environment.etc."bird/peers/nixos-gw.conf".source
-    config.environment.etc."bird/peers/nixos-dante.conf".source
   ];
   systemd.tmpfiles.rules = [
     "f /var/log/bird.log 0644 bird bird -"
