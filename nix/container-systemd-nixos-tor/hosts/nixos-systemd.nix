1255cb8abdec5b7d6df9e7b6730c67aae55231e4
Oleg Pykhalov <go.wigust@gmail.com>
2026-01-11 22:51:37 +0300

    nix: container-systemd-nixos-tor: Add extraSTopCommands to firewall.

--- nix/container-systemd-nixos-tor/hosts/nixos-systemd.nix
+++ nix/container-systemd-nixos-tor/hosts/nixos-systemd.nix
@@ -73,11 +73,17 @@
     9050                        # tor
     31247                       # prometheus mtr exporters
   ];
-  networking.firewall.enable = lib.mkForce true;
-  networking.firewall.extraCommands = ''
-    iptables -t nat -A PREROUTING -s 192.168.0.0/24 -p tcp --dport 80 -j REDIRECT --to-ports 8888
-    iptables -t nat -A PREROUTING -s 192.168.0.0/24 -p tcp --dport 443 -j REDIRECT --to-ports 8888
-  '';
+  networking.firewall = {
+    enable = lib.mkForce true;
+    extraCommands = ''
+      iptables -t nat -A PREROUTING -s 192.168.0.0/24 -p tcp --dport 80 -j REDIRECT --to-ports 8888
+      iptables -t nat -A PREROUTING -s 192.168.0.0/24 -p tcp --dport 443 -j REDIRECT --to-ports 8888
+    ''
+    extraSTopCommands = ''
+      iptables -t nat -D PREROUTING -s 192.168.0.0/24 -p tcp --dport 80 -j REDIRECT --to-ports 8888
+      iptables -t nat -D PREROUTING -s 192.168.0.0/24 -p tcp --dport 443 -j REDIRECT --to-ports 8888
+    ''
+  };
   services.prometheus.exporters.bird = {
     enable = true;
   };
