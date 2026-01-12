8e588468662ab9cd82ce586d7397d88128a4b882
Oleg Pykhalov <go.wigust@gmail.com>
2026-01-12 18:06:30 +0300

    nix: container-systemd-nixos-zapret: peers: Delete nixos-workstation.

--- nix/container-systemd-nixos-zapret/hosts/nixos-systemd.nix
+++ nix/container-systemd-nixos-zapret/hosts/nixos-systemd.nix
@@ -73,10 +73,6 @@
       text = lib.readFile ./../peers/nixos-antifilter.conf;
       mode = "0644";
     };
-    "bird/peers/nixos-workstation.conf" = {
-      text = lib.readFile ./../peers/nixos-workstation.conf;
-      mode = "0644";
-    };
     "bird/peers/nixos-dante.conf" = {
       text = lib.readFile ./../peers/nixos-dante.conf;
       mode = "0644";
@@ -85,7 +81,6 @@
   systemd.services.bird.reloadTriggers = [
     config.environment.etc."bird/bird.conf".source
     config.environment.etc."bird/peers/nixos-antifilter.conf".source
-    config.environment.etc."bird/peers/nixos-workstation.conf".source
     config.environment.etc."bird/peers/nixos-dante.conf".source
   ];
   systemd.tmpfiles.rules = [
