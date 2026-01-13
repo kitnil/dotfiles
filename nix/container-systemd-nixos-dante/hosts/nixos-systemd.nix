011047009b03c15078d587e14c25385bb8f885a5
Oleg Pykhalov <go.wigust@gmail.com>
2026-01-14 02:11:28 +0300

    nix: container-systemd-nixos-dante: Disable nscd.

--- nix/container-systemd-nixos-dante/hosts/nixos-systemd.nix
+++ nix/container-systemd-nixos-dante/hosts/nixos-systemd.nix
@@ -129,5 +129,5 @@ socks pass {
   local.services.prometheus.exporters.blackbox = {
     enable = true;
   };
-  services.nscd.enableNsncd = false; # high cpu usage
+  services.nscd.enable = false; # high cpu usage
 }
