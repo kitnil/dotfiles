febb9276f6cd781b95f50b75084ddc4930f2d7b0
Oleg Pykhalov <go.wigust@gmail.com>
2026-01-14 13:21:26 +0300

    nix: container-systemd-taskexecutor: Delete home.stateVersion.
    Conflicts.

--- nix/container-systemd-taskexecutor/oleg/home-manager.nix
+++ nix/container-systemd-taskexecutor/oleg/home-manager.nix
@@ -31,10 +31,4 @@
   ];
 
   services.vendir.enable = true;
-
-  # The home.stateVersion option no longer has a default value. It used to
-  # default to “18.09”, which was the Home Manager version that introduced the
-  # option. If your configuration does not explicitly set this option then you
-  # need to add
-  home.stateVersion = "22.05";
 }
