f598694d0c11836594bdd31f66d449c50bcc513f
Oleg Pykhalov <go.wigust@gmail.com>
2026-01-14 16:41:08 +0300

    nix: container-systemd-nixos-dante: peers: Add majordomo.

--- nix/container-systemd-nixos-dante/hosts/nixos-systemd.nix
+++ nix/container-systemd-nixos-dante/hosts/nixos-systemd.nix
@@ -41,6 +41,10 @@
       text = lib.readFile ./../peers/kubernetes.conf;
       mode = "0644";
     };
+    "bird/peers/nixos-majordomo.conf" = {
+      text = lib.readFile ./../peers/nixos-majordomo.conf;
+      mode = "0644";
+    };
   };
   systemd.services.bird.reloadTriggers = [
     config.environment.etc."bird/bird.conf".source
@@ -50,6 +54,7 @@
     config.environment.etc."bird/peers/nixos-awg.conf".source
     config.environment.etc."bird/peers/nixos-gw.conf".source
     config.environment.etc."bird/peers/kubernetes.conf".source
+    config.environment.etc."bird/peers/nixos-majordomo.conf".source
   ];
   systemd.tmpfiles.rules = [
     "f /var/log/bird.log 0644 bird bird -"
