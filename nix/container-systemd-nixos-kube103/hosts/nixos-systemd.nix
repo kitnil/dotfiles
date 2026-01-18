8f000da8c1f5720740697ca007074c0233582da5
Oleg Pykhalov <go.wigust@gmail.com>
2026-01-18 16:43:38 +0300

    nix: container-systemd-nixos-kube103: Add bird.

--- nix/container-systemd-nixos-kube103/hosts/nixos-systemd.nix
+++ nix/container-systemd-nixos-kube103/hosts/nixos-systemd.nix
@@ -8,6 +8,22 @@
   boot.kernel.sysctl = {
     "net.ipv4.conf.all.forwarding" = 1;
   };
+  services.bird = {
+    enable = true;
+    config = lib.readFile ./../bird.1.conf;
+    checkConfig = false;
+  };
+  environment.etc = {
+    "bird/bird.conf" = {
+      mode = "0644";
+    };
+  };
+  systemd.services.bird.reloadTriggers = [
+    config.environment.etc."bird/bird.conf".source
+  ];
+  systemd.tmpfiles.rules = [
+    "f /var/log/bird.log 0644 bird bird -"
+  ];
   services.webhook-custom = {
     enable = true;
     flake = ".#container-systemd-nixos-kube103";
