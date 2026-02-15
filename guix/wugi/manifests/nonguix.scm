e14af8165c1084f49a766f78377f7fdcffb8a5e1
Oleg Pykhalov <go.wigust@gmail.com>
2026-02-16 02:05:03 +0300

    guix: manifests: nonguix: Do not use inferiors.

--- guix/wugi/manifests/nonguix.scm
+++ guix/wugi/manifests/nonguix.scm
@@ -7,84 +7,26 @@
   #:use-module (srfi srfi-1)
   #:use-module (wugi etc guix channels workstation)
   #:use-module (wugi utils)
+  #:use-module (nongnu packages fonts)
+  #:use-module (nongnu packages k8s)
+  #:use-module (nongnu packages game-client)
   #:export (%nonguix-manifest))
 
 (define (%nonguix-manifest)
-  (define inferior
-    (inferior-for-channels %channels-workstation
-                           #:cache-directory "/home/oleg/.cache/guix/inferiors"))
-
-  (define inferior-font-packages
-    (inferior-eval `(begin
-                      (use-modules (nongnu packages fonts)
-                                   (srfi srfi-1))
-                      (fold (lambda (package result)
-                              (let ((id (object-address package)))
-                                (hashv-set! %package-table id package)
-                                (cons (list (package-name package)
-                                            (package-version package)
-                                            id)
-                                      result)))
-                            '()
-                            (list font-awesome-nonfree
-                                  font-microsoft-andale-mono
-                                  font-microsoft-arial
-                                  font-microsoft-arial-black
-                                  font-microsoft-comic-sans-ms
-                                  font-microsoft-couirer-new
-                                  font-microsoft-courier-new
-                                  font-microsoft-georgia
-                                  font-microsoft-impact
-                                  font-microsoft-times-new-roman
-                                  font-microsoft-trebuchet-ms
-                                  font-microsoft-verdana
-                                  font-microsoft-web-core-fonts
-                                  font-microsoft-webdings
-                                  font-ubuntu)))
-                   inferior))
-
-  (define inferior-k8s-packages
-    (inferior-eval `(begin
-                      (use-modules (nongnu packages k8s)
-                                   (srfi srfi-1))
-                      (fold (lambda (package result)
-                              (let ((id (object-address package)))
-                                (hashv-set! %package-table id package)
-                                (cons (list (package-name package)
-                                            (package-version package)
-                                            id)
-                                      result)))
-                            '()
-                            (list kubectl)))
-                   inferior))
-
-  (define inferior-steam-packages
-    (inferior-eval `(begin
-                      (use-modules (nonguix multiarch-container)
-                                   (srfi srfi-1))
-                      (fold (lambda (package result)
-                              (let ((id (object-address package)))
-                                (hashv-set! %package-table id package)
-                                (cons (list (package-name package)
-                                            (package-version package)
-                                            id)
-                                      result)))
-                            '()
-                            (list steam)))
-                   inferior))
-
-  (packages->manifest (append (map (match-lambda
-                                     ((name version id)
-                                      ((@@ (guix inferior) inferior-package)
-                                       inferior name version id)))
-                                   inferior-font-packages)
-                              (map (match-lambda
-                                     ((name version id)
-                                      ((@@ (guix inferior) inferior-package)
-                                       inferior name version id)))
-                                   inferior-k8s-packages)
-                              (map (match-lambda
-                                     ((name version id)
-                                      ((@@ (guix inferior) inferior-package)
-                                       inferior name version id)))
-                                   inferior-steam-packages))))
+  (packages->manifest (append (list font-awesome-nonfree
+                                    font-microsoft-andale-mono
+                                    font-microsoft-arial
+                                    font-microsoft-arial-black
+                                    font-microsoft-comic-sans-ms
+                                    font-microsoft-couirer-new
+                                    font-microsoft-courier-new
+                                    font-microsoft-georgia
+                                    font-microsoft-impact
+                                    font-microsoft-times-new-roman
+                                    font-microsoft-trebuchet-ms
+                                    font-microsoft-verdana
+                                    font-microsoft-web-core-fonts
+                                    font-microsoft-webdings
+                                    font-ubuntu)
+                              (list kubectl)
+                              (list steam))))
