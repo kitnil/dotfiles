e3dad31d54f9e06bf481497a19dd7988c267635a
Oleg Pykhalov <go.wigust@gmail.com>
2026-02-16 00:25:38 +0300

    guix: Update pc0-guix-workstation channel.

--- guix/wugi/etc/guix/channels/pc0-guix-workstation.scm
+++ guix/wugi/etc/guix/channels/pc0-guix-workstation.scm
@@ -1,18 +1,29 @@
-(define-module (wugi etc guix channels docker-image)
+(define-module (wugi etc guix channels pc0)
   #:use-module (guix channels)
-  #:export (%channels-docker-image))
+  #:export (%channels-pc0))
 
-(define %channels-docker-image
+(define %channels-pc0
   (list (channel
          (name 'guix)
          (url "https://cgit.wugi.info/git/guix/guix")
-         (branch "pc0-guix-workstation")
+         (branch "master")
          (commit
-          "e5a896808f2ca704f455f9b042bed57264e11692")
+          "7113a63ff4bdcf24fbc4bb92367ea6354dd33290")
          (introduction
           (make-channel-introduction
            "9edb3f66fd807b096b48283debdcddccfea34bad"
            (openpgp-fingerprint
-            "BBB0 2DDF 2CEA F6A8 0D1D  E643 A2A0 6DF2 A33A 54FA"))))))
+            "BBB0 2DDF 2CEA F6A8 0D1D  E643 A2A0 6DF2 A33A 54FA"))))
+        (channel
+         (name 'nonguix)
+         (url "https://cgit.wugi.info/git/guix/nonguix")
+         (branch "master")
+         (commit
+          "bbbc850e8a94833c9958625eb4c78a206fbacc1d")
+         (introduction
+          (make-channel-introduction
+           "897c1a470da759236cc11798f4e0a5f7d4d59fbc"
+           (openpgp-fingerprint
+            "2A39 3FFF 68F4 EF7A 3D29  12AF 6F51 20A0 22FB B2D5"))))))
 
-%channels-docker-image
+%channels-pc0
