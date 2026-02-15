5c5f9ff7b8a75e2d5900e32495af4d64719e09c0
Oleg Pykhalov <go.wigust@gmail.com>
2026-02-15 16:31:00 +0300

    guix: pc0: Update channels.

--- guix/wugi/etc/guix/channels/pc0.scm
+++ guix/wugi/etc/guix/channels/pc0.scm
@@ -8,7 +8,7 @@
          (url "https://cgit.wugi.info/git/guix/guix")
          (branch "master")
          (commit
-          "6913fd701884fad3af579efdc298ae444aeaf57c")
+          "7113a63ff4bdcf24fbc4bb92367ea6354dd33290")
          (introduction
           (make-channel-introduction
            "9edb3f66fd807b096b48283debdcddccfea34bad"
@@ -19,7 +19,7 @@
          (url "https://cgit.wugi.info/git/guix/nonguix")
          (branch "master")
          (commit
-          "bd8c5ca23e694be0b1a8cf72f05f81c70b1d7fc5")
+          "bbbc850e8a94833c9958625eb4c78a206fbacc1d")
          (introduction
           (make-channel-introduction
            "897c1a470da759236cc11798f4e0a5f7d4d59fbc"
