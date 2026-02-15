db107ca362d89ff643d90684b71c03c314c7688d
Oleg Pykhalov <go.wigust@gmail.com>
2026-02-16 02:06:51 +0300

    nix: container-systemd-nixos-workstation-pc0: Add steam.

--- nix/container-systemd-nixos-workstation-pc0/hosts/nixos-systemd.nix
+++ nix/container-systemd-nixos-workstation-pc0/hosts/nixos-systemd.nix
@@ -67,4 +67,14 @@
     9324                        # prometheus bird exporter
     31247                       # prometheus mtr exporters
   ];
+  # https://nixos.wiki/wiki/Steam
+  programs.steam = {
+    enable = true;
+    # Open ports in the firewall for Steam Remote Play
+    remotePlay.openFirewall = true;
+    # Open ports in the firewall for Source Dedicated Server
+    dedicatedServer.openFirewall = true;
+    # Open ports in the firewall for Steam Local Network Game Transfers
+    localNetworkGameTransfers.openFirewall = true;
+  };
 }
