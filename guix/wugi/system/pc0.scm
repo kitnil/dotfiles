7be182a1a9e3cf630f7a87afc63a435d2432871f
Oleg Pykhalov <go.wigust@gmail.com>
2026-02-16 04:13:53 +0300

    guix: pc0: Return vfio.

--- guix/wugi/system/pc0.scm
+++ guix/wugi/system/pc0.scm
@@ -450,7 +450,7 @@
                         ;; crash the VM.
                         "kvm.ignore_msrs=1"
 
-                        ;; "vfio-pci.ids=1002:7550,1002:ab40"
+                        "vfio-pci.ids=1002:7550,1002:ab40"
 
                         "amdgpu.ppfeaturemask=0xffffffff"))
 
@@ -509,22 +509,22 @@
                                       (openssh openssh-sans-x)
                                       (permit-root-login 'prohibit-password)))
 
-                            ;; (simple-service 'vfio-override boot-service-type
-                            ;;                 '(and (call-with-output-file "/sys/bus/pci/devices/0000:03:00.0/driver_override"
-                            ;;                         (lambda (p)
-                            ;;                           (display "vfio-pci" p)))
-                            ;;                       (call-with-output-file "/sys/bus/pci/drivers/vfio-pci/new_id"
-                            ;;                         (lambda (p)
-                            ;;                           (display "1002 7550" p)))
-                            ;;                       (call-with-output-file "/sys/bus/pci/devices/0000:03:00.1/driver_override"
-                            ;;                         (lambda (p)
-                            ;;                           (display "vfio-pci" p)))
-                            ;;                       (call-with-output-file "/sys/bus/pci/drivers/vfio-pci/new_id"
-                            ;;                         (lambda (p)
-                            ;;                           (display "1002 ab40" p)))))
+                            (simple-service 'vfio-override boot-service-type
+                                            '(and (call-with-output-file "/sys/bus/pci/devices/0000:03:00.0/driver_override"
+                                                    (lambda (p)
+                                                      (display "vfio-pci" p)))
+                                                  (call-with-output-file "/sys/bus/pci/drivers/vfio-pci/new_id"
+                                                    (lambda (p)
+                                                      (display "1002 7550" p)))
+                                                  (call-with-output-file "/sys/bus/pci/devices/0000:03:00.1/driver_override"
+                                                    (lambda (p)
+                                                      (display "vfio-pci" p)))
+                                                  (call-with-output-file "/sys/bus/pci/drivers/vfio-pci/new_id"
+                                                    (lambda (p)
+                                                      (display "1002 ab40" p)))))
 
                             (service kernel-module-loader-service-type
-                                     '(;; "vfio-pci"
+                                     '("vfio-pci"
                                        "amdgpu"
 
                                        "dm-snapshot"
