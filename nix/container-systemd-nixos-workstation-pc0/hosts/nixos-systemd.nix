8c3b5c8517efa44bb5acb6c501f3ef09854f990d
Oleg Pykhalov <go.wigust@gmail.com>
2026-02-28 07:13:07 +0300

    nix: container-systemd-nixos-workstation-pc0: Delete gamescope.

--- nix/container-systemd-nixos-workstation-pc0/hosts/nixos-systemd.nix
+++ nix/container-systemd-nixos-workstation-pc0/hosts/nixos-systemd.nix
@@ -10,10 +10,6 @@
   };
   programs.niri.enable = true;
   programs.sway.enable = true;
-  programs.gamescope = {
-    enable = true;
-    capSysNice = false;
-  };
   services.bird = {
     enable = true;
     config = lib.readFile ./../bird.1.conf;
