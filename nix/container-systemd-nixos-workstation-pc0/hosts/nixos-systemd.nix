f472bcd6a1efaee3299d68733ff50a9350e1ac2d
Oleg Pykhalov <go.wigust@gmail.com>
2026-02-28 07:00:31 +0300

    nix: container-systemd-nixos-workstation-pc0: Specify package.

--- nix/container-systemd-nixos-workstation-pc0/hosts/nixos-systemd.nix
+++ nix/container-systemd-nixos-workstation-pc0/hosts/nixos-systemd.nix
@@ -81,5 +81,20 @@
       pkgs.adwaita-icon-theme
       pkgs.gamescope
     ];
+    package = pkgs.steam.override {
+      extraPkgs = pkgs: with pkgs; [
+        libXcursor
+        libXi
+        libXinerama
+        libXScrnSaver
+        libpng
+        libpulseaudio
+        libvorbis
+        stdenv.cc.cc.lib # Provides libstdc++.so.6
+        libkrb5
+        keyutils
+        # Add other libraries as needed
+      ];
+    };
   };
 }
