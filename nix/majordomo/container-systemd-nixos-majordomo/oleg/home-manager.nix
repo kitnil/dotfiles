51c9428a11741c94635c5e1983cc463744380d7d
Oleg Pykhalov <go.wigust@gmail.com>
2026-03-18 18:58:08 +0300

    nix: majordomo: container-systemd-nixos-majordomo: Disable ssl in mariadb.

--- nix/majordomo/container-systemd-nixos-majordomo/oleg/home-manager.nix
+++ nix/majordomo/container-systemd-nixos-majordomo/oleg/home-manager.nix
@@ -82,6 +82,14 @@
     '';
   };
 
+  home.file = {
+    ".my.cnf" = {
+      text = ''
+        [client]
+        skip-ssl = true
+      '';
+  };
+
   # The home.stateVersion option no longer has a default value. It used to
   # default to “18.09”, which was the Home Manager version that introduced the
   # option. If your configuration does not explicitly set this option then you
