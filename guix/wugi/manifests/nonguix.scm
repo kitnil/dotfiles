44b8211a36c602bc43279af9b0d50b293afb2b2d
Oleg Pykhalov <go.wigust@gmail.com>
2026-04-01 09:26:28 +0300

    guix: Comment microsoft fonts.

--- guix/wugi/manifests/nonguix.scm
+++ guix/wugi/manifests/nonguix.scm
@@ -13,18 +13,18 @@
 
 (define (%nonguix-manifest)
   (packages->manifest (append (list font-awesome-nonfree
-                                    font-microsoft-andale-mono
-                                    font-microsoft-arial
-                                    font-microsoft-arial-black
-                                    font-microsoft-comic-sans-ms
-                                    font-microsoft-couirer-new
-                                    font-microsoft-courier-new
-                                    font-microsoft-georgia
-                                    font-microsoft-impact
-                                    font-microsoft-times-new-roman
-                                    font-microsoft-trebuchet-ms
-                                    font-microsoft-verdana
-                                    font-microsoft-web-core-fonts
-                                    font-microsoft-webdings
+                                    ;; font-microsoft-andale-mono
+                                    ;; font-microsoft-arial
+                                    ;; font-microsoft-arial-black
+                                    ;; font-microsoft-comic-sans-ms
+                                    ;; font-microsoft-couirer-new
+                                    ;; font-microsoft-courier-new
+                                    ;; font-microsoft-georgia
+                                    ;; font-microsoft-impact
+                                    ;; font-microsoft-times-new-roman
+                                    ;; font-microsoft-trebuchet-ms
+                                    ;; font-microsoft-verdana
+                                    ;; font-microsoft-web-core-fonts
+                                    ;; font-microsoft-webdings
                                     font-ubuntu)
                               (list kubectl))))
