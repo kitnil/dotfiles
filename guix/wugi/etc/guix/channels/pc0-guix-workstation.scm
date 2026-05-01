4478bb673656645e4c2f8c309f967cb54c777483
Oleg Pykhalov <go.wigust@gmail.com>
2026-05-01 20:13:05 +0300

    guix: channels: Update pc0-guix-workstation.

--- guix/wugi/etc/guix/channels/pc0-guix-workstation.scm
+++ guix/wugi/etc/guix/channels/pc0-guix-workstation.scm
@@ -7,7 +7,7 @@
          (name 'guix)
          (url "https://cgit.wugi.info/git/guix/guix")
          (commit
-          "304f18ad6928e6a770f2695846f74de3ae769bff")
+          "896f17d4b58e0c27875c1023013ba275930f4ec8")
          (introduction
           (make-channel-introduction
            "9edb3f66fd807b096b48283debdcddccfea34bad"
@@ -17,7 +17,7 @@
          (name 'nonguix)
          (url "https://cgit.wugi.info/git/guix/nonguix")
          (commit
-          "a3f4e7bff779da4593a2922516064a8edaafa3e6")
+          "f2a2a606f25a2bcb1be4a0137cba5db85984e1a0")
          (introduction
           (make-channel-introduction
            "897c1a470da759236cc11798f4e0a5f7d4d59fbc"
